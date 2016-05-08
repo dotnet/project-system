93af0ae33a9dcdf5b6c1a756e351f2af44587314
David Kean <dave@keansbox.com>
2016-05-08 14:36:17 -0700

    Add code base binding redirects for the projec system binaries
    This enables us to F5 and override binaries in VS install enabling us to test AppDesigner and Editors.

---
+++ src/ProjectSystemSetup/AssemblyRedirects.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio;
+
+[assembly: ProvideProjectSystemBindingRedirection("Microsoft.VisualStudio.Editors")]
+[assembly: ProvideProjectSystemBindingRedirection("Microsoft.VisualStudio.AppDesigner")]
+[assembly: ProvideProjectSystemBindingRedirection("Microsoft.VisualStudio.ProjectSystem.CSharp")]
+[assembly: ProvideProjectSystemBindingRedirection("Microsoft.VisualStudio.ProjectSystem.CSharp.VS")]
+[assembly: ProvideProjectSystemBindingRedirection("Microsoft.VisualStudio.ProjectSystem.Managed")]
+[assembly: ProvideProjectSystemBindingRedirection("Microsoft.VisualStudio.ProjectSystem.Managed.VS")]
+[assembly: ProvideProjectSystemBindingRedirection("Microsoft.VisualStudio.ProjectSystem.VisualBasic")]
+[assembly: ProvideProjectSystemBindingRedirection("Microsoft.VisualStudio.ProjectSystem.VisualBasic.VS")]
---
+++ src/ProjectSystemSetup/ProvideBindingRedirectionAttribute.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using Microsoft.VisualStudio.Shell;
+
+namespace Microsoft.VisualStudio
+{
+    /// <summary>
+    ///     A <see cref="RegistrationAttribute"/> that provides binding redirects for the project system.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    internal sealed class ProvideProjectSystemBindingRedirectionAttribute : RegistrationAttribute
+    {
+        private readonly ProvideBindingRedirectionAttribute _redirectionAttribute;
+
+        public ProvideProjectSystemBindingRedirectionAttribute(string assemblyName)
+        {
+            // ProvideBindingRedirectionAttribute is sealed, so we can't inherit from it to provide defaults.
+            // Instead, we'll do more of an aggregation pattern here.
+            _redirectionAttribute = new ProvideBindingRedirectionAttribute
+            {
+                AssemblyName = assemblyName,
+                OldVersionLowerBound = "0.0.0.0",
+                CodeBase = assemblyName + ".dll",
+            };
+        }
+
+        public override void Register(RegistrationContext context)
+        {
+            _redirectionAttribute.Register(context);
+        }
+
+        public override void Unregister(RegistrationContext context)
+        {
+            _redirectionAttribute.Unregister(context);
+        }
+    }
+}
