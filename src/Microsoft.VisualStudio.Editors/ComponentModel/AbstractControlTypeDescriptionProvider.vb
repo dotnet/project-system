597b97142a78291c1a71b00f6881afe2f18db2d2
David Kean <dave@keansbox.com>
2016-05-08 21:22:18 -0700

    Added attribute that allows abstract forms to be designed

---
+++ src/Microsoft.VisualStudio.Editors/ComponentModel/AbstractControlTypeDescriptionProvider.vb
@@ -0,0 +1,45 @@
+﻿Namespace System.ComponentModel
+
+    ''' <summary>
+    '''     A <see cref="TypeDescriptionProvider"/> that allows you swap out an abstract class with a concrete 
+    '''     implementation, so that derived types can be opened And designed in the editor.
+    ''' </summary>
+    Friend Class AbstractControlTypeDescriptionProvider(Of TAbstract, TDerived)
+        Inherits TypeDescriptionProvider
+
+        ' Based on Brian Pepin's post: http://www.pocketsilicon.com/post/Using-Visual-Studio-Whidbey-to-Design-Abstract-Forms
+
+        Public Sub New()
+
+            MyBase.New(TypeDescriptor.GetProvider(GetType(TAbstract)))
+
+        End Sub
+
+        Public Overrides Function GetReflectionType(objectType As Type, instance As Object) As Type
+
+            ' If the designer Is asking for the abstract control,
+            ' return the "concrete" version of it instead
+            If (objectType = GetType(TAbstract)) Then
+                Return GetType(TDerived)
+            End If
+
+            Return MyBase.GetReflectionType(objectType, instance)
+
+        End Function
+
+        Public Overrides Function CreateInstance(provider As IServiceProvider, objectType As Type, argTypes As Type(), args As Object()) As Object
+
+            ' If the designer Is asking to create the abstract 
+            ' control, instantiate the "concrete" version of it instead
+            If (objectType = GetType(TAbstract)) Then
+                objectType = GetType(TDerived)
+            End If
+
+            Return MyBase.CreateInstance(provider, objectType, argTypes, args)
+
+        End Function
+
+    End Class
+
+End Namespace
+
