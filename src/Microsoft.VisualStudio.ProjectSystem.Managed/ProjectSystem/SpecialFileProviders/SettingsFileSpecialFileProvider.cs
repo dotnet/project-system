26b613e183c1d5146e6ac95f2ae072ee2dfb16c4
Srivatsn Narayanan <srivatsn@microsoft.com>
2016-05-21 14:58:28 -0700

    Add a skeleton for a settings file special file provider and a rule for settings files to make the custom tool DTE property map to the generator tag in the project file.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/SpecialFileProviders/SettingsFileSpecialFileProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Diagnostics.Debug;
+
+namespace Microsoft.VisualStudio.ProjectSystem.ProjectSystem.SpecialFileProviders
+{
+    [ExportSpecialFileProvider(SpecialFiles.AppSettings)]
+    [AppliesTo(ProjectCapability.CSharpOrVisualBasic)]
+    public class SettingsFileSpecialFileProvider : ISpecialFileProvider
+    {
+        /// <summary>
+        /// Gets the physical tree provider.
+        /// </summary>
+        [Import(ExportContractNames.ProjectTreeProviders.PhysicalViewTree)]
+        private Lazy<IProjectTreeProvider> PhysicalProjectTreeProvider { get; set; }
+
+        /// <summary>
+        /// Gets or sets the project tree service.
+        /// </summary>
+        [Import(ExportContractNames.ProjectTreeProviders.PhysicalProjectTreeService)]
+        private IProjectTreeService ProjectTreeService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the accessor to project items.
+        /// </summary>
+        [Import(ExportContractNames.ProjectItemProviders.Folders)]
+        private Lazy<IProjectItemProvider> Folders { get; set; }
+
+        /// <summary>
+        /// Gets or sets the accessor to project items.
+        /// </summary>
+        [Import(ExportContractNames.ProjectItemProviders.SourceFiles)]
+        private Lazy<IProjectItemProvider> SourceItems { get; set; }
+
+        public Task<string> GetFileAsync(SpecialFiles fileId, SpecialFileFlags flags, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            Assert(fileId == SpecialFiles.AppSettings);
+
+            IProjectTree appDesignerFolder = ProjectTreeService.CurrentTree.Tree.Children.FirstOrDefault(child => child.IsFolder && child.Flags.HasFlag(ProjectTreeFlags.Common.AppDesignerFolder));
+            if (appDesignerFolder != null)
+            {
+                IProjectTree settingsNode;
+                appDesignerFolder.TryFindImmediateChild("Settings.settings", out settingsNode);
+
+                return Task.FromResult(settingsNode?.FilePath);
+            }
+
+            //string rootDir = Path.GetDirectoryName(this.ProjectTreeService.CurrentTree.ProjectSnapshot.Value.FullPath);
+            //string settingsPath = Path.Combine(rootDir, "Settings.settings");
+            //return settingsPath;
+
+            return null;
+        }
+    }
+}
