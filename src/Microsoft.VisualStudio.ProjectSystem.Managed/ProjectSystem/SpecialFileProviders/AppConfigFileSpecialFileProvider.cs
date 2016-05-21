f671cf5a446a807555c9d2a25f553ab3f8d272d1
Srivatsn Narayanan <srivatsn@microsoft.com>
2016-05-21 15:30:13 -0700

    Adding special file provider skeletons for App.Config and resx files.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/SpecialFileProviders/AppConfigFileSpecialFileProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Diagnostics.Debug;
+
+namespace Microsoft.VisualStudio.ProjectSystem.ProjectSystem.SpecialFileProviders
+{
+    [ExportSpecialFileProvider(SpecialFiles.AppConfig)]
+    [AppliesTo(ProjectCapability.CSharpOrVisualBasic)]
+    internal class AppConfigFileSpecialFileProvider : ISpecialFileProvider
+    {
+        /// <summary>
+        /// Gets or sets the project tree service.
+        /// </summary>
+        [Import(ExportContractNames.ProjectTreeProviders.PhysicalProjectTreeService)]
+        private IProjectTreeService ProjectTreeService { get; set; }
+
+        public Task<string> GetFileAsync(SpecialFiles fileId, SpecialFileFlags flags, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            Assert(fileId == SpecialFiles.AppConfig);
+            
+            IProjectTree settingsNode;
+            ProjectTreeService.CurrentTree.Tree.TryFindImmediateChild("App.config", out settingsNode);
+
+            return Task.FromResult(settingsNode?.FilePath);
+
+            //string rootDir = Path.GetDirectoryName(this.ProjectTreeService.CurrentTree.ProjectSnapshot.Value.FullPath);
+            //string settingsPath = Path.Combine(rootDir, "Settings.settings");
+            //return settingsPath;
+        }
+    }
+}
--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/SpecialFileProviders/ResourcesFileSpecialFileProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/SpecialFileProviders/ResourcesFileSpecialFileProvider.cs
@@ -9,45 +9,27 @@ using static System.Diagnostics.Debug;
 
 namespace Microsoft.VisualStudio.ProjectSystem.ProjectSystem.SpecialFileProviders
 {
-    [ExportSpecialFileProvider(SpecialFiles.AppSettings)]
+    [ExportSpecialFileProvider(SpecialFiles.AssemblyResource)]
     [AppliesTo(ProjectCapability.CSharpOrVisualBasic)]
-    internal class SettingsFileSpecialFileProvider : ISpecialFileProvider
+    internal class ResourcesFileSpecialFileProvider : ISpecialFileProvider
     {
-        /// <summary>
-        /// Gets the physical tree provider.
-        /// </summary>
-        [Import(ExportContractNames.ProjectTreeProviders.PhysicalViewTree)]
-        private Lazy<IProjectTreeProvider> PhysicalProjectTreeProvider { get; set; }
-
         /// <summary>
         /// Gets or sets the project tree service.
         /// </summary>
         [Import(ExportContractNames.ProjectTreeProviders.PhysicalProjectTreeService)]
         private IProjectTreeService ProjectTreeService { get; set; }
 
-        /// <summary>
-        /// Gets or sets the accessor to project items.
-        /// </summary>
-        [Import(ExportContractNames.ProjectItemProviders.Folders)]
-        private Lazy<IProjectItemProvider> Folders { get; set; }
-
-        /// <summary>
-        /// Gets or sets the accessor to project items.
-        /// </summary>
-        [Import(ExportContractNames.ProjectItemProviders.SourceFiles)]
-        private Lazy<IProjectItemProvider> SourceItems { get; set; }
-
         public Task<string> GetFileAsync(SpecialFiles fileId, SpecialFileFlags flags, CancellationToken cancellationToken = default(CancellationToken))
         {
-            Assert(fileId == SpecialFiles.AppSettings);
+            Assert(fileId == SpecialFiles.AssemblyResource);
 
             IProjectTree appDesignerFolder = ProjectTreeService.CurrentTree.Tree.Children.FirstOrDefault(child => child.IsFolder && child.Flags.HasFlag(ProjectTreeFlags.Common.AppDesignerFolder));
             if (appDesignerFolder != null)
             {
-                IProjectTree settingsNode;
-                appDesignerFolder.TryFindImmediateChild("Settings.settings", out settingsNode);
+                IProjectTree resourcesNode;
+                appDesignerFolder.TryFindImmediateChild("Resources.resx", out resourcesNode);
 
-                return Task.FromResult(settingsNode?.FilePath);
+                return Task.FromResult(resourcesNode?.FilePath);
             }
 
             //string rootDir = Path.GetDirectoryName(this.ProjectTreeService.CurrentTree.ProjectSnapshot.Value.FullPath);
