04294ebbe8e490b6f1d7bffb809d4de207b9ad42
Jinu Joseph <jinuj@microsoft.com>
2016-05-26 12:04:11 -0700

    Switched to Roslyn API with time delay workaround

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/FileRenameTracker.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/FileRenameTracker.cs
@@ -7,6 +7,12 @@
 //
 // Copyright(c) 2015 Microsoft Corporation
 //--------------------------------------------------------------------------------------------
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.LanguageServices;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Threading;
@@ -15,6 +21,7 @@ using System.Collections.Immutable;
 using System.ComponentModel.Composition;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Task = System.Threading.Tasks.Task;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS
@@ -24,6 +31,9 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
     [AppliesTo(ProjectCapability.CSharp)]
     internal class FileRenameTracker : IProjectChangeHintReceiver
     {
+
+        private IComponentModel componentModel;
+        private VisualStudioWorkspace visualStudioWorkspace;
 
         protected readonly IUnconfiguredProjectVsServices UnconfiguredProjectVsServices;
         /// <summary>
@@ -48,7 +58,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
             Requires.NotNull(projectVsServices, nameof(projectVsServices));
             UnconfiguredProjectVsServices = projectVsServices;
         }
-        
+
         public async Task HintedAsync(IImmutableDictionary<Guid, IImmutableSet<IProjectChangeHint>> hints)
         {
             var files = hints.GetValueOrDefault(ProjectChangeFileSystemEntityRenameHint.RenamedFile) ?? ImmutableHashSet.Create<IProjectChangeHint>();
@@ -67,90 +77,114 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
         {
             return TplExtensions.CompletedTask;
         }
-       
+
         /// <summary>
         /// static helper method to do the class rename. Must be called on the UI thread
         /// </summary>
         private async Task DoRenameClassAsync(string oldFilePath, string newFilePath)
         {
-            await ThreadingService.SwitchToUIThread();
+            
 
             string codeExtension = Path.GetExtension(newFilePath);
-            if (codeExtension == null || !oldFilePath.EndsWith(codeExtension, StringComparison.OrdinalIgnoreCase) || !newFilePath.EndsWith(codeExtension, StringComparison.OrdinalIgnoreCase))
+            if (codeExtension == null || !oldFilePath.EndsWith(codeExtension, StringComparison.OrdinalIgnoreCase))
             {
                 return;
             }
 
             string oldName = Path.GetFileNameWithoutExtension(oldFilePath);
             string newName = Path.GetFileNameWithoutExtension(newFilePath);
-            if (Path.GetFileName(oldName).Equals(Path.GetFileName(newName)))
-            {
-                return;
-            }
 
+            await ThreadingService.SwitchToUIThread();
+            if (visualStudioWorkspace == null )
+            {
+                componentModel = (IComponentModel)ServiceProvider.GetService(typeof(SComponentModel));
+                visualStudioWorkspace = componentModel.GetService<VisualStudioWorkspace>();
+            }
+           
             IVsHierarchy hierarchy = UnconfiguredProjectVsServices.Hierarchy;
             EnvDTE.Project project = hierarchy.GetDTEProject();
-                        
-            // We need the full namespace
-            string defNamespace = hierarchy.GetDefaultNamespaceForFile(oldFilePath);
-            if (!string.IsNullOrEmpty(defNamespace))
-            {
-                defNamespace += ".";
-            }
-            string qualifiedOldName = defNamespace + oldName;
-            string qualifiedNewName = defNamespace + newName;
+            var myProject = visualStudioWorkspace
+                .CurrentSolution
+                .Projects.Where(p => String.Equals(p.FilePath, project.FullName, StringComparison.OrdinalIgnoreCase))
+                .FirstOrDefault();
 
-            // Get a whole bunch of DTE objects. If any fail, we silently return
-            EnvDTE.ProjectItem projectItem = hierarchy.GetDTEProjectItemForFile(oldFilePath);
-            if (projectItem == null)
+      
+            var TemporaryTimmer = Task.Delay(10 * 1000).ContinueWith(async (_) =>
             {
-                return;
-            }
-            EnvDTE.FileCodeModel fileCM = projectItem.FileCodeModel;
-            if (fileCM == null)
-            {
-                return;
-            }
-            EnvDTE.CodeElements codeElements = fileCM.CodeElements;
-            if (codeElements == null)
-            {
-                return;
-            }
-            EnvDTE80.CodeElement2 codeElement = GetCodeElementByName(codeElements, qualifiedOldName) as EnvDTE80.CodeElement2;
-            if (codeElement == null)
-            {
-                return;
-            }
+                Document newDocument = (from d in visualStudioWorkspace.CurrentSolution.GetProject(myProject.Id).Documents where d.FilePath.Equals(newFilePath, StringComparison.OrdinalIgnoreCase) select d).FirstOrDefault();
+                if (newDocument == null)
+                    return;
 
-            // We're about to do a symbolic rename.If the user has asked us to prompt, We need to open a dialog and ask them  
-            //  Otherwise go ahead and do the rename.
-            EnvDTE.DTE dte = ServiceProvider.GetService<EnvDTE.DTE, EnvDTE.DTE>();
-            var props = dte.Properties["Environment", "ProjectsAndSolution"];
-            if (props != null)
-            {
-                if ((bool)props.Item("PromptForRenameSymbol").Value)
+                var root = await newDocument.GetSyntaxRootAsync().ConfigureAwait(false);
+                if (root == null)
+                    return;
+
+                var declaration = root.DescendantNodes().Where(n => IsTypeNode(n, oldName)).FirstOrDefault();
+                if (declaration == null)
+                    return;
+
+                var semanticModel = await newDocument.GetSemanticModelAsync().ConfigureAwait(false);
+                if (semanticModel == null)
+                    return;
+                
+                var symbol = semanticModel.GetDeclaredSymbol(declaration);
+                if (symbol == null)
+                    return;
+
+                // We're about to do a symbolic rename.If the user has asked us to prompt, We need to open a dialog and ask them  
+                //  Otherwise go ahead and do the rename.
+                EnvDTE.DTE dte = ServiceProvider.GetService<EnvDTE.DTE, EnvDTE.DTE>();
+                var props = dte.Properties["Environment", "ProjectsAndSolution"];
+                if (props != null)
                 {
-                    string promptMessage = string.Format(Resources.RenameSymbolPrompt, oldName);
-                    var result = VsShellUtilities.ShowMessageBox(ServiceProvider, promptMessage, null, OLEMSGICON.OLEMSGICON_QUERY,
-                                                    OLEMSGBUTTON.OLEMSGBUTTON_YESNO, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-                    if (result == (int)VSConstants.MessageBoxResult.IDNO)
+                    if ((bool)props.Item("PromptForRenameSymbol").Value)
                     {
-                        return;
+                        string promptMessage = string.Format(Resources.RenameSymbolPrompt, oldName);
+                        var result = VsShellUtilities.ShowMessageBox(ServiceProvider, promptMessage, null, OLEMSGICON.OLEMSGICON_QUERY,
+                                                        OLEMSGBUTTON.OLEMSGBUTTON_YESNO, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+                        if (result == (int)VSConstants.MessageBoxResult.IDNO)
+                        {
+                            return;
+                        }
                     }
                 }
-            }
 
-            // Now do the rename
-            try
+                // Now do the rename
+                var optionSet = newDocument.Project.Solution.Workspace.Options;
+                var renamedSolution = await Renamer.RenameSymbolAsync(newDocument.Project.Solution, symbol, newName, optionSet).ConfigureAwait(false);
+                
+                await ThreadingService.SwitchToUIThread();
+
+                if (!newDocument.Project.Solution.Workspace.TryApplyChanges(renamedSolution))
+                {
+                    string promptMessage = string.Format(Resources.RenameSymbolFailed, oldName);
+                    var result = VsShellUtilities.ShowMessageBox(ServiceProvider, promptMessage, null, OLEMSGICON.OLEMSGICON_WARNING,
+                                                     OLEMSGBUTTON.OLEMSGBUTTON_OK, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+                }
+                 }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private bool IsTypeNode(SyntaxNode syntax, string oldName)
+        {
+            if (syntax is BaseTypeDeclarationSyntax || syntax is DelegateDeclarationSyntax)
             {
-                codeElement.RenameSymbol(newName);
+                switch (syntax.Kind())
+                {
+                    case SyntaxKind.ClassDeclaration:
+                        var classDecl = (ClassDeclarationSyntax)syntax;
+                        if (classDecl.Identifier.ToString() == oldName)
+                        {
+                            return true;
+                        }
+                        break;
+
+
+                    default:
+                        break;
+                }
+                return false;
             }
-            catch (Exception ex)
-            {
-                string promptMessage = string.Format(Resources.RenameSymbolFailed, oldName, ex.Message); 
-                var result = VsShellUtilities.ShowMessageBox(ServiceProvider, promptMessage, null, OLEMSGICON.OLEMSGICON_WARNING,
-                                                OLEMSGBUTTON.OLEMSGBUTTON_OK, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-            }
+            return false;
         }
 
         /// <summary>
