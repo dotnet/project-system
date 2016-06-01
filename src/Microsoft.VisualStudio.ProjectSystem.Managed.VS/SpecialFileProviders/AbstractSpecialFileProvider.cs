819927105e581599cb1d214ff95ac68e5d4d9321
Srivatsn Narayanan <srivatsn@microsoft.com>
2016-05-31 17:19:29 -0700

    Add checks to cover cases where the file exists in the project but not on disk and vice versa.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/SpecialFileProviders/AbstractSpecialFileProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/SpecialFileProviders/AbstractSpecialFileProvider.cs
@@ -8,6 +8,7 @@ using Microsoft.VisualStudio.Shell.Interop;
 using EnvDTE;
 using EnvDTE80;
 using Diagnostics = System.Diagnostics;
+using System.Collections.Immutable;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.SpecialFileProviders
 {
@@ -28,7 +29,10 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.SpecialFileProviders
 
         protected abstract string GetTemplateForSpecialFile(SpecialFiles fileId);
 
-        private string FindFileWithinNode(IProjectTree parentNode, string fileName)
+        /// <summary>
+        /// Find a file with the given filename within given the node.
+        /// </summary>
+        private IProjectTree FindFileWithinNode(IProjectTree parentNode, string fileName)
         {
             IProjectTree fileNode;
             parentNode.TryFindImmediateChild(fileName, out fileNode);
@@ -47,25 +51,18 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.SpecialFileProviders
                     // parentNodeFilePath = Path.Get(fileNode.FilePath);
                 }
                 
-                if (!fileNode.Flags.IsIncludedInProject())
-                {
-                    return null;
-                }
-
-                // TODO : if in project check?
-                // TODO: AssureLocalCheck?
-                if (!File.Exists(fileNode.FilePath))
-                {
-                    return null;
-                }
-
-                return fileNode.FilePath;
+                return fileNode;
             }
 
             return null;
         }
 
-        private string FindFile(string specialFileName)
+        /// <summary>
+        /// Find a file with the given file name. The algorithm used is :
+        ///       Look under the appdesigner folder for files that normally live there.
+        ///       Look under the project root for all files.
+        /// </summary>
+        private IProjectTree FindFile(string specialFileName)
         {
             IProjectTree rootNode = ProjectTreeService.CurrentTree.Tree;
 
@@ -74,27 +71,68 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.SpecialFileProviders
                 return null;
             }
 
-            string specialFilePath;
+            IProjectTree specialFileNode;
             // First, we look in the AppDesigner folder.
             IProjectTree appDesignerFolder = rootNode.Children.FirstOrDefault(child => child.IsFolder && child.Flags.HasFlag(ProjectTreeFlags.Common.AppDesignerFolder));
             if (appDesignerFolder != null && CreatedByDefaultUnderAppDesignerFolder)
             {
-                specialFilePath = FindFileWithinNode(appDesignerFolder, specialFileName);
+                specialFileNode = FindFileWithinNode(appDesignerFolder, specialFileName);
 
-                if (specialFilePath != null)
+                if (specialFileNode != null)
                 {
-                    return specialFilePath;
+                    return specialFileNode;
                 }
             }
 
             // Now try the root folder.
-            specialFilePath = FindFileWithinNode(rootNode, specialFileName);
-            if (specialFilePath != null)
+            specialFileNode = FindFileWithinNode(rootNode, specialFileName);
+            if (specialFileNode != null)
             {
-                return specialFilePath;
+                return specialFileNode;
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// Check to see if a given node is both part of a project and exists on disk.
+        /// </summary>
+        private async Task<bool> IsNodeInSyncWithDiskAsync(IProjectTree specialFileNode, bool forceSync, CancellationToken cancellationToken)
+        {
+            if (!specialFileNode.Flags.IsIncludedInProject())
+            {
+                if (forceSync)
+                {
+                    var showAllFilesProvider = ProjectTreeService.CurrentTree.TreeProvider as IShowAllFilesProjectTreeProvider;
+
+                    // Cannot include files if tree provider doesn't support it.
+                    if (showAllFilesProvider == null)
+                    {
+                        return false;
+                    }
+
+                    await showAllFilesProvider.IncludeItemsAsync(ImmutableHashSet.Create(specialFileNode));
+                    await ProjectTreeService.PublishLatestTreeAsync(cancellationToken: cancellationToken);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            // TODO: AssureLocalCheck?
+            if (!File.Exists(specialFileNode.FilePath))
+            {
+                if (forceSync)
+                {
+                    await ProjectTreeService.CurrentTree.TreeProvider.RemoveAsync(ImmutableHashSet.Create(specialFileNode));
+                    await ProjectTreeService.PublishLatestTreeAsync(cancellationToken: cancellationToken);
+                }
+
+                return false;
+            }
+
+            return true;
         }
 
         private string GetTemplateLanguage(Project project)
@@ -141,7 +179,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.SpecialFileProviders
 
                     if (specialFilePath != null)
                     {
-                        return specialFilePath;
+                        return specialFilePath.FilePath;
                     }
                     Diagnostics.Debug.Fail("We added the file successfully but didn't find it!");
                 }
@@ -163,31 +201,34 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.SpecialFileProviders
         ///      Force-create in app-designer folder unless it's the app.config file, which
         ///          the users expect in the root folder.
         /// </summary>
-        public Task<string> GetFileAsync(SpecialFiles fileId, SpecialFileFlags flags, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<string> GetFileAsync(SpecialFiles fileId, SpecialFileFlags flags, CancellationToken cancellationToken = default(CancellationToken))
         {
             string specialFileName = GetFileNameOfSpecialFile(fileId);
 
             // Search for the file in the app designer and root folders.
-            string specialFilePath = FindFile(specialFileName);
-            if (specialFilePath != null)
+            IProjectTree specialFileNode = FindFile(specialFileName);
+            if (specialFileNode != null)
             {
-                return Task.FromResult(specialFilePath);
+                if (await IsNodeInSyncWithDiskAsync(specialFileNode, forceSync: flags.HasFlag(SpecialFileFlags.CreateIfNotExist), cancellationToken: cancellationToken))
+                {
+                    return specialFileNode.FilePath;
+                }
             }
 
             // File doesn't exist. Create it if we've been asked to.
             if (flags.HasFlag(SpecialFileFlags.CreateIfNotExist))
             {
-                specialFilePath = CreateFile(fileId, specialFileName);
-                if (specialFilePath != null)
+                string createdFilePath = CreateFile(fileId, specialFileName);
+                if (createdFilePath != null)
                 {
-                    return Task.FromResult(specialFilePath);
+                    return createdFilePath;
                 }
             }
 
             // We haven't found the file but return the default file path as that's the contract.
             string rootFilePath = ProjectVsServices.ActiveConfiguredProject.UnconfiguredProject.FullPath; // PhysicalProjectTreeProvider.Value.GetPath(rootNode);
             string fullPath = Path.Combine(rootFilePath, specialFileName);
-            return Task.FromResult(fullPath);
+            return fullPath;
         }
     }
 }
