f883c3f5b9d1f8fa226e384fe2e9fbe2b8ba9053
Srivatsn Narayanan <srivatsn@microsoft.com>
2016-06-04 13:55:37 -0700

    Fix break.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/SpecialFileProviders/ResourcesFileSpecialFileProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/SpecialFileProviders/ResourcesFileSpecialFileProvider.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using System.ComponentModel.Composition;
 using Microsoft.VisualStudio.ProjectSystem.Utilities;
 using static System.Diagnostics.Debug;
@@ -13,7 +14,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.SpecialFileProviders
         [ImportingConstructor]
         public ResourcesFileSpecialFileProvider([Import(ExportContractNames.ProjectTreeProviders.PhysicalProjectTreeService)] IProjectTreeService projectTreeService,
                                                 [Import(ExportContractNames.ProjectItemProviders.SourceFiles)] IProjectItemProvider sourceItemsProvider,
-                                                [Import(AllowDefault = true)] ICreateFileFromTemplateService templateFileCreationService,
+                                                [Import(AllowDefault = true)] Lazy<ICreateFileFromTemplateService> templateFileCreationService,
                                                 IFileSystem fileSystem) 
             : base(projectTreeService, sourceItemsProvider, templateFileCreationService, fileSystem)
         {
