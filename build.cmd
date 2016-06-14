19df7908ab0de9091144b893faa159b0d56584aa
Balaji Soundrarajan <basoundr@microsoft.com>
2016-06-13 21:55:40 -0700

    Update VSL.Imports.Targets for signing in Microbuild V2
    With this change, We are signing all our present binaries and the single VSIX. In the future, if more binaries gets added to the VSIX then those binaries will be automatically signed as well
    
    But we are shipping a product binary out of a VSIX then Imports.targets needs to updated accordingly
    
    Note: Set the variable RunningInMicroBuild = true in Microbuild definition to sign the product while building the product in microbuildV2

--- build.cmd
+++ build.cmd
@@ -33,7 +33,7 @@ set BinariesDirectory=%Root%bin\%BuildConfiguration%\
 set LogFile=%BinariesDirectory%Build.log
 if not exist "%BinariesDirectory%" mkdir "%BinariesDirectory%" || goto :BuildFailed
 
-msbuild /nologo /consoleloggerparameters:Verbosity=minimal /fileLogger /fileloggerparameters:LogFile="%LogFile%";verbosity=detailed /t:"%MSBuildTarget%" /p:Configuration="%BuildConfiguration%" "%Root%build\build.proj" %MSBuildAdditionalArguments%
+msbuild /nologo /nodeReuse:false /consoleloggerparameters:Verbosity=minimal /fileLogger /fileloggerparameters:LogFile="%LogFile%";verbosity=detailed /t:"%MSBuildTarget%" /p:Configuration="%BuildConfiguration%" "%Root%build\build.proj" %MSBuildAdditionalArguments%
 if ERRORLEVEL 1 (
     echo.
     echo Build failed, for full log see %LogFile%.
