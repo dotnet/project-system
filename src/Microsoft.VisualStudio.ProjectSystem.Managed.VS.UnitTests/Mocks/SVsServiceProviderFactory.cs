68f6f15c98e07704058caddb43fe13edd6d0e965
Jinu Joseph <jinuj@microsoft.com>
2016-06-21 01:40:26 -0700

    Adding UT for OptionsSettings

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/SVsServiceProviderFactory.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Moq;
+using Microsoft.VisualStudio.Shell;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS
+{
+    internal static class SVsServiceProviderFactory
+    {
+        public static SVsServiceProvider Create()
+        {
+            return Mock.Of<SVsServiceProvider>();
+        }
+    }
+}
