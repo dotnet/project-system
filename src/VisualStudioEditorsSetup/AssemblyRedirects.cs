6c41329029e53b27f764a113a605ec92d645d335
Balaji Soundrarajan <basoundr@microsoft.com>
2016-06-24 15:05:10 -0700

    VS editors vsix
    Add reference to VS editors setup

---
+++ src/VisualStudioEditorsSetup/AssemblyRedirects.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio;
+
+[assembly: ProvideEditorsBindingRedirection("Microsoft.VisualStudio.Editors")]
+[assembly: ProvideEditorsBindingRedirection("Microsoft.VisualStudio.AppDesigner")]
