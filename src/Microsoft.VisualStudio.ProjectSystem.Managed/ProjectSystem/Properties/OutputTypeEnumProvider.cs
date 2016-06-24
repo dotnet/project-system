4362d5d6a43171424dece300aa31a56ec75d7bbd
Srivatsn Narayanan <srivatsn@microsoft.com>
2016-06-24 02:21:08 -0700

    Change OutputType to be a DynamicEnumProperty

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/OutputTypeEnumProvider.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Build.Framework.XamlTypes;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties
+{
+    [ExportDynamicEnumValuesProvider("OutputTypeEnumProvider")]
+    [AppliesTo(ProjectCapability.CSharpOrVisualBasic)]
+    internal class OutputTypeEnumProvider : IDynamicEnumValuesProvider
+    {
+        public Task<IDynamicEnumValuesGenerator> GetProviderAsync(IList<NameValuePair> options)
+        {
+            return Task.FromResult<IDynamicEnumValuesGenerator>(new OutputTypeEnumValuesGenerator());
+        }
+
+        internal class OutputTypeEnumValuesGenerator : IDynamicEnumValuesGenerator
+        {
+            private readonly Dictionary<string, IEnumValue> _outputTypeValues = new Dictionary<string, IEnumValue>
+            {
+                { "dll",             new PageEnumValue(new EnumValue {Name = "dll",    DisplayName = "0" }) },
+                { "exe",             new PageEnumValue(new EnumValue {Name = "exe",    DisplayName = "1" }) },
+                { "winexe",          new PageEnumValue(new EnumValue {Name = "winexe", DisplayName = "2" }) },
+                { "winmdobj",        new PageEnumValue(new EnumValue {Name = "exe",    DisplayName = "0" }) },
+                { "appcontainerexe", new PageEnumValue(new EnumValue {Name = "winexe", DisplayName = "1" }) }
+            };
+
+            public bool AllowCustomValues => false;
+
+            public Task<ICollection<IEnumValue>> GetListedValuesAsync()
+            {
+                return Task.FromResult<ICollection<IEnumValue>>(_outputTypeValues.Values.Take(3).ToList());
+            }
+
+            public Task<IEnumValue> TryCreateEnumValueAsync(string userSuppliedValue)
+            {
+                if (_outputTypeValues.ContainsKey(userSuppliedValue))
+                {
+                    return Task.FromResult(_outputTypeValues[userSuppliedValue]);
+                }
+
+                return Task.FromResult<IEnumValue>(null);
+            }
+        }
+    }
+}
