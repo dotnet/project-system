829137b34a76952cb33ec315ae157dc1e41837ba
Jinu Joseph <jinuj@microsoft.com>
2016-06-27 13:05:41 -0700

    Rename file should rename matching multiple symbols in the file.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/IRoslynServicesFactory.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using Moq;
+using Microsoft.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS
+{
+    internal static class IRoslynServicesFactory
+    {
+        public static IRoslynServices Implement(Func<Solution, ISymbol, string, Task<Solution>> renameSymbolAsync, Func<Workspace,Solution,bool> applyChangesToSolution)
+        {
+            var mock = new Mock<IRoslynServices>();
+           
+            mock.Setup(h => h.RenameSymbolAsync(It.IsAny<Solution>(), It.IsAny<ISymbol>(), It.IsAny<string>()))
+                .Returns(renameSymbolAsync);
+
+            mock.Setup(h => h.ApplyChangesToSolution(It.IsAny<Workspace>(), It.IsAny<Solution>()))
+                .Returns(applyChangesToSolution);
+
+            return mock.Object;
+        }
+    }
+}
+
+
--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/RenamerTests.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/RenamerTests.cs
@@ -12,6 +12,8 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
     public class RenamerTests
     {
         bool ConfirmForRenameWasCalled { get; set; }
+        bool RenameServiceWasCalled { get; set; }
+        bool ApplySolutionWasCalled { get; set; }
 
         [Theory]
         [InlineData("class Foo{}", "Foo.cs", "Bar.cs")]
@@ -20,10 +22,38 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
         [InlineData("partial class Foo {} partial class Foo {}", "Foo.cs", "Bar.cs")]
         [InlineData("struct Foo { decimal price; string title; string author;}", "Foo.cs", "Bar.cs" )]
         [InlineData("enum Foo { None, enum1, enum2, enum3, enum4 };", "Foo.cs", "Bar.cs")]
+        [InlineData("namespace n1 {class Foo{}} namespace n2 {class Foo{}}", "Foo.cs", "Bar.cs")]
+        public async Task Rename_Symbol_Should_TriggerUserConfirmationAsync(string soureCode, string oldFilePath, string newFilePath)
+        {
+            var userNotificationServices = IUserNotificationServicesFactory.Implement(f =>
+            {
+                ConfirmForRenameWasCalled = true;
+                return false;
+            });
+            await RenameAsync(soureCode, oldFilePath, newFilePath, userNotificationServices);
+            Assert.True(ConfirmForRenameWasCalled);
+            Assert.False(RenameServiceWasCalled);
+        }
+
+        [Theory]
+        [InlineData("class Foo{}", "Foo.cs", "Bar.cs")]
+        [InlineData("interface Foo { void m1(); void m2();}", "Foo.cs", "Bar.cs")]
+        [InlineData("delegate int Foo(string s);", "Foo.cs", "Bar.cs")]
+        [InlineData("partial class Foo {} partial class Foo {}", "Foo.cs", "Bar.cs")]
+        [InlineData("struct Foo { decimal price; string title; string author;}", "Foo.cs", "Bar.cs")]
+        [InlineData("enum Foo { None, enum1, enum2, enum3, enum4 };", "Foo.cs", "Bar.cs")]
+        [InlineData("namespace n1 {class Foo{}} namespace n2 {class Foo{}}", "Foo.cs", "Bar.cs")]
         public async Task Rename_Symbol_Should_HappenAsync(string soureCode, string oldFilePath, string newFilePath)
         {
-            await RenameAsync(soureCode, oldFilePath, newFilePath);
+            var userNotificationServices = IUserNotificationServicesFactory.Implement(f =>
+            {
+                ConfirmForRenameWasCalled = true;
+                return true;
+            });
+            await RenameAsync(soureCode, oldFilePath, newFilePath, userNotificationServices);
             Assert.True(ConfirmForRenameWasCalled);
+            Assert.True(RenameServiceWasCalled);
+            Assert.True(ApplySolutionWasCalled);
         }
 
         [Theory]
@@ -36,11 +66,19 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
         [InlineData("enum Foo1 { None, enum1, enum2, enum3, enum4 };", "Foo.cs", "Bar.cs")]
         public async Task Rename_Symbol_Should_Not_HappenAsync(string soureCode, string oldFilePath, string newFilePath)
         {
-            await RenameAsync(soureCode, oldFilePath, newFilePath);
+            var userNotificationServices = IUserNotificationServicesFactory.Implement(f => 
+            {
+                ConfirmForRenameWasCalled = true;
+                return false;
+            });
+
+            await RenameAsync(soureCode, oldFilePath, newFilePath, userNotificationServices);
             Assert.False(ConfirmForRenameWasCalled);
+            Assert.False(RenameServiceWasCalled);
+            Assert.False(ApplySolutionWasCalled);
         }
 
-        private async Task RenameAsync(string soureCode, string oldFilePath, string newFilePath)
+        private async Task RenameAsync(string soureCode, string oldFilePath, string newFilePath, IUserNotificationServices userNotificationServices)
         {
             using (var ws = new AdhocWorkspace())
             {
@@ -48,20 +86,31 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
                 Solution solution = ws.AddSolution(InitializeWorkspace(projectId, newFilePath, soureCode));
                 Project project = (from d in solution.Projects where d.Id == projectId select d).FirstOrDefault();
 
-                var userNotificationServices = IUserNotificationServicesFactory.Implement(f => ConfirmRename(""));
+                var roslynServices = IRoslynServicesFactory.Implement(async(s, a, n) => await RenameSymbolAsync(s, a, n, projectId, newFilePath),(w,s) => ApplyChangesToSolution(w,s));
                 var optionsSettingsFactory = IOptionsSettingsFactory.Implement((string category, string page, string property, bool defaultValue) => { return true; });
                 
-                var renamer = new Renamer(ws, IProjectThreadingServiceFactory.Create(), userNotificationServices, optionsSettingsFactory, null, project, oldFilePath, newFilePath);
+                var renamer = new Renamer(ws, IProjectThreadingServiceFactory.Create(), userNotificationServices, optionsSettingsFactory, roslynServices, project, oldFilePath, newFilePath);
                 await renamer.RenameAsync(project);
             }
         }
 
-        private bool ConfirmRename(string message)
+        private async Task<Solution> RenameSymbolAsync(Solution solution, ISymbol symbol, string newName, ProjectId projectId, string newFilePath)
         {
-            ConfirmForRenameWasCalled = true;
-            return false;
+            using (var ws = new AdhocWorkspace())
+            {
+                RenameServiceWasCalled = true;
+
+                Solution _solution = ws.AddSolution(InitializeWorkspace(projectId, newFilePath, "class temp{}"));
+                return await Task.FromResult(_solution);
+            }
         }
-        
+
+        private bool ApplyChangesToSolution(Workspace ws, Solution renamedSolution)
+        {
+            ApplySolutionWasCalled = true;
+            return true;
+        }
+
         private SolutionInfo InitializeWorkspace(ProjectId projectId, string fileName, string code)
         {
             var solutionId = SolutionId.CreateNewId();
