c135a3df5172f4fd886b0b2d08dbbc81fe4d73c7
Nat Ayewah <nayewah@microsoft.com>
2016-06-29 23:14:33 -0700

    Implement Implicit Project Properties Provider

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/ProjectPropertiesProviders/DelegatedProjectPropertiesBase.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.ProjectSystem.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.ProjectPropertiesProviders
+{
+    /// <summary>
+    /// An implementation of IProjectProperties that delegates its operations
+    /// to another IProjectProperties object
+    /// </summary>
+    internal class DelegatedProjectPropertiesBase : IProjectProperties
+    {
+        protected internal IProjectProperties DelegatedProperties;
+
+        public DelegatedProjectPropertiesBase(IProjectProperties properties)
+        {
+            DelegatedProperties = properties;
+        }
+
+        public virtual IProjectPropertiesContext Context => DelegatedProperties.Context;
+
+        public virtual string FileFullPath => DelegatedProperties.FileFullPath;
+
+        public virtual PropertyKind PropertyKind => DelegatedProperties.PropertyKind;
+
+        public virtual Task DeleteDirectPropertiesAsync() 
+            => DelegatedProperties.DeleteDirectPropertiesAsync();
+
+        public virtual Task DeletePropertyAsync(string propertyName, IReadOnlyDictionary<string, string> dimensionalConditions = null)
+            => DelegatedProperties.DeletePropertyAsync(propertyName, dimensionalConditions);
+
+        public virtual Task<IEnumerable<string>> GetDirectPropertyNamesAsync() 
+            => DelegatedProperties.GetDirectPropertyNamesAsync();
+
+        public virtual Task<string> GetEvaluatedPropertyValueAsync(string propertyName) 
+            => DelegatedProperties.GetEvaluatedPropertyValueAsync(propertyName);
+
+        public virtual Task<IEnumerable<string>> GetPropertyNamesAsync() 
+            => DelegatedProperties.GetPropertyNamesAsync();
+
+        public virtual Task<string> GetUnevaluatedPropertyValueAsync(string propertyName) 
+            => DelegatedProperties.GetUnevaluatedPropertyValueAsync(propertyName);
+
+        public virtual Task<bool> IsValueInheritedAsync(string propertyName) 
+            => DelegatedProperties.IsValueInheritedAsync(propertyName);
+
+        public virtual Task SetPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IReadOnlyDictionary<string, string> dimensionalConditions = null) 
+            => DelegatedProperties.SetPropertyValueAsync(propertyName, unevaluatedPropertyValue, dimensionalConditions);
+
+
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/ProjectPropertiesProviders/ImplicitProjectPropertiesProvider.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.ProjectSystem.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.ProjectPropertiesProviders
+{
+    /// <summary>
+    /// Provides project properties that normally should not live in the project
+    /// file but may be written from an external source (e.g. the solution file).
+    /// This provider avoids writing these properties to the project file, but if
+    /// they are already present there, it updates the value to keep in sync with
+    /// the external source.
+    /// </summary>
+    [Export("Implicit", typeof(IProjectPropertiesProvider))]
+    [Export(typeof(IProjectPropertiesProvider))]
+    [ExportMetadata("Name", "Implicit")]
+    [AppliesTo(ProjectCapability.CSharpOrVisualBasic)]
+    internal class ImplicitProjectPropertiesProvider : DelegatedProjectPropertiesProviderBase
+    {
+        [ImportingConstructor]
+        public ImplicitProjectPropertiesProvider(
+            [Import("Microsoft.VisualStudio.ProjectSystem.ProjectFile")] IProjectPropertiesProvider provider)
+            : base(provider)
+        {
+        }
+
+        public override IProjectProperties GetProperties(string file, string itemType, string item)
+            => new ImplicitProjectProperties(DelegatedProvider.GetProperties(file, itemType, item));
+
+        private class ImplicitProjectProperties : DelegatedProjectPropertiesBase
+        {
+            public ImplicitProjectProperties(IProjectProperties properties)
+                : base(properties)
+            {
+            }
+
+            public override async Task SetPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IReadOnlyDictionary<string, string> dimensionalConditions = null)
+            {
+                var propertyNames = await DelegatedProperties.GetPropertyNamesAsync().ConfigureAwait(false);
+                if (propertyNames.Contains(propertyName))
+                {
+                    // overwrite the property if it exists
+                    await DelegatedProperties.SetPropertyValueAsync(propertyName, unevaluatedPropertyValue, dimensionalConditions).ConfigureAwait(false);
+                }
+            }
+        }
+    }
+}
