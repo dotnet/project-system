3604b34df818806f883df3982e2e71c1021a36de
Fredric Silberberg <frsilb@microsoft.com>
2016-07-26 11:31:34 -0700

    Added tests for LocalDebuggerCommand

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Properties/InterceptedProjectProperties/LocalDebuggerCommandProviderTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Properties
+{
+    [ProjectSystemTrait]
+    public class LocalDebuggerCommandProviderTests
+    {
+        private const string LocalDebuggerCommandPropertyName = "LocalDebuggerCommand";
+
+        [Theory]
+        [InlineData("cmd.exe")]
+        [InlineData("dotnet")]
+        [InlineData("notavalidexecutable")]
+        public async Task LocalDebuggerCommand_ExistingCommand_ReturnsUnmodified(string existingCommand)
+        {
+            var properties = IProjectPropertiesFactory.CreateWithPropertyAndValue(WindowsLocalDebugger.LocalDebuggerCommandProperty, existingCommand);
+            var provider = new LocalDebuggerCommandValueProvider();
+
+            Assert.Equal(existingCommand, await provider.OnGetEvaluatedPropertyValueAsync(existingCommand, properties));
+        }        
+
+        [Fact]
+        public async Task LocalDebuggerCommand_EmptyCommand_ReturnsDotnet()
+        {
+            var properties = IProjectPropertiesFactory.CreateWithPropertyAndValue(WindowsLocalDebugger.LocalDebuggerCommandProperty, string.Empty);
+            var provider = new LocalDebuggerCommandValueProvider();
+
+            Assert.Equal("dotnet.exe", await provider.OnGetEvaluatedPropertyValueAsync(string.Empty, properties));
+        }
+    }
+}
