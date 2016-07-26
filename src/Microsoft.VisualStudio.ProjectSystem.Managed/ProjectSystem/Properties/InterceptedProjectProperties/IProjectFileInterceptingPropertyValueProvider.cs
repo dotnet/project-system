dd5d0e80ecc14b3b540b346ae05929dca946159d
Fredric Silberberg <frsilb@microsoft.com>
2016-07-26 11:42:50 -0700

    Added missing license headers, removed prototype extraneous interface

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/IProjectFileInterceptingPropertyValueProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.VisualStudio.ProjectSystem.Properties;
-
-namespace Microsoft.VisualStudio.ProjectSystem.ProjectSystem.Properties
-{
-    public interface IProjectFileInterceptingPropertyValueProvider : IInterceptingPropertyValueProvider
-    {
-    }
-}
