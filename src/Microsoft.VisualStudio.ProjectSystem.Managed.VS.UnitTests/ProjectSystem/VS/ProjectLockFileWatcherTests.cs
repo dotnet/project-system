b8a40505a34fcf555ab5728d4915028538709699
Srivatsn Narayanan <srivatsn@microsoft.com>
2016-08-08 12:25:31 -0700

    Adding some more unittests

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/ProjectLockFileWatcherTests.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/ProjectLockFileWatcherTests.cs
@@ -15,10 +15,13 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
     {
         [Theory]
         [InlineData(@"
-Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\""
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""", null)]
+        [InlineData(@"
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""
     project.json, FilePath: ""C:\Foo\project.json""", @"C:\Foo\project.lock.json")]
         [InlineData(@"
-Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\""", null)]
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""
+    foo.project.json, FilePath: ""C:\Foo\foo.project.json""", @"C:\Foo\foo.project.lock.json")]
         public void VerifyFileWatcherRegistration(string inputTree, string fileToWatch)
         {
             var spMock = new IServiceProviderMoq();
@@ -34,8 +37,65 @@ Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\""", null)]
             watcher.ProjectTree_ChangedAsync(IProjectVersionedValueFactory<IProjectTreeSnapshot>.Create(IProjectTreeSnapshotFactory.Create(tree)));
 
             uint adviseCookie = 100;
+            // If fileToWatch is null then we expect to not register any filewatcher.
             var times = fileToWatch == null ? Times.Never() : Times.Once();
             Mock.Get<IVsFileChangeEx>(fileChangeService).Verify(s => s.AdviseFileChange(fileToWatch ?? It.IsAny<string>(), It.IsAny<uint>(), watcher, out adviseCookie), times);
+        }
+
+        [Theory]
+        // Add file
+        [InlineData(@"
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""", @"
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""
+    project.json, FilePath: ""C:\Foo\project.json""", 1, 0)]
+        // Remove file
+        [InlineData(@"
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""
+    project.json, FilePath: ""C:\Foo\project.json""", @"
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""", 1, 1)]
+        // Rename file to projectName.project.json
+        [InlineData(@"
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""
+    project.json, FilePath: ""C:\Foo\project.json""", @"
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""
+    foo.project.json, FilePath: ""C:\Foo\foo.project.json""", 2, 1)]
+        // Rename file to somethingelse
+        [InlineData(@"
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""
+    project.json, FilePath: ""C:\Foo\foo.project.json""", @"
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""
+    fooproject.json, FilePath: ""C:\Foo\fooproject.json""", 1, 1)]
+        // Unrelated change
+        [InlineData(@"
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""
+    project.json, FilePath: ""C:\Foo\foo.project.json""", @"
+Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\foo.proj""
+    project.json, FilePath: ""C:\Foo\project.json""
+    somefile.json, FilePath: ""C:\Foo\somefile.json""", 1, 0)]
+
+        public void VerifyFileWatcherRegistrationOnTreeChange(string inputTree, string changedTree, int numRegisterCalls, int numUnregisterCalls)
+        {
+            var spMock = new IServiceProviderMoq();
+            var fileChangeService = IVsFileChangeExFactory.CreateWithAdviseUnadviseFileChange(100);
+            spMock.AddService(typeof(IVsFileChangeEx), typeof(SVsFileChangeEx), fileChangeService);
+
+            var watcher = new ProjectLockFileWatcher(spMock,
+                                                     IProjectTreeProviderFactory.Create(),
+                                                     IUnconfiguredProjectCommonServicesFactory.Create(IUnconfiguredProjectFactory.Create(filePath: @"C:\Foo\foo.proj")),
+                                                     IProjectLockServiceFactory.Create());
+
+            var firstTree = ProjectTreeParser.Parse(inputTree);
+            watcher.ProjectTree_ChangedAsync(IProjectVersionedValueFactory<IProjectTreeSnapshot>.Create(IProjectTreeSnapshotFactory.Create(firstTree)));
+
+            var secondTree = ProjectTreeParser.Parse(changedTree);
+            watcher.ProjectTree_ChangedAsync(IProjectVersionedValueFactory<IProjectTreeSnapshot>.Create(IProjectTreeSnapshotFactory.Create(secondTree)));
+
+            uint adviseCookie = 100;
+            // If fileToWatch is null then we expect to not register any filewatcher.
+            Mock<IVsFileChangeEx> fileChangeServiceMock = Mock.Get<IVsFileChangeEx>(fileChangeService);
+            fileChangeServiceMock.Verify(s => s.AdviseFileChange(It.IsAny<string>(), It.IsAny<uint>(), watcher, out adviseCookie), 
+                                         Times.Exactly(numRegisterCalls));
+            fileChangeServiceMock.Verify(s => s.UnadviseFileChange(adviseCookie), Times.Exactly(numUnregisterCalls));
         }
     }
 }
