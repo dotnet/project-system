303e414ed9d7636ee4719654ff7880899101ece6
Balaji Soundrarajan <basoundr@microsoft.com>
2016-08-16 22:08:50 -0700

    Add StartupProjectRegistrar which adds to or removes from the project list a project whose Active Project configuration is debuggable.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Debug/StartupProjectRegistrar.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.VisualStudio.ProjectSystem.Debug;
+using Microsoft.VisualStudio.ProjectSystem.Imaging;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks.Dataflow;
+using Tasks = System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Debug
+{
+    internal class StartupProjectRegistrar :
+        IDisposable
+    {
+        private readonly IVsStartupProjectsListService _startupProjectsListService;
+        private readonly IUnconfiguredProjectVsServices _projectVsServices;
+        private readonly IProjectThreadingService _threadingService;
+        private readonly IActiveConfiguredProjectSubscriptionService _activeConfiguredProjectSubscriptionService;
+        private readonly ActiveConfiguredProject<DebuggerLaunchProviders> _launchProviders;
+
+        private Guid _guid;
+        private IDisposable _evaluationSubscriptionLink;
+        private bool _isDebuggable;
+
+        [ImportingConstructor]
+        public StartupProjectRegistrar(
+            ActiveConfiguredProject<ConfiguredProject> activeConfiguredProject,
+            IUnconfiguredProjectVsServices projectVsServices,
+            SVsServiceProvider serviceProvider,
+            IProjectThreadingService threadingService,
+            IActiveConfiguredProjectSubscriptionService activeConfiguredProjectSubscriptionService,
+            ActiveConfiguredProject<DebuggerLaunchProviders> launchProviders)
+        {
+            Requires.NotNull(serviceProvider, nameof(serviceProvider));
+            Requires.NotNull(activeConfiguredProject, nameof(activeConfiguredProject));
+            Requires.NotNull(threadingService, nameof(threadingService));
+            Requires.NotNull(projectVsServices, nameof(projectVsServices));
+            Requires.NotNull(activeConfiguredProjectSubscriptionService, nameof(activeConfiguredProjectSubscriptionService));
+
+            _startupProjectsListService = serviceProvider.GetService<IVsStartupProjectsListService, SVsStartupProjectsListService>();
+            _threadingService = threadingService;
+            _projectVsServices = projectVsServices;
+            _activeConfiguredProjectSubscriptionService = activeConfiguredProjectSubscriptionService;
+            _launchProviders = launchProviders;
+        }
+
+        [ProjectAutoLoad]
+        [AppliesTo(ProjectCapability.CSharpOrVisualBasic)]
+        private async Tasks.Task OnProjectFactoryCompletedAsync()
+        {
+            ConfigurationGeneral projectProperties =
+                await _projectVsServices.ActiveConfiguredProjectProperties.GetConfigurationGeneralPropertiesAsync().ConfigureAwait(false);
+            _guid = new Guid((string)await projectProperties.ProjectGuid.GetValueAsync().ConfigureAwait(false));
+            Assumes.False(_guid == Guid.Empty);
+
+            await InitializeAsync().ConfigureAwait(false);
+        }
+
+        public async Tasks.Task InitializeAsync()
+        {
+            await AddOrRemoveProjectFromStartupProjectList(initialize: true).ConfigureAwait(false);
+
+            var watchedEvaluationRules = Empty.OrdinalIgnoreCaseStringSet.Add(ConfigurationGeneral.SchemaName);
+            var evaluationBlock = new ActionBlock<IProjectVersionedValue<IProjectSubscriptionUpdate>>(
+                ConfigurationGeneralRuleBlock_ChangedAsync);
+            _evaluationSubscriptionLink = _activeConfiguredProjectSubscriptionService.ProjectRuleSource.SourceBlock.LinkTo(
+                evaluationBlock,
+                ruleNames: watchedEvaluationRules, suppressVersionOnlyUpdates:true
+                );
+        }
+
+        private async Tasks.Task ConfigurationGeneralRuleBlock_ChangedAsync(IProjectVersionedValue<IProjectSubscriptionUpdate> e)
+        {
+            IProjectChangeDescription projectChange = e.Value.ProjectChanges[ConfigurationGeneral.SchemaName];
+
+            if (projectChange.Difference.ChangedProperties.Contains(ConfigurationGeneral.OutputTypeProperty))
+            {
+                await AddOrRemoveProjectFromStartupProjectList().ConfigureAwait(false);
+            }
+        }
+
+        private async Tasks.Task AddOrRemoveProjectFromStartupProjectList(bool initialize = false)
+        {
+            bool isDebuggable = await IsDebuggable().ConfigureAwait(false);
+
+            if (initialize || isDebuggable != _isDebuggable)
+            {
+                _isDebuggable = isDebuggable;
+                await _threadingService.SwitchToUIThread();
+                if (isDebuggable)
+                {
+                    _startupProjectsListService.AddProject(ref _guid);
+                }
+                else
+                {
+                    _startupProjectsListService.RemoveProject(ref _guid);
+                }
+            }
+        }
+
+        private async Tasks.Task<bool> IsDebuggable()
+        {
+            foreach (var provider in _launchProviders.Value.Debuggers)
+            {
+                if (await provider.Value.CanLaunchAsync(DebugLaunchOptions.StopAtEntryPoint).ConfigureAwait(true))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _evaluationSubscriptionLink?.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+
+        [Export]
+        internal class DebuggerLaunchProviders
+        {
+            [ImportingConstructor]
+            public DebuggerLaunchProviders(ConfiguredProject project)
+            {
+                Debuggers = new OrderPrecedenceImportCollection<IDebugLaunchProvider, IDebugLaunchProviderMetadataView>(projectCapabilityCheckProvider: project);
+            }
+
+            [ImportMany]
+            public OrderPrecedenceImportCollection<IDebugLaunchProvider, IDebugLaunchProviderMetadataView> Debuggers
+            {
+                get;
+            }
+        }
+    }
+}
