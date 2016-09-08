bffdbde4f491ccb28527d0ec7a9c366c56898c57
Balaji Soundrarajan <basoundr@microsoft.com>
2016-09-07 17:58:12 -0700

    Service that provides Target Frameworks

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/IUnconfiguredProjectPropertyProviderService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties
+{
+    /// <summary>
+    /// This service provides value for the properties defined in Unconfigured project
+    /// </summary>
+    public interface IUnconfiguredProjectPropertyProviderService
+    {
+        /// <summary>
+        /// Provides the semicolon separated list of Target frameworks
+        /// </summary>
+        Task<string> GetTargetFrameworksAsync();
+    }
+}
