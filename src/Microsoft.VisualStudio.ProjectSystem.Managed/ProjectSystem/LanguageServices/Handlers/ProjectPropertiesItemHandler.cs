997de13a741fc21f71ec9581759e19b5a892fbd6
David Kean <davkean@microsoft.com>
2016-09-15 09:50:17 -0700

    'later' -> 'latter' in comment

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/LanguageServices/Handlers/ProjectPropertiesItemHandler.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/LanguageServices/Handlers/ProjectPropertiesItemHandler.cs
@@ -60,7 +60,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.LanguageServices.Handlers
             // so that it can automatically hook up project-to-project references. It does this by matching the 
             // bin output path with the another project's /reference argument, if they match, then it automatically 
             // introduces a project reference between the two. We pass the intermediate path via the /out 
-            // command-line argument and set via one of the other handlers, where as the later is calculate via 
+            // command-line argument and set via one of the other handlers, where as the latter is calculate via 
             // the TargetPath property and explictly set on the context.
 
             if (projectChange.Difference.ChangedProperties.Contains(ConfigurationGeneral.TargetPathProperty))
