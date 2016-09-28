a2b2b8158afa4d0932124601ff42ee441d667bea
Fredric Silberberg <frsilb@microsoft.com>
2016-09-28 10:49:19 -0700

    Added several more tests for XprojMigrationFactory

--- src/Microsoft.VisualStudio.ProjectSystem.CSharp.VS.UnitTests/ProjectSystem/VS/Xproj/MigrateXprojProjectFactoryTests.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.CSharp.VS.UnitTests/ProjectSystem/VS/Xproj/MigrateXprojProjectFactoryTests.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using Microsoft.VisualStudio.Packaging;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.Collections.Generic;
@@ -98,7 +99,132 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Xproj
             Assert.Equal(0, loggedMessages.Count);
         }
 
-        private Tuple<string, string, string> CreateTempProjectLocation()
+        [Fact]
+        public void MigrateXprojProjectFactory_MigrateOutput_LoggedCorrectly()
+        {
+            var projectDirectory = @"C:\Test";
+            var xproj = @"C:\Test\XprojMigrationTests.xproj";
+
+            // Runner returns valid response, standard exit code
+            var procRunner = ProcessRunnerFactory.ImplementRunner(pr =>
+            {
+                Assert.Equal("dotnet.exe", pr.FileName);
+                Assert.Equal("migrate -s -p \"C:\\Test\" -x \"C:\\Test\\XprojMigrationTests.xproj\"", pr.Arguments);
+            }, outputText: "Standard Output", errorText: "Standard Error");
+            var migrator = new MigrateXprojProjectFactory(procRunner);
+
+            var loggedMessages = new List<LogMessage>();
+            var logger = IVsUpgradeLoggerFactory.CreateLogger(loggedMessages);
+
+            Assert.True(migrator.MigrateProject(projectDirectory, xproj, "XprojMigrationTests", logger));
+            Assert.Equal(2, loggedMessages.Count);
+            Assert.Equal(new LogMessage
+            {
+                File = xproj,
+                Level = (uint)__VSUL_ERRORLEVEL.VSUL_INFORMATIONAL,
+                Message = "Standard Output",
+                Project = "XprojMigrationTests"
+            }, loggedMessages[0]);
+            Assert.Equal(new LogMessage
+            {
+                File = xproj,
+                Level = (uint)__VSUL_ERRORLEVEL.VSUL_WARNING,
+                Message = "Standard Error",
+                Project = "XprojMigrationTests"
+            }, loggedMessages[1]);
+        }
+
+        [Fact]
+        public void MigrateXprojProjectFactory_MigrateError_ReturnsFalse()
+        {
+            var projectDirectory = @"C:\Test";
+            var xproj = @"C:\Test\XprojMigrationTests.xproj";
+
+            // Runner returns valid response, standard exit code
+            var procRunner = ProcessRunnerFactory.ImplementRunner(pr =>
+            {
+                Assert.Equal("dotnet.exe", pr.FileName);
+                Assert.Equal("migrate -s -p \"C:\\Test\" -x \"C:\\Test\\XprojMigrationTests.xproj\"", pr.Arguments);
+            }, exitCode: VSConstants.E_FAIL);
+            var migrator = new MigrateXprojProjectFactory(procRunner);
+
+            var loggedMessages = new List<LogMessage>();
+            var logger = IVsUpgradeLoggerFactory.CreateLogger(loggedMessages);
+            Assert.False(migrator.MigrateProject(projectDirectory, xproj, "XprojMigrationTests", logger));
+            Assert.Equal(1, loggedMessages.Count);
+            Assert.Equal(new LogMessage
+            {
+                Level = (uint)__VSUL_ERRORLEVEL.VSUL_ERROR,
+                Project = "XprojMigrationTests",
+                File = xproj,
+                Message = $"Failed to migrate XProj project XprojMigrationTests. 'dotnet migrate -s -p \"{projectDirectory}\" -x \"{xproj}\"' exited with error code {VSConstants.E_FAIL}."
+            }, loggedMessages[0]);
+        }
+
+        [Fact]
+        public void MigrateXprojProjectFactory_ValidCsproj_FindsCsproj()
+        {
+            var tuple = CreateTempProjectLocation();
+            var xproj = tuple.Item2;
+            var projectDir = tuple.Item4;
+            var csproj = Path.Combine(projectDir, "XprojMigrationTests.csproj");
+
+            File.Create(csproj).Dispose();
+
+            var loggedMessages = new List<LogMessage>();
+            var logger = IVsUpgradeLoggerFactory.CreateLogger(loggedMessages);
+            var migrator = new MigrateXprojProjectFactory(ProcessRunnerFactory.CreateRunner());
+
+            Assert.Equal(csproj, migrator.GetCsproj(projectDir, "XprojMigrationTests", xproj, logger));
+            Assert.Equal(0, loggedMessages.Count);
+        }
+
+        [Fact]
+        public void MigrateXprojProjectFactory_NoCsproj_LogsError()
+        {
+            var tuple = CreateTempProjectLocation();
+            var xproj = tuple.Item2;
+            var projectDir = tuple.Item4;
+            var csproj = Path.Combine(projectDir, "XprojMigrationTests.csproj");
+
+            var loggedMessages = new List<LogMessage>();
+            var logger = IVsUpgradeLoggerFactory.CreateLogger(loggedMessages);
+            var migrator = new MigrateXprojProjectFactory(ProcessRunnerFactory.CreateRunner());
+
+            Assert.Equal("", migrator.GetCsproj(projectDir, "XprojMigrationTests", xproj, logger));
+            Assert.Equal(1, loggedMessages.Count);
+            Assert.Equal(new LogMessage
+            {
+                Level = (uint)__VSUL_ERRORLEVEL.VSUL_ERROR,
+                Project = "XprojMigrationTests",
+                File = xproj,
+                Message = $"Expected to find migrated cpsroj in {projectDir}, but did not find any."
+            }, loggedMessages[0]);
+        }
+
+        [Fact]
+        public void MigrateXprojProjectFactory_ValidProject_IsUpgradable()
+        {
+            var tuple = CreateTempProjectLocation();
+            var xproj = tuple.Item2;
+
+            var loggedMessages = new List<LogMessage>();
+            var logger = IVsUpgradeLoggerFactory.CreateLogger(loggedMessages);
+            int upgradeRequired;
+            Guid newProjectFactory;
+            uint capabilityFlags;
+
+            var migrator = new MigrateXprojProjectFactory(ProcessRunnerFactory.CreateRunner());
+
+            Assert.Equal(VSConstants.S_OK,
+                migrator.UpgradeProject_CheckOnly(xproj, logger, out upgradeRequired, out newProjectFactory, out capabilityFlags));
+            Assert.Equal((int)__VSPPROJECTUPGRADEVIAFACTORYREPAIRFLAGS.VSPUVF_PROJECT_ONEWAYUPGRADE, upgradeRequired);
+            Assert.Equal(Guid.Parse(CSharpProjectSystemPackage.ProjectTypeGuid), newProjectFactory);
+            Assert.Equal((uint)(__VSPPROJECTUPGRADEVIAFACTORYFLAGS.PUVFF_BACKUPSUPPORTED | __VSPPROJECTUPGRADEVIAFACTORYFLAGS.PUVFF_COPYBACKUP),
+                capabilityFlags);
+        }
+
+        private Tuple<string, string, string, string> CreateTempProjectLocation()
         {
             var directory = Path.Combine(Path.GetTempPath(), $"XprojMigrationTests_{Guid.NewGuid().ToString()}");
             Directory.CreateDirectory(directory);
@@ -109,7 +235,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Xproj
             var projectJson = Path.Combine(directory, "project.json");
             File.Create(projectJson).Dispose();
 
-            return Tuple.Create(backupDirectory, xproj, projectJson);
+            return Tuple.Create(backupDirectory, xproj, projectJson, directory);
         }
     }
 }
