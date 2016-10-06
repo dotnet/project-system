15574fb19611c31975ae41509ba024c44666c767
Manish Vasani <mavasani@microsoft.com>
2016-10-05 20:52:52 -0700

    Add extension check before adding ProjectCapability.ProjectConfigurationsInferredFromUsage

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/ProjectConfigurationsInferredFromUsageCapabilitiesProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/ProjectConfigurationsInferredFromUsageCapabilitiesProvider.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Immutable;
 using System.ComponentModel.Composition;
 using System.Threading;
@@ -11,12 +12,21 @@ namespace Microsoft.VisualStudio.ProjectSystem
     [AppliesTo(ProjectCapabilities.AlwaysApplicable)]
     internal sealed class ProjectConfigurationsInferredFromUsageCapabilitiesProvider : UnconfiguredProjectCapabilitiesProviderBase
     {
-        private static readonly ImmutableHashSet<string> _capabilities = ImmutableHashSet<string>.Empty.Add(ProjectCapabilities.ProjectConfigurationsInferredFromUsage);
+        private readonly ImmutableHashSet<string> _capabilities;
 
         [ImportingConstructor]
-        public ProjectConfigurationsInferredFromUsageCapabilitiesProvider(UnconfiguredProject unconfiguedProject)
-            : base(typeof(ProjectConfigurationsInferredFromUsageCapabilitiesProvider).Name, unconfiguedProject)
+        public ProjectConfigurationsInferredFromUsageCapabilitiesProvider(UnconfiguredProject unconfiguredProject)
+            : base(typeof(ProjectConfigurationsInferredFromUsageCapabilitiesProvider).Name, unconfiguredProject)
         {
+            var capabilities = ImmutableHashSet<string>.Empty;
+            if (unconfiguredProject.FullPath != null &&
+                (unconfiguredProject.FullPath.EndsWith(".csproj", StringComparison.OrdinalIgnoreCase) ||
+                unconfiguredProject.FullPath.EndsWith(".vbproj", StringComparison.OrdinalIgnoreCase)))
+            {
+                capabilities = capabilities.Add(ProjectCapabilities.ProjectConfigurationsInferredFromUsage);
+            }
+
+            _capabilities = capabilities;
         }
 
         protected override Task<ImmutableHashSet<string>> GetCapabilitiesAsync(CancellationToken cancellationToken)
