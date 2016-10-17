e4be63e6e08bcda2c02ebc8a16fbde0a37cb74b5
Manish Vasani <mavasani@microsoft.com>
2016-10-16 19:23:35 -0700

    Set environment variables for Jenkins runs

--- netci.groovy
+++ netci.groovy
@@ -17,7 +17,11 @@ def branch = GithubBranchName
                 // Indicates that a batch script should be run with the build string (see above)
                 // Also available is:
                 // shell (for unix scripting)
-                batchFile("build.cmd /${configuration.toLowerCase()}")
+                batchFile("""SET VS150COMNTOOLS=%ProgramFiles(x86)%\\Microsoft Visual Studio\\VS15Preview\\Common7\\Tools\\
+SET VSSDK150Install=%ProgramFiles(x86)%\\Microsoft Visual Studio\\VS15Preview\\VSSDK\\
+SET VSSDKInstall=%ProgramFiles(x86)%\\Microsoft Visual Studio\\VS15Preview\\VSSDK\\
+
+build.cmd /${configuration.toLowerCase()}""")
             }
         }
 
