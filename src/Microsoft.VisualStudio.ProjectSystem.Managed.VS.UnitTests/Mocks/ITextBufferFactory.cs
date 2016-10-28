909c27d5a375cc482625d0b35c8d9da46b612b5a
Fredric Silberberg <frsilb@microsoft.com>
2016-10-27 18:56:57 -0700

    Added missing license headers

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/ITextBufferFactory.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/ITextBufferFactory.cs
@@ -1,9 +1,6 @@
-﻿using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Moq;
 
 namespace Microsoft.VisualStudio.Text
 {
--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/ITextDocumentFactory.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/ITextDocumentFactory.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.VisualStudio.Text;
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.VisualStudio.Text
 {
