8631690c417d574855df1247224abff8c477b73a
Fredric Silberberg <frsilb@microsoft.com>
2016-11-04 17:34:32 -0700

    Added a different, non-behavior-changing example of throw expressions

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/UI/RelayCommand.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/UI/RelayCommand.cs
@@ -25,10 +25,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.UI
         /// </summary>
         public RelayCommand(Action<object> execute, Predicate<object> canExecute)
         {
-            if (execute == null)
-                throw new ArgumentNullException("execute");
-
-            _execute = execute;
+            _execute = execute ?? throw new ArgumentNullException("execute");
             _canExecute = canExecute;
         }
 
