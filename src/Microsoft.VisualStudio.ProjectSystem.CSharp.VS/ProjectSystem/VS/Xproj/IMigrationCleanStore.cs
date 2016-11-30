51c25cde2e9ccb74957b379fcc0dfde197fc15f4
Fredric Silberberg <frsilb@microsoft.com>
2016-11-29 18:14:01 -0800

    Enabled removing project.json and xproj files on migration.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.CSharp.VS/ProjectSystem/VS/Xproj/IMigrationCleanStore.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Xproj
+{
+    internal interface IMigrationCleanStore
+    {
+        /// <summary>
+        /// The list of files that need to be cleaned, clearing the list after return.
+        /// </summary>
+        ISet<string> DrainFiles();
+
+        /// <summary>
+        /// Adds a new file to the list of files that need to be cleaned.
+        /// </summary>
+        void AddFiles(params string[] files);
+
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.CSharp.VS/ProjectSystem/VS/Xproj/MigrationCleanStore.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Xproj
+{
+    [Export(typeof(IMigrationCleanStore))]
+    class MigrationCleanStore : IMigrationCleanStore
+    {
+        private readonly object _lock = new object();
+
+        private ISet<string> _files = new HashSet<string>();
+
+        public ISet<string> DrainFiles()
+        {
+            lock (_lock)
+            {
+                var files = _files;
+                if (files.Count > 0)
+                {
+                    _files = new HashSet<string>();
+                }
+                return files;
+            }
+        }
+
+        public void AddFiles(params string[] fullPaths)
+        {
+            lock (_lock)
+            {
+                _files.AddRange(fullPaths);
+            }
+        }
+    }
+}
