ea08020d916335353981dedc3f5e1e57fb9884a9
Manish Vasani <mavasani@microsoft.com>
2016-12-14 14:24:11 -0800

    Add back the accidentally deleted property and also add Package property page to VB

--- src/Microsoft.VisualStudio.ProjectSystem.VisualBasic.VS/ProjectSystem/VS/Properties/VisualBasicProjectDesignerPage.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.VisualBasic.VS/ProjectSystem/VS/Properties/VisualBasicProjectDesignerPage.cs
@@ -10,6 +10,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Properties
     internal static class VisualBasicProjectDesignerPage
     {
         public static readonly ProjectDesignerPageMetadata Application = new ProjectDesignerPageMetadata(new Guid("{d6a537ac-1046-43ee-8c31-a6d72deb6b67}"), pageOrder:0, hasConfigurationCondition:false);
+        public static readonly ProjectDesignerPageMetadata Package = new ProjectDesignerPageMetadata(new Guid("{21b78be8-3957-4caa-bf2f-e626107da58e}"), pageOrder: 0, hasConfigurationCondition: false);
         public static readonly ProjectDesignerPageMetadata References = new ProjectDesignerPageMetadata(new Guid("{4E43F4AB-9F03-4129-95BF-B8FF870AF6AB}"), pageOrder: 1, hasConfigurationCondition: false);
         public static readonly ProjectDesignerPageMetadata Debug = new ProjectDesignerPageMetadata(new Guid("{0273C280-1882-4ED0-9308-52914672E3AA}"), pageOrder: 2, hasConfigurationCondition: false);
     }
