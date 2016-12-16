ea5d53487b3eb0d72a4f0fd27c476b4b63cd3874
David Kean <davkean@microsoft.com>
2016-12-16 16:57:21 +1100

    Added IPhysicalProjectTreeStorage to IPhysicalProjectTree

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Mocks/IPhysicalProjectTreeStorageFactory.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using Moq;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    internal class IPhysicalProjectTreeStorageFactory
+    {
+        public static IPhysicalProjectTreeStorage Create()
+        {
+            return Mock.Of<IPhysicalProjectTreeStorage>();
+        }
+    }
+}
