8bc9231ad39353f08676895b60193d8f5b63d790
David Kean <davkean@microsoft.com>
2016-12-30 11:40:47 +1100

    Change the base name to reflect LogicalName
    Filed https://github.com/dotnet/roslyn-project-system/issues/1058 to track this issue.

--- src/Microsoft.VisualStudio.AppDesigner/Resources/Designer.Designer.vb
+++ src/Microsoft.VisualStudio.AppDesigner/Resources/Designer.Designer.vb
@@ -43,7 +43,7 @@ Namespace My.Resources
         Friend Shared ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
             Get
                 If Object.ReferenceEquals(resourceMan, Nothing) Then
-                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("Designer", GetType(Designer).Assembly)
+                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("Microsoft.VisualStudio.AppDesigner.Designer", GetType(Designer).Assembly)
                     resourceMan = temp
                 End If
                 Return resourceMan
