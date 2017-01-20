aa373ecc7a5eddbf0d9624016e74514377428029
Manish Vasani <mavasani@microsoft.com>
2017-01-20 10:33:27 -0800

    Ensure that we pass absolute reference paths to Roslyn Language Services.
    For certain kinds of references, the command line argument uses relative paths (e.g. COM references for the repro here). Command line parser doesn't normalize these paths to the passed base directory, and we end up passing relative paths to IWorkspaceProjectContext.AddMetadataReference (RemoveMetadataReference) APIs. These get resolved relative to working directory instead of project directory, and hence cause invalid full paths and missing intellisense, in addition to squiggles for any references to these assemblies, even though build succeeds without errors.
    Fix is to make sure if make the reference paths rooted before passing them down to the language service.
    
    Fixes #786

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/LanguageServices/Handlers/MetadataReferenceItemHandlerTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem.LanguageServices.Handlers
+{
+    [ProjectSystemTrait]
+    public class MetadataReferenceItemHandlerTests
+    {
+        [Fact]
+        public void Constructor()
+        {
+            Assert.Throws<ArgumentNullException>(() => new MetadataReferenceItemHandler(project: null));
+            new MetadataReferenceItemHandler(UnconfiguredProjectFactory.Create());
+        }
+
+        [Fact]
+        public void DuplicateMetadataReferencesPushedToWorkspace()
+        {
+            var referencesPushedToWorkspace = new HashSet<string>(StringComparers.Paths);
+            Action<string> onReferenceAdded = s => referencesPushedToWorkspace.Add(s);
+            Action<string> onReferenceRemoved = s => referencesPushedToWorkspace.Remove(s);
+
+            var project = UnconfiguredProjectFactory.Create(filePath: @"C:\Myproject.csproj");
+            var context = IWorkspaceProjectContextFactory.CreateForMetadataReferences(project, onReferenceAdded, onReferenceRemoved);
+
+            var handler = new MetadataReferenceItemHandler(project);
+            var projectDir = Path.GetDirectoryName(project.FullPath);
+            var added = CSharpCommandLineParser.Default.Parse(args: new[] { @"/reference:C:\Assembly1.dll", @"/reference:C:\Assembly2.dll", @"/reference:C:\Assembly1.dll" }, baseDirectory: projectDir, sdkDirectory: null);
+            var empty = CSharpCommandLineParser.Default.Parse(args: new string[] { }, baseDirectory: projectDir, sdkDirectory: null);
+
+            handler.Handle(added: added, removed: empty, context: context, isActiveContext: true);
+
+            Assert.Equal(2, referencesPushedToWorkspace.Count);
+            Assert.Contains(@"C:\Assembly1.dll", referencesPushedToWorkspace);
+            Assert.Contains(@"C:\Assembly2.dll", referencesPushedToWorkspace);
+
+            var removed = CSharpCommandLineParser.Default.Parse(args: new[] { @"/reference:C:\Assembly1.dll", @"/reference:C:\Assembly1.dll" }, baseDirectory: projectDir, sdkDirectory: null);
+            handler.Handle(added: empty, removed: removed, context: context, isActiveContext: true);
+
+            Assert.Equal(1, referencesPushedToWorkspace.Count);
+            Assert.Contains(@"C:\Assembly2.dll", referencesPushedToWorkspace);
+        }
+
+        [Fact]
+        public void RootedReferencesPushedToWorkspace()
+        {
+            var referencesPushedToWorkspace = new HashSet<string>(StringComparers.Paths);
+            Action<string> onReferenceAdded = s => referencesPushedToWorkspace.Add(s);
+            Action<string> onReferenceRemoved = s => referencesPushedToWorkspace.Remove(s);
+
+            var project = UnconfiguredProjectFactory.Create(filePath: @"C:\ProjectFolder\Myproject.csproj");
+            var context = IWorkspaceProjectContextFactory.CreateForMetadataReferences(project, onReferenceAdded, onReferenceRemoved);
+
+            var handler = new MetadataReferenceItemHandler(project);
+            var projectDir = Path.GetDirectoryName(project.FullPath);
+            var added = CSharpCommandLineParser.Default.Parse(args: new[] { @"/reference:Assembly1.dll", @"/reference:C:\ProjectFolder\Assembly2.dll", @"/reference:..\ProjectFolder\Assembly2.dll" }, baseDirectory: projectDir, sdkDirectory: null);
+            var removed = CSharpCommandLineParser.Default.Parse(args: new string[] { }, baseDirectory: projectDir, sdkDirectory: null);
+
+            handler.Handle(added: added, removed: removed, context: context, isActiveContext: true);
+
+            Assert.Equal(2, referencesPushedToWorkspace.Count);
+            Assert.Contains(@"C:\ProjectFolder\Assembly1.dll", referencesPushedToWorkspace);
+            Assert.Contains(@"C:\ProjectFolder\Assembly2.dll", referencesPushedToWorkspace);
+        }
+    }
+}
