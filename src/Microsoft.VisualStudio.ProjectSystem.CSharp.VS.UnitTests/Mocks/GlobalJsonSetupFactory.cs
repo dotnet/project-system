bab31728eb3474ebb4762cf46f0b6f54ed5315b4
Fredric Silberberg <frsilb@microsoft.com>
2017-01-30 14:38:55 -0800

    Defer removal of global.json until after migration has been completed. Remove sdk element from global.json when first discovered, to ensure that the sdk isn't inappropriately downgraded.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.CSharp.VS.UnitTests/Mocks/GlobalJsonSetupFactory.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio.IO;
+using Microsoft.VisualStudio.Shell.Interop;
+using Moq;
+using System;
+using static Microsoft.VisualStudio.ProjectSystem.VS.Xproj.GlobalJsonRemover;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Xproj
+{
+    internal static class GlobalJsonSetupFactory
+    {
+        public static GlobalJsonSetup Create(bool retVal = false)
+        {
+            var mock = new Mock<GlobalJsonSetup>();
+            mock.Setup(g => g.SetupRemoval(It.IsAny<IVsSolution>(), It.IsAny<IServiceProvider>(), It.IsAny<IFileSystem>())).Returns(retVal);
+            return mock.Object;
+        }
+    }
+}
