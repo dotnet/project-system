9d09af3af2affd308f239dd71347108f552b4163
Anton Piskunov <antonpis@microsoft.com>
2017-01-30 11:11:54 -0800

    Follow up with feedback and add doc comments for new contract

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/NuGetDependenciesSubTreeProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/NuGetDependenciesSubTreeProvider.cs
@@ -559,6 +559,13 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies
 
         // INuGetPackagesDataProvider methods
 
+        /// <summary>
+        /// Allows to other providers to use nuget package dependencies search on a given node if it 
+        /// turns out to be a nuget package.
+        /// </summary>
+        /// <param name="packageItemSpec">Package reference items spec for which we need to do a search</param>
+        /// <param name="searchTerm">String to be searched</param>
+        /// <returns></returns>
         public Task<IEnumerable<IDependencyNode>> SearchAsync(string packageItemSpec, string searchTerm)
         {
             lock (_snapshotLock)
@@ -588,6 +595,15 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies
             }
         }
 
+        /// <summary>
+        /// For given node tries to find matching nuget package and adds direct package dependencies
+        /// nodes to given node's children collection. Use case is, when other provider has a node,
+        /// that is actually a package, it would call this method when GraphProvider is about to 
+        /// check if node has children or not.
+        /// </summary>
+        /// <param name="packageItemSpec">Package reference items spec that is supposed to be associated 
+        /// with the given node</param>
+        /// <param name="originalNode">Node to fill children for, if it's is a package</param>
         public void UpdateNodeChildren(string packageItemSpec, IDependencyNode originalNode)
         {
             lock (_snapshotLock)
