f8852c81c2d765c95d8633748e35032286e747ce
Raul Perez <raperez@microsoft.com>
2017-02-01 10:00:03 -0800

    Remove comment

--- src/Microsoft.VisualStudio.ProjectSystem.CSharp.VS/ProjectSystem/VS/Properties/CSharpProjectDesignerPageProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.CSharp.VS/ProjectSystem/VS/Properties/CSharpProjectDesignerPageProvider.cs
@@ -36,10 +36,6 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Properties
             }
 
             builder.Add(CSharpProjectDesignerPage.Debug);
-
-            // Removed until the page is revamped, see: https://github.com/dotnet/roslyn-project-system/issues/1397.
-            // builder.Add(CSharpProjectDesignerPage.ReferencePaths);
-
             builder.Add(CSharpProjectDesignerPage.Signing);
 
             return Task.FromResult<IReadOnlyCollection<IPageMetadata>>(builder.ToImmutable());
