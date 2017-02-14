79ea553ea2621e637846c03233a04349584452ce
Raul Perez <raperez@microsoft.com>
2017-02-13 18:45:31 -0800

    Refactor generic MsBuild manipulation and add additional utility functions

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/MsBuild/MsBuildUtilitiesTest.cs
@@ -0,0 +1,361 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    [ProjectSystemTrait]
+    public class MsBuildUtilitiesTest
+    {
+        [Fact]
+        public void MsBuildUtilities_GetProperty_MissingProperty()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>MyPropertyValue</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                var property = MsBuildUtilities.GetProperty(project.Project, "NonExistantProperty");
+                Assert.Null(property);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetProperty_ExistentProperty()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>MyPropertyValue</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetPropertyValues_ProjectRootElementNoProperty()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup/>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                var values = MsBuildUtilities.GetPropertyValues(project.Project, "MyProperty");
+                Assert.Equal(0, values.Length);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetPropertyValues_PropertyNoValue()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty/>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                var values = MsBuildUtilities.GetPropertyValues(project.Project, "MyProperty");
+                Assert.Equal(0, values.Length);
+            }
+            
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetPropertyValues_SingleValue()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>MyPropertyValue</MyProperty>
+  </PropertyGroup>
+</Project>";
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                var values = MsBuildUtilities.GetPropertyValues(project.Project, "MyProperty");
+                Assert.Equal(1, values.Length);
+                Assert.Equal("MyPropertyValue", values[0]);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetPropertyValues_MultipleValues()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1;2</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                var values = MsBuildUtilities.GetPropertyValues(project.Project, "MyProperty");
+                Assert.Equal(2, values.Length);
+                Assert.Equal("1", values[0]);
+                Assert.Equal("2", values[1]);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetPropertyValues_NonDefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1|2</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                var values = MsBuildUtilities.GetPropertyValues(project.Project, "MyProperty", '|');
+                Assert.Equal(2, values.Length);
+                Assert.Equal("1", values[0]);
+                Assert.Equal("2", values[1]);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetPropertyValues_EmptyValues()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1;   ;;;2</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                var values = MsBuildUtilities.GetPropertyValues(project.Project, "MyProperty");
+                Assert.Equal(2, values.Length);
+                Assert.Equal("1", values[0]);
+                Assert.Equal("2", values[1]);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_AddProperty_NoGroups()
+        {
+            using (var project = new MsBuildTempProjectFile())
+            {
+                MsBuildUtilities.AddProperty(project.Project, "MyProperty", "1");
+                Assert.Equal(1, project.Project.PropertyGroups.Count);
+                var group = project.Project.PropertyGroups.First();
+                Assert.Equal(1, group.Properties.Count);
+                var property = group.Properties.First();
+                Assert.Equal("1", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_AddProperty_FirstGroup()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup/>
+  <PropertyGroup/>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.AddProperty(project.Project, "MyProperty", "1");
+                Assert.Equal(2, project.Project.PropertyGroups.Count);
+                var group = project.Project.PropertyGroups.First();
+                Assert.Equal(1, group.Properties.Count);
+                var property = group.Properties.First();
+                Assert.Equal("1", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_AppendPropertyValue_DefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1;2</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.AppendPropertyValue(project.Project, "MyProperty", "3");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1;2;3", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_AppendPropertyValue_EmptyProperty()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty/>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.AppendPropertyValue(project.Project, "MyProperty", "1");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_AppendPropertyValue_MissingProperty()
+        {
+            using (var project = new MsBuildTempProjectFile())
+            {
+                MsBuildUtilities.AppendPropertyValue(project.Project, "MyProperty", "1");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_AppendPropertyValue_NonDefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.AppendPropertyValue(project.Project, "MyProperty", "2", '|');
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1|2", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RemovePropertyValue_DefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1;2</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.RemovePropertyValue(project.Project, "MyProperty", "2");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RemovePropertyValue_NonDefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1|2|3</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.RemovePropertyValue(project.Project, "MyProperty", "2", '|');
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1|3", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RemovePropertyValue_EmptyAfterRemove()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.RemovePropertyValue(project.Project, "MyProperty", "1");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal(string.Empty, property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RemovePropertyValue_MissingProperty()
+        {
+            using (var project = new MsBuildTempProjectFile())
+            {
+                MsBuildUtilities.RemovePropertyValue(project.Project, "MyProperty", "1");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.Null(property);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RenamePropertyValue_DefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1;2</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.RenamePropertyValue(project.Project, "MyProperty", "2", "5");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1;5", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RenamePropertyValue_NonDefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1|2|3</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.RenamePropertyValue(project.Project, "MyProperty", "2", "5", '|');
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1|5|3", property.Value);
+            }
+        }
+    }
+}
--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Configurations/TargetFrameworkProjectConfigurationDimensionProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Configurations/TargetFrameworkProjectConfigurationDimensionProvider.cs
@@ -1,13 +1,10 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Build.Construction;
-using Microsoft.Build.Evaluation;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Build
 {
@@ -18,7 +15,6 @@ namespace Microsoft.VisualStudio.ProjectSystem.Build
     [AppliesTo(ProjectCapabilities.ProjectConfigurationsInferredFromUsage)]
     internal class TargetFrameworkProjectConfigurationDimensionProvider : IProjectConfigurationDimensionsProvider
     {
-        internal const string TargetFrameworkPropertyName = "TargetFramework";
         private readonly IProjectLockService _projectLockService;
 
         [ImportingConstructor]
@@ -38,7 +34,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.Build
                 return ImmutableArray<KeyValuePair<string, string>>.Empty;
             }
 
-            return ImmutableArray.Create(new KeyValuePair<string, string>(TargetFrameworkPropertyName, targetFrameworks.First()));
+            return ImmutableArray.Create(new KeyValuePair<string, string>(ConfigurationGeneral.TargetFrameworkProperty, targetFrameworks.First()));
         }
 
         public async Task<IEnumerable<KeyValuePair<string, IEnumerable<string>>>> GetProjectConfigurationDimensionsAsync(UnconfiguredProject project)
@@ -49,15 +45,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.Build
                 return ImmutableArray<KeyValuePair<string, IEnumerable<string>>>.Empty;
             }
 
-            return ImmutableArray.Create(new KeyValuePair<string, IEnumerable<string>>(TargetFrameworkPropertyName, targetFrameworks));
-        }
-
-        private string GetProperty(ProjectRootElement projectRoot, string propertyName)
-        {
-            return projectRoot.Properties
-                .Where(p => string.Equals(p.Name, propertyName, StringComparison.OrdinalIgnoreCase))
-                .Select(p => ProjectCollection.Unescape(p.Value)) // it was escaped on the way in, so unescape on the way out.
-                .FirstOrDefault();
+            return ImmutableArray.Create(new KeyValuePair<string, IEnumerable<string>>(ConfigurationGeneral.TargetFrameworkProperty, targetFrameworks));
         }
 
         private async Task<ImmutableArray<string>> GetOrderedTargetFrameworksAsync(UnconfiguredProject project)
@@ -69,8 +57,8 @@ namespace Microsoft.VisualStudio.ProjectSystem.Build
                 var projectRoot = await access.GetProjectXmlAsync(project.FullPath).ConfigureAwait(false);
 
                 // If the project already defines a specific "TargetFramework" to target, then this is not a cross-targeting project and we don't need a target framework dimension.
-                var targetFrameworkProperty = GetProperty(projectRoot, TargetFrameworkPropertyName);
-                if (!string.IsNullOrEmpty(targetFrameworkProperty))
+                var targetFrameworkProperty = MsBuildUtilities.GetProperty(projectRoot, ConfigurationGeneral.TargetFrameworkProperty);
+                if (targetFrameworkProperty != null)
                 {
                     return ImmutableArray<string>.Empty;
                 }
@@ -78,32 +66,8 @@ namespace Microsoft.VisualStudio.ProjectSystem.Build
                 // Read the "TargetFrameworks" property from the project file.
                 // TODO: https://github.com/dotnet/roslyn-project-system/issues/547
                 //       We should read the "TargetFrameworks" properties from msbuild project evaluation at unconfigured project level, but there doesn't seem to be a way to do so.
-                var targetFrameworksProperty = GetProperty(projectRoot, ConfigurationGeneral.TargetFrameworksProperty);
-                if (string.IsNullOrEmpty(targetFrameworksProperty))
-                {
-                    return ImmutableArray<string>.Empty;
-                }
-
-                return ParseTargetFrameworks(targetFrameworksProperty);
+                return MsBuildUtilities.GetPropertyValues(projectRoot, ConfigurationGeneral.TargetFrameworksProperty);
             }
-        }
-
-        internal static ImmutableArray<string> ParseTargetFrameworks(string targetFrameworksProperty)
-        {
-            // TargetFrameworks contains semicolon delimited list of frameworks, for example "net45;netcoreapp1.0;netstandard1.4"
-            var targetFrameworksValue = targetFrameworksProperty.Split(';').Select(f => f.Trim());
-
-            // We need to ensure that we return the target frameworks in the specified order.
-            var targetFrameworksBuilder = ImmutableArray.CreateBuilder<string>();
-            foreach (var targetFramework in targetFrameworksValue)
-            {
-                if (!string.IsNullOrEmpty(targetFramework))
-                {
-                    targetFrameworksBuilder.Add(targetFramework);
-                }
-            }
-
-            return targetFrameworksBuilder.Distinct(StringComparer.OrdinalIgnoreCase).ToImmutableArray();
         }
     }
 }
