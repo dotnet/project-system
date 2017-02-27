a2f2d0246b9dd947af8abb0bc41f30e52f80ede2
David Kean <davkean@microsoft.com>
2017-02-27 18:19:11 +1100

    Build.cmd now builds the tree and modern VSIX together

--- build.cmd
+++ build.cmd
@@ -17,8 +17,6 @@ if /I "%1" == "/?" call :Usage && exit /b 1
 if /I "%1" == "/debug" set BuildConfiguration=Debug&&shift&& goto :ParseArguments
 if /I "%1" == "/release" set BuildConfiguration=Release&&shift&& goto :ParseArguments
 if /I "%1" == "/rebuild" set MSBuildTarget=Rebuild&&shift&& goto :ParseArguments
-if /I "%1" == "/restore" set MSBuildTarget=RestorePackages&&shift&& goto :ParseArguments
-if /I "%1" == "/modernvsixonly" set MSBuildTarget=BuildModernVsixPackages&&shift&& goto :ParseArguments
 if /I "%1" == "/skiptests" set RunTests=false&&shift&& goto :ParseArguments
 if /I "%1" == "/no-deploy-extension" set DeployVsixExtension=false&&shift&& goto :ParseArguments
 if /I "%1" == "/no-node-reuse" set NodeReuse=false&&shift&& goto :ParseArguments
@@ -46,18 +44,26 @@ if "%VisualStudioVersion%" == "" (
 )
 
 set BinariesDirectory=%Root%bin\%BuildConfiguration%\
-set LogFile=%BinariesDirectory%Build.log
 if not exist "%BinariesDirectory%" mkdir "%BinariesDirectory%" || goto :BuildFailed
 
-msbuild /nologo /nodeReuse:%NodeReuse% /consoleloggerparameters:Verbosity=minimal /fileLogger /fileloggerparameters:LogFile="%LogFile%";verbosity=diagnostic /t:"%MSBuildTarget%" /p:Configuration="%BuildConfiguration%" /p:RunTests="%RunTests%" /p:DeployVsixExtension="%DeployVsixExtension%" "%Root%build\build.proj" %MSBuildAdditionalArguments%
-if ERRORLEVEL 1 (
+REM We need to build the VSIX and Modern VSIX packages in a different MSBuild process because
+REM MicroBuild when building the modern VSIX packages has a dependency on a dll with the same 
+REM version but different contents.
+for %%T IN (%MSBuildTarget%, BuildModernVsixPackages) do (
+  
+  set LogFile=%BinariesDirectory%%%T.log
+  set LogFiles=!LogFiles!!LogFile! 
+  
+  msbuild /nologo /warnaserror /nodeReuse:%NodeReuse% /consoleloggerparameters:Verbosity=minimal /fileLogger /fileloggerparameters:LogFile="!LogFile!";verbosity=diagnostic /t:"%%T" /p:Configuration="%BuildConfiguration%" /p:RunTests="%RunTests%" /p:DeployVsixExtension="%DeployVsixExtension%" "%Root%build\build.proj" %MSBuildAdditionalArguments%
+  if ERRORLEVEL 1 (
     echo.
-    call :PrintColor Red "Build failed, for full log see %LogFile%."
+    call :PrintColor Red "Build failed, for full log see !LogFile!."
     exit /b 1
+  )
 )
 
 echo.
-call :PrintColor Green "Build completed successfully, for full log see %LogFile%"
+call :PrintColor Green "Build completed successfully, for full logs see %LogFiles%"
 exit /b 0
 
 :Usage
@@ -65,8 +71,6 @@ echo Usage: %BatchFile% [/rebuild^|/restore^|/modernvsixonly] [/debug^|/release]
 echo.
 echo   Build targets:
 echo     /rebuild                 Perform a clean, then build
-echo     /restore                 Only restore NuGet packages
-echo     /modernvsixonly          Only build modern vsman VSIXes
 echo     /skiptests               Don't run unit tests
 echo.
 echo   Build options:
