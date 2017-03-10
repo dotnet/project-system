ab5d957fbc2186d397a7e1aaf44a69dec36408e8
Balaji Soundrarajan <basoundr@microsoft.com>
2017-03-09 16:19:35 -0800

    Add license header to SettingsDesigner unit tests

--- src/Microsoft.VisualStudio.Editors.SettingsDesigner.UnitTests/VSCodeGenAccessors.vb
+++ src/Microsoft.VisualStudio.Editors.SettingsDesigner.UnitTests/VSCodeGenAccessors.vb
@@ -1,4 +1,6 @@
-﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
 Imports Microsoft.VisualStudio.Shell.Interop
 Imports System.ComponentModel.Design
 '------------------------------------------------------------------------------
