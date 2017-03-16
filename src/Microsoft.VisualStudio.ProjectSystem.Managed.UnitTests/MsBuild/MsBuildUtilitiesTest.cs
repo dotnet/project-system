3dbc4e8b15a56d58f3078a67da6b5c4d9983602b
Raul Perez <raperez@microsoft.com>
2017-03-16 16:14:31 -0700

    Add new configuration/platform providers

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/MsBuild/MsBuildUtilitiesTest.cs
@@ -0,0 +1,362 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    [ProjectSystemTrait]
+    public class MsBuildUtilitiesTest
+    {
+        [Fact]
+        public void MsBuildUtilities_GetProperty_MissingProperty()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>MyPropertyValue</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                var property = MsBuildUtilities.GetProperty(project.Project, "NonExistantProperty");
+                Assert.Null(property);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetProperty_ExistentProperty()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>MyPropertyValue</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetPropertyValues_SingleValue()
+        {
+            var values = MsBuildUtilities.GetPropertyValues("MyPropertyValue");
+            Assert.Equal(1, values.Length);
+            Assert.Equal("MyPropertyValue", values[0]);
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetPropertyValues_MultipleValues()
+        {
+            var values = MsBuildUtilities.GetPropertyValues("1;2");
+            Assert.Equal(2, values.Length);
+            Assert.Equal("1", values[0]);
+            Assert.Equal("2", values[1]);
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetPropertyValues_NonDefaultDelimiter()
+        {
+            var values = MsBuildUtilities.GetPropertyValues("1|2", '|');
+            Assert.Equal(2, values.Length);
+            Assert.Equal("1", values[0]);
+            Assert.Equal("2", values[1]);
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetPropertyValues_EmptyValues()
+        {
+            var values = MsBuildUtilities.GetPropertyValues("1;   ;;;2");
+            Assert.Equal(2, values.Length);
+            Assert.Equal("1", values[0]);
+            Assert.Equal("2", values[1]);
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetOrAddProperty_NoGroups()
+        {
+            using (var project = new MsBuildTempProjectFile())
+            {
+                MsBuildUtilities.GetOrAddProperty(project.Project, "MyProperty");
+                Assert.Equal(1, project.Project.Properties.Count);
+                Assert.Equal(1, project.Project.PropertyGroups.Count);
+                var group = project.Project.PropertyGroups.First();
+                Assert.Equal(1, group.Properties.Count);
+                var property = group.Properties.First();
+                Assert.Equal(string.Empty, property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetOrAddProperty_FirstGroup()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup/>
+  <PropertyGroup/>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.GetOrAddProperty(project.Project, "MyProperty");
+                Assert.Equal(1, project.Project.Properties.Count);
+                Assert.Equal(2, project.Project.PropertyGroups.Count);
+                var group = project.Project.PropertyGroups.First();
+                Assert.Equal(1, group.Properties.Count);
+                var property = group.Properties.First();
+                Assert.Equal(string.Empty, property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_GetOrAddProperty_ExistingProperty()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+    <MyProperty>1</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.GetOrAddProperty(project.Project, "MyProperty");
+                Assert.Equal(1, project.Project.Properties.Count);
+                Assert.Equal(1, project.Project.PropertyGroups.Count);
+                var group = project.Project.PropertyGroups.First();
+                Assert.Equal(1, group.Properties.Count);
+                var property = group.Properties.First();
+                Assert.Equal("1", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_AppendPropertyValue_DefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1;2</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.AppendPropertyValue(project.Project, "1;2", "MyProperty", "3");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1;2;3", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_AppendPropertyValue_EmptyProperty()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty/>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.AppendPropertyValue(project.Project, "", "MyProperty", "1");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_AppendPropertyValue_InheritedValue()
+        {
+            using (var project = new MsBuildTempProjectFile())
+            {
+                MsBuildUtilities.AppendPropertyValue(project.Project, "1;2", "MyProperty", "3");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1;2;3", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_AppendPropertyValue_MissingProperty()
+        {
+            using (var project = new MsBuildTempProjectFile())
+            {
+                MsBuildUtilities.AppendPropertyValue(project.Project, "", "MyProperty", "1");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_AppendPropertyValue_NonDefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.AppendPropertyValue(project.Project, "1", "MyProperty", "2", '|');
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1|2", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RemovePropertyValue_DefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1;2</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.RemovePropertyValue(project.Project, "1;2", "MyProperty", "2");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RemovePropertyValue_NonDefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1|2|3</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.RemovePropertyValue(project.Project, "1|2|3", "MyProperty", "2", '|');
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1|3", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RemovePropertyValue_EmptyAfterRemove()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.RemovePropertyValue(project.Project, "1", "MyProperty", "1");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal(string.Empty, property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RemovePropertyValue_InheritedValue()
+        {
+            using (var project = new MsBuildTempProjectFile())
+            {
+                MsBuildUtilities.RemovePropertyValue(project.Project, "1;2", "MyProperty", "1");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("2", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RemovePropertyValue_MissingProperty()
+        {
+            using (var project = new MsBuildTempProjectFile())
+            {
+                MsBuildUtilities.RemovePropertyValue(project.Project, "", "MyProperty", "1");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal(string.Empty, property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RenamePropertyValue_DefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1;2</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.RenamePropertyValue(project.Project, "1;2", "MyProperty", "2", "5");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1;5", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RenamePropertyValue_NonDefaultDelimiter()
+        {
+            string projectXml =
+@"<Project>
+  <PropertyGroup>
+     <MyProperty>1|2|3</MyProperty>
+  </PropertyGroup>
+</Project>";
+
+            using (var project = new MsBuildTempProjectFile(projectXml))
+            {
+                MsBuildUtilities.RenamePropertyValue(project.Project, "1|2|3", "MyProperty", "2", "5", '|');
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("1|5|3", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RenamePropertyValue_InheritedValue()
+        {
+            using (var project = new MsBuildTempProjectFile())
+            {
+                MsBuildUtilities.RenamePropertyValue(project.Project, "1;2", "MyProperty", "1", "3");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal("3;2", property.Value);
+            }
+        }
+
+        [Fact]
+        public void MsBuildUtilities_RenamePropertyValue_MissingProperty()
+        {
+            using (var project = new MsBuildTempProjectFile())
+            {
+                MsBuildUtilities.RenamePropertyValue(project.Project, "", "MyProperty", "1", "2");
+                var property = MsBuildUtilities.GetProperty(project.Project, "MyProperty");
+                Assert.NotNull(property);
+                Assert.Equal(string.Empty, property.Value);
+            }
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Configuration/PlatformProjectConfigurationDimensionProvider.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.VS.Editor;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Configuration
+{
+    /// <summary>
+    /// Provides 'Platform' project configuration dimension and values.
+    /// </summary>
+    /// <remarks>
+    /// The Order attribute will determine the order of the dimensions inside the configuration
+    /// service. We want Configuration|Platform|TargetFramework as the defaults so the values
+    /// start at MaxValue and get decremented for each in order for future extenders to fall
+    /// below these 3 providers.
+    /// </remarks>
+    [Export(typeof(IProjectConfigurationDimensionsProvider))]
+    [AppliesTo(ProjectCapabilities.ProjectConfigurationsDeclaredDimensions)]
+    [Order(int.MaxValue -1)]
+    internal class PlatformProjectConfigurationDimensionProvider : BaseProjectConfigurationDimensionProvider
+    {
+        [ImportingConstructor]
+        public PlatformProjectConfigurationDimensionProvider(IProjectXmlAccessor projectXmlAccessor)
+            : base(projectXmlAccessor, ConfigurationGeneral.PlatformProperty, "Platforms")
+        {
+        }
+
+        /// <summary>
+        /// Modifies the project when there's a configuration change.
+        /// </summary>
+        /// <param name="args">Information about the configuration dimension value change.</param>
+        /// <returns>A task for the async operation.</returns>
+        public override async Task OnDimensionValueChangedAsync(ProjectConfigurationDimensionValueChangedEventArgs args)
+        {
+            if (args.DimensionName == _dimensionName)
+            {
+                if (args.Stage == ChangeEventStage.Before)
+                {
+                    switch (args.Change)
+                    {
+                        case ConfigurationDimensionChange.Add:
+                            await OnPlatformAdded(args.Project, args.DimensionValue).ConfigureAwait(false);
+                            break;
+                        case ConfigurationDimensionChange.Delete:
+                            await OnPlatformDeleted(args.Project, args.DimensionValue).ConfigureAwait(false);
+                            break;
+                        case ConfigurationDimensionChange.Rename:
+                            // Platform doesn't currently supports rename, this should never be called.
+                            break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds a platform to the project.
+        /// </summary>
+        /// <param name="unconfiguredProject">Unconfigured project for which the configuration change.</param>
+        /// <param name="platformName">Name of the new platform.</param>
+        /// <returns>If the method succeeds, it returns S_OK. If it fails, it returns an error code.</returns>
+        private async Task OnPlatformAdded(UnconfiguredProject unconfiguredProject, string platformName)
+        {
+            string evaluatedPropertyValue = await GetPropertyValue(unconfiguredProject).ConfigureAwait(false);
+            await _projectXmlAccessor.ExecuteInWriteLock(msbuildProject =>
+            {
+                MsBuildUtilities.AppendPropertyValue(msbuildProject, evaluatedPropertyValue, _propertyName, platformName);
+            }).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Removes a platform from the project.
+        /// </summary>
+        /// <param name="unconfiguredProject">Unconfigured project for which the configuration change.</param>
+        /// <param name="platformName">Name of the deleted platform.</param>
+        /// <returns>If the method succeeds, it returns S_OK. If it fails, it returns an error code.</returns>
+        private async Task OnPlatformDeleted(UnconfiguredProject unconfiguredProject, string platformName)
+        {
+            string evaluatedPropertyValue = await GetPropertyValue(unconfiguredProject).ConfigureAwait(false);
+            await _projectXmlAccessor.ExecuteInWriteLock(msbuildProject =>
+            {
+                MsBuildUtilities.RemovePropertyValue(msbuildProject, evaluatedPropertyValue, _propertyName, platformName);
+            }).ConfigureAwait(false);
+        }
+    }
+}
