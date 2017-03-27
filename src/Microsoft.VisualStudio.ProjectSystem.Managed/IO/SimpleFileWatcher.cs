917b739e02de9138f3f98cc39c299d619af1a0f8
David Kean <davkean@microsoft.com>
2017-03-27 11:36:02 +1100

    Format SimpleFileWatcher

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/IO/SimpleFileWatcher.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/IO/SimpleFileWatcher.cs
@@ -10,32 +10,31 @@ namespace Microsoft.VisualStudio.IO
     /// </summary>
     internal class SimpleFileWatcher : IDisposable
     {
-
         private FileSystemWatcher FileWatcher { get; set; }
         private FileSystemEventHandler _handler;
         private RenamedEventHandler _renameHandler;
-        
+
         // For unit tests
         public SimpleFileWatcher()
         {
         }
-        
-        public SimpleFileWatcher(string dirToWatch, bool includeSubDirs, NotifyFilters notifyFilters, string fileFilter, 
+
+        public SimpleFileWatcher(string dirToWatch, bool includeSubDirs, NotifyFilters notifyFilters, string fileFilter,
                                        FileSystemEventHandler handler, RenamedEventHandler renameHandler)
-        {               
+        {
             FileWatcher = new FileSystemWatcher(dirToWatch);
             FileWatcher.IncludeSubdirectories = includeSubDirs;
             FileWatcher.NotifyFilter = notifyFilters;
             FileWatcher.Filter = fileFilter;
 
-            if(handler != null)
+            if (handler != null)
             {
                 FileWatcher.Created += handler;
                 FileWatcher.Deleted += handler;
                 FileWatcher.Changed += handler;
             }
 
-            if(renameHandler != null)
+            if (renameHandler != null)
             {
                 FileWatcher.Renamed += renameHandler;
             }
@@ -48,18 +47,18 @@ namespace Microsoft.VisualStudio.IO
         /// Cleans up our watcher on the Project.Json file
         /// </summary>
         public void Dispose()
-        {               
-            if(FileWatcher != null)
+        {
+            if (FileWatcher != null)
             {
                 FileWatcher.EnableRaisingEvents = false;
-                if(_handler != null)
+                if (_handler != null)
                 {
                     FileWatcher.Created += _handler;
                     FileWatcher.Deleted += _handler;
                     FileWatcher.Changed += _handler;
                     _handler = null;
                 }
-                if(_renameHandler != null)
+                if (_renameHandler != null)
                 {
                     FileWatcher.Renamed += _renameHandler;
                     _renameHandler = null;
