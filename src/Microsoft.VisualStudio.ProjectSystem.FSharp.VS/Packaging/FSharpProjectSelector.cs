fb94eff8e1a8702080d719e1d94a61cd506164a4
Srivatsn Narayanan <srivatsn@microsoft.com>
2017-03-30 02:40:24 -0700

    Implement a project selector for F#

---
+++ src/Microsoft.VisualStudio.ProjectSystem.FSharp.VS/Packaging/FSharpProjectSelector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.Shell.Interop1;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Runtime.InteropServices;
+using System.Xml;
+using Microsoft.VisualStudio.ProjectSystem.VS.Generators;
+
+namespace Microsoft.VisualStudio.Packaging
+{
+    [ComVisible(true)]
+    [Guid(SelectorGuid)]
+    [ClassRegistration(SelectorGuid, "Microsoft.VisualStudio.Packaging.FSharpProjectSelector, Microsoft.VisualStudio.ProjectSystem.FSharp.VS")]
+    public sealed class FSharpProjectSelector : IVsProjectSelector
+    {
+        public const string SelectorGuid = "E720DAD0-1854-47FC-93AF-E719B54B84E6";
+
+        public void GetProjectFactoryGuid(Guid guidProjectType, string pszFilename, out Guid guidProjectFactory)
+        {
+            XDocument doc = XDocument.Load(pszFilename);
+            XmlNamespaceManager nsm = new XmlNamespaceManager(new NameTable());
+            nsm.AddNamespace("msb", "http://schemas.microsoft.com/developer/msbuild/2003");
+            var hasProjectElementWithSdkAttribute = doc.XPathSelectElement("/msb:Project[@Sdk]", nsm) != null || doc.XPathSelectElement("/Project[@Sdk]") != null;
+            var hasImportElementWithSdk = doc.XPathSelectElement("msb:Import[@Sdk]", nsm) != null || doc.XPathSelectElement("Import[@Sdk]") != null;
+
+            if (hasProjectElementWithSdkAttribute || hasImportElementWithSdk)
+            {
+                guidProjectFactory = Guid.Parse(FSharpProjectSystemPackage.ProjectTypeGuid);
+                return;
+            }
+
+            guidProjectFactory = Guid.Parse(FSharpProjectSystemPackage.LegacyProjectTypeGuid);
+        }
+    }
+}
--- src/Microsoft.VisualStudio.ProjectSystem.FSharp.VS/Packaging/FSharpProjectSystemPackage.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.FSharp.VS/Packaging/FSharpProjectSystemPackage.cs
@@ -2,13 +2,45 @@
 
 using System;
 using System.Runtime.InteropServices;
+using System.Threading;
 using Microsoft.VisualStudio.Packaging;
 using Microsoft.VisualStudio.ProjectSystem.VS;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.Shell.Interop1;
 
 // We register ourselves as a new CPS "project type"
 [assembly: ProjectTypeRegistration(projectTypeGuid: FSharpProjectSystemPackage.ProjectTypeGuid, displayName: "#1", displayProjectFileExtensions: "#2", defaultProjectExtension: "fsproj", language: "FSharp", resourcePackageGuid: FSharpProjectSystemPackage.PackageGuid, Capabilities = ManagedProjectSystemPackage.DefaultCapabilities + "; " + ProjectCapability.FSharp, DisableAsynchronousProjectTreeLoad = true)]
+
+namespace Microsoft.VisualStudio.Shell.Interop1
+{
+    [Guid("B042860A-5A69-4259-BC88-F1C79AE16C50")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IVsRegisterProjectSelector
+    {
+        void RegisterProjectSelector(ref Guid rguidProjType, IVsProjectSelector pProjectSelector, [ComAliasName("VsShell.VSCOOKIE")] out uint pdwCookie);
+        void UnregisterProjectSelector([ComAliasName("VsShell.VSCOOKIE")] uint dwCookie);
+    }
+
+    [Guid("DFAD4C39-FCB2-4BDF-A389-2EA6DB28F062")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IVsProjectSelector
+    {
+        void GetProjectFactoryGuid(Guid guidProjectType, string pszFilename, out Guid guidProjectFactory);
+    }
+
+    //
+    // Summary:
+    //     Passed to System.IServiceProvider.GetService(System.Type) to return a reference
+    //     to Microsoft.VisualStudio.Shell.Interop.IVsRegisterProjectTypes.
+    [ComVisible(false)]
+    [Guid("F08400BB-0960-47f4-9E12-591DBF370546")]
+#pragma warning disable IDE1006 // Naming Styles
+    public interface SVsRegisterProjectTypes
+#pragma warning restore IDE1006 // Naming Styles
+    {
+    }
+}
 
 namespace Microsoft.VisualStudio.Packaging
 {
@@ -22,8 +54,26 @@ namespace Microsoft.VisualStudio.Packaging
         public const string PackageGuid = "a724c878-e8fd-4feb-b537-60baba7eda83";
         private const string ProjectTypeGuidFormatted = "{" + ProjectTypeGuid + "}";
 
+        private IVsRegisterProjectSelector _projectSelectorRegistrar;
+        private uint _projectSelectorCookie;
+
         public FSharpProjectSystemPackage()
         {
+        }
+
+        protected override System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            _projectSelectorRegistrar = this.GetService<IVsRegisterProjectSelector, SVsRegisterProjectTypes>();
+            var selectorGuid = typeof(FSharpProjectSelector).GUID;
+            _projectSelectorRegistrar.RegisterProjectSelector(ref selectorGuid, new FSharpProjectSelector(), out _projectSelectorCookie);
+
+            return base.InitializeAsync(cancellationToken, progress);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _projectSelectorRegistrar.UnregisterProjectSelector(_projectSelectorCookie);
+            base.Dispose(disposing);
         }
     }
 }
