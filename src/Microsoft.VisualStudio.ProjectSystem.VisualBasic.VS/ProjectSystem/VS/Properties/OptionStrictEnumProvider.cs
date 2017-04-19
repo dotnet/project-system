5dcc0616fa33eef5b08c8aebf06550368cb7b76e
Balaji Soundrarajan <basoundr@microsoft.com>
2017-04-19 09:54:33 -0700

    Add comment on working of OptionStrictEnumProvider

--- src/Microsoft.VisualStudio.ProjectSystem.VisualBasic.VS/ProjectSystem/VS/Properties/OptionStrictEnumProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.VisualBasic.VS/ProjectSystem/VS/Properties/OptionStrictEnumProvider.cs
@@ -11,6 +11,12 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Properties
     [AppliesTo(ProjectCapability.VisualBasic)]
     internal class OptionStrictEnumProvider : IDynamicEnumValuesProvider
     {
+        /// The value obtained from the EnumValue, when asked for value, is the <see cref="EnumValue.DisplayName"/>, unless otherwise
+        /// a different overload is used to obtain the value of <see cref="EnumValue.Name"/>. Hence a provider usually needs just a map
+        /// with a typical entry looking like, [ValuePersistedByUI] -> EnumValue {Name = [ValueToPersist] DisplayName = [ValueToReturnForGet]}
+        /// In the case of OptionStrict, Compile Property Page explicitly gets the value from <see cref="EnumValue.Name"/> for the Enums and 
+        /// casts them to integer. This requires us to make <see cref="EnumValue.Name"/> as '0' and '1' and hence cannot reuse 
+        /// <see cref="_persistOptionStrictEnumValues"/> Values to be used when trying to retrieve value for the enum.
         private readonly ICollection<IEnumValue> _listedOptionStrictEnumValues = new List<IEnumValue>
             {
                 new PageEnumValue(new EnumValue {Name = "0",    DisplayName = "Off", IsDefault = true }),
