847da819cff0e05173075da5312af145e38687b4
Jonathon Marolf <jmarolf@microsoft.com>
2017-04-20 15:59:09 -0700

    updating GenealBrowseObject and logic for getting TFM

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Properties/InterceptedProjectProperties/TargetFrameworkMonikersValueProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Properties/InterceptedProjectProperties/TargetFrameworkMonikersValueProvider.cs
@@ -4,48 +4,33 @@ using System.Collections.Immutable;
 using System.ComponentModel.Composition;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.ProjectSystem.Properties;
-using static Microsoft.VisualStudio.ProjectSystem.Build.TargetFrameworkProjectConfigurationDimensionProvider;
-using System.Linq;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Properties.InterceptedProjectProperties
 {
     [ExportInterceptingPropertyValueProvider("TargetFrameworkMonikers", ExportInterceptingPropertyValueProviderFile.ProjectFile)]
-    internal sealed class TargetFrameworkMonikersValueProvider : InterceptingPropertyValueProviderBase
+    internal class TargetFrameworkMonikersValueProvider : InterceptingPropertyValueProviderBase
     {
         private readonly ActiveConfiguredProjectsProvider _projectProvider;
-        private readonly ProjectProperties _properties;
 
         [ImportingConstructor]
-        public TargetFrameworkMonikersValueProvider(ActiveConfiguredProjectsProvider projectProvider, ProjectProperties properties)
+        public TargetFrameworkMonikersValueProvider(ActiveConfiguredProjectsProvider projectProvider)
         {
             _projectProvider = projectProvider;
-            _properties = properties;
         }
 
         public override async Task<string> OnGetEvaluatedPropertyValueAsync(string evaluatedPropertyValue, IProjectProperties defaultProperties)
         {
-            var activeProjectConfigurations = await _projectProvider.GetActiveProjectConfigurationsAsync().ConfigureAwait(false);
-            var isCrossTarging = activeProjectConfigurations.All(c => c.IsCrossTargeting());
-            if (isCrossTarging)
+            var configuredProjects = await _projectProvider.GetActiveConfiguredProjectsAsync().ConfigureAwait(true);
+            var builder = ImmutableArray.CreateBuilder<string>();
+            foreach (var configuredProject in configuredProjects)
             {
-                var builder = ImmutableArray.CreateBuilder<string>();
-                foreach (var activeProjectConfiguration in activeProjectConfigurations)
-                {
-                    if (activeProjectConfiguration.Dimensions.TryGetValue(TargetFrameworkPropertyName, out var tfm))
-                    {
-                        builder.Add(tfm);
-                    }
-                }
+                var projectProperties = configuredProject.Services.ExportProvider.GetExportedValue<ProjectProperties>();
+                var configuration = await projectProperties.GetConfigurationGeneralPropertiesAsync().ConfigureAwait(true);
+                var currentTargetFrameworkMoniker = (string)await configuration.TargetFrameworkMoniker.GetValueAsync().ConfigureAwait(true);
+                builder.Add(currentTargetFrameworkMoniker);
+            }
 
-                builder.Sort();
-                return string.Join(";", builder.ToArray());
-            }
-            else
-            {
-                var configuration = await _properties.GetConfigurationGeneralPropertiesAsync().ConfigureAwait(true);
-                var currentTargetFrameworkMoniker = (string)await configuration.TargetFrameworkMoniker.GetValueAsync().ConfigureAwait(false);
-                return currentTargetFrameworkMoniker;
-            }
+            return string.Join(";", builder.ToArray());
         }
     }
 }
