76f2eaf4d2e26e39728e61a2d426e85a63e4baa1
Jonathon Marolf <jmarolf@microsoft.com>
2017-04-24 16:54:41 -0700

    Add new project property "TargetFrameworkMonikers" that lists all specified target monikers in the project.
    # Conflicts:
    #	src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/Microsoft.VisualStudio.ProjectSystem.Managed.VS.csproj

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Properties/InterceptedProjectProperties/TargetFrameworkMonikersValueProvider.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+using static Microsoft.VisualStudio.ProjectSystem.Build.TargetFrameworkProjectConfigurationDimensionProvider;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Properties.InterceptedProjectProperties
+{
+    [ExportInterceptingPropertyValueProvider("TargetFrameworkMonikers", ExportInterceptingPropertyValueProviderFile.ProjectFile)]
+    internal sealed class TargetFrameworkMonikersValueProvider : InterceptingPropertyValueProviderBase
+    {
+        private readonly ProjectProperties _properties;
+
+        [ImportingConstructor]
+        public TargetFrameworkMonikersValueProvider(ProjectProperties properties)
+        {
+            _properties = properties;
+        }
+
+        public override async Task<string> OnGetEvaluatedPropertyValueAsync(string evaluatedPropertyValue, IProjectProperties defaultProperties)
+        {
+            var configuration = await _properties.GetConfigurationGeneralPropertiesAsync().ConfigureAwait(true);
+            var currentTargetFramework = (string)await configuration.TargetFramework.GetValueAsync().ConfigureAwait(true);
+            var currentTargetFrameworks = (string)await configuration.TargetFrameworks.GetValueAsync().ConfigureAwait(true);
+            if (!string.IsNullOrEmpty(currentTargetFrameworks))
+            {
+                // sorting the target frameworks so projects that specify the same set of frameworks return the same string
+                return string.Join(";", ParseTargetFrameworks(currentTargetFrameworks).Sort());
+            }
+            else if (!string.IsNullOrEmpty(currentTargetFramework))
+            {
+                return currentTargetFramework;
+            }
+
+            return string.Empty;
+        }
+    }
+}
