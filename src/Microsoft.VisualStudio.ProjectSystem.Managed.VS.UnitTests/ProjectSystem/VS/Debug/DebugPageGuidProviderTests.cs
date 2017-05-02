b78f0bea7bfca6685648e268f7f056ebf0aed539
Balaji Soundrarajan <basoundr@microsoft.com>
2017-05-02 15:43:52 -0700

    Add tests for DebugPageGuidProvider
    Also add copyright header for all new files

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Debug/DebugPageGuidProviderTests.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Debug
+{
+    [ProjectSystemTrait]
+    public class DebugPageGuidProviderTests
+    {
+        [Fact]
+        public async Task ProjectDebuggerProvider_GetDebugEngineForFrameworkTests()
+        {
+            var guid = new Guid("{0273C280-1882-4ED0-9308-52914672E3AA}");
+            Assert.True(await new DebugPageGuidProvider().GetDebugPropertyPageGuidAsync() == guid);
+        }
+    }
+}
