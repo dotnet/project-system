b805fdd7b086298b92ae27f4bfd5de5aaad1b687
Balaji Soundrarajan <basoundr@microsoft.com>
2017-05-02 15:36:14 -0700

    Add DebugPage Guid Provider for C#, VB and F#

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Debug/DebugPageGuidProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Debug
+{
+    [Export(typeof(IDebugPageGuidProvider))]
+    [AppliesTo(ProjectCapability.LaunchProfiles)]
+    [Order(Order.Default)]
+    internal class DebugPageGuidProvider : IDebugPageGuidProvider
+    {
+        // This is the Guid of C#, VB and F# Debug property page
+        private readonly Guid _guid = new Guid("{0273C280-1882-4ED0-9308-52914672E3AA}");
+
+        public Task<Guid> GetDebugPropertyPageGuidAsync()
+        {
+            return Task.FromResult(_guid);
+        }
+    }
+}
