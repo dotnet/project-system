73ab21f91fd23372455c53d22dfc269331455498
David Kean <davkean@microsoft.com>
2017-05-03 12:48:53 +1000

    Add IProjectLogger
    Added IProjectLogger that enables components to log to the new Project output window.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/IProjectSystemOptions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.ProjectSystem.Utilities;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS
+{
+    [Export(typeof(IProjectSystemOptions))]
+    internal class EnvironmentVariableProjectSystemOptions : IProjectSystemOptions
+    {
+        private readonly IEnvironmentHelper _environment;
+
+        [ImportingConstructor]
+        public EnvironmentVariableProjectSystemOptions(IEnvironmentHelper environment)
+        {
+            Requires.NotNull(environment, nameof(environment));
+
+            _environment = environment;
+        }
+
+        public bool IsProjectOutputPaneEnabled
+        {
+            get { return !IsEnabled("PROJECTSYSTEM_PROJECTOUTPUTPANEENABLED"); }
+        }
+
+        private bool IsEnabled(string variable)
+        {
+            string value = _environment.GetEnvironmentVariable(variable);
+
+            return string.Equals(value, "1", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
