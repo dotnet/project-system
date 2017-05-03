40be1d86ef1e777b768b3b8847a9b9b9066ff139
David Kean <davkean@microsoft.com>
2017-05-03 16:21:01 +1000

    Add tests for ProjectOutputWindowPaneProvider

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/IVsOutputWindowPaneFactory.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Moq;
+using System;
+
+namespace Microsoft.VisualStudio.Shell.Interop
+{
+    internal static class IVsOutputWindowPaneFactory
+    {
+        public static IVsOutputWindowPane Create()
+        {
+            var mock = new Mock<IVsOutputWindowPane>();
+            return mock.Object;
+        }
+
+        public static IVsOutputWindowPane ImplementActivate(Func<int> action)
+        {
+            var mock = new Mock<IVsOutputWindowPane>();
+            mock.Setup(p => p.Activate())
+                .Returns(action);
+
+            return mock.Object;
+        }
+    }
+}
