25deb32d99048ce8d91f3e0231e6cdcc26fe9655
Balaji Soundrarajan <basoundr@microsoft.com>
2017-05-17 17:14:11 -0700

    Extract the base Intercepting logic into a base class, OutputTypeValueProviderBase
    This base class will now be inherited by OutputType and OutputTypeEx value providers for BrowseObject

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Properties/InterceptedProjectProperties/OutputTypeExValueProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Properties/InterceptedProjectProperties/OutputTypeExValueProvider.cs
@@ -2,57 +2,44 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.ComponentModel.Composition;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.ProjectSystem.Properties;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Properties
 {
     /// OutputTypeEx acts as a converter for the MSBuild OutputType value expressed as <see cref="VSLangProj110.prjOutputTypeEx"/>.
     [ExportInterceptingPropertyValueProvider("OutputTypeEx", ExportInterceptingPropertyValueProviderFile.ProjectFile)]
-    internal sealed class OutputTypeExValueProvider : InterceptingPropertyValueProviderBase
+    internal sealed class OutputTypeExValueProvider : OutputTypeValueProviderBase
     {
         private readonly ProjectProperties _properties;
 
-        private readonly Dictionary<string, string> _getOutputTypeExMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        private static readonly ImmutableDictionary<string, string> s_getOutputTypeExMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
         {
             {"WinExe",          "0" },
             {"Exe",             "1" },
             {"Library",         "2" },
             {"AppContainerExe", "3" },
             {"WinMDObj",        "4" },
-        };
+        }.ToImmutableDictionary();
 
-        private readonly Dictionary<string, string> _setOutputTypeExMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        private static readonly ImmutableDictionary<string, string> s_setOutputTypeExMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
         {
             {"0", "WinExe" },
             {"1", "Exe" },
             {"2", "Library" },
             {"3", "AppContainerExe" },
             {"4", "WinMDObj"},
-        };
+        }.ToImmutableDictionary();
+
+        protected override ImmutableDictionary<string, string> GetMap => s_getOutputTypeExMap;
+
+        protected override ImmutableDictionary<string, string> SetMap => s_setOutputTypeExMap;
 
         [ImportingConstructor]
         public OutputTypeExValueProvider(ProjectProperties properties)
+            : base(properties)
         {
-            _properties = properties;
-        }
-
-        public override async Task<string> OnGetEvaluatedPropertyValueAsync(string evaluatedPropertyValue, IProjectProperties defaultProperties)
-        {
-            var configuration = await _properties.GetConfigurationGeneralPropertiesAsync().ConfigureAwait(false);
-            var value = await configuration.OutputType.GetEvaluatedValueAtEndAsync().ConfigureAwait(false);
-            return _getOutputTypeExMap[value];
-        }
-
-        public override async Task<string> OnSetPropertyValueAsync(string unevaluatedPropertyValue, IProjectProperties defaultProperties, IReadOnlyDictionary<string, string> dimensionalConditions = null)
-        {
-            var value = _setOutputTypeExMap[unevaluatedPropertyValue];
-            var configuration = await _properties.GetConfigurationGeneralPropertiesAsync().ConfigureAwait(false);
-            await configuration.OutputType.SetValueAsync(value).ConfigureAwait(false);
-
-            // We need to return null so we dont persist a value for the MSBuild property 'OutputTypeEx'
-            return null;
         }
     }
 }
--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Properties/InterceptedProjectProperties/OutputTypeValueProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Properties/InterceptedProjectProperties/OutputTypeValueProvider.cs
@@ -2,55 +2,42 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.ComponentModel.Composition;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.ProjectSystem.Properties;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Properties
 {
     /// OutputType acts as a converter for the MSBuild OutputType value expressed as <see cref="VSLangProj.prjOutputType"/>.
     [ExportInterceptingPropertyValueProvider("OutputType", ExportInterceptingPropertyValueProviderFile.ProjectFile)]
-    internal sealed class OutputTypeValueProvider : InterceptingPropertyValueProviderBase
+    internal sealed class OutputTypeValueProvider : OutputTypeValueProviderBase
     {
         private readonly ProjectProperties _properties;
 
-        private readonly Dictionary<string, string> _getOutputTypeMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        private static readonly ImmutableDictionary<string, string> s_getOutputTypeMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
         {
             {"WinExe",          "0" },
             {"Exe",             "1" },
             {"Library",         "2" },
             {"AppContainerExe", "1" },
             {"WinMDObj",        "2" },
-        };
+        }.ToImmutableDictionary();
 
-        private readonly Dictionary<string, string> _setOutputTypeMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        private static readonly ImmutableDictionary<string, string> s_setOutputTypeMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
         {
             {"0", "WinExe" },
             {"1", "Exe" },
             {"2", "Library" },
-        };
+        }.ToImmutableDictionary();
+
+        protected override ImmutableDictionary<string, string> GetMap => s_getOutputTypeMap;
+
+        protected override ImmutableDictionary<string, string> SetMap => s_setOutputTypeMap;
 
         [ImportingConstructor]
         public OutputTypeValueProvider(ProjectProperties properties)
+            : base(properties)
         {
-            _properties = properties;
-        }
-
-        public override async Task<string> OnGetEvaluatedPropertyValueAsync(string evaluatedPropertyValue, IProjectProperties defaultProperties)
-        {
-            var configuration = await _properties.GetConfigurationGeneralPropertiesAsync().ConfigureAwait(false);
-            var value = await configuration.OutputType.GetEvaluatedValueAtEndAsync().ConfigureAwait(false);
-            return _getOutputTypeMap[value];
-        }
-
-        public override async Task<string> OnSetPropertyValueAsync(string unevaluatedPropertyValue, IProjectProperties defaultProperties, IReadOnlyDictionary<string, string> dimensionalConditions = null)
-        {
-            var value = _setOutputTypeMap[unevaluatedPropertyValue];
-            var configuration = await _properties.GetConfigurationGeneralPropertiesAsync().ConfigureAwait(false);
-            await configuration.OutputType.SetValueAsync(value).ConfigureAwait(false);
-
-            // The value is set so there is no need to set the value again.
-            return null;
         }
     }
 }
