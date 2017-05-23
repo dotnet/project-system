4d0bddec0f4901bace6e6880c682f1ec8a233c0f
David Kean <davkean@microsoft.com>
2017-05-23 16:59:16 +1000

    Add skeleton test class for ProjectOutputWindowProjectLogger

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/IProjectOutputWindowPaneProviderFactory.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Moq;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Logging
+{
+    internal static class IProjectOutputWindowPaneProviderFactory
+    {
+        public static IProjectOutputWindowPaneProvider Create()
+        {
+            return Mock.Of<IProjectOutputWindowPaneProvider>();
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Logging/ProjectOutputWindowProjectLoggerTests.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Logging
+{
+    [ProjectSystemTrait]
+    public class ProjectOutputWindowProjectLoggerTests
+    {
+        private static ProjectOutputWindowProjectLogger CreateInstance(IProjectThreadingService threadingService = null, IProjectSystemOptions options = null, IProjectOutputWindowPaneProvider outputWindowProvider = null)
+        {
+            threadingService = threadingService ?? IProjectThreadingServiceFactory.Create();
+            options = options ?? IProjectSystemOptionsFactory.Create();
+            outputWindowProvider = outputWindowProvider ?? IProjectOutputWindowPaneProviderFactory.Create();
+
+            return new ProjectOutputWindowProjectLogger(threadingService, options, outputWindowProvider);
+        }
+    }
+}
