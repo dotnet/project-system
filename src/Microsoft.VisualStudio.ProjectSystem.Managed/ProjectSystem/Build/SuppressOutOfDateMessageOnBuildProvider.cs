d6112081983cb10aca1b4b60db844c7d9e186063
Paul Vick <paulvick@hotmail.com>
2017-06-02 14:40:20 -0700

    Suppress dialog on out of date build

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Build/SuppressOutOfDateMessageOnBuildProvider.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.ComponentModel.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Build
+{
+    /// <summary>
+    ///     Turns on SuppressOutOfDateMessageOnBuild to prevent dialog popping up during builds, see: https://github.com/dotnet/project-system/issues/2358.
+    /// </summary>
+    [Export(typeof(IProjectGlobalPropertiesProvider))]
+    [AppliesTo(ProjectCapability.CSharpOrVisualBasicOrFSharpLanguageService)]
+    internal class SuppressOutOfDateMessageOnBuildProvider : StaticGlobalPropertiesProviderBase
+    {
+        private static readonly Task<IImmutableDictionary<string, string>> BuildProperties = Task.FromResult<IImmutableDictionary<string, string>>(
+            Empty.PropertiesMap.Add(BuildProperty.SuppressOutOfDateMessageOnBuild, "true"));
+
+        [ImportingConstructor]
+        public SuppressOutOfDateMessageOnBuildProvider(IProjectService projectService)
+            : base(projectService.Services)
+        {
+        }
+
+        public override Task<IImmutableDictionary<string, string>> GetGlobalPropertiesAsync(CancellationToken cancellationToken)
+        {
+            return BuildProperties;
+        }
+    }
+}
