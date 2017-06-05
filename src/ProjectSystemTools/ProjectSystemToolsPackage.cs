da97e1ec3030166bce1eaf73d2e615f5b152bec4
Paul Vick <paulvick@hotmail.com>
2017-06-05 06:04:16 -0700

    Start package

---
+++ src/ProjectSystemTools/ProjectSystemToolsPackage.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Tools
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration(productName: "#110", productDetails: "#112", productId: "1.0", IconResourceID = 400)]
+    [Guid(PackageGuidString)]
+    public sealed class ProjectSystemToolsPackage : Package
+    {
+        public const string PackageGuidString = "e3bfb509-b8fd-4692-b4c4-4b2f6ed62bc7";
+    }
+}
