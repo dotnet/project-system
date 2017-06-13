ed7a50910dc1e565983d8cfc372c0951ab2f39d7
Jonathon Marolf <jmarolf@microsoft.com>
2017-06-12 17:27:42 -0700

    remove OnceInitializedOnceDisposed and instead import services via MEF

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/CrossTarget/TargetFrameworkProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/CrossTarget/TargetFrameworkProvider.cs
@@ -3,71 +3,32 @@
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
-using Microsoft.VisualStudio.ComponentModelHost;
-using Microsoft.VisualStudio.Shell;
 using NuGet.VisualStudio;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.CrossTarget
 {
     [Export(typeof(ITargetFrameworkProvider))]
     [AppliesTo(ProjectCapability.DependenciesTree)]
-    internal class TargetFrameworkProvider : OnceInitializedOnceDisposed, ITargetFrameworkProvider
+    internal sealed class TargetFrameworkProvider : ITargetFrameworkProvider
     {
         [ImportingConstructor]
-        public TargetFrameworkProvider(SVsServiceProvider serviceProvider)
+        public TargetFrameworkProvider(
+            IVsFrameworkCompatibility nugetComparer,
+            IVsFrameworkParser nugetFrameworkParser)
         {
-            ServiceProvider = serviceProvider;
+            NugetComparer = nugetComparer;
+            NugetFrameworkParser = nugetFrameworkParser;
         }
-        
-        private SVsServiceProvider ServiceProvider { get; }
-        protected IVsFrameworkCompatibility NugetComparer { get; set; }
-        protected IVsFrameworkParser NugetFrameworkParser { get; set; }
+
+        private IVsFrameworkCompatibility NugetComparer { get; }
+
+        private IVsFrameworkParser NugetFrameworkParser { get; }
 
         private readonly object _targetsLock = new object();
         private List<ITargetFramework> CachedTargetFrameworks = new List<ITargetFramework>();
 
-        protected override void Initialize()
-        {
-            if (!ShouldInitialize())
-            {
-                return;
-            }
-
-            ThreadHelper.JoinableTaskFactory.Run(async delegate
-            {
-                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
-                var container = ServiceProvider.GetService<IComponentModel, SComponentModel>();
-                if (container != null)
-                {
-                    NugetComparer = container.GetExtensions<IVsFrameworkCompatibility>().FirstOrDefault();
-                    NugetFrameworkParser = container.GetExtensions<IVsFrameworkParser>().FirstOrDefault();
-                }
-            });
-        }
-
-        /// <summary>
-        /// For unit tests to avoid UI initialization.
-        /// </summary>
-        /// <returns></returns>
-        protected virtual bool ShouldInitialize()
-        {
-            return NugetComparer == null || NugetFrameworkParser == null;
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                NugetComparer = null;
-                NugetFrameworkParser = null;
-            }
-        }
-
         public ITargetFramework GetTargetFramework(string shortOrFullName)
         {
-            EnsureInitialized();
-
             if (string.IsNullOrEmpty(shortOrFullName))
             {
                 return null;
@@ -90,7 +51,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.CrossTarget
                     {
                         var shortName = NugetFrameworkParser.GetShortFrameworkName(frameworkName);
                         targetFramework = new TargetFramework(frameworkName, shortName);
-                        // remember target framework - there can not bee too many of them accross the solution.
+                        // remember target framework - there can not bee too many of them across the solution.
                         CachedTargetFrameworks.Add(targetFramework);
                     }
                 }
@@ -104,11 +65,9 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.CrossTarget
             return targetFramework;
         }
 
-        public ITargetFramework GetNearestFramework(ITargetFramework targetFramework, 
+        public ITargetFramework GetNearestFramework(ITargetFramework targetFramework,
                                                     IEnumerable<ITargetFramework> otherFrameworks)
         {
-            EnsureInitialized();
-
             if (targetFramework == null || otherFrameworks == null || !otherFrameworks.Any())
             {
                 return null;
