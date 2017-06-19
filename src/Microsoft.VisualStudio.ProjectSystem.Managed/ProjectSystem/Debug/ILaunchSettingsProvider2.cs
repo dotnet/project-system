430cb45f82313d3427f7b709d83391d639679f7a
David Kean <davkean@microsoft.com>
2017-06-19 16:57:37 +1000

    Update doc comments

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/ILaunchSettingsProvider2.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/ILaunchSettingsProvider2.cs
@@ -4,11 +4,17 @@ using System.Threading.Tasks;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Debug
 {
-  
+    /// <summary>
+    ///     Provides an implementation of <see cref="ILaunchSettingsProvider"/> with an
+    ///     additional method <see cref="GetLaunchSettingsFilePathAsync"/> for retrieving
+    ///     the launch settings file.
+    /// </summary>
     public interface ILaunchSettingsProvider2 : ILaunchSettingsProvider
     {
         /// <summary>
-        ///     Gets the path to the lauch settings file, typically, "launchSettings.json".
+        ///     Gets the full path to the launch settings file, typically located under
+        ///     "Properties\launchSettings.json" or "My Project\launchSettings.json" of
+        ///     the project directory.
         /// </summary>
         Task<string> GetLaunchSettingsFilePathAsync();
     }
