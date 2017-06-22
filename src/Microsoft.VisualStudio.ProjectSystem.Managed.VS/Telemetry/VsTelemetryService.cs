14c3c242008c42811c62c658dffff9ec3010caa5
Balaji Soundrarajan <basoundr@microsoft.com>
2017-06-22 14:33:30 -0700

    Move ITelemetryService to the Managed layer from the VS layer. This enables the services in the non-VS layer to report telemetry as well.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/Telemetry/VsTelemetryService.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/Telemetry/VsTelemetryService.cs
@@ -1,43 +1,92 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Microsoft.VisualStudio.Telemetry
 {
     [Export(typeof(ITelemetryService))]
     internal class VsTelemetryService : ITelemetryService
     {
-        public void PostEvent(string eventName)
+        private const string EventPrefix = "vs/projectsystem/managed/";
+        private const string PropertyPrefix = "VS.ProjectSystem.Managed.";
+
+        /// <summary>
+        /// Post an event with the event name also with the corresponding Property name and Property value. This 
+        /// event will be correlated with the Project Telemetry Correlation asset.
+        /// </summary>
+        /// <param name="eventName">Name of the event.</param>
+        /// <param name="propertyName">Property name to be reported.</param>
+        /// <param name="propertyValue">Property value to be reported.</param>
+        public void PostProperty(string eventName, string propertyName, string propertyValue)
         {
             Requires.NotNullOrEmpty(eventName, nameof(eventName));
+            Requires.NotNullOrEmpty(propertyName, nameof(propertyName));
+            Requires.NotNullOrEmpty(propertyValue, nameof(propertyValue));
 
-            TelemetryService.DefaultSession.PostEvent(eventName);
+            TelemetryEvent telemetryEvent = new TelemetryEvent(EventPrefix + eventName.ToLower());
+            telemetryEvent.Properties.Add(BuildPropertyName(eventName, propertyName), propertyValue);
+            TelemetryService.DefaultSession.PostEvent(telemetryEvent);
+        }
+        /// <summary>
+        /// Post an event with the event name also with the corresponding Property names and Property values. This 
+        /// event will be correlated with the Project Telemetry Correlation asset.
+        /// </summary>
+        /// <param name="eventName">Name of the event.</param>
+        /// <param name="properties">List of Property name and corresponding values. PropertyName and PropertyValue cannot be null or empty.</param>
+        public void PostProperties(string eventName, IEnumerable<(string propertyName, string propertyValue)> properties)
+        {
+            Requires.NotNullOrEmpty(eventName, nameof(eventName));
+            Requires.NotNullOrEmpty(properties, nameof(properties));
+
+            TelemetryEvent telemetryEvent = new TelemetryEvent(EventPrefix + eventName.ToLower());
+            foreach (var property in properties)
+            {
+                telemetryEvent.Properties.Add(BuildPropertyName(eventName, property.propertyName), property.propertyValue);
+            }
+
+            TelemetryService.DefaultSession.PostEvent(telemetryEvent);
         }
 
-        public TelemetryEventCorrelation PostOperation(string eventName, TelemetryResult result, string resultSummary = null, TelemetryEventCorrelation[] correlatedWith = null)
+        /// <summary>
+        /// Hashes personally identifiable information for telemetry consumption.
+        /// </summary>
+        /// <param name="value">Value to hashed.</param>
+        /// <returns>Hashed value.</returns>
+        public string HashValue(string value)
         {
-            Requires.NotNullOrEmpty(eventName, nameof(eventName));
-            if (result == TelemetryResult.None)
-                throw new ArgumentException(null, nameof(result));
-
-            return TelemetryService.DefaultSession.PostOperation(
-                eventName: eventName,
-                result: result,
-                resultSummary: resultSummary,
-                correlatedWith: correlatedWith);
+            var inputBytes = Encoding.UTF8.GetBytes(value);
+            var hashedBytes = new SHA256CryptoServiceProvider().ComputeHash(inputBytes);
+            return BitConverter.ToString(hashedBytes);
         }
 
-        public TelemetryEventCorrelation Report(string eventName, string description, Exception exception, Func<IFaultUtility, int> callback = null)
-        {
-            Requires.NotNullOrEmpty(eventName, nameof(eventName));
-            Requires.NotNull(exception, nameof(exception));
 
-            return TelemetryService.DefaultSession.PostFault(
-                eventName: eventName,
-                description: description,
-                exceptionObject: exception,
-                gatherEventDetails: callback);
+        /// <summary>
+        /// Build a fully qualified property name based on it's parent event and the property name
+        /// </summary>
+        /// <param name="eventName">Name of the parent event.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <returns>Fully qualified property name.</returns>
+        /// <remarks>
+        /// Properties are expected to be in the following format - EventName.PropertyName
+        /// with the the slashes from the vent name replaced by periods.
+        /// e.g. vs/myevent would translate to VS.MyEvent.MyProperty
+        /// </remarks>
+        private string BuildPropertyName(string eventName, string propertyName)
+        {
+            string name = PropertyPrefix + eventName.Replace('/', '.');
+
+            if (!name.EndsWith("."))
+            {
+                name += ".";
+            }
+
+            name += propertyName;
+
+            return name;
         }
     }
 }
