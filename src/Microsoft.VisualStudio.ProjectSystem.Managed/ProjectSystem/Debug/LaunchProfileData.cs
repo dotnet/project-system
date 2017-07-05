12d536a753bab5259c7addf310d6c60b802e0a03
David Kean <davkean@microsoft.com>
2017-07-05 15:26:23 +1000

    Make KnownProfileProperties readonly

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/LaunchProfileData.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/LaunchProfileData.cs
@@ -23,7 +23,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
         const string Prop_launchUrl = "launchUrl";
         const string Prop_environmentVariables = "environmentVariables";
 
-        static HashSet<string> KnownProfileProperties = new HashSet<string>(StringComparer.Ordinal)
+        static readonly HashSet<string> KnownProfileProperties = new HashSet<string>(StringComparer.Ordinal)
         {
             {Prop_commandName},
             {Prop_executablePath},
