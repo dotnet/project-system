0145257604c855a33e3bcf0d8105955d529401d2
Paul Vick <paulvick@hotmail.com>
2017-07-05 23:03:54 -0700

    Cleanup

---
+++ src/ProjectSystemTools/BuildLogging/UI/Commands.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Windows.Input;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Tools.BuildLogging.UI
+{
+    internal static class Commands
+    {
+        public static RoutedUICommand Open { get; }
+
+        static Commands()
+        {
+            Open = new RoutedUICommand(Resources.OpenCommand, "Open", typeof(Commands));
+        }
+    }
+}
