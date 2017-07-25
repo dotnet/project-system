bc1a1c492758f700cbbdb0a0d2e37956715801e8
Tomas Matousek <tomat@microsoft.com>
2017-07-25 13:18:30 -0700

    Delete obsolete infrastructure files

--- build/Scripts/copyOutput.cmd
+++
@@ -1,7 +0,0 @@
-@if not defined _echo @echo off
-setlocal enabledelayedexpansion
-
-set BinariesDirectory=%1
-
-echo Copy the Modern Vsixes manifest into VsixV3
-robocopy %BinariesDirectory% %BinariesDirectory%VsixV3 Microsoft.VisualStudio.Editors.vsman Microsoft.VisualStudio.ProjectSystem.Managed.vsman Microsoft.VisualStudio.NetCore.ProjectTemplates.vsman Microsoft.VisualStudio.NetCore.ProjectTemplates.1.x.vsman /njh /njs /np /xx
