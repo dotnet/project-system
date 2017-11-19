827670373acc02799443da27c76f2a10f9604646
Paul Vick <paulv@microsoft.com>
2017-11-19 10:50:49 -0800

    Fixup files

--- src/ProjectSystemTools/BuildLogging/Model/LoggerBase.cs
+++ src/ProjectSystemTools/BuildLogging/Model/LoggerBase.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Build.Framework;
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.Build.Framework;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Tools.BuildLogging.Model
 {
