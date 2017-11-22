b1e4a8acac1de06fdc19428032d1a800ed4877e3
Tomáš Matoušek <tmat@users.noreply.github.com>
2017-11-22 08:27:12 -0800

    Update to RepoToolset v54 (#2950)
    Update to RepoToolset v58

--- build/build.ps1
+++ build/build.ps1
@@ -56,24 +56,20 @@ if ($help -or (($properties -ne $null) -and ($properties.Contains("/help") -or $
   exit 0
 }
 
-$InVSEnvironment = !($env:VS150COMNTOOLS -eq $null) -and (Test-Path $env:VS150COMNTOOLS)
-$RepoRoot = Join-Path $PSScriptRoot "..\"
-$ToolsRoot = Join-Path $RepoRoot ".tools"
-$BuildProj = Join-Path $PSScriptRoot "build.proj"
-$DependenciesProps = Join-Path $PSScriptRoot "Versions.props"
-$ArtifactsDir = Join-Path $RepoRoot "artifacts"
-$LogDir = Join-Path (Join-Path $ArtifactsDir $configuration) "log"
-$TempDir = Join-Path (Join-Path $ArtifactsDir $configuration) "tmp"
-
 function Create-Directory([string[]] $path) {
-  if (!(Test-Path -path $path)) {
+  if (!(Test-Path $path)) {
     New-Item -path $path -force -itemType "Directory" | Out-Null
   }
 }
 
-function GetVSWhereVersion {
-  [xml]$xml = Get-Content $DependenciesProps
-  return $xml.Project.PropertyGroup.VSWhereVersion
+function GetVersion([string] $name) {
+  foreach ($propertyGroup in $VersionsXml.Project.PropertyGroup) {
+    if (Get-Member -inputObject $propertyGroup -name $name) {
+        return $propertyGroup.$name
+    }
+  }
+
+  throw "Failed to find $name in Versions.props"
 }
 
 function LocateVisualStudio {
@@ -81,18 +77,18 @@ function LocateVisualStudio {
     return Join-Path $env:VS150COMNTOOLS "..\.."
   }
 
-  $vswhereVersion = GetVSWhereVersion
+  $vswhereVersion = GetVersion("VSWhereVersion")
   $vsWhereDir = Join-Path $ToolsRoot "vswhere\$vswhereVersion"
   $vsWhereExe = Join-Path $vsWhereDir "vswhere.exe"
   
   if (!(Test-Path $vsWhereExe)) {
-    Create-Directory $vsWhereDir   
+    Create-Directory $vsWhereDir
     Write-Host "Downloading vswhere"
     Invoke-WebRequest "http://github.com/Microsoft/vswhere/releases/download/$vswhereVersion/vswhere.exe" -OutFile $vswhereExe
   }
   
   $vsInstallDir = & $vsWhereExe -latest -property installationPath -requires Microsoft.Component.MSBuild -requires Microsoft.VisualStudio.Component.VSSDK -requires Microsoft.Net.Component.4.6.TargetingPack -requires Microsoft.VisualStudio.Component.Roslyn.Compiler -requires Microsoft.VisualStudio.Component.VSSDK
-  
+
   if (!(Test-Path $vsInstallDir)) {
     throw "Failed to locate Visual Studio (exit code '$lastExitCode')."
   }
@@ -100,16 +96,13 @@ function LocateVisualStudio {
   return $vsInstallDir
 }
 
-function Build {
-  $vsInstallDir = LocateVisualStudio
-  $msbuildExe = Join-Path $vsInstallDir "MSBuild\15.0\Bin\msbuild.exe"
-
-  if (!$InVSEnvironment) {
-    $env:VS150COMNTOOLS = Join-Path $vsInstallDir "Common7\Tools\"
-    $env:VSSDK150Install = Join-Path $vsInstallDir "VSSDK\"
-    $env:VSSDKInstall = Join-Path $vsInstallDir "VSSDK\"
+function InstallToolset {
+  if (!(Test-Path $ToolsetBuildProj)) {
+    & $MsbuildExe $ToolsetRestoreProj /t:restore /m /nologo /clp:None /warnaserror /v:quiet /p:DeployDeps=$deployDeps /p:NuGetPackageRoot=$NuGetPackageRoot /p:BaseIntermediateOutputPath=$ToolsetDir /p:ExcludeRestorePackageImports=true
   }
+}
 
+function Build {
   if ($ci -or $log) {
     Create-Directory($logDir)
     $logCmd = "/bl:" + (Join-Path $LogDir "Build.binlog")
@@ -117,13 +110,9 @@ function Build {
     $logCmd = ""
   }
 
-  if ($ci) {
-    Write-Host "Using $msbuildExe"
-  }
-
   $nodeReuse = !$ci
 
-  & $msbuildExe $BuildProj /m /nologo /clp:Summary /nodeReuse:$nodeReuse /warnaserror /v:$verbosity $logCmd /p:Configuration=$configuration /p:SolutionPath=$solution /p:Restore=$restore /p:DeployDeps=$deployDeps /p:Build=$build /p:Rebuild=$rebuild /p:Deploy=$deploy /p:Test=$test /p:IntegrationTest=$integrationTest /p:Sign=$sign /p:Pack=$pack /p:CIBuild=$ci $properties
+  & $MsbuildExe $ToolsetBuildProj /m /nologo /clp:Summary /nodeReuse:$nodeReuse /warnaserror /v:$verbosity $logCmd /p:Configuration=$configuration /p:SolutionPath=$solution /p:Restore=$restore /p:DeployDeps=$deployDeps /p:Build=$build /p:Rebuild=$rebuild /p:Deploy=$deploy /p:Test=$test /p:IntegrationTest=$integrationTest /p:Sign=$sign /p:Pack=$pack /p:CIBuild=$ci /p:NuGetPackageRoot=$NuGetPackageRoot $properties
 }
 
 function Stop-Processes() {
@@ -141,15 +130,53 @@ function Clear-NuGetCache() {
 }
 
 try {
+  $InVSEnvironment = !($env:VS150COMNTOOLS -eq $null) -and (Test-Path $env:VS150COMNTOOLS)
+  $RepoRoot = Join-Path $PSScriptRoot "..\"
+  $ToolsRoot = Join-Path $RepoRoot ".tools"
+  $ToolsetRestoreProj = Join-Path $PSScriptRoot "Toolset.proj"
+  $ArtifactsDir = Join-Path $RepoRoot "artifacts"
+  $ToolsetDir = Join-Path $ArtifactsDir "toolset"
+  $LogDir = Join-Path (Join-Path $ArtifactsDir $configuration) "log"
+  $TempDir = Join-Path (Join-Path $ArtifactsDir $configuration) "tmp"
+  [xml]$VersionsXml = Get-Content(Join-Path $PSScriptRoot "Versions.props")
+
+  if ($solution -eq "") {
+    $solution = @(gci(Join-Path $RepoRoot "*.sln"))[0]
+  }
+
+  if ($env:NUGET_PACKAGES -ne $null) {
+    $NuGetPackageRoot = $env:NUGET_PACKAGES.TrimEnd("\") + "\"
+  } else {
+    $NuGetPackageRoot = Join-Path $env:UserProfile ".nuget\packages\"
+  }
+
+  $ToolsetVersion = GetVersion("RoslynToolsMicrosoftRepoToolsetVersion")
+  $ToolsetBuildProj = Join-Path $NuGetPackageRoot "RoslynTools.Microsoft.RepoToolset\$ToolsetVersion\tools\Build.proj"
+
+  $vsInstallDir = LocateVisualStudio
+  $MsbuildExe = Join-Path $vsInstallDir "MSBuild\15.0\Bin\msbuild.exe"
+
   if ($ci) {
     Create-Directory $TempDir
     $env:TEMP = $TempDir
     $env:TMP = $TempDir
+
+    Write-Host "Using $MsbuildExe"
+  }
+
+  if (!$InVSEnvironment) {
+    $env:VS150COMNTOOLS = Join-Path $vsInstallDir "Common7\Tools\"
+    $env:VSSDK150Install = Join-Path $vsInstallDir "VSSDK\"
+    $env:VSSDKInstall = Join-Path $vsInstallDir "VSSDK\"
   }
 
   # Preparation of a CI machine
   if ($prepareMachine) {
     Clear-NuGetCache
+  }
+
+  if ($restore) {
+    InstallToolset
   }
 
   Build
