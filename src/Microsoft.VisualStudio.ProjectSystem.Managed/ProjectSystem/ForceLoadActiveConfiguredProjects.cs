6a5c9f765e7375bb2bc4be14b78ce29cb728d333
David Kean <davkean@microsoft.com>
2017-12-08 17:07:48 +1100

    Force load all "active configs"
    CPS force loads the active configuration during project initialization and when the active configuration changes.  This ensures that any ConfiguredProjectAutoLoad services are created and run.
    
    This is the equivalent, except for all configurations that we consider "active".  In the following multi-targeted project:
    
     -> All known project configurations:
    
          Configuration Platform    TargetFramework
          -------------------------------------------
                  Debug |   AnyCPU  |           net45
                  Debug |   AnyCPU  |           net46
                Release |   AnyCPU  |           net45
                Release |   AnyCPU  |           net46
    
      -> Active solution configuration:
    
                  Debug |   AnyCPU  |           net45
    
    These configurations are explicitly loaded:
    
                  Debug |   AnyCPU  |           net45
                  Debug |   AnyCPU  |           net46
    
    Previously, these configurations were implicitly loaded when an auto-load component (typically language service) happend to call through IActiveConfiguredProjectsProvider. This makes loading of configurations deliberate and explicit, and always occur regardless of auto-load component that is run.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/ForceLoadActiveConfiguredProjects.cs
@@ -0,0 +1,60 @@
+﻿// Copyright(c) Microsoft.All Rights Reserved.Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    /// <summary>
+    ///     Force loads the active <see cref="ConfiguredProject"/> objects so that any configured project-level 
+    ///     services, such as evaluation and build services, are started.
+    /// </summary>
+    internal class ForceLoadActiveConfiguredProjects : OnceInitializedOnceDisposed
+    {
+        private readonly IActiveConfigurationGroupService _activeConfigurationGroupService;
+        private readonly UnconfiguredProject _project;
+        private IDisposable _subscription;
+
+        [ImportingConstructor]
+        public ForceLoadActiveConfiguredProjects(UnconfiguredProject project, IActiveConfigurationGroupService activeConfigurationGroupService)
+        {
+            _project = project;
+            _activeConfigurationGroupService = activeConfigurationGroupService;
+        }
+
+        [ProjectAutoLoad(ProjectLoadCheckpoint.ProjectInitialCapabilitiesEstablished)]
+        [AppliesTo(ProjectCapability.CSharpOrVisualBasicOrFSharpLanguageService)]
+        private Task OnProjectFactoryCompletedAsync()
+        {
+            Initialize();
+            return Task.CompletedTask;
+        }
+
+        protected override void Initialize()
+        {
+            Action<IProjectVersionedValue<IConfigurationGroup<ProjectConfiguration>>> target = OnActiveConfigurationsChanged;
+
+            _subscription = _activeConfigurationGroupService.ActiveConfigurationGroupSource.SourceBlock.LinkTo(
+                target: new ActionBlock<IProjectVersionedValue<IConfigurationGroup<ProjectConfiguration>>>(target),
+                linkOptions: new DataflowLinkOptions() { PropagateCompletion = true });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _subscription?.Dispose();
+            }
+        }
+
+        private void OnActiveConfigurationsChanged(IProjectVersionedValue<IConfigurationGroup<ProjectConfiguration>> e)
+        {
+            foreach (ProjectConfiguration configuration in e.Value)
+            {
+                _project.LoadConfiguredProjectAsync(configuration);
+            }
+        }
+    }
+}
