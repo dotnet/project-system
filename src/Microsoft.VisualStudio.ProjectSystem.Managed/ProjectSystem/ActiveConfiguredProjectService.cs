02d25b712105abb826a5e40455583b0c54dc4ffa
David Kean <davkean@microsoft.com>
2017-12-18 14:43:49 +1100

    Add IActiveConfiguredProjectService
    Let's configured-project services await on becoming active.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/ActiveConfiguredProjectService.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+using Microsoft.VisualStudio.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    [Export(typeof(IActiveConfiguredProjectService))]
+    internal class ActiveConfiguredProjectService : OnceInitializedOnceDisposed, IActiveConfiguredProjectService
+    {
+        private readonly ConfiguredProject _project;
+        private readonly IProjectAsynchronousTasksService _tasksService;
+        private readonly IActiveConfigurationGroupService _activeConfigurationGroupService;
+        private readonly ActionBlock<IProjectVersionedValue<IConfigurationGroup<ProjectConfiguration>>> _targetBlock;
+        private TaskCompletionSource<object> _isActiveCompletionSource = new TaskCompletionSource<object>();
+        private IDisposable _subscription;
+
+        public ActiveConfiguredProjectService(ConfiguredProject project, [Import(ExportContractNames.Scopes.ConfiguredProject)]IProjectAsynchronousTasksService tasksService, IActiveConfigurationGroupService activeConfigurationGroupService)
+        {
+            _project = project;
+            _tasksService = tasksService;
+            _activeConfigurationGroupService = activeConfigurationGroupService;
+            _targetBlock = new ActionBlock<IProjectVersionedValue<IConfigurationGroup<ProjectConfiguration>>>((Action<IProjectVersionedValue<IConfigurationGroup<ProjectConfiguration>>>)OnActiveConfigurationsChanged);
+        }
+
+        public ITargetBlock<IProjectVersionedValue<IConfigurationGroup<ProjectConfiguration>>> TargetBlock => _targetBlock;
+
+        public bool IsActive
+        {
+            get
+            {
+                EnsureInitialized();
+
+                return _isActiveCompletionSource.Task.IsCompleted;
+            }
+        }
+
+        public Task IsActiveTask
+        {
+            get
+            {
+                EnsureInitialized();
+
+                return _isActiveCompletionSource.Task;
+            }
+        }
+
+        protected override void Initialize()
+        {
+            _subscription = _activeConfigurationGroupService.ActiveConfigurationGroupSource.SourceBlock.LinkTo(
+                target: _targetBlock,
+                linkOptions: new DataflowLinkOptions() { PropagateCompletion = true });
+
+            _tasksService.UnloadCancellationToken.Register(RegisterOptions.ExecuteImmediatelyIfAlreadyCanceled, () =>
+            {
+                // Unloading, notify anyone listening that we're never going to be active
+                _isActiveCompletionSource.TrySetCanceled();
+            });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _subscription?.Dispose();
+            _targetBlock.Complete();
+
+            // Notify anyone listening that we're never going to be active
+            _isActiveCompletionSource.TrySetCanceled();
+        }
+
+        private void OnActiveConfigurationsChanged(IProjectVersionedValue<IConfigurationGroup<ProjectConfiguration>> e)
+        {
+            if (IsDisposing || IsDisposed)
+                return;
+
+            bool currentlyActive = e.Value.Contains(_project.ProjectConfiguration);
+            bool previouslyActive = _isActiveCompletionSource.Task.IsCompleted;
+
+            // Are there any changes for my configuration?
+            if (currentlyActive == previouslyActive)
+                return;
+
+            if (currentlyActive)
+            {
+                // Our configuration has become active
+                _isActiveCompletionSource.TrySetResult(null);
+            }
+            else
+            {
+                // Our configuration has become in-active
+                _isActiveCompletionSource = new TaskCompletionSource<object>();
+                Thread.MemoryBarrier();
+            }
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/IActiveConfiguredProjectService.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Composition;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    [ProjectSystemContract(ProjectSystemContractScope.ConfiguredProject, ProjectSystemContractProvider.Private, Cardinality = ImportCardinality.ExactlyOne)]
+    internal interface IActiveConfiguredProjectService
+    {
+        /// <summary>
+        ///     Gets a value indicating whether the configuration group  containing the 
+        ///     current <see cref="ConfiguredProject"/> is active.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">
+        ///     The <see cref="IActiveConfigurationGroupService"/> has been disposed of.
+        /// </exception>
+        bool IsActive
+        {
+            get;
+        }
+
+        /// <summary>
+        ///     Gets a task that is completed when the configuration group containing the
+        ///     current <see cref="ConfiguredProject"/> becomes active.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">
+        ///     The <see cref="IActiveConfigurationGroupService"/> has been disposed of.
+        /// </exception>
+        /// <remarks>
+        ///     The returned <see cref="Task"/> is cancelled when the 
+        ///     <see cref="ConfiguredProject"/> is unloaded.
+        /// </remarks>
+        Task IsActiveTask
+        {
+            get;
+        }
+    }
+}
