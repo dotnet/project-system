62e1eae48133bf03010111aad16d843e8e2ee4e2
David Kean <davkean@microsoft.com>
2017-12-18 13:32:20 +1100

    Add extension for CancellationToken.Register
    Handles the case when the CancellationToken has already been been disposed.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/Threading/Tasks/RegisterOptions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.VisualStudio.Threading.Tasks
+{
+    /// <summary>
+    ///     Provides options for <see cref="CancellationTokenExtensions.Register(CancellationToken, Action, RegisterOptions)"/>.
+    /// </summary>
+    internal enum RegisterOptions
+    {
+        /// <summary>
+        ///     Throw <see cref="ObjectDisposedException"/> if the <see cref="CancellationToken"/> is already disposed.
+        /// </summary>
+        None,
+
+        /// <summary>
+        ///     Execute the specified callback immediately if the <see cref="CancellationToken"/> is already disposed.
+        /// </summary>
+        ExecuteImmediatelyIfAlreadyCanceled,
+    }
+}
