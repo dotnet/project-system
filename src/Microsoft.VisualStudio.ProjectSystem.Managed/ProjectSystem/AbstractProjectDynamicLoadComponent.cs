799fdabd1e5ff1619c60c2d5c497aa180f268a2c
David Kean <davkean@microsoft.com>
2018-01-03 15:37:34 +1100

    Add base class for simplifying IProjectDynamicLoadComponent
    Base class simplifies the implementation of IProjectDynamicLoadComponent so that it only needs to handle Init/Dispose logic/single thread instead of Load/Unload.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/AbstractProjectDynamicLoadComponent.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    /// <summary>
+    ///     An <see langword="abstract"/> base class that simplifies the lifetime of a
+    ///     <see cref="IProjectDynamicLoadComponent"/> implementation.
+    /// </summary>
+    internal abstract partial class AbstractProjectDynamicLoadComponent : IProjectDynamicLoadComponent
+    {
+        private readonly object _lock = new object();
+        private AbstractProjectDynamicLoadInstance _instance;
+
+        protected AbstractProjectDynamicLoadComponent()
+        {
+        }
+
+        public Task LoadAsync()
+        {
+            AbstractProjectDynamicLoadInstance instance;
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = CreateInstance();
+                }
+
+                instance = _instance;
+            }
+
+            return instance.InitializeAsync();
+        }
+
+        
+        public Task UnloadAsync()
+        {
+            AbstractProjectDynamicLoadInstance instance = null;
+
+            lock (_lock)
+            {
+                if (_instance != null)
+                {
+                    instance = _instance;
+                    _instance = null;
+                }
+            }
+
+            if (instance != null)
+            {
+                return instance.DisposeAsync();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        ///     Creates a new instance of the underlying <see cref="AbstractProjectDynamicLoadInstance"/>.
+        /// </summary>
+        protected abstract AbstractProjectDynamicLoadInstance CreateInstance();
+    }
+}
