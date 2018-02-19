4f107eb7f16ea2f18c6a2aa81d70ae022790e274
David Kean <davkean@microsoft.com>
2018-02-19 12:47:49 +1100

    Use var instead of explicit type

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/PropertyPages/PropertyPageTests.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/PropertyPages/PropertyPageTests.cs
@@ -19,7 +19,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.DotNet.Test.PropertyPages
 
             var page = new Mock<PropertyPage>(false);
             page.Protected().Setup<string>("PropertyPageName").Returns("MyPage");
-            PROPPAGEINFO[] pageInfoArray = new PROPPAGEINFO[1];
+            var pageInfoArray = new PROPPAGEINFO[1];
             page.Object.GetPageInfo(pageInfoArray);
             page.Object.Help(string.Empty);
 
@@ -37,7 +37,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.DotNet.Test.PropertyPages
         {
             Castle.DynamicProxy.Generators.AttributesToAvoidReplicating.Add(typeof(System.Security.Permissions.UIPermissionAttribute));
 
-            RECT[] rect = new RECT[] { new RECT() { left = 25, top = 25 } };
+            var rect = new RECT[] { new RECT() { left = 25, top = 25 } };
             var page = new Mock<PropertyPage>(false);
             page.CallBase = true;
             page.Object.Move(rect);
