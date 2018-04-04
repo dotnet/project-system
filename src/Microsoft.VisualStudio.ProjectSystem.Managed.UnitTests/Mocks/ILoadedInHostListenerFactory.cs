48e8da2fcc83d3c4417a8a3ce0f745b61f4db9b5
David Kean <davkean@microsoft.com>
2018-04-04 17:51:43 +1000

    Add ProjectLoadedInHost and PrioritizedProjectLoadedInHost
    Add two properties intended as a replacement for IProjectAsyncLoadDashboard usage. This has an added advantage of:
    
    - Being able to hook onto both OnAfterOpenProject *and* PrioritizedOnAfterOpenProject solution events, instead of just OnAfterOpenProject.
    
    - Avoid needing to combine IProjectAsyncLoadDashboard.ProjectLoadedInHost with IProjectAsynchronousTasksService.UnloadCancellationToken to avoid hanging when project fails to open.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Mocks/ILoadedInHostListenerFactory.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+
+using Moq;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    internal static class ILoadedInHostListenerFactory
+    {
+        public static ILoadedInHostListener Create()
+        {
+            return Mock.Of<ILoadedInHostListener>();
+        }
+
+        public static ILoadedInHostListener ImplementStartListeningAsync(Action action)
+        {
+            var mock = new Mock<ILoadedInHostListener>();
+            mock.Setup(t => t.StartListeningAsync())
+                .ReturnsAsync(action);
+
+            return mock.Object;
+        }
+    }
+}
--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/UnconfiguredProjectTasksService.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/UnconfiguredProjectTasksService.cs
@@ -3,20 +3,42 @@
 using System;
 using System.ComponentModel.Composition;
 using System.Threading.Tasks;
-
 using Microsoft.VisualStudio.Threading;
 
 namespace Microsoft.VisualStudio.ProjectSystem
 {
+    [Export]
     [Export(typeof(IUnconfiguredProjectTasksService))]
+    [AppliesTo(ProjectCapability.CSharpOrVisualBasicOrFSharp)]
     internal class UnconfiguredProjectTasksService : IUnconfiguredProjectTasksService
     {
         private readonly IProjectAsynchronousTasksService _tasksService;
+        private readonly ILoadedInHostListener _loadedInHostListener;
+        private readonly TaskCompletionSource<object> _projectLoadedInHost = new TaskCompletionSource<object>();
+        private readonly TaskCompletionSource<object> _prioritizedProjectLoadedInHost = new TaskCompletionSource<object>();
 
         [ImportingConstructor]
-        public UnconfiguredProjectTasksService([Import(ExportContractNames.Scopes.UnconfiguredProject)]IProjectAsynchronousTasksService tasksService)
+        public UnconfiguredProjectTasksService([Import(ExportContractNames.Scopes.UnconfiguredProject)]IProjectAsynchronousTasksService tasksService, ILoadedInHostListener loadedInHostListener)
         {
             _tasksService = tasksService;
+            _loadedInHostListener = loadedInHostListener;
+        }
+
+        [ProjectAutoLoad(completeBy:ProjectLoadCheckpoint.ProjectFactoryCompleted)]
+        [AppliesTo(ProjectCapability.CSharpOrVisualBasicOrFSharp)]
+        public Task OnProjectFactoryCompleted()
+        {
+            return _loadedInHostListener.StartListeningAsync();
+        }
+
+        public Task ProjectLoadedInHost
+        {
+            get { return _projectLoadedInHost.Task.WithCancellation(_tasksService.UnloadCancellationToken); }
+        }
+
+        public Task PrioritizedProjectLoadedInHost
+        {
+            get { return _prioritizedProjectLoadedInHost.Task.WithCancellation(_tasksService.UnloadCancellationToken); }
         }
 
         public Task LoadedProjectAsync(Func<Task> action)
@@ -31,6 +53,16 @@ namespace Microsoft.VisualStudio.ProjectSystem
             JoinableTask<T> joinable = _tasksService.LoadedProjectAsync(action);
 
             return joinable.Task;
+        }
+
+        public void OnProjectLoadedInHost()
+        {
+            _projectLoadedInHost.SetResult(null);
+        }
+
+        public void OnPrioritizedProjectLoadedInHost()
+        {
+            _prioritizedProjectLoadedInHost.SetResult(null);
         }
     }
 }
