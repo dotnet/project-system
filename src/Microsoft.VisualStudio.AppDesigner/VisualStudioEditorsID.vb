a83f7a3dfc4e5af4bd318e7b27a05815404b269e
David Kean <davkean@microsoft.com>
2018-05-07 11:28:21 +1000

    Remove unused fields

--- src/Microsoft.VisualStudio.AppDesigner/VisualStudioEditorsID.vb
+++ src/Microsoft.VisualStudio.AppDesigner/VisualStudioEditorsID.vb
@@ -18,15 +18,8 @@ Namespace Microsoft.VisualStudio.Editors
             ' Constants for menu command IDs and GUIDs. 
             ' *** These must match the constants in designerui\VisualStudioEditorsID.h *****
 
-
-            ' *********************************************************************
-
-            'Some common stuff
-            Private Shared ReadOnly s_CMDSETID_StandardCommandSet97 As New Guid("5efc7975-14bc-11cf-9b2b-00aa00573819")
-            Private Shared ReadOnly s_CMDSETID_StandardCommandSet2K As New Guid("1496A755-94DE-11D0-8C3F-00C04FC2AAE2")
-
-            Friend Shared ReadOnly GuidVSStd97 As Guid = s_CMDSETID_StandardCommandSet97
-            Friend Shared ReadOnly GuidVSStd2K As Guid = s_CMDSETID_StandardCommandSet2K
+            Friend Shared ReadOnly GuidVSStd97 As New Guid("5efc7975-14bc-11cf-9b2b-00aa00573819")
+            Friend Shared ReadOnly GuidVSStd2K As New Guid("1496A755-94DE-11D0-8C3F-00C04FC2AAE2")
             Private Const CmdIdCopy As Integer = 15
             Private Const CmdIdCut As Integer = 16
             Friend Const CmdIdFileClose As Integer = 223
