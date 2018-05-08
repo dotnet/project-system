525f1ccee533978c63ba1c70fc68b6cfc27adbbe
TIHan <lol.tihan@gmail.com>
2018-05-08 15:20:24 -0700

    Updated ordering tests to reflect changes in OrderingHelper

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Input/Commands/Ordering/MoveDownCommandTests.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Input/Commands/Ordering/MoveDownCommandTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Immutable;
 using System.Threading.Tasks;
 
 using Microsoft.VisualStudio.Packaging;
-
+using Microsoft.VisualStudio.ProjectSystem.VS.Utilities;
 using Xunit;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Input.Commands.Ordering
@@ -15,12 +15,27 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Input.Commands.Ordering
         [Fact]
         public async Task GetCommandStatusAsync_File_ReturnsStatusEnabled()
         {
-            var command = CreateAbstractInstance();
+            var projectRootElement = @"
+<Project>
+
+  <PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <Compile Include=""test1.fs"" />
+    <Compile Include=""test2.fs"" />
+  </ItemGroup>
+
+</Project>
+".AsProjectRootElement();
+
+            var command = CreateAbstractInstance(accessor: IProjectAccessorFactory.Create(projectRootElement));
 
             var tree = ProjectTreeParser.Parse(@"
 Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
-    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 1
-    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 2
+    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 1, ItemName: ""test1.fs""
+    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 2, ItemName: ""test2.fs""
 ");
 
             var nodes = ImmutableHashSet.Create(tree.Children[0]); // test1.fs
@@ -34,12 +49,27 @@ Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
         [Fact]
         public async Task GetCommandStatusAsync_File_ReturnsStatusNinched()
         {
-            var command = CreateAbstractInstance();
+            var projectRootElement = @"
+<Project>
+
+  <PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <Compile Include=""test1.fs"" />
+    <Compile Include=""test2.fs"" />
+  </ItemGroup>
+
+</Project>
+".AsProjectRootElement();
+
+            var command = CreateAbstractInstance(accessor: IProjectAccessorFactory.Create(projectRootElement));
 
             var tree = ProjectTreeParser.Parse(@"
 Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
-    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 1
-    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 2
+    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 1, ItemName: ""test1.fs""
+    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 2, ItemName: ""test2.fs""
 ");
 
             var nodes = ImmutableHashSet.Create(tree.Children[1]); // test2.fs
@@ -53,15 +83,32 @@ Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
         [Fact]
         public async Task GetCommandStatusAsync_FileInFolder_ReturnsStatusEnabled()
         {
-            var command = CreateAbstractInstance();
+            var projectRootElement = @"
+<Project>
+
+  <PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <Compile Include=""test1.fs"" />
+    <Compile Include=""test2.fs"" />
+    <Compile Include=""test\test3.fs"" />
+    <Compile Include=""test\test4.fs"" />
+  </ItemGroup>
+
+</Project>
+".AsProjectRootElement();
+
+            var command = CreateAbstractInstance(accessor: IProjectAccessorFactory.Create(projectRootElement));
 
             var tree = ProjectTreeParser.Parse(@"
 Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
-    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 1
-    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 2
+    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 1, ItemName: ""test1.fs""
+    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 2, ItemName: ""test2.fs""
     Folder (flags: {Folder}), DisplayOrder: 3
-        File (flags: {}), FilePath: ""C:\Foo\test3.fs"", DisplayOrder: 4
-        File (flags: {}), FilePath: ""C:\Foo\test4.fs"", DisplayOrder: 5
+        File (flags: {}), FilePath: ""C:\Foo\test\test3.fs"", DisplayOrder: 4, ItemName: ""test\test3.fs""
+        File (flags: {}), FilePath: ""C:\Foo\test\test4.fs"", DisplayOrder: 5, ItemName: ""test\test4.fs""
 ");
 
             var nodes = ImmutableHashSet.Create(tree.Children[2].Children[0]); // test3.fs
@@ -75,15 +122,32 @@ Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
         [Fact]
         public async Task GetCommandStatusAsync_FileInFolder_ReturnsStatusNinched()
         {
-            var command = CreateAbstractInstance();
+            var projectRootElement = @"
+<Project>
+
+  <PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <Compile Include=""test1.fs"" />
+    <Compile Include=""test2.fs"" />
+    <Compile Include=""test\test3.fs"" />
+    <Compile Include=""test\test4.fs"" />
+  </ItemGroup>
+
+</Project>
+".AsProjectRootElement();
+
+            var command = CreateAbstractInstance(accessor: IProjectAccessorFactory.Create(projectRootElement));
 
             var tree = ProjectTreeParser.Parse(@"
 Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
-    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 1
-    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 2
+    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 1, ItemName: ""test1.fs""
+    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 2, ItemName: ""test2.fs""
     Folder (flags: {Folder}), DisplayOrder: 3
-        File (flags: {}), FilePath: ""C:\Foo\test3.fs"", DisplayOrder: 4
-        File (flags: {}), FilePath: ""C:\Foo\test4.fs"", DisplayOrder: 5
+        File (flags: {}), FilePath: ""C:\Foo\test\test3.fs"", DisplayOrder: 4, ItemName: ""test\test3.fs""
+        File (flags: {}), FilePath: ""C:\Foo\test\test4.fs"", DisplayOrder: 5, ItemName: ""test\test4.fs""
 ");
 
             var nodes = ImmutableHashSet.Create(tree.Children[2].Children[1]); // test4.fs
@@ -97,18 +161,37 @@ Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
         [Fact]
         public async Task GetCommandStatusAsync_FolderOverFolder_ReturnsStatusEnabled()
         {
-            var command = CreateAbstractInstance();
+            var projectRootElement = @"
+<Project>
+
+  <PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <Compile Include=""test1.fs"" />
+    <Compile Include=""test2.fs"" />
+    <Compile Include=""test\test3.fs"" />
+    <Compile Include=""test\test4.fs"" />
+    <Compile Include=""test2\test5.fs"" />
+    <Compile Include=""test2\test6.fs"" />
+  </ItemGroup>
+
+</Project>
+".AsProjectRootElement();
+
+            var command = CreateAbstractInstance(accessor: IProjectAccessorFactory.Create(projectRootElement));
 
             var tree = ProjectTreeParser.Parse(@"
 Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
-    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 1
-    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 2
+    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 1, ItemName: ""test1.fs""
+    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 2, ItemName: ""test2.fs""
     Folder (flags: {Folder}), DisplayOrder: 3
-        File (flags: {}), FilePath: ""C:\Foo\test3.fs"", DisplayOrder: 4
-        File (flags: {}), FilePath: ""C:\Foo\test4.fs"", DisplayOrder: 5
+        File (flags: {}), FilePath: ""C:\Foo\test\test3.fs"", DisplayOrder: 4, ItemName: ""test\test3.fs""
+        File (flags: {}), FilePath: ""C:\Foo\test\test4.fs"", DisplayOrder: 5, ItemName: ""test\test4.fs""
     Folder (flags: {Folder}), DisplayOrder: 6
-        File (flags: {}), FilePath: ""C:\Foo\test5.fs"", DisplayOrder: 7
-        File (flags: {}), FilePath: ""C:\Foo\test6.fs"", DisplayOrder: 8
+        File (flags: {}), FilePath: ""C:\Foo\test2\test5.fs"", DisplayOrder: 7, ItemName: ""test2\test5.fs""
+        File (flags: {}), FilePath: ""C:\Foo\test2\test6.fs"", DisplayOrder: 8, ItemName: ""test2\test6.fs""
 ");
 
             var nodes = ImmutableHashSet.Create(tree.Children[2]); // first folder
@@ -122,18 +205,37 @@ Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
         [Fact]
         public async Task GetCommandStatusAsync_FolderOverFile_ReturnsStatusEnabled()
         {
-            var command = CreateAbstractInstance();
+            var projectRootElement = @"
+<Project>
+
+  <PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <Compile Include=""test\test3.fs"" />
+    <Compile Include=""test\test4.fs"" />
+    <Compile Include=""test1.fs"" />
+    <Compile Include=""test2.fs"" />
+    <Compile Include=""test2\test5.fs"" />
+    <Compile Include=""test2\test6.fs"" />
+  </ItemGroup>
+
+</Project>
+".AsProjectRootElement();
+
+            var command = CreateAbstractInstance(accessor: IProjectAccessorFactory.Create(projectRootElement));
 
             var tree = ProjectTreeParser.Parse(@"
 Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
     Folder (flags: {Folder}), DisplayOrder: 1
-        File (flags: {}), FilePath: ""C:\Foo\test3.fs"", DisplayOrder: 2
-        File (flags: {}), FilePath: ""C:\Foo\test4.fs"", DisplayOrder: 3
-    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 4
-    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 5
+        File (flags: {}), FilePath: ""C:\Foo\test\test3.fs"", DisplayOrder: 2, ItemName: ""test\test3.fs""
+        File (flags: {}), FilePath: ""C:\Foo\test\test4.fs"", DisplayOrder: 3, ItemName: ""test\test4.fs""
+    File (flags: {}), FilePath: ""C:\Foo\test1.fs"", DisplayOrder: 4, ItemName: ""test1.fs""
+    File (flags: {}), FilePath: ""C:\Foo\test2.fs"", DisplayOrder: 5, ItemName: ""test2.fs""
     Folder (flags: {Folder}), DisplayOrder: 6
-        File (flags: {}), FilePath: ""C:\Foo\test5.fs"", DisplayOrder: 7
-        File (flags: {}), FilePath: ""C:\Foo\test6.fs"", DisplayOrder: 8
+        File (flags: {}), FilePath: ""C:\Foo\test2\test5.fs"", DisplayOrder: 7, ItemName: ""test2\test5.fs""
+        File (flags: {}), FilePath: ""C:\Foo\test2\test6.fs"", DisplayOrder: 8, ItemName: ""test2\test6.fs""
 ");
 
             var nodes = ImmutableHashSet.Create(tree.Children[0]); // first folder
@@ -147,16 +249,33 @@ Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
         [Fact]
         public async Task GetCommandStatusAsync_Folder_ReturnsStatusNinched()
         {
-            var command = CreateAbstractInstance();
+            var projectRootElement = @"
+<Project>
+
+  <PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <Compile Include=""test\test3.fs"" />
+    <Compile Include=""test\test4.fs"" />
+    <Compile Include=""test2\test5.fs"" />
+    <Compile Include=""test2\test6.fs"" />
+  </ItemGroup>
+
+</Project>
+".AsProjectRootElement();
+
+            var command = CreateAbstractInstance(accessor: IProjectAccessorFactory.Create(projectRootElement));
 
             var tree = ProjectTreeParser.Parse(@"
 Root (flags: {ProjectRoot}), FilePath: ""C:\Foo\testing.fsproj""
     Folder (flags: {Folder}), DisplayOrder: 1
-        File (flags: {}), FilePath: ""C:\Foo\test3.fs"", DisplayOrder: 2
-        File (flags: {}), FilePath: ""C:\Foo\test4.fs"", DisplayOrder: 3
+        File (flags: {}), FilePath: ""C:\Foo\test\test3.fs"", DisplayOrder: 2, ItemName: ""test\test3.fs""
+        File (flags: {}), FilePath: ""C:\Foo\test\test4.fs"", DisplayOrder: 3, ItemName: ""test\test4.fs""
     Folder (flags: {Folder}), DisplayOrder: 4
-        File (flags: {}), FilePath: ""C:\Foo\test5.fs"", DisplayOrder: 5
-        File (flags: {}), FilePath: ""C:\Foo\test6.fs"", DisplayOrder: 6
+        File (flags: {}), FilePath: ""C:\Foo\test2\test5.fs"", DisplayOrder: 5, ItemName: ""test2\test5.fs""
+        File (flags: {}), FilePath: ""C:\Foo\test2\test6.fs"", DisplayOrder: 6, ItemName: ""test2\test6.fs""
 ");
 
             var nodes = ImmutableHashSet.Create(tree.Children[1]); // second folder
