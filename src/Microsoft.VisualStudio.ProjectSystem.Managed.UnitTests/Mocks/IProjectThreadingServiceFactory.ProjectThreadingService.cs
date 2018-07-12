fa26f001b2507b4103c2fb0d4cacf3a9e3d9d060
David Kean <davkean@microsoft.com>
2018-07-12 16:46:50 +1000

    Remove DispatchThread from unit tests
    Fixes: #602
    
    During some of the hangs I've been seeing during test runs I saw a couple of times where AppDomain.Unload wouldn't complete because the DispatchThread was still running. I've spent a large chunk of time trying to figure it out underlying cause without avail. We don't actually make use of the DispatchThread in our tests - so replace with a simplier implementation.
    
    We now use the default JoinableTaskContext which uses the Current thread as the main thread, and uses XUnit's sync context which basically just executes actions on the same thread.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Mocks/IProjectThreadingServiceFactory.ProjectThreadingService.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.Threading;
+
+using Task = System.Threading.Tasks.Task;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    internal partial class IProjectThreadingServiceFactory
+    {
+        private class ProjectThreadingService : IProjectThreadingService
+        {
+            public JoinableTaskContextNode JoinableTaskContext { get; } = new JoinableTaskContextNode(new JoinableTaskContext());
+
+            public JoinableTaskFactory JoinableTaskFactory
+            {
+                get { return JoinableTaskContext.Factory; }
+            }
+
+            public bool IsOnMainThread
+            {
+                get { return JoinableTaskContext.IsOnMainThread; }
+            }
+
+            public void ExecuteSynchronously(Func<Task> asyncAction)
+            {
+                asyncAction().GetAwaiter().GetResult();
+            }
+
+            public T ExecuteSynchronously<T>(Func<Task<T>> asyncAction)
+            {
+                return asyncAction().GetAwaiter().GetResult();
+            }
+
+            public void VerifyOnUIThread()
+            {
+                if (!IsOnMainThread)
+                    throw new InvalidOperationException();
+            }
+
+            public IDisposable SuppressProjectExecutionContext()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Fork(Func<Task> asyncAction,
+                      JoinableTaskFactory factory = null,
+                      UnconfiguredProject project = null,
+                      ConfiguredProject configuredProject = null,
+                      ErrorReportSettings watsonReportSettings = null,
+                      ProjectFaultSeverity faultSeverity = ProjectFaultSeverity.Recoverable,
+                      ForkOptions options = ForkOptions.Default)
+            {
+                Task.Run(asyncAction).Wait();
+            }
+        }
+    }
+}
