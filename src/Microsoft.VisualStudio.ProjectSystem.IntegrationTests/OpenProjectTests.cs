1290f0f7982f5d6b5272b7c9fd5b6a8d8419403b
Jonathon Marolf <jmarolf@microsoft.com>
2018-07-31 14:00:06 -0700

    adding sample tests

---
+++ src/Microsoft.VisualStudio.ProjectSystem.IntegrationTests/OpenProjectTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Test.Apex.VisualStudio;
+using Microsoft.Test.Apex.VisualStudio.Solution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.VisualStudio.ProjectSystem.IntegrationTests
+{
+    [TestClass]
+    public class CreateProjectTests : VisualStudioHostTest
+    {
+        [TestMethod]
+        public void CreateProject_CreateRestoreAndBuild()
+        {
+            var proj = VisualStudio.ObjectModel.Solution.CreateProject(ProjectLanguage.CSharp, ProjectTemplate.ClassLibrary);
+            VisualStudio.ObjectModel.Solution.Verify.HasProject();
+            VisualStudio.ObjectModel.Shell.ToolWindows.ErrorList.WaitForReady();
+            VisualStudio.ObjectModel.Shell.ToolWindows.ErrorList.WaitForNoErrorListItems();
+            VisualStudio.ObjectModel.Solution.Build();
+            VisualStudio.ObjectModel.Shell.ToolWindows.ErrorList.Verify.ErrorCountIs(0);
+        }
+    }
+}
