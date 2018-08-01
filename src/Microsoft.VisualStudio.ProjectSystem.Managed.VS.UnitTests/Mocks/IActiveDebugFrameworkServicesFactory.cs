8d293d5f67b4a5f12e3bb9d2ed3fe95e165a4181
David Kean <davkean@microsoft.com>
2018-08-01 17:47:41 +1000

    Introduce new base class for mocks

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/IActiveDebugFrameworkServicesFactory.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/IActiveDebugFrameworkServicesFactory.cs
@@ -7,46 +7,41 @@ using Moq;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Debug
 {
-    internal class IActiveDebugFrameworkServicesFactory
+    internal class IActiveDebugFrameworkServicesFactory : AbstractMock<IActiveDebugFrameworkServices>
     {
-        private Mock<IActiveDebugFrameworkServices> _mock;
-        public IActiveDebugFrameworkServicesFactory(MockBehavior mockBehavior = MockBehavior.Strict)
+        public IActiveDebugFrameworkServicesFactory()
+            : base(MockBehavior.Strict)
         {
-            _mock = new Mock<IActiveDebugFrameworkServices>(mockBehavior);
-        }
-
-        public IActiveDebugFrameworkServices Object => _mock.Object;
-
-        public void Verify()
-        {
-            _mock.Verify();
         }
 
         public IActiveDebugFrameworkServicesFactory ImplementGetActiveDebuggingFrameworkPropertyAsync(string activeFramework)
         {
-            _mock.Setup(x => x.GetActiveDebuggingFrameworkPropertyAsync())
-                              .Returns(Task.FromResult(activeFramework));
+            Setup(x => x.GetActiveDebuggingFrameworkPropertyAsync())
+                        .Returns(Task.FromResult(activeFramework));
             return this;
         }
 
         public IActiveDebugFrameworkServicesFactory ImplementGetConfiguredProjectForActiveFrameworkAsync(ConfiguredProject project)
         {
-            _mock.Setup(x => x.GetConfiguredProjectForActiveFrameworkAsync())
-                              .Returns(Task.FromResult(project));
+            Setup(x => x.GetConfiguredProjectForActiveFrameworkAsync())
+                        .Returns(Task.FromResult(project));
+
             return this;
         }
 
         public IActiveDebugFrameworkServicesFactory ImplementGetProjectFrameworksAsync(List<string> frameworsks)
         {
-            _mock.Setup(x => x.GetProjectFrameworksAsync())
-                              .Returns(Task.FromResult(frameworsks));
+            Setup(x => x.GetProjectFrameworksAsync())
+                        .Returns(Task.FromResult(frameworsks));
+
             return this;
         }
 
         public IActiveDebugFrameworkServicesFactory ImplementSetActiveDebuggingFrameworkPropertyAsync(string aciiveFramework)
         {
-            _mock.Setup(x => x.SetActiveDebuggingFrameworkPropertyAsync(aciiveFramework))
-                              .Returns(Task.CompletedTask);
+            Setup(x => x.SetActiveDebuggingFrameworkPropertyAsync(aciiveFramework))
+                        .Returns(Task.CompletedTask);
+
             return this;
         }
     }
