61816762bc6f33e649fb9b1597e97c2962230cb6
David Kean <davkean@microsoft.com>
2018-08-02 14:58:48 +1000

    Capture metadata in tests

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/LanguageServices/Handlers/AbstractEvaluationCommandLineHandlerTests.EvaluationCommandLineHandler.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/LanguageServices/Handlers/AbstractEvaluationCommandLineHandlerTests.EvaluationCommandLineHandler.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Collections.ObjectModel;
 using Microsoft.VisualStudio.ProjectSystem.Logging;
 
 namespace Microsoft.VisualStudio.ProjectSystem.LanguageServices.Handlers
@@ -13,22 +13,27 @@ namespace Microsoft.VisualStudio.ProjectSystem.LanguageServices.Handlers
             public EvaluationCommandLineHandler(UnconfiguredProject project) 
                 : base(project)
             {
-                FileNames = new Collection<string>();
+                Files = new Dictionary<string, IImmutableDictionary<string, string>>();
             }
 
-            public Collection<string> FileNames
+            public ICollection<string> FileNames
+            {
+                get { return Files.Keys; }
+            }
+
+            public Dictionary<string, IImmutableDictionary<string, string>> Files
             {
                 get;
             }
 
             protected override void AddToContext(string fullPath, IImmutableDictionary<string, string> metadata, bool isActiveContext, IProjectLogger logger)
             {
-                FileNames.Add(fullPath);
+                Files.Add(fullPath, metadata);
             }
 
             protected override void RemoveFromContext(string fullPath, IProjectLogger logger)
             {
-                FileNames.Remove(fullPath);
+                Files.Remove(fullPath);
             }
         }
     }
