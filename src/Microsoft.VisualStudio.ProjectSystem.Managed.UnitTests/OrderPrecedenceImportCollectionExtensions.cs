8436613fabe27c85c67711f0fc87bb658633c514
David Kean <davkean@microsoft.com>
2018-08-13 16:51:04 +1000

    Add ApplyChangesToWorkspaceContext
    Add a service that is responsible for taking project snapshot data and sending it IEvaluationHandler/ICommandLineHandler.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/OrderPrecedenceImportCollectionExtensions.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/OrderPrecedenceImportCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.ComponentModel.Composition;
 
 namespace Microsoft.VisualStudio.ProjectSystem
 {
@@ -13,6 +14,15 @@ namespace Microsoft.VisualStudio.ProjectSystem
             var export = new Lazy<T, IOrderPrecedenceMetadataView>(() => value, metadata);
 
             collection.Add(export);
+        }
+
+        public static void Add<T>(this OrderPrecedenceExportFactoryCollection<T> collection, T value, string appliesTo = null, int orderPrecedence = 0)
+        {
+            var metadata = IOrderPrecedenceMetadataViewFactory.Create(appliesTo, orderPrecedence);
+
+            var factory = ExportFactoryFactory.ImplementCreateValueWithAutoDispose(() => value, metadata);
+
+            collection.Add(factory);
         }
     }
 }
