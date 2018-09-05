67f233fc1c5f45b28d605f4aa83910a0233ed0da
David Kean <davkean@microsoft.com>
2018-09-05 21:37:10 +1000

    Fire up/tear down workspace context based on implicitly active
    Fire up and tear down IWorkspaceProjectContext based on being implicitly active/deactive. This lets us react to active configuration changes.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/LanguageServices/WorkspaceContextHost.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.LanguageServices.ProjectSystem;
+
+namespace Microsoft.VisualStudio.ProjectSystem.LanguageServices
+{
+    /// <summary>
+    ///     Responsible for creating and initializing <see cref="IWorkspaceProjectContext"/> and sending 
+    ///     on changes to the project to the <see cref="IApplyChangesToWorkspaceContext"/> service.
+    /// </summary>
+    [Export(typeof(IImplicitlyActiveService))]
+    [AppliesTo(ProjectCapability.DotNetLanguageService2)]
+    internal partial class WorkspaceContextHost : AbstractMultiLifetimeComponent, IImplicitlyActiveService
+    {
+        private readonly ConfiguredProject _project;
+        private readonly IProjectThreadingService _threadingService;
+        private readonly IUnconfiguredProjectTasksService _tasksService;
+        private readonly IProjectSubscriptionService _projectSubscriptionService;
+        private readonly Lazy<IWorkspaceProjectContextProvider> _workspaceProjectContextProvider;
+        private readonly ExportFactory<IApplyChangesToWorkspaceContext> _applyChangesToWorkspaceContextFactory;
+
+        [ImportingConstructor]
+        public WorkspaceContextHost(ConfiguredProject project,
+                                    IProjectThreadingService threadingService,
+                                    IUnconfiguredProjectTasksService tasksService,
+                                    IProjectSubscriptionService projectSubscriptionService,
+                                    Lazy<IWorkspaceProjectContextProvider> workspaceProjectContextProvider,
+                                    ExportFactory<IApplyChangesToWorkspaceContext> applyChangesToWorkspaceContextFactory)
+            : base(threadingService.JoinableTaskContext)
+        {
+            _project = project;
+            _threadingService = threadingService;
+            _tasksService = tasksService;
+            _projectSubscriptionService = projectSubscriptionService;
+            _workspaceProjectContextProvider = workspaceProjectContextProvider;
+            _applyChangesToWorkspaceContextFactory = applyChangesToWorkspaceContextFactory;
+        }
+
+        public Task ActivateAsync()
+        {
+            return LoadAsync();
+        }
+
+        public Task DeactivateAsync()
+        {
+            return UnloadAsync();
+        }
+
+        protected override IMultiLifetimeInstance CreateInstance()
+        {
+            return new WorkspaceContextHostInstance(_project, _threadingService, _tasksService, _projectSubscriptionService, _workspaceProjectContextProvider, _applyChangesToWorkspaceContextFactory);
+        }
+    }
+}
