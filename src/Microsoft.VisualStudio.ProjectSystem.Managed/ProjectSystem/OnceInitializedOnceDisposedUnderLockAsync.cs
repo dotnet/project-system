bb21dc804505a88ace99640b8f06a0f3a3816213
David Kean <davkean@microsoft.com>
2018-09-05 18:06:35 +1000

    Add OnceInitializedOnceDisposedUnderLockAsync
    This is an implementation of OnceInitializedOnceDisposedAsync that prevents overlapping dispose with other operations. The intention is to replace all usage of Semaphore usage around the tree, including SemaphoreSlimExtensions.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/OnceInitializedOnceDisposedUnderLockAsync.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Threading;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    /// <summary>
+    ///     Provides an implementation of <see cref="OnceInitializedOnceDisposedAsync"/> that lets 
+    ///     implementors protect themselves from being disposed while doing work.
+    /// </summary>
+    /// <remarks>
+    ///     <see cref="OnceInitializedOnceDisposed"/> lets implementors prevent themselves from being disposed
+    ///     by locking <see cref="OnceInitializedOnceDisposed.SyncObject"/>. This class provides a similar 
+    ///     mechanism by passing a delegate into <see cref="ExecuteUnderLockAsync"/>.
+    /// </remarks>
+    internal abstract class OnceInitializedOnceDisposedUnderLockAsync : OnceInitializedOnceDisposedAsync
+    {
+        private readonly ReentrantSemaphore _semaphore;
+
+        protected OnceInitializedOnceDisposedUnderLockAsync(JoinableTaskContextNode joinableTaskContextNode)
+            : base(joinableTaskContextNode)
+        {
+            _semaphore = ReentrantSemaphore.Create(1, joinableTaskContextNode.Context, ReentrantSemaphore.ReentrancyMode.Stack);
+        }
+
+        protected override sealed async Task DisposeCoreAsync(bool initialized)
+        {
+            await _semaphore.ExecuteAsync(() => DisposeCoreUnderLockAsync(initialized))
+                            .ConfigureAwait(true);
+
+            _semaphore.Dispose();
+        }
+
+        /// <summary>
+        ///     Disposes of managed and unmanaged resources owned by this instance, under a lock
+        ///     that prevents overlap with any currently executing actions passed to 
+        ///     <see cref="ExecuteUnderLockAsync(Func{CancellationToken, Task}, CancellationToken)"/>.
+        /// </summary>
+        /// <param name="initialized">
+        ///     A value indicating whether this instance had been previously initialized.
+        /// </param>
+        protected abstract Task DisposeCoreUnderLockAsync(bool initialized);
+
+        /// <summary>
+        ///     Executes the specified action under a lock that presents overlap with any currently executing actions passed to
+        ///     <see cref="ExecuteUnderLockAsync(Func{CancellationToken, Task}, CancellationToken)"/> and 
+        ///     <see cref="OnceInitializedOnceDisposedAsync.DisposeAsync"/>.
+        /// </summary>
+        /// <param name="action">
+        ///     The action to execute under the lock.
+        /// </param>
+        /// <param name="cancellationToken">
+        ///     The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.
+        /// </param>
+        /// <returns></returns>
+        /// <exception cref="OperationCanceledException">
+        ///     The result is awaited and <paramref name="cancellationToken"/> is cancelled.
+        ///     <para>
+        ///         -or-
+        ///     </para>
+        ///     The result is awaited and the s<see cref="ThreadSafeOnceInitializedOnceDisposedAsync"/> 
+        ///     has been disposed of.
+        /// </exception>
+        protected Task ExecuteUnderLockAsync(Func<CancellationToken, Task> action, CancellationToken cancellationToken = default)
+        {
+            Requires.NotNull(action, nameof(action));
+
+            return ExecuteUnderLockCoreAsync(action, cancellationToken);
+        }
+
+        private async Task ExecuteUnderLockCoreAsync(Func<CancellationToken, Task> action, CancellationToken cancellationToken = default)
+        {
+            using (var source = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, DisposalToken))
+            {
+                CancellationToken jointCancellationToken = source.Token;
+
+                await _semaphore.ExecuteAsync(() => action(jointCancellationToken), jointCancellationToken)
+                                .ConfigureAwait(true);
+            }
+        }
+    }
+}
