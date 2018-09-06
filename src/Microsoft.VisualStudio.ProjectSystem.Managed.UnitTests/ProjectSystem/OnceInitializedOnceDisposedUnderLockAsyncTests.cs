1931c5d8463be65de2ccc8a485831daf4aa41ffe
David Kean <davkean@microsoft.com>
2018-09-06 12:39:44 +1000

    Remove unneeded cancel
    This was left over from previous incarnation of the test.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/OnceInitializedOnceDisposedUnderLockAsyncTests.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/OnceInitializedOnceDisposedUnderLockAsyncTests.cs
@@ -41,8 +41,6 @@ namespace Microsoft.VisualStudio.ProjectSystem
 
             }, cancellationTokenSource.Token);
 
-            cancellationTokenSource.Cancel();
-
             Assert.True(result);
         }
 
