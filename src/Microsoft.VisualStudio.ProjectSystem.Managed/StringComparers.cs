89be343270f80d1856bf69b792ef7686d15657a8
Drew Noakes <git@drewnoakes.com>
2018-10-26 18:08:27 +0100

    Add StringComparisons static class

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/StringComparers.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/StringComparers.cs
@@ -8,6 +8,9 @@ namespace Microsoft.VisualStudio
     /// <summary>
     ///     Contains commonly used <see cref="IEqualityComparer{String}"/> instances.
     /// </summary>
+    /// <remarks>
+    ///     Mirrors values in <see cref="StringComparisons"/>.
+    /// </remarks>
     internal static class StringComparers
     {
         public static IEqualityComparer<string> WorkspaceProjectContextIds
@@ -49,5 +52,23 @@ namespace Microsoft.VisualStudio
         {
             get { return StringComparer.OrdinalIgnoreCase; }
         }
+    }
+
+    /// <summary>
+    ///     Contains commonly used <see cref="StringComparison"/> instances.
+    /// </summary>
+    /// <remarks>
+    ///     Mirrors values in <see cref="StringComparers"/>.
+    /// </remarks>
+    internal static class StringComparisons
+    {
+        public static StringComparison WorkspaceProjectContextIds => StringComparison.Ordinal;
+        public static StringComparison Paths => StringComparison.OrdinalIgnoreCase;
+        public static StringComparison PropertyNames => StringComparison.OrdinalIgnoreCase;
+        public static StringComparison PropertyValues => StringComparison.OrdinalIgnoreCase;
+        public static StringComparison RuleNames => StringComparison.OrdinalIgnoreCase;
+        public static StringComparison ConfigurationDimensionNames => StringComparison.Ordinal;
+        public static StringComparison DependencyProviderTypes => StringComparison.OrdinalIgnoreCase;
+        public static StringComparison ItemTypes => StringComparison.OrdinalIgnoreCase;
     }
 }
