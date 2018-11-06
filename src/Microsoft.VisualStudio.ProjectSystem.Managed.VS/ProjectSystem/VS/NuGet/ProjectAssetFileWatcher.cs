88e65d632018b08fced3a191ca9d5f5b2ee5c06d
Drew Noakes <git@drewnoakes.com>
2018-11-06 21:44:18 +0000

    Dispose ProjectAssetFileWatcher

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/NuGet/ProjectAssetFileWatcher.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/NuGet/ProjectAssetFileWatcher.cs
@@ -134,6 +134,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.NuGet
         {
             if (initialized)
             {
+                _taskDelayScheduler?.Dispose();
                 _treeWatcher.Dispose();
                 await UnregisterFileWatcherIfAnyAsync();
             }
@@ -209,6 +210,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.NuGet
             if (projectLockJsonFilePath != null)
             {
                 _previousContentsHash = GetFileHashOrNull(projectLockJsonFilePath);
+                _taskDelayScheduler?.Dispose();
                 _taskDelayScheduler = new TaskDelayScheduler(
                     s_notifyDelay,
                     _projectServices.ThreadingService,
