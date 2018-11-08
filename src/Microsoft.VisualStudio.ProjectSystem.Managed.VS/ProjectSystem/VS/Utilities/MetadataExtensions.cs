1a2e092c1fcad3ca88c0ab9f4d36af7d4c7c2e17
Drew Noakes <git@drewnoakes.com>
2018-11-08 16:16:08 +0000

    Document MetadataExtensions methods

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Utilities/MetadataExtensions.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Utilities/MetadataExtensions.cs
@@ -25,6 +25,16 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Utilities
                 : null;
         }
 
+        /// <summary>
+        /// Attempts to get the string value corresponding to <paramref name="key"/>.
+        /// </summary>
+        /// <remarks>
+        /// Missing and empty string values are treated in the same fashion.
+        /// </remarks>
+        /// <param name="properties">The property dictionary to query.</param>
+        /// <param name="key">The key that identifies the property to look up.</param>
+        /// <param name="stringValue">The value of the string if found and non-empty, otherwise <see langword="null"/>.</param>
+        /// <returns><see langword="true"/> if the property was found with a non-empty value, otherwise <see langword="false"/>.</returns>
         public static bool TryGetStringProperty(this IImmutableDictionary<string, string> properties, string key, out string stringValue)
         {
             if (properties != null &&
@@ -38,11 +48,24 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Utilities
             return false;
         }
 
+        /// <summary>
+        /// Gets the string value corresponding to <paramref name="key"/>, or <see langword="null"/> if the property was not found or had an empty value.
+        /// </summary>
+        /// <param name="properties">The property dictionary to query.</param>
+        /// <param name="key">The key that identifies the property to look up.</param>
+        /// <returns>The string value if found and non-empty, otherwise <see langword="null"/>.</returns>
         public static string GetStringProperty(this IImmutableDictionary<string, string> properties, string key)
         {
             return properties.TryGetStringProperty(key, out string value) ? value : null;
         }
 
+        /// <summary>
+        /// Attempts to get the boolean interpretation of the value corresponding to <paramref name="key"/>.
+        /// </summary>
+        /// <param name="properties">The property dictionary to query.</param>
+        /// <param name="key">The key that identifies the property to look up.</param>
+        /// <param name="boolValue">The boolean value of the property if found and successfully parsed, otherwise <see langword="null"/>.</param>
+        /// <returns><see langword="true"/> if the property was found with successfully parsed as a boolean, otherwise <see langword="false"/>.</returns>
         public static bool TryGetBoolProperty(this IImmutableDictionary<string, string> properties, string key, out bool boolValue)
         {
             if (properties.TryGetStringProperty(key, out string valueString) &&
@@ -55,11 +78,25 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Utilities
             return false;
         }
 
+        /// <summary>
+        /// Gets the boolean value corresponding to <paramref name="key"/>, or <see langword="null"/> if the property was missing or could not be parsed as a boolean.
+        /// </summary>
+        /// <param name="properties">The property dictionary to query.</param>
+        /// <param name="key">The key that identifies the property to look up.</param>
+        /// <returns>The boolean value if found and successfully parsed as a boolean, otherwise <see langword="null"/>.</returns>
         public static bool? GetBoolProperty(this IImmutableDictionary<string, string> properties, string key)
         {
             return properties.TryGetBoolProperty(key, out bool value) ? value : default;
         }
 
+        /// <summary>
+        /// Attempts to get the enum type <typeparamref name="T"/> interpretation of the value corresponding to <paramref name="key"/>.
+        /// </summary>
+        /// <param name="properties">The property dictionary to query.</param>
+        /// <param name="key">The key that identifies the property to look up.</param>
+        /// <param name="enumValue">The enum value of the property if found and successfully parsed, otherwise <see langword="null"/>.</param>
+        /// <returns><see langword="true"/> if the property was found with successfully parsed as enum type <typeparamref name="T"/>, otherwise <see langword="false"/>.</returns>
+        /// <typeparam name="T">The enum type.</typeparam>
         public static bool TryGetEnumProperty<T>(this IImmutableDictionary<string, string> properties, string key, out T enumValue) where T : struct
         {
             if (properties.TryGetStringProperty(key, out string valueString) &&
@@ -72,6 +109,13 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Utilities
             return false;
         }
 
+        /// <summary>
+        /// Gets the enum value corresponding to <paramref name="key"/>, or <see langword="null"/> if the property was missing or could not be parsed as an enum.
+        /// </summary>
+        /// <param name="properties">The property dictionary to query.</param>
+        /// <param name="key">The key that identifies the property to look up.</param>
+        /// <returns>The enum value if found and successfully parsed as enum type <typeparamref name="T"/>, otherwise <see langword="null"/>.</returns>
+        /// <typeparam name="T">The enum type.</typeparam>
         public static T? GetEnumProperty<T>(this IImmutableDictionary<string, string> properties, string key) where T : struct
         {
             return properties.TryGetEnumProperty(key, out T value) ? value : default;
