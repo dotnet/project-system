f5edc0eb24f14c59cd9cd0b527aedd2f77e98e36
Drew Noakes <git@drewnoakes.com>
2018-11-13 22:18:35 +0000

    Add equality/hashing
    RemovedDependencyIdentity is used in a HashSet.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/IDependenciesChanges.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/IDependenciesChanges.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Immutable;
 using System.Diagnostics;
 
@@ -15,13 +16,16 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies
     }
 
     [DebuggerDisplay("({" + nameof(ProviderType) + ("}, {" + nameof(DependencyId) + "})"))]
-    public readonly struct RemovedDependencyIdentity
+    public readonly struct RemovedDependencyIdentity : IEquatable<RemovedDependencyIdentity>
     {
         public string ProviderType { get; }
         public string DependencyId { get; }
 
         public RemovedDependencyIdentity(string providerType, string dependencyId) : this()
         {
+            Requires.NotNull(providerType, nameof(providerType));
+            Requires.NotNull(dependencyId, nameof(dependencyId));
+
             ProviderType = providerType;
             DependencyId = dependencyId;
         }
@@ -31,5 +35,25 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies
             providerType = ProviderType;
             dependencyId = DependencyId;
         }
+
+        public bool Equals(RemovedDependencyIdentity other)
+        {
+            return string.Equals(ProviderType, other.ProviderType, StringComparisons.DependencyProviderTypes) && 
+                   string.Equals(DependencyId, other.DependencyId, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Equals(object obj) => obj is RemovedDependencyIdentity other && Equals(other);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (StringComparer.OrdinalIgnoreCase.GetHashCode(ProviderType) * 397) ^
+                        StringComparer.OrdinalIgnoreCase.GetHashCode(DependencyId);
+            }
+        }
+
+        public static bool operator ==(RemovedDependencyIdentity left, RemovedDependencyIdentity right) => left.Equals(right);
+        public static bool operator !=(RemovedDependencyIdentity left, RemovedDependencyIdentity right) => !left.Equals(right);
     }
 }
