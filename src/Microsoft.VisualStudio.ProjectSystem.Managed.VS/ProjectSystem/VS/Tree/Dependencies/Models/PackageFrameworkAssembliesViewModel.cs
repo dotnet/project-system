0fae5cb7a727c47fa876c4e12bdfb35329cac011
Drew Noakes <git@drewnoakes.com>
2018-11-16 22:59:45 +0000

    Reduce size of dependency view models
    Implementations of IDependencyViewModel properties generally have fixed
    values or can delegate to a source object.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/Models/PackageFrameworkAssembliesViewModel.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/Models/PackageFrameworkAssembliesViewModel.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Collections.Immutable;
+
 using Microsoft.VisualStudio.Imaging;
 using Microsoft.VisualStudio.Imaging.Interop;
 using Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Models;
@@ -7,17 +9,19 @@ using Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Snapshot;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies
 {
-    internal class PackageFrameworkAssembliesViewModel : DependencyViewModel
+    internal sealed class PackageFrameworkAssembliesViewModel : IDependencyViewModel
     {
         public static readonly ImageMoniker RegularIcon = KnownMonikers.Library;
 
-        public PackageFrameworkAssembliesViewModel()
-        {
-            Caption = VSResources.FrameworkAssembliesNodeName;
-            Icon = RegularIcon;
-            ExpandedIcon = Icon;
-            Priority = Dependency.FrameworkAssemblyNodePriority;
-            Flags = DependencyTreeFlags.FrameworkAssembliesNodeFlags;
-        }
+        public string Caption => VSResources.FrameworkAssembliesNodeName;
+        public string FilePath => null;
+        public string SchemaName => null;
+        public string SchemaItemType => null;
+        public int Priority => Dependency.FrameworkAssemblyNodePriority;
+        public ImageMoniker Icon => RegularIcon;
+        public ImageMoniker ExpandedIcon => RegularIcon;
+        public IImmutableDictionary<string, string> Properties => null;
+        public ProjectTreeFlags Flags => DependencyTreeFlags.FrameworkAssembliesNodeFlags;
+        public IDependency OriginalModel => null;
     }
 }
