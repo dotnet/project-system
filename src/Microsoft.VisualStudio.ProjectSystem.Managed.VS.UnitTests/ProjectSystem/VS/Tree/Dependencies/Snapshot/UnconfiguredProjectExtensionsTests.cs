dotnet/project-system

commit e1fa8239ec2c4d30ae04f4388a7180b91e234b09

Avoid temporary strings due to trimming

src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/UnconfiguredProjectExtensions.cs
@@ -10,9 +10,17 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Snapshot
         public static string GetRelativePath(this UnconfiguredProject self, string path)
         {
             string projectFolder = Path.GetDirectoryName(self.FullPath);
+
             if (path.StartsWith(projectFolder, StringComparison.OrdinalIgnoreCase))
             {
-                path = path.Substring(projectFolder.Length).TrimStart('\\');
+                // Remove starting slashes
+                int startingSlashCount = 0;
+                while (path.Length > startingSlashCount && path[startingSlashCount] == '\\')
+                {
+                    startingSlashCount++;
+                }
+
+                path = path.Substring(startingSlashCount, projectFolder.Length - startingSlashCount);
             }
 
             return path;

src/Microsoft.VisualStudio.ProjectSystem.Managed/Build/BuildUtilities.cs
@@ -71,11 +71,9 @@ namespace Microsoft.VisualStudio.Build
             // We need to ensure that we return values in the specified order.
             foreach (string value in new LazyStringSplit(propertyValue, delimiter))
             {
-                string s = value.Trim();
-
-                if (!string.IsNullOrEmpty(s))
+                if (!string.IsNullOrWhiteSpace(value))
                 {
-                    yield return s;
+                    yield return value.Trim();
                 }
             }
         }
@@ -131,8 +129,12 @@ namespace Microsoft.VisualStudio.Build
             {
                 if (string.Compare(value, valueToRemove, StringComparison.Ordinal) != 0)
                 {
+                    if (newValue.Length != 0)
+                    {
+                        newValue.Append(delimiter);
+                    }
+
                     newValue.Append(value);
-                    newValue.Append(delimiter);
                 }
                 else
                 {
@@ -140,7 +142,7 @@ namespace Microsoft.VisualStudio.Build
                 }
             }
 
-            property.Value = newValue.ToString().TrimEnd(delimiter);
+            property.Value = newValue.ToString();
 
             if (!valueFound)
             {
@@ -172,6 +174,11 @@ namespace Microsoft.VisualStudio.Build
             bool valueFound = false;
             foreach (string propertyValue in GetPropertyValues(evaluatedPropertyValue, delimiter))
             {
+                if (value.Length != 0)
+                {
+                    value.Append(delimiter);
+                }
+
                 if (string.Compare(propertyValue, oldValue, StringComparison.Ordinal) == 0)
                 {
                     value.Append(newValue);
@@ -181,11 +188,9 @@ namespace Microsoft.VisualStudio.Build
                 {
                     value.Append(propertyValue);
                 }
-
-                value.Append(delimiter);
             }
 
-            property.Value = value.ToString().TrimEnd(delimiter);
+            property.Value = value.ToString();
 
             if (!valueFound)
             {

commit dc53b056d3ba537346766a084be34434338acbcc

Fix bug computing relative path
This bug was introduced in e1fa8239ec2c4d30ae04f4388a7180b91e234b09
and reported in:

https://dev.azure.com/devdiv/DevDiv/_workitems/edit/739527

src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/UnconfiguredProjectExtensions.cs
@@ -14,13 +14,13 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Snapshot
             if (path.StartsWith(projectFolder, StringComparison.OrdinalIgnoreCase))
             {
                 // Remove starting slashes
-                int startingSlashCount = 0;
-                while (path.Length > startingSlashCount && path[startingSlashCount] == '\\')
+                int startIndex = projectFolder.Length;
+                while (path.Length > startIndex && path[startIndex] == '\\')
                 {
-                    startingSlashCount++;
+                    startIndex++;
                 }
 
-                path = path.Substring(startingSlashCount, projectFolder.Length - startingSlashCount);
+                path = path.Substring(startIndex, path.Length - startIndex);
             }
 
             return path;

src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Tree/Dependencies/Snapshot/UnconfiguredProjectExtensionsTests.cs
@@ -1,0 +1,22 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Snapshot;
+
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies
+{
+    public sealed class UnconfiguredProjectExtensionsTests
+    {
+        [Theory]
+        [InlineData(@"C:\Project\MyProject.csproj", @"C:\Project\Foo.cs",           @"Foo.cs")]
+        [InlineData(@"C:\Project\MyProject.csproj", @"D:\Other\Foo.cs",             @"D:\Other\Foo.cs")]
+        [InlineData(@"C:\Project\MyProject.csproj", @"C:\Project\MyProject.csproj", @"MyProject.csproj")]
+        public void GetRelativePath(string projectPath, string path, string expectedRelative)
+        {
+            var project = UnconfiguredProjectFactory.ImplementFullPath(projectPath);
+
+            Assert.Equal(expectedRelative, project.GetRelativePath(path));
+        }
+    }
+}