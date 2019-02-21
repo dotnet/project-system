6f6a89e284a954e338f85509dce4be2a159f49ba
Jonathon Marolf <jmarolf@microsoft.com>
2019-02-21 14:55:04 -0800

    adding IFileRenameHandler interface

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Rename/IFileRenameHandler.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Rename
+{
+    /// <summary>
+    /// General abstraction of type that needs to be notified when a file rename occurs in the project system
+    /// </summary>
+    internal interface IFileRenameHandler
+    {
+        /// <summary>
+        /// Notifies the handler that the given file has been renamed from oldFilePath to newFilePath.
+        /// </summary>
+        /// <param name="oldFilePath">The original path and name of the file</param>
+        /// <param name="newFilePath">The new path and name of the file</param>
+        ValueTask HandleRenameAsync(string oldFilePath, string newFilePath);
+    }
+}
