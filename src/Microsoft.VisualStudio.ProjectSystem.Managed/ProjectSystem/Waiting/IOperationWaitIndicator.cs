6d0718ac4c090ae6fa0eb1bc7bd7e624cee4ca29
Jonathon Marolf <jmarolf@microsoft.com>
2019-02-24 19:24:48 -0800

    adding interfaces for operation cancellation

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Waiting/IOperationWaitIndicator.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Waiting
+{
+    internal interface IOperationWaitIndicator
+    {
+        void WaitForOperation(string title, string message, bool allowCancel, Action<CancellationToken> action);
+        T WaitForOperation<T>(string title, string message, bool allowCancel, Func<CancellationToken, T> action);
+        void WaitForAsyncOperation(string title, string message, bool allowCancel, Func<CancellationToken, Task> asyncFunction);
+        T WaitForAsyncOperation<T>(string title, string message, bool allowCancel, Func<CancellationToken, Task<T>> asyncFunction);
+        WaitIndicatorResult WaitForOperationWithResult(string title, string message, bool allowCancel, Action<CancellationToken> action);
+        (WaitIndicatorResult, T) WaitForOperationWithResult<T>(string title, string message, bool allowCancel, Func<CancellationToken, T> function);
+        WaitIndicatorResult WaitForAsyncOperationWithResult(string title, string message, bool allowCancel, Func<CancellationToken, Task> asyncFunction);
+        (WaitIndicatorResult, T) WaitForAsyncOperationWithResult<T>(string title, string message, bool allowCancel, Func<CancellationToken, Task<T>> asyncFunction);
+    }
+}
