daa9b9c5eba637c0c9d64ebf07f9ce44b024653a
David Kean <davkean@microsoft.com>
2019-03-01 21:58:37 +1100

    Always return a new CodeDomProvider
    This mimics legacy behavior. Consumers of IVSMDCodeDomCreator.CreateCodeDomProvider always get a new version of the CodeDomProvider, in CPS we were sharing this per project.
    
    CodeDomProviders indirectly have mutable state via the underlying generators that keeps track of statement depth and options.
    
    There is no current bug that this fixes, but as we start addressing UI delays, async editors we might start using these provides off UI thread, and want to avoid having designers overlap their usage, which would only be a problem in CPS.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/LanguageServices/CSharp/CSharpCodeDomProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/LanguageServices/CSharp/CSharpCodeDomProvider.cs
@@ -4,26 +4,25 @@ using System.CodeDom.Compiler;
 using System.ComponentModel.Composition;
 
 using Microsoft.CSharp;
+using Microsoft.VisualStudio.Designer.Interfaces;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.LanguageServices.CSharp
 {
     /// <summary>
     ///     Provides the C# <see cref="CodeDomProvider"/> for use by designers and code generators.
     /// </summary>
+    /// <remarks>
+    ///     This service is requested by <see cref="IVSMDCodeDomCreator.CreateCodeDomProvider(object, int)"/> and 
+    ///     returned by <see cref="IVSMDCodeDomProvider.CodeDomProvider"/>.
+    /// </remarks>
     [ExportVsProfferedProjectService(typeof(CodeDomProvider))]
     [AppliesTo(ProjectCapability.CSharp)]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
     internal class CSharpCodeDomProvider : CSharpCodeProvider
     {
         [ImportingConstructor]
         public CSharpCodeDomProvider()
         {
-        }
-
-        [Import]
-        public UnconfiguredProject Project   // Put ourselves in the UnconfiguredProject scope
-        {
-            get;
-            set;
         }
     }
 }
