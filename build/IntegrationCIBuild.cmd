64e0614624d29856aa86a8519b280ba676db6de4
Jonathon Marolf <jmarolf@microsoft.com>
2019-05-11 01:55:49 -0700

    deploy using vssdk on build

--- build/IntegrationCIBuild.cmd
+++ build/IntegrationCIBuild.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass %~dp0Build.ps1 -restore -build -pack -ci -integrationTest %*
+powershell -ExecutionPolicy ByPass %~dp0Build.ps1 -restore -build -pack -ci /p:RootSuffix= %*
 exit /b %ErrorLevel%
