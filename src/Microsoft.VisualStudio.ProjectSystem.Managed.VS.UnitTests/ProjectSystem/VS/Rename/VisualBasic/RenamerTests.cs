3c58952870f8a7b3e2fa3c8628097a2d10c0cc11
Jonathon Marolf <jmarolf@microsoft.com>
2019-05-28 10:32:36 -0700

    remove and sort usings

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Rename/VisualBasic/RenamerTests.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Rename/VisualBasic/RenamerTests.cs
@@ -1,9 +1,10 @@
-using System;
-using System.Linq;
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
 using System.Threading.Tasks;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.VisualStudio.ProjectSystem.LanguageServices.VisualBasic;
+
 using Moq;
 
 using Xunit;
