095a4fed1be0a6a0f827ecf23b8f39c8ada313a2
Drew Noakes <git@drewnoakes.com>
2019-05-31 14:54:20 +1000

    Fix docs

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/Tree/Dependencies/CrossTarget/IDependenciesRuleHandler.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/Tree/Dependencies/CrossTarget/IDependenciesRuleHandler.cs
@@ -16,17 +16,13 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.CrossTarget
     internal interface IDependenciesRuleHandler
     {
         /// <summary>
-        ///     Gets the rule this handler handles.
+        /// Gets the set of rule names this handler handles.
         /// </summary>
-        /// <value>
-        ///     A <see cref="string"/> containing the rule that this <see cref="IDependenciesRuleHandler"/> 
-        ///     handles.
-        /// </value>
         ImmutableHashSet<string> GetRuleNames(RuleHandlerType handlerType);
 
         /// <summary>
-        ///     Handles the specified set of changes to a rule, and applies them
-        ///     to the given <see cref="CrossTargetDependenciesChangesBuilder"/>.
+        /// Handles the specified set of changes to a rule, and applies them
+        /// to the given <see cref="CrossTargetDependenciesChangesBuilder"/>.
         /// </summary>
         void Handle(
             IImmutableDictionary<string, IProjectChangeDescription> changesByRuleName,
