a68f3271cc3f0c4dd6b18817a1b2bfbaead96085
Drew Noakes <git@drewnoakes.com>
2019-06-06 21:43:48 +1000

    Support adding assembly references in integration tests

--- src/Microsoft.VisualStudio.ProjectSystem.IntegrationTests/Construction/Project.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.IntegrationTests/Construction/Project.cs
@@ -20,6 +20,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
 
         private List<Project>? _referencedProjects;
         private List<PackageReference>? _packageReferences;
+        private List<AssemblyReference>? _assemblyReferences;
         private List<IFile>? _files;
 
         public XElement XElement { get; } = new XElement("Project");
@@ -57,6 +58,15 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
                     _referencedProjects.Select(p => new XElement(
                         "ProjectReference",
                         new XAttribute("Include", $"..\\{p.RelativeProjectFilePath}")))));
+            }
+
+            if (_assemblyReferences != null)
+            {
+                XElement.Add(new XElement(
+                    "ItemGroup",
+                    _assemblyReferences.Select(p => new XElement(
+                        "Reference",
+                        new XAttribute("Include", p.Name)))));
             }
 
             if (_packageReferences != null)
@@ -100,6 +110,12 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
             _packageReferences.Add(packageReference);
         }
 
+        public void Add(AssemblyReference assemblyReference)
+        {
+            _assemblyReferences ??= new List<AssemblyReference>();
+            _assemblyReferences.Add(assemblyReference);
+        }
+
         public void Add(IFile file)
         {
             _files ??= new List<IFile>();
@@ -121,6 +137,16 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS
         {
             PackageId = packageId;
             Version = version;
+        }
+    }
+
+    public readonly struct AssemblyReference
+    {
+        public string Name { get; }
+
+        public AssemblyReference(string name)
+        {
+            Name = name;
         }
     }
 
