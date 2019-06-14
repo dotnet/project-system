6e8687a0f511f5b4f86b46f8dd3b339885b030ef
Drew Noakes <git@drewnoakes.com>
2019-06-14 16:21:17 +1000

    Formatting and copyright banner

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/Rules/XamlRuleTests.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/Rules/XamlRuleTests.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml;
 using System.Xml.Linq;
+
 using Xunit;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Rules
