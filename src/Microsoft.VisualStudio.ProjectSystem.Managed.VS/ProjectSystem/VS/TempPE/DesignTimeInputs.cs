13f5f82fd6b178c795a65b3437bf7c4721f2078b
David Wengier <david.wengier@microsoft.com>
2019-07-01 18:51:24 +1000

    Add data source that listens to project items and produces a list of design time and shared design time inputs

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/TempPE/DesignTimeInputs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.TempPE
+{
+    internal class DesignTimeInputs
+    {
+        public string[] Inputs = Array.Empty<string>();
+        public string[] SharedInputs = Array.Empty<string>();
+    }
+}
