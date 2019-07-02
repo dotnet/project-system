e708e7f4e8b7a050199ba38a7be9998fd77f33eb
Jonathon Marolf <jmarolf@microsoft.com>
2019-07-02 14:17:42 -0700

    initial refactor notify impl

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Refactor/VisualStudioRefactorNotifyService.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+using EnvDTE;
+
+using Microsoft.VisualStudio.ProjectSystem.Refactor;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Refactor
+{
+    [Export(typeof(IImplicitlyActiveService))]
+    [Export(typeof(IRefactorNotifyService))]
+    [AppliesTo(ProjectCapability.CSharpOrVisualBasic)]
+    internal sealed partial class VisualStudioRefactorNotifyService : AbstractMultiLifetimeComponent<VisualStudioRefactorNotifyService.Instance>, IImplicitlyActiveService, IRefactorNotifyService
+    {
+        private readonly IVsService<SDTE, DTE> _dte;
+        private readonly IVsService<SVsSolution, IVsSolution> _solutionService;
+
+        [ImportingConstructor]
+        public VisualStudioRefactorNotifyService(UnconfiguredProject unconfiguredProject,
+                                                 IProjectThreadingService threadingService,
+                                                 IVsService<SDTE, DTE> dte,
+                                                 IVsService<SVsSolution, IVsSolution> solutionService)
+            : base(threadingService.JoinableTaskContext)
+        {
+            _dte = dte;
+            _solutionService = solutionService;
+        }
+
+        public Task ActivateAsync() => LoadAsync();
+        public Task DeactivateAsync() => UnloadAsync();
+
+        protected override Instance CreateInstance() => new Instance(_dte, _solutionService);
+
+        public async Task<bool> TryOnBeforeGlobalSymbolRenamedAsync(string projectPath, IEnumerable<string> filePaths, string rqName, string newName)
+        {
+            if (projectPath is null)
+                throw new System.ArgumentNullException(nameof(projectPath));
+            if (filePaths is null)
+                throw new System.ArgumentNullException(nameof(filePaths));
+            if (rqName is null)
+                throw new System.ArgumentNullException(nameof(rqName));
+            if (newName is null)
+                throw new System.ArgumentNullException(nameof(newName));
+
+            Instance instance = await WaitForLoadedAsync();
+            return await instance.TryOnBeforeGlobalSymbolRenamedAsync(projectPath, filePaths, rqName, newName);
+        }
+
+        public async Task<bool> TryOnAfterGlobalSymbolRenamedAsync(string projectPath, IEnumerable<string> filePaths, string rqName, string newName)
+        {
+            if (projectPath is null)
+                throw new System.ArgumentNullException(nameof(projectPath));
+            if (filePaths is null)
+                throw new System.ArgumentNullException(nameof(filePaths));
+            if (rqName is null)
+                throw new System.ArgumentNullException(nameof(rqName));
+            if (newName is null)
+                throw new System.ArgumentNullException(nameof(newName));
+
+            Instance instance = await WaitForLoadedAsync();
+            return await instance.TryOnAfterGlobalSymbolRenamedAsync(projectPath, filePaths, rqName, newName);
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Refactor/IRefactorNotifyService.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Refactor
+{
+    internal interface IRefactorNotifyService
+    {
+        Task<bool> TryOnBeforeGlobalSymbolRenamedAsync(string projectPath, IEnumerable<string> filePaths, string rqName, string newName);
+        Task<bool> TryOnAfterGlobalSymbolRenamedAsync(string projectPath, IEnumerable<string> filePaths, string rqName, string newName);
+    }
+}
