75b90b46dcc8e117e26289761b753bb9fa1cb313
David Wengier <david.wengier@microsoft.com>
2019-07-05 09:41:37 +1000

    Make DesignTimeInputs immutable, with immutable fields

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/TempPE/DesignTimeInputs.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/TempPE/DesignTimeInputs.cs
@@ -1,12 +1,19 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.TempPE
 {
     internal class DesignTimeInputs
     {
-        public string[] Inputs = Array.Empty<string>();
-        public string[] SharedInputs = Array.Empty<string>();
+        public readonly ImmutableArray<string> Inputs;
+        public readonly ImmutableArray<string> SharedInputs;
+
+        public DesignTimeInputs(IEnumerable<string> inputs, IEnumerable<string> sharedInputs)
+        {
+            Inputs = ImmutableArray<string>.Empty.AddRange(inputs);
+            SharedInputs = ImmutableArray<string>.Empty.AddRange(sharedInputs);
+        }
     }
 }
