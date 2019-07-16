4b7f924bd631791f47fc12d2ff3e6401e518b36e
David Kean <davkean@microsoft.com>
2019-07-16 18:22:51 +1000

    Move types to their own file

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/ILaunchSettingsUIProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/ILaunchSettingsUIProvider.cs
@@ -7,15 +7,6 @@ using Microsoft.VisualStudio.Composition;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Debug
 {
-    public static class UIProfilePropertyName
-    {
-        public const string Executable = "Executable";
-        public const string Arguments = "Arguments";
-        public const string LaunchUrl = "LaunchUrl";
-        public const string EnvironmentVariables = "EnvironmentVariables";
-        public const string WorkingDirectory = "WorkingDirectory";
-        public const string NativeDebugging = "NativeDebugging";
-    }
 
     /// <summary>
     /// Interface definition which allows a launch settings provider to participate in the debug property page UI. The Set of 
