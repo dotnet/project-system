84830830495837a914da43de8bab384c32d8b281
David Kean <davkean@microsoft.com>
2019-07-22 14:28:42 +1000

    Rename build.cmd tests commands
    Adds new test commands to build.cmd to replace /skiptests and /integrationtests:
    
    build /[no-]test
    build /[no-]integration

--- build.cmd
+++ build.cmd
@@ -16,15 +16,17 @@ set OptLog=$false
 :ParseArguments
 if    "%1" == "" goto :DoneParsing
 if /I "%1" == "/?" call :Usage && exit /b 1
-if /I "%1" == "/build"               set OptBuild=$true  && set OptRebuild=$false && shift && goto :ParseArguments
-if /I "%1" == "/rebuild"             set OptBuild=$false && set OptRebuild=$true  && shift && goto :ParseArguments
-if /I "%1" == "/debug"               set BuildConfiguration=Debug                 && shift && goto :ParseArguments
-if /I "%1" == "/release"             set BuildConfiguration=Release               && shift && goto :ParseArguments
-if /I "%1" == "/skiptests"           set OptTest=$false                           && shift && goto :ParseArguments
-if /I "%1" == "/no-deploy-extension" set OptDeploy=$false                         && shift && goto :ParseArguments
-if /I "%1" == "/diagnostic"          set OptLog=$true                             && shift && goto :ParseArguments
-if /I "%1" == "/integrationtests"    set OptIntegrationTest=$true                 && shift && goto :ParseArguments
-if /I "%1" == "/rootsuffix"          set PropRootSuffix=/p:RootSuffix=%2          && shift && shift && goto :ParseArguments
+if /I "%1" == "/build"                set OptBuild=$true  && set OptRebuild=$false && shift && goto :ParseArguments
+if /I "%1" == "/rebuild"              set OptBuild=$false && set OptRebuild=$true  && shift && goto :ParseArguments
+if /I "%1" == "/test"                 set OptTest=$true                            && shift && goto :ParseArguments
+if /I "%1" == "/no-test"              set OptTest=$false                           && shift && goto :ParseArguments
+if /I "%1" == "/integration"          set OptIntegrationTest=$true                 && shift && goto :ParseArguments
+if /I "%1" == "/no-integration"       set OptIntegrationTest=$false                && shift && goto :ParseArguments
+if /I "%1" == "/debug"                set BuildConfiguration=Debug                 && shift && goto :ParseArguments
+if /I "%1" == "/release"              set BuildConfiguration=Release               && shift && goto :ParseArguments
+if /I "%1" == "/no-deploy-extension"  set OptDeploy=$false                         && shift && goto :ParseArguments
+if /I "%1" == "/diagnostic"           set OptLog=$true                             && shift && goto :ParseArguments
+if /I "%1" == "/rootsuffix"           set PropRootSuffix=/p:RootSuffix=%2          && shift && shift && goto :ParseArguments
 call :Usage && exit /b 1
 :DoneParsing
 
@@ -32,20 +34,22 @@ powershell -ExecutionPolicy ByPass -Command "& """%Root%build\Build.ps1""" -conf
 exit /b %ERRORLEVEL%
 
 :Usage
-echo Usage: %BatchFile% [/build^|/rebuild] [/debug^|/release] [/skiptests] [/no-deploy-extension] [/diagnostic] [/integrationtests] [/rootsuffix hive]
+echo Usage: %BatchFile% [options]
 echo.
 echo   Build targets:
-echo     /build                  Perform a build (default)
-echo     /rebuild                Perform a clean, then build
+echo     /build                    Perform a build (default)
+echo     /rebuild                  Perform a clean, then build
+echo.
+echo   Test targets:
+echo     /[no-]test                Run (default) or skip unit tests
+echo     /[no-]integration         Run or skip (default) integration tests
 echo.
 echo   Configurations:
-echo     /debug                  Perform debug build (default)
-echo     /release                Perform release build
+echo     /debug                    Perform debug build (default)
+echo     /release                  Perform release build
 echo.
 echo   Build options:
-echo     /skiptests              Does not run unit tests
-echo     /diagnostic             Turns on logging to a binlog
-echo     /rootsuffix             Visual Studio hive to deploy VSIX extensions to (default is Exp)
-echo     /no-deploy-extension    Does not deploy VSIX extensions when building the solution
-echo     /integrationtests       Runs integration tests
+echo     /diagnostic               Turns on logging to a binlog
+echo     /rootsuffix ^<hive^>      Visual Studio hive to deploy VSIX extensions to (default is Exp)
+echo     /no-deploy-extension      Does not deploy VSIX extensions when building the solution
 goto :eof
