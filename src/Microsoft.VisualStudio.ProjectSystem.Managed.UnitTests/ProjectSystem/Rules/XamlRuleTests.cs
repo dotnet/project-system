e4661c729e389c627ac3902d3f98772d64d9cc7f
Drew Noakes <git@drewnoakes.com>
2019-07-31 22:36:48 +1000

    Validate data sources of resolved dependency rules

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/Rules/XamlRuleTests.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/Rules/XamlRuleTests.cs
@@ -18,6 +18,8 @@ namespace Microsoft.VisualStudio.ProjectSystem.Rules
 {
     public class XamlRuleTests
     {
+        private const string MSBuildNamespace = "http://schemas.microsoft.com/build/2009/properties";
+
         [Theory]
         [MemberData(nameof(GetFileItemRules))]
         public void FileRulesShouldMatchNone(string ruleName, string fullPath)
@@ -297,6 +299,60 @@ namespace Microsoft.VisualStudio.ProjectSystem.Rules
             }
         }
 
+        [Theory]
+        [MemberData(nameof(GetDependenciesRules))]
+        public void VisibleEditableResolvedDependenciesMustHaveDataSource(string ruleName, string fullPath)
+        {
+            // Resolved rules get their data from design time targets. Any editable properties need a
+            // property-level data source that specifies the storage for that property as the project file
+            // so that changes made in the properties pane are reflected in the project file and vice versa.
+
+            if (!ruleName.StartsWith("Resolved"))
+            {
+                return;
+            }
+
+            XElement rule = LoadXamlRule(fullPath);
+
+            var itemType = rule
+                 .Element(XName.Get("Rule.DataSource", MSBuildNamespace))
+                ?.Element(XName.Get("DataSource", MSBuildNamespace))
+                ?.Attribute("ItemType")?.Value;
+
+            Assert.NotNull(itemType);
+
+            foreach (var property in GetProperties(rule))
+            {
+                // Properties are visible and non-readonly by default
+                string visibleValue = property.Attribute("Visible")?.Value ?? "true";
+                string readOnlyValue = property.Attribute("ReadOnly")?.Value ?? "false";
+
+                Assert.True(bool.TryParse(visibleValue, out bool visible));
+                Assert.True(bool.TryParse(readOnlyValue, out bool readOnly));
+
+                if (!visible || readOnly)
+                {
+                    continue;
+                }
+
+                var dataSourceElementName = $"{property.Name.LocalName}.DataSource";
+
+                var dataSource = property
+                     .Element(XName.Get(dataSourceElementName, MSBuildNamespace))
+                    ?.Element(XName.Get("DataSource", MSBuildNamespace));
+
+                if (dataSource == null)
+                {
+                    throw new Xunit.Sdk.XunitException($"Resolved dependency rule {ruleName} has visible, non-readonly property {property.Attribute("Name")} with no {dataSourceElementName} value.");
+                }
+
+                Assert.Equal("False",        dataSource.Attribute("HasConfigurationCondition")?.Value, StringComparer.OrdinalIgnoreCase);
+                Assert.Equal("ProjectFile",  dataSource.Attribute("Persistence")?.Value,               StringComparer.Ordinal);
+                Assert.Equal("AfterContext", dataSource.Attribute("SourceOfDefaultValue")?.Value,      StringComparer.Ordinal);
+                Assert.Equal(itemType,       dataSource.Attribute("ItemType")?.Value,                  StringComparer.Ordinal);
+            }
+        }
+
         public static IEnumerable<object[]> GetBrowseObjectItemRules()
         {
             // Special case for Folder because it is both File and BrowseObject context (for now), but is named like a File.
@@ -387,7 +443,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.Rules
             }
 
             var namespaceManager = new XmlNamespaceManager(reader.NameTable);
-            namespaceManager.AddNamespace("msb", "http://schemas.microsoft.com/build/2009/properties");
+            namespaceManager.AddNamespace("msb", MSBuildNamespace);
 
             return root.XPathSelectElement(@"/msb:Rule/msb:Rule.DataSource/msb:DataSource", namespaceManager);
         }
