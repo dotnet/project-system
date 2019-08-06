d9bb918cfa3e4092857522d4cc99bf8c25bbf617
David Wengier <david.wengier@microsoft.com>
2019-08-06 13:17:39 +1000

    Add block that consumes design time inputs and file changes, and produces a set of both combined.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/TempPE/DesignTimeInputsDelta.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.TempPE
+{
+    internal class DesignTimeInputsDelta
+    {
+        public readonly ImmutableArray<string> AllInputs;
+        public readonly ImmutableArray<string> AllSharedInputs;
+        public readonly ImmutableArray<DesignTimeInputFileChange> ChangedInputs;
+        public readonly string OutputPath;
+
+        public DesignTimeInputsDelta(IEnumerable<string> allInputs, IEnumerable<string> allSharedInputs, IEnumerable<DesignTimeInputFileChange> changedInputs, string outputPath)
+        {
+            AllInputs = ImmutableArray<string>.Empty.AddRange(allInputs);
+            AllSharedInputs = ImmutableArray<string>.Empty.AddRange(allSharedInputs);
+            ChangedInputs = ImmutableArray<DesignTimeInputFileChange>.Empty.AddRange(changedInputs);
+            OutputPath = outputPath;
+        }
+    }
+}
