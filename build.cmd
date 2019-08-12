46337b90b4a6fc2034ee3f4de0c0494803a186e9
David Kean <davkean@microsoft.com>
2019-08-12 11:28:53 +1000

    Move SetVSEnvironment -> script directory

--- build.cmd
+++ build.cmd
@@ -53,7 +53,7 @@ if "%OptDiagnostic%" == "true" (
     set LogCmdLine=/v:minimal
 )
 
-call "%Root%\build\Bootstrap\SetVSEnvironment.cmd" || exit /b 1
+call "%Root%\build\script\SetVSEnvironment.cmd" || exit /b 1
 
 msbuild %Root%build\build.proj /m /warnaserror /nologo /clp:Summary /nodeReuse:%OptNodeReuse% /p:Configuration=%BuildConfiguration% /p:Build=%OptBuild% /p:Rebuild=%OptRebuild% /p:Deploy=%OptDeploy% /p:Test=%OptTest% /p:IntegrationTest=%OptIntegrationTest% /p:Sign=%OptSign% /p:CIBuild=%OptCI% /p:EnableIbc=%OptIbc% /p:ClearNuGetCache=%OptClearNuGetCache% %LogCmdLine% %RootSuffixCmdLine%
 exit /b %ERRORLEVEL%
