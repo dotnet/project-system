e3291616eb4c9308bf02601856564fee4e6edc89
Drew Noakes <git@drewnoakes.com>
2019-08-14 13:50:12 +1000

    Extract code for running STA tests

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/PropertyPages/PropertyPageControlTests.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/PropertyPages/PropertyPageControlTests.cs
@@ -1,8 +1,9 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
-using System.Threading;
 using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.ProjectSystem.VS.Utilities;
 
 using Moq;
 using Moq.Protected;
@@ -16,40 +17,31 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.PropertyPages
     public class PropertyPageControlTests
     {
         [Fact]
-        public void Test()
+        public Task Test()
         {
-
-            var thread = new Thread(new ThreadStart(CallPropertyPageControl));
-
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
-            thread.Join();
-
-
-        }
-
-        private void CallPropertyPageControl()
-        {
-            var ppvm = new Mock<PropertyPageViewModel>();
-            ppvm.Setup(m => m.Save()).ReturnsAsync(VSConstants.S_OK);
-            ppvm.Setup(m => m.Initialize()).Returns(new Task(() => { }));
-            ppvm.CallBase = true;
-
-            var ppc = new Mock<PropertyPageControl>(MockBehavior.Loose)
+            return TestUtil.RunStaTestAsync(() =>
             {
-                CallBase = true
-            };
+                var ppvm = new Mock<PropertyPageViewModel>();
+                ppvm.Setup(m => m.Save()).ReturnsAsync(VSConstants.S_OK);
+                ppvm.Setup(m => m.Initialize()).Returns(new Task(() => { }));
+                ppvm.CallBase = true;
 
-            ppc.Object.InitializePropertyPage(ppvm.Object);
-            ppc.Object.IsDirty = true;
-            int result = ppc.Object.Apply().Result;
+                var ppc = new Mock<PropertyPageControl>(MockBehavior.Loose)
+                {
+                    CallBase = true
+                };
 
-            Assert.Equal(VSConstants.S_OK, result);
-            Assert.Equal(ppvm.Object, ppc.Object.ViewModel);
-            Assert.Equal(ppvm.Object, ppc.Object.DataContext);
-            ppc.Protected().Verify("OnApply", Times.Once());
-            ppc.Protected().Verify("OnStatusChanged", Times.Exactly(2), ItExpr.IsAny<EventArgs>());
-            ppvm.Verify(m => m.Save(), Times.Once());
+                ppc.Object.InitializePropertyPage(ppvm.Object);
+                ppc.Object.IsDirty = true;
+                int result = ppc.Object.Apply().Result;
+
+                Assert.Equal(VSConstants.S_OK, result);
+                Assert.Equal(ppvm.Object, ppc.Object.ViewModel);
+                Assert.Equal(ppvm.Object, ppc.Object.DataContext);
+                ppc.Protected().Verify("OnApply", Times.Once());
+                ppc.Protected().Verify("OnStatusChanged", Times.Exactly(2), ItExpr.IsAny<EventArgs>());
+                ppvm.Verify(m => m.Save(), Times.Once());
+            });
         }
     }
 }
