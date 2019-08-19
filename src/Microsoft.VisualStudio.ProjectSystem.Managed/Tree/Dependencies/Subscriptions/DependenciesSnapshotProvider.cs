a3bf7e7d36ed76a72bd4414a5fcf8ba200b289ad
Drew Noakes <git@drewnoakes.com>
2019-08-19 20:19:42 +1000

    Extract ContextTracker from DependenciesSnapshotProvider
    Breaks context handling logic out into its own class. This is further work
    towards breaking up the large and entangled DependenciesSnapshotProvider class.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/Tree/Dependencies/Subscriptions/DependenciesSnapshotProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/Tree/Dependencies/Subscriptions/DependenciesSnapshotProvider.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.ComponentModel.Composition;
 using System.Linq;
@@ -39,13 +38,12 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Subscription
 #pragma warning disable IDE0069 // Field is not disposed
         private readonly SnapshotUpdater _snapshot;
 #pragma warning restore IDE0069 // Field is not disposed
+        private readonly ContextTracker _context;
 
         private readonly ITargetFrameworkProvider _targetFrameworkProvider;
         private readonly IUnconfiguredProjectCommonServices _commonServices;
-        private readonly Lazy<IAggregateCrossTargetProjectContextProvider> _contextProvider;
         private readonly IUnconfiguredProjectTasksService _tasksService;
         private readonly IActiveConfiguredProjectSubscriptionService _activeConfiguredProjectSubscriptionService;
-        private readonly IActiveProjectConfigurationRefreshService _activeProjectConfigurationRefreshService;
         private readonly IAggregateDependenciesSnapshotProvider _aggregateSnapshotProvider;
 
         [ImportMany] private readonly OrderPrecedenceImportCollection<IDependencyCrossTargetSubscriber> _dependencySubscribers;
@@ -72,21 +70,6 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Subscription
         private int _isInitialized;
         private bool _isDisposed;
 
-        /// <summary>
-        ///     Current <see cref="AggregateCrossTargetProjectContext"/>, which is an immutable map of
-        ///     configured project to target framework.
-        /// </summary>
-        /// <remarks>
-        /// <para>
-        ///     Updates of this field are serialized within a lock, but reads are free threaded as any
-        ///     potential race can only be handled outside this class.
-        /// </para>
-        /// <para>
-        ///     Value is null before initialization, and not null after.
-        /// </para>
-        /// </remarks>
-        private AggregateCrossTargetProjectContext? _currentAggregateProjectContext;
-
         [ImportingConstructor]
         public DependenciesSnapshotProvider(
             IUnconfiguredProjectCommonServices commonServices,
@@ -98,18 +81,14 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Subscription
             IAggregateDependenciesSnapshotProvider aggregateSnapshotProvider)
             : base(commonServices.ThreadingService.JoinableTaskContext)
         {
-            Requires.NotNull(contextProvider, nameof(contextProvider));
             Requires.NotNull(tasksService, nameof(tasksService));
             Requires.NotNull(activeConfiguredProjectSubscriptionService, nameof(activeConfiguredProjectSubscriptionService));
-            Requires.NotNull(activeProjectConfigurationRefreshService, nameof(activeProjectConfigurationRefreshService));
             Requires.NotNull(targetFrameworkProvider, nameof(targetFrameworkProvider));
             Requires.NotNull(aggregateSnapshotProvider, nameof(aggregateSnapshotProvider));
 
             _commonServices = commonServices;
-            _contextProvider = contextProvider;
             _tasksService = tasksService;
             _activeConfiguredProjectSubscriptionService = activeConfiguredProjectSubscriptionService;
-            _activeProjectConfigurationRefreshService = activeProjectConfigurationRefreshService;
             _targetFrameworkProvider = targetFrameworkProvider;
             _aggregateSnapshotProvider = aggregateSnapshotProvider;
 
@@ -123,6 +102,8 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Subscription
             _subTreeProviders = new OrderPrecedenceImportCollection<IProjectDependenciesSubTreeProvider>(
                 ImportOrderPrecedenceComparer.PreferenceOrder.PreferredComesLast,
                 projectCapabilityCheckProvider: commonServices.Project);
+
+            _context = new ContextTracker(targetFrameworkProvider, commonServices, contextProvider, activeProjectConfigurationRefreshService);
 
             _snapshot = new SnapshotUpdater(commonServices, tasksService.UnloadCancellationToken);
 
@@ -396,13 +377,13 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Subscription
 
             await EnsureInitializedAsync();
 
-            return _currentAggregateProjectContext;
+            return _context.Current;
         }
 
         /// <inheritdoc />
         public ConfiguredProject? GetConfiguredProject(ITargetFramework target)
         {
-            return _currentAggregateProjectContext!.GetInnerConfiguredProject(target);
+            return _context.Current!.GetInnerConfiguredProject(target);
         }
 
         private Task OnConfiguredProjectEvaluatedAsync(IProjectVersionedValue<IProjectSubscriptionUpdate> e)
@@ -433,7 +414,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Subscription
             // Prevent concurrent project context updates.
             await _contextUpdateGate.ExecuteWithinLockAsync(JoinableCollection, JoinableFactory, async () =>
                 {
-                    AggregateCrossTargetProjectContext? newProjectContext = await TryUpdateCurrentAggregateProjectContextAsync();
+                    AggregateCrossTargetProjectContext? newProjectContext = await _context.TryUpdateCurrentAggregateProjectContextAsync();
 
                     if (newProjectContext != null)
                     {
@@ -464,92 +445,6 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Subscription
 
             return;
 
-            async Task<AggregateCrossTargetProjectContext?> TryUpdateCurrentAggregateProjectContextAsync()
-            {
-                AggregateCrossTargetProjectContext? previousContext = _currentAggregateProjectContext;
-
-                // Check if we have already computed the project context.
-                if (previousContext != null)
-                {
-                    // For non-cross targeting projects, we can use the current project context if the TargetFramework hasn't changed.
-                    // For cross-targeting projects, we need to verify that the current project context matches latest frameworks targeted by the project.
-                    // If not, we create a new one and dispose the current one.
-                    ConfigurationGeneral projectProperties = await _commonServices.ActiveConfiguredProjectProperties.GetConfigurationGeneralPropertiesAsync();
-
-                    if (!previousContext.IsCrossTargeting)
-                    {
-                        string newTargetFrameworkName = (string)await projectProperties.TargetFramework.GetValueAsync();
-                        ITargetFramework? newTargetFramework = _targetFrameworkProvider.GetTargetFramework(newTargetFrameworkName);
-                        if (previousContext.ActiveTargetFramework.Equals(newTargetFramework))
-                        {
-                            // No change
-                            return null;
-                        }
-                    }
-                    else
-                    {
-                        // Check if the current project context is up-to-date for the current active and known project configurations.
-                        ProjectConfiguration activeProjectConfiguration = _commonServices.ActiveConfiguredProject.ProjectConfiguration;
-                        IImmutableSet<ProjectConfiguration> knownProjectConfigurations = await _commonServices.Project.Services.ProjectConfigurationsService.GetKnownProjectConfigurationsAsync();
-                        if (knownProjectConfigurations.All(c => c.IsCrossTargeting()) &&
-                            HasMatchingTargetFrameworks(activeProjectConfiguration, knownProjectConfigurations))
-                        {
-                            // No change
-                            return null;
-                        }
-                    }
-                }
-
-                // Force refresh the CPS active project configuration (needs UI thread).
-                await _commonServices.ThreadingService.SwitchToUIThread();
-                await _activeProjectConfigurationRefreshService.RefreshActiveProjectConfigurationAsync();
-
-                // Create new project context.
-                AggregateCrossTargetProjectContext newContext = await _contextProvider.Value.CreateProjectContextAsync();
-
-                _currentAggregateProjectContext = newContext;
-
-                return newContext;
-
-                bool HasMatchingTargetFrameworks(
-                    ProjectConfiguration activeProjectConfiguration,
-                    IReadOnlyCollection<ProjectConfiguration> knownProjectConfigurations)
-                {
-                    Assumes.True(activeProjectConfiguration.IsCrossTargeting());
-
-                    ITargetFramework? activeTargetFramework = _targetFrameworkProvider.GetTargetFramework(activeProjectConfiguration.Dimensions[ConfigurationGeneral.TargetFrameworkProperty]);
-
-                    if (!previousContext.ActiveTargetFramework.Equals(activeTargetFramework))
-                    {
-                        // Active target framework is different.
-                        return false;
-                    }
-
-                    var targetFrameworkMonikers = knownProjectConfigurations
-                        .Select(c => c.Dimensions[ConfigurationGeneral.TargetFrameworkProperty])
-                        .Distinct()
-                        .ToList();
-
-                    if (targetFrameworkMonikers.Count != previousContext.TargetFrameworks.Length)
-                    {
-                        // Different number of target frameworks.
-                        return false;
-                    }
-
-                    foreach (string targetFrameworkMoniker in targetFrameworkMonikers)
-                    {
-                        ITargetFramework? targetFramework = _targetFrameworkProvider.GetTargetFramework(targetFrameworkMoniker);
-
-                        if (targetFramework == null || !previousContext.TargetFrameworks.Contains(targetFramework))
-                        {
-                            // Differing TargetFramework
-                            return false;
-                        }
-                    }
-
-                    return true;
-                }
-            }
 
             void AddSubscriptions(AggregateCrossTargetProjectContext newProjectContext)
             {
