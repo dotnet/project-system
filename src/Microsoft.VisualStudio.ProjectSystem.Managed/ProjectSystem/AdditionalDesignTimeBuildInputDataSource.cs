0dfdd7d97624097f9a4c85a118e675a6e8202637
Daniel Monroy <dmon@microsoft.com>
2019-09-10 13:18:33 -0700

    Revert "Merge pull request #5437 from davkean/RemoveExport"
    This reverts commit e652168a2d3d5364cc8e83d2dd199314948eb59f, reversing
    changes made to 49144adfd7dd20f7a9ef03794ea0e5fd58d5b839.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/AdditionalDesignTimeBuildInputDataSource.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.ComponentModel.Composition;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    /// <summary>
+    ///     Responsible for producing AdditionalDesignTimeBuildInput items (such as project.assets.json)
+    ///     so that they can be watched, and when changed, trigger re-evaluation.
+    /// </summary>
+    [Export(typeof(IFileWatchDataSource))]
+    [AppliesTo(ProjectCapability.PackageReferences)]
+    internal class AdditionalDesignTimeBuildInputDataSource : AbstractItemFileWatchDataSource
+    {
+        [ImportingConstructor]
+        public AdditionalDesignTimeBuildInputDataSource(ConfiguredProject project, IProjectSubscriptionService projectSubscriptionService)
+            : base(project, projectSubscriptionService)
+        {
+        }
+
+        public override string ItemSchemaName => AdditionalDesignTimeBuildInput.SchemaName;
+
+        public override string FullPathProperty => AdditionalDesignTimeBuildInput.FullPathProperty;
+
+        public override FileWatchChangeKinds FileWatchChangeKinds => FileWatchChangeKinds.Added | FileWatchChangeKinds.Removed | FileWatchChangeKinds.Changed;
+
+    }
+}
