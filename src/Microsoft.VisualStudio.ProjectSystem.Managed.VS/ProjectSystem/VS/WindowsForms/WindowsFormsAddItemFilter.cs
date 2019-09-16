e51120655355c2a10d63912db9b4a7a4838d19e9
David Wengier <david.wengier@microsoft.com>
2019-09-16 18:09:32 +1000

    Hide all Windows Forms templates from non-Windows Forms projects

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/WindowsForms/WindowsFormsAddItemFilter.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.WindowsForms
+{
+    /// <summary>
+    /// A filter for the Add New Item dialog that filters out Windows Forms items from non-Windows Forms projects.
+    /// </summary>
+    [ExportProjectNodeComService(typeof(IVsFilterAddProjectItemDlg))]
+    [AppliesTo("!" + ProjectCapability.WindowsForms)]
+    internal class WindowsFormsAddItemFilter : IVsFilterAddProjectItemDlg
+    {
+        public int FilterTreeItemByLocalizedName(ref Guid rguidProjectItemTemplates, string pszLocalizedName, out int pfFilter)
+        {
+            pfFilter = 0;
+            return HResult.NotImplemented;
+        }
+
+        public int FilterTreeItemByTemplateDir(ref Guid rguidProjectItemTemplates, string pszTemplateDir, out int pfFilter)
+        {
+            pfFilter = 0;
+            // Most of the templates for Windows Forms items are filtered by capabilities in the .vstemplate but there are a couple
+            // that use an older .vsz tmeplate format that doesn't support capabilities, so we filter them out here.
+            // The AppliesTo on this class ensures this code only runs for Windows Forms projects
+            if (pszTemplateDir.EndsWith("\\Windows Forms", StringComparisons.Paths))
+            {
+                pfFilter = 1;
+            }
+            return HResult.OK;
+        }
+
+        public int FilterListItemByLocalizedName(ref Guid rguidProjectItemTemplates, string pszLocalizedName, out int pfFilter)
+        {
+            pfFilter = 0;
+            return HResult.NotImplemented;
+        }
+
+        public int FilterListItemByTemplateFile(ref Guid rguidProjectItemTemplates, string pszTemplateFile, out int pfFilter)
+        {
+            pfFilter = 0;
+            return HResult.NotImplemented;
+        }
+    }
+}
---
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/WindowsForms/WindowsFormsAddItemFilterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.WindowsForms
+{
+    public class WindowsFormsAddItemFilterTests
+    {
+        [Theory]
+        [InlineData(new object[] { @"C:\Program Files\Visual Studio\Templates\Non-Windows Forms", false })]
+        [InlineData(new object[] { @"C:\Program Files\Visual Studio\Templates\General", false })]
+        [InlineData(new object[] { @"C:\Program Files\Visual Studio\Extensions\dh64yf.343\ProjectTemplates\CSharp", false })]
+        [InlineData(new object[] { @"C:\Program Files\Visual Studio\Templates\Windows Forms", true })]
+        public void FiltersItemTemplateFolders(string templateDir, bool shouldBeFiltered)
+        {
+            var filter = new WindowsFormsAddItemFilter();
+
+            Guid guid = Guid.Empty;
+            var result = filter.FilterTreeItemByTemplateDir(ref guid, templateDir, out int filterResult);
+
+            Assert.Equal(0, result);
+            Assert.Equal(shouldBeFiltered, (filterResult == 1));
+        }
+    }
+}
