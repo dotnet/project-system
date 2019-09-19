3e45771423e04a18b331065cc1bf1a30f939372d
Drew Noakes <git@drewnoakes.com>
2019-09-19 19:40:12 +1000

    Suppress "Open in Object Browser" for NuGet packages
    There's nothing obviously correct to show for NuGet packages themselves in the Object Browser.
    
    Instead we should in future allow the Object Browser to be launched from child nodes representing assemblies within the packages themselves (the other part of #1256).
    
    Legacy doesn't offer Open in Object Browser for packages either, though if using `packages.config` then package assemblies are added directly as references and do work with the Object Browser.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/DependenciesNodeCommandGroupHandler.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies
+{
+    /// <summary>
+    /// Command handler that provides special handling for standard commands on dependency nodes.
+    /// </summary>
+    /// <remarks>
+    /// CPS provides a handler for the same commands, however it does not know anything special about dependency nodes.
+    /// </remarks>
+    [ExportCommandGroup(VSConstants.CMDSETID.StandardCommandSet2K_string)]
+    [AppliesTo(ProjectCapability.DependenciesTree)]
+    [Order(10)]
+    internal sealed class DependenciesNodeCommandGroupHandler : ICommandGroupHandler
+    {
+        private static readonly CommandStatusResult s_suppressedResult = new CommandStatusResult(handled: true, null, CommandStatus.NotSupported | CommandStatus.Invisible);
+
+        public CommandStatusResult GetCommandStatus(IImmutableSet<IProjectTree> nodes, long commandId, bool focused, string commandText, CommandStatus progressiveStatus)
+        {
+            var cmdId = (VSConstants.VSStd2KCmdID)commandId;
+
+            switch (cmdId)
+            {
+                case VSConstants.VSStd2KCmdID.QUICKOBJECTSEARCH: // Open in Object Browser
+                {
+                    if (nodes.Count == 1)
+                    {
+                        IProjectTree tree = nodes.First();
+
+                        if (tree.Flags.Contains(DependencyTreeFlags.NuGetPackageDependency))
+                        {
+                            // Suppress "Open in Object Browser" for NuGet packages
+                            return s_suppressedResult;
+                        }
+                    }
+
+                    break;
+                }
+            }
+
+            return CommandStatusResult.Unhandled;
+        }
+
+        public bool TryHandleCommand(IImmutableSet<IProjectTree> nodes, long commandId, bool focused, long commandExecuteOptions, IntPtr variantArgIn, IntPtr variantArgOut)
+        {
+            return false;
+        }
+    }
+}
