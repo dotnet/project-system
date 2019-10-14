4f065b74f16164c6f98ce5f0f5cec8a520ae226d
Tom Meschter <tom.meschter@microsoft.com>
2019-10-14 13:00:32 -0700

    Replace another ArrayList
    Replace an `ArrayList` with the equivalent `List(Of Resource)`. Again, this also means updating `ResourceComparer` to implement `IComparer(Of Resource)` instead of just `IComparer`.

--- src/Microsoft.VisualStudio.Editors/ResourceEditor/ResourceComparer.vb
+++ src/Microsoft.VisualStudio.Editors/ResourceEditor/ResourceComparer.vb
@@ -8,19 +8,19 @@ Imports System.Globalization
 Namespace Microsoft.VisualStudio.Editors.ResourceEditor
 
     ''' <summary>
-    ''' This is an Icomparer implementation used to sort Resources for UI purposes (ResourceListView and
+    ''' This is an <see cref="IComparer(Of T)"/> implementation used to sort <see cref="Resource"/>s for UI purposes (ResourceListView and
     '''    ResourceStringTable).
     ''' </summary>
     ''' <remarks></remarks>
     Friend NotInheritable Class ResourceComparer
-        Implements IComparer
+        Implements IComparer(Of Resource)
 
         ''' <summary>
-        ''' Sorts an ArrayList of Resources for UI purposes
+        ''' Sorts a <see cref="List(Of Resource)"/> for UI purposes
         ''' </summary>
-        ''' <param name="Resources">ArrayList of Resources to source (will be sorted in place)</param>
+        ''' <param name="Resources"><see cref="List(Of Resource)"/> to sort (will be sorted in place)</param>
         ''' <remarks></remarks>
-        Public Shared Sub SortResources(Resources As ArrayList)
+        Public Shared Sub SortResources(Resources As List(Of Resource))
             Resources.Sort(New ResourceComparer)
         End Sub
 
@@ -31,13 +31,9 @@ Namespace Microsoft.VisualStudio.Editors.ResourceEditor
         ''' <param name="y">Second object to compare.</param>
         ''' <returns>-1, 0 or 1, depending on whether x is less than, equal to or greater than y, respectively.</returns>
         ''' <remarks>This function gets called by ArrayList.Sort for each pair of resources to be sorted.</remarks>
-        Private Function Compare(x As Object, y As Object) As Integer Implements IComparer.Compare
-            Debug.Assert(TypeOf x Is Resource AndAlso TypeOf y Is Resource, "ResourceComparer: expected Resources")
-            Dim Resource1 As Resource = DirectCast(x, Resource)
-            Dim Resource2 As Resource = DirectCast(y, Resource)
-
+        Private Function Compare(x As Resource, y As Resource) As Integer Implements IComparer(Of Resource).Compare
             'We currently only support sorting alphabetically according to Name.
-            Return String.Compare(Resource1.Name, Resource2.Name, ignoreCase:=True, culture:=CultureInfo.CurrentUICulture)
+            Return String.Compare(x.Name, y.Name, ignoreCase:=True, culture:=CultureInfo.CurrentUICulture)
         End Function
     End Class
 
