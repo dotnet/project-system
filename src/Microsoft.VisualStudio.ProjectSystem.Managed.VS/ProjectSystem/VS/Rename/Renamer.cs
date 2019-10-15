951e451074bd61a89e7718245ca2b8360148737c
Jonathon Marolf <jmarolf@microsoft.com>
2019-10-14 17:03:55 -0700

    use a more clear negation style

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Rename/Renamer.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Rename/Renamer.cs
@@ -91,7 +91,8 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Rename
                 return;
 
             // Ask if the user wants to rename the symbol
-            if (await CheckUserConfirmation(oldName)) != true)
+            bool userWantsToRenameSymbol = await CheckUserConfirmation(oldName);
+            if (!userWantsToRenameSymbol)
                 return;
 
             // Try and apply the changes to the current solution
