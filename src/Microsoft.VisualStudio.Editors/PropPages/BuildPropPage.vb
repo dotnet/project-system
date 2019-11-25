dotnet/project-system

commit cb72e7b0c477dfa19afb72e899cc6244a85d555e

Use string.Equals over string.Compare
The former is clearer in code, and is faster in execution.

src/Microsoft.VisualStudio.Editors/PropPages/BuildPropPage.vb
@@ -472,7 +472,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
                     '// convert to the one with the space for this specific case
 
                     '// Convert the no-space to one with a space
-                    If (String.Compare(strPlatform, "AnyCPU", StringComparison.Ordinal) = 0) Then
+                    If (String.Equals(strPlatform, "AnyCPU", StringComparison.Ordinal)) Then
                         strPlatform = "Any CPU"
                     End If
 
@@ -486,7 +486,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
                             '// platform target to the user.
 
                             Dim stValue As String = TryCast(value, String)
-                            If (String.Compare(Trim(stValue), "Itanium", StringComparison.Ordinal) = 0) Then
+                            If (String.Equals(Trim(stValue), "Itanium", StringComparison.Ordinal)) Then
                                 cboPlatformTarget.Items.Add("Itanium")
                                 cboPlatformTarget.SelectedItem = stValue
                             Else
@@ -581,9 +581,9 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
                                 If (Path.IsPathRooted(stOutputPath)) Then
                                     '// stOutputPath is an Absolute path so check to see if its within the project path
 
-                                    If (String.Compare(Path.GetFullPath(stProjectDirectory),
-                                                       VisualBasic.Left(Path.GetFullPath(stOutputPath), Len(stProjectDirectory)),
-                                                       StringComparison.Ordinal) = 0) Then
+                                    If (String.Equals(Path.GetFullPath(stProjectDirectory),
+                                                      VisualBasic.Left(Path.GetFullPath(stOutputPath), Len(stProjectDirectory)),
+                                                      StringComparison.Ordinal)) Then
 
                                         '// The output path is within the project so suggest the output directory (or suggest just the filename
                                         '// which will put it in the default location
@@ -971,7 +971,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
 
                 If (Not (IsNothing(rgConstants))) Then
                     For Each stTemp In rgConstants
-                        If (String.Compare(Trim(stTemp), stSymbol, StringComparison.Ordinal) = 0) Then
+                        If (String.Equals(Trim(stTemp), stSymbol, StringComparison.Ordinal)) Then
                             bFound = True
                             Exit For
                         End If
@@ -1009,7 +1009,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
 
                 If (Not (IsNothing(rgConstants))) Then
                     For Each stTemp In rgConstants
-                        If (String.Compare(Trim(stTemp), stSymbol, StringComparison.Ordinal) = 0) Then
+                        If (String.Equals(Trim(stTemp), stSymbol, StringComparison.Ordinal)) Then
                             Return True
                         End If
                     Next
@@ -1035,7 +1035,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
 
                 If (Not (IsNothing(rgConstants))) Then
                     For Each stTemp In rgConstants
-                        If (String.Compare(Trim(stTemp), stSymbol, StringComparison.Ordinal) <> 0) Then
+                        If (String.Equals(Trim(stTemp), stSymbol, StringComparison.Ordinal)) Then
                             If (stNewConstants <> "") Then
                                 stNewConstants += ";"
                             End If

src/Microsoft.VisualStudio.AppDesigner/ApplicationDesigner/ApplicationDesignerView.vb
@@ -743,7 +743,7 @@ Namespace Microsoft.VisualStudio.Editors.ApplicationDesigner
                         Dim FileName As String = DirectCast(AppDesignerItems(Index - PropertyPages.Length), String)
 
                         .EditFlags = CUInt(_VSRDTFLAGS.RDT_DontAddToMRU)
-                        If String.Compare(VisualBasic.Right(FileName, 5), ".resx", StringComparison.OrdinalIgnoreCase) = 0 Then
+                        If String.Equals(VisualBasic.Right(FileName, 5), ".resx", StringComparison.OrdinalIgnoreCase) Then
                             'Add .resx file with a known editor so user config cannot change
                             .EditorGuid = New Guid(My.Resources.Designer.ResourceEditorFactory_GUID)
                             .EditorCaption = My.Resources.Designer.APPDES_ResourceTabTitle
@@ -760,7 +760,7 @@ Namespace Microsoft.VisualStudio.Editors.ApplicationDesigner
                             Else
                                 .CustomViewProvider = New SpecialFileCustomViewProvider(Me, DesignerPanel, __PSFFILEID2.PSFFILEID_AssemblyResource, My.Resources.Designer.APPDES_ClickHereCreateResx)
                             End If
-                        ElseIf String.Compare(VisualBasic.Right(FileName, 9), ".settings", StringComparison.OrdinalIgnoreCase) = 0 Then
+                        ElseIf String.Equals(VisualBasic.Right(FileName, 9), ".settings", StringComparison.OrdinalIgnoreCase) Then
                             'Add .settings file with a known editor so user config cannot change
                             .EditorGuid = New Guid(My.Resources.Designer.SettingsDesignerEditorFactory_GUID)
                             .EditorCaption = My.Resources.Designer.APPDES_SettingsTabTitle

src/Microsoft.VisualStudio.AppDesigner/PropPages/PropPageUserControlBase.vb
@@ -3387,7 +3387,7 @@ NextControl:
             End If
 
             ' Remove the project directory path
-            If String.Compare(BasePath, Strings.Left(DirectoryPath, Len(BasePath)), StringComparison.OrdinalIgnoreCase) = 0 Then
+            If String.Equals(BasePath, Strings.Left(DirectoryPath, Len(BasePath)), StringComparison.OrdinalIgnoreCase) Then
                 Dim ch As Char = CChar(Mid(DirectoryPath, Len(BasePath), 1))
                 If ch = Path.DirectorySeparatorChar OrElse ch = Path.AltDirectorySeparatorChar Then
                     RelativePath = Mid(DirectoryPath, Len(BasePath) + 1)
@@ -3473,7 +3473,7 @@ NextControl:
         ''' <returns></returns>
         ''' <remarks></remarks>
         Public Function IsVBProject() As Boolean
-            Return String.Compare(ProjectLanguage, EnvDTE.CodeModelLanguageConstants.vsCMLanguageVB, StringComparison.OrdinalIgnoreCase) = 0
+            Return String.Equals(ProjectLanguage, EnvDTE.CodeModelLanguageConstants.vsCMLanguageVB, StringComparison.OrdinalIgnoreCase)
         End Function
 
         ''' <summary>
@@ -3482,7 +3482,7 @@ NextControl:
         ''' <returns></returns>
         ''' <remarks></remarks>
         Public Function IsCSProject() As Boolean
-            Return String.Compare(ProjectLanguage, EnvDTE.CodeModelLanguageConstants.vsCMLanguageCSharp, StringComparison.OrdinalIgnoreCase) = 0
+            Return String.Equals(ProjectLanguage, EnvDTE.CodeModelLanguageConstants.vsCMLanguageCSharp, StringComparison.OrdinalIgnoreCase)
         End Function
 
 #End Region

src/Microsoft.VisualStudio.Editors/Common/DTEUtils.vb
@@ -231,7 +231,7 @@ Namespace Microsoft.VisualStudio.Editors.Common
                     projectItem.FileCount > 0 Then
 
                     Dim itemFileName As String = Path.GetFileName(projectItem.FileNames(1))
-                    If String.Compare(fileName, itemFileName, StringComparison.OrdinalIgnoreCase) = 0 Then
+                    If String.Equals(fileName, itemFileName, StringComparison.OrdinalIgnoreCase) Then
                         Return projectItem
                     End If
                 End If

src/Microsoft.VisualStudio.Editors/Common/Utils.vb
@@ -927,8 +927,8 @@ Namespace Microsoft.VisualStudio.Editors.Common
 
             ' If the 2 paths have different root paths, return Path. 
             ' It's harder to deal with UNC root path in the algorithm.
-            If String.Compare(IO.Path.GetPathRoot(BaseDirectory), IO.Path.GetPathRoot(Path),
-                    StringComparison.OrdinalIgnoreCase) <> 0 Then
+            If Not String.Equals(IO.Path.GetPathRoot(BaseDirectory), IO.Path.GetPathRoot(Path),
+                    StringComparison.OrdinalIgnoreCase) Then
                 Return RemoveEndingSeparator(Path)
             End If
 
@@ -1574,7 +1574,7 @@ Namespace Microsoft.VisualStudio.Editors.Common
             Dim frameworkName As FrameworkName = Nothing
             If TryGetTargetFrameworkMoniker(hierarchy, frameworkName) Then
                 ' Verify that we are targeting .NET
-                If String.Compare(frameworkName.Identifier, ".NETFramework", StringComparison.OrdinalIgnoreCase) <> 0 Then
+                If Not String.Equals(frameworkName.Identifier, ".NETFramework", StringComparison.OrdinalIgnoreCase) Then
                     Return False
                 End If
 
@@ -1610,7 +1610,7 @@ Namespace Microsoft.VisualStudio.Editors.Common
         ''' <returns>Value indicating whether the specified framework is .Net Core</returns>
         Friend Function IsTargetingDotNetCore(frameworkIdentifier As String) As Boolean
 
-            Return String.Compare(frameworkIdentifier, ".NETCoreApp", StringComparison.OrdinalIgnoreCase) = 0
+            Return String.Equals(frameworkIdentifier, ".NETCoreApp", StringComparison.OrdinalIgnoreCase)
 
         End Function
 

src/Microsoft.VisualStudio.Editors/PropPages/AdvBuildSettingsPropPage.vb
@@ -211,7 +211,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
             Dim StringValue As String = Trim(control.Text)
 
             'DLL Baseaddress must be 0xNNNNNNNN format
-            If String.Compare(VBStrings.Left(StringValue, 2), "0x", StringComparison.OrdinalIgnoreCase) = 0 Then
+            If String.Equals(VBStrings.Left(StringValue, 2), "0x", StringComparison.OrdinalIgnoreCase) Then
                 StringValue = "&h" + Mid(StringValue, 3)
                 If IsNumeric(StringValue) Then
                     Dim LongValue As ULong

src/Microsoft.VisualStudio.Editors/PropPages/AdvCompilerSettingsPropPage.vb
@@ -128,7 +128,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
             Dim StringValue As String = Trim(control.Text)
 
             'DLL Baseaddress must be &Hxxxxxxxx format
-            If String.Compare(VBStrings.Left(StringValue, 2), "&H", StringComparison.OrdinalIgnoreCase) = 0 AndAlso IsNumeric(StringValue) Then
+            If String.Equals(VBStrings.Left(StringValue, 2), "&H", StringComparison.OrdinalIgnoreCase) AndAlso IsNumeric(StringValue) Then
                 Try
                     Dim LongValue As ULong = CULng(StringValue)
                     If LongValue < UInteger.MaxValue Then
@@ -179,7 +179,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
                     '// Need to special case pdb-only because it's stored in the property without the dash but it's
                     '// displayed in the dialog with a dash.
 
-                    If (String.Compare(stValue, "pdbonly", StringComparison.OrdinalIgnoreCase) <> 0) Then
+                    If Not String.Equals(stValue, "pdbonly", StringComparison.OrdinalIgnoreCase) Then
                         DebugInfoComboBox.Text = stValue
                     Else
                         DebugInfoComboBox.Text = "pdb-only"
@@ -234,7 +234,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
             If StringValue = "" Then
                 DllBaseTextbox.Text = DEFAULT_DLLBASEADDRESS
 
-            ElseIf String.Compare(VBStrings.Left(StringValue, 2), "&H", StringComparison.OrdinalIgnoreCase) = 0 AndAlso IsNumeric(StringValue) Then
+            ElseIf String.Equals(VBStrings.Left(StringValue, 2), "&H", StringComparison.OrdinalIgnoreCase) AndAlso IsNumeric(StringValue) Then
                 Dim LongValue As ULong = CULng(StringValue)
                 If LongValue < UInteger.MaxValue Then
                     'Reformat into clean

src/Microsoft.VisualStudio.Editors/PropPages/ApplicationPropPageInternalBase.vb
@@ -246,7 +246,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
             Else
                 Dim frameworkName As New FrameworkName(currentTarget.Moniker)
 
-                If String.Compare(frameworkName.Identifier, ".NETFramework", StringComparison.Ordinal) = 0 Then
+                If String.Equals(frameworkName.Identifier, ".NETFramework", StringComparison.Ordinal) Then
                     bindingRedirectsCheckBox.Visible = True
                 Else
                     bindingRedirectsCheckBox.Visible = False

commit 10ecf85325e806ea13fd67c9a231fae6a9d0a63e

Fix incorrect symbol removal logic
Bug was introduced in https://github.com/dotnet/project-system/commit/cb72e7b0

src/Microsoft.VisualStudio.Editors/PropPages/BuildPropPage.vb
@@ -1034,7 +1034,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
 
                 If (Not (IsNothing(rgConstants))) Then
                     For Each stTemp In rgConstants
-                        If (String.Equals(Trim(stTemp), stSymbol, StringComparison.Ordinal)) Then
+                        If Not String.Equals(Trim(stTemp), stSymbol, StringComparison.Ordinal) Then
                             If (stNewConstants <> "") Then
                                 stNewConstants += ";"
                             End If