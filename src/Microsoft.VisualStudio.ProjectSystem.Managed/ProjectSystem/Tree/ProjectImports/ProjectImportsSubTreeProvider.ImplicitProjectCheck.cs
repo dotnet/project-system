c58940eecd2d73db0321a677bebae71109d0feae
Drew Noakes <git@drewnoakes.com>
2019-12-03 18:53:59 +1100

    Identify implicit files in non-default VS install dir

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Tree/ProjectImports/ProjectImportsSubTreeProvider.ImplicitProjectCheck.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Tree/ProjectImports/ProjectImportsSubTreeProvider.ImplicitProjectCheck.cs
@@ -8,10 +8,18 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tree.ProjectImports
     {
         private sealed class ImplicitProjectCheck
         {
+            private readonly string _windows;
             private readonly string _programFiles86;
             private readonly string? _programFiles64;
-            private readonly string _windows;
+            private readonly string? _vsInstallationDirectory;
 
+            /// <summary>
+            /// Gets and sets the <c>MSBuildProjectExtensionsPath</c> property value for this project.
+            /// Project files under this folder are considered implicit.
+            /// </summary>
+            /// <remarks>
+            /// Example value: <c>"C:\repos\MySolution\MyProject\obj\"</c>
+            /// </remarks>
             public string? ProjectExtensionsPath { get; set; }
 
             public ImplicitProjectCheck()
@@ -22,6 +30,29 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tree.ProjectImports
                 // In a 32-bit process, SpecialFolder.ProgramFiles returns the 32-bit path.
                 // The 64-bit path is available via an environment variable however.
                 _programFiles64 = Environment.GetEnvironmentVariable("ProgramW6432");
+
+                _vsInstallationDirectory = GetVSInstallationDirectory();
+
+                return;
+
+                static string? GetVSInstallationDirectory()
+                {
+                    string? dir = Environment.GetEnvironmentVariable("VSAPPIDDIR");
+
+                    // The path provided is not the installation root, but rather the location of devenv.exe.
+                    // __VSSPROPID.VSSPROPID_InstallDirectory has the same value.
+                    // Failing a better way to obtain the installation root, remove that suffix.
+                    // Obviously this is brittle against changes to the relative path of devenv.exe, however that seems
+                    // unlikely and should be easy to work around if ever needed.
+                    const string DevEnvExeRelativePath = "Common7\\IDE\\";
+
+                    if (dir != null && dir.EndsWith(DevEnvExeRelativePath))
+                    {
+                        dir = dir.Substring(0, dir.Length - DevEnvExeRelativePath.Length);
+                    }
+
+                    return dir;
+                }
             }
 
             public bool IsImplicit(string importPath)
@@ -29,7 +60,8 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tree.ProjectImports
                 return (_programFiles64 != null && importPath.StartsWith(_programFiles64, StringComparisons.Paths))
                     || importPath.StartsWith(_programFiles86, StringComparisons.Paths)
                     || importPath.StartsWith(_windows, StringComparisons.Paths)
-                    || (ProjectExtensionsPath != null && importPath.StartsWith(ProjectExtensionsPath, StringComparisons.Paths));
+                    || (ProjectExtensionsPath != null && importPath.StartsWith(ProjectExtensionsPath, StringComparisons.Paths)
+                    || (_vsInstallationDirectory != null && importPath.StartsWith(_vsInstallationDirectory, StringComparisons.Paths)));
             }
         }
     }
