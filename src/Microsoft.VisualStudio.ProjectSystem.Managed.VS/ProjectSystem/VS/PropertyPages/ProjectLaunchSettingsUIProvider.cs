cc4b79a187bd9e2e52937cf05273c7f9fa39c3e4
Drew Noakes <git@drewnoakes.com>
2020-01-13 12:37:48 +1100

    Consolidate duplicated documentation

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/PropertyPages/ProjectLaunchSettingsUIProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/PropertyPages/ProjectLaunchSettingsUIProvider.cs
@@ -14,42 +14,26 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.PropertyPages
     [Order(Order.Lowest)]              // Lowest priority to allow this to be overridden
     internal class ProjectLaunchSettingsUIProvider : ILaunchSettingsUIProvider
     {
-        /// <summary>
-        /// Required to control the MEF scope
-        /// </summary>
         [ImportingConstructor]
-        public ProjectLaunchSettingsUIProvider(UnconfiguredProject uncProject)
+        public ProjectLaunchSettingsUIProvider(UnconfiguredProject _) // force MEF scope
         {
         }
 
-        /// <summary>
-        /// The name of the command that is written to the launchSettings.json file
-        /// </summary>
         public string CommandName => LaunchSettingsProvider.RunProjectCommandName;
 
-        /// <summary>
-        /// The name to display in the dropdown for this command
-        /// </summary>
         public string FriendlyName => PropertyPageResources.ProfileKindProjectName;
 
-        /// <summary>
-        /// Disable the executable and launch url controls
-        /// </summary>
         public bool ShouldEnableProperty(string propertyName)
         {
+            // Disable the executable and launch url controls
             return !string.Equals(propertyName, UIProfilePropertyName.Executable, StringComparisons.UIPropertyNames) &&
                    !string.Equals(propertyName, UIProfilePropertyName.LaunchUrl, StringComparisons.UIPropertyNames);
         }
 
-        /// <summary>
-        /// No custom UI
-        /// </summary>
+        /// <inheritdoc />
+        /// <remarks>This implementation does not provide any UI.</remarks>
         public UserControl? CustomUI => null;
 
-        /// <summary>
-        /// Called when the selected profile changes to a profile which matches this command. curSettings will contain 
-        /// the current values from the page, and activeProfile will point to the active one.
-        /// </summary>
         public void ProfileSelected(IWritableLaunchSettings curSettings)
         {
         }
