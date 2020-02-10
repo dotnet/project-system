31fb978d94fc83b181378fbfce272ad5a533c781
David Kean <davkean@microsoft.com>
2020-02-10 14:36:31 +1100

    Remove unneeded usings
    Also moved mock -> correct namespace.

--- tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.Client.UnitTests/Mocks/WorkspaceVisualNodeBaseFactory.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.Client.UnitTests/Mocks/WorkspaceVisualNodeBaseFactory.cs
@@ -1,9 +1,8 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
-using Microsoft.VisualStudio.Workspace.VSIntegration.UI;
 
-namespace Microsoft.VisualStudio.Mocks
+namespace Microsoft.VisualStudio.Workspace.VSIntegration.UI
 {
     internal static class WorkspaceVisualNodeBaseFactory
     {
