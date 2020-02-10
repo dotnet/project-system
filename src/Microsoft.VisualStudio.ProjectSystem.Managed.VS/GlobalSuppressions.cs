10a5afc4a894024934f97c99fe10de247cb2ba3a
David Kean <davkean@microsoft.com>
2020-02-10 14:45:46 +1100

    Suppress naming warning due to bug

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/GlobalSuppressions.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/GlobalSuppressions.cs
@@ -1,4 +1,6 @@
-﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/1432", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.EditAndContinue.EditAndContinueProvider.Dispose")]
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/1432", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.EditAndContinue.EditAndContinueProvider.Dispose")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/1432", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.PropertyPages.BuildMacroInfo.Dispose")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/1432", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.References.DesignTimeAssemblyResolution.Dispose")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD110:Observe result of async calls", Justification = "https://github.com/dotnet/project-system/issues/3921", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.PropertyPages.PropertyPage.SetObjects(System.UInt32,System.Object[])")]
@@ -13,3 +15,5 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/2416", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.PackageRestore.ProjectAssetFileWatcher.ProjectAssetFileWatcherInstance.GetFileHashOrNull(System.String)~System.Byte[]")]
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2215:Dispose methods should call base class dispose", Justification = "<Pending>", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.PackageRestore.PackageRestoreService.DisposeCoreAsync(System.Boolean)~System.Threading.Tasks.Task")]
+
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "https://github.com/dotnet/roslyn/issues/41531", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.Automation.VSProject.get_Extender(System.String)~System.Object")]
