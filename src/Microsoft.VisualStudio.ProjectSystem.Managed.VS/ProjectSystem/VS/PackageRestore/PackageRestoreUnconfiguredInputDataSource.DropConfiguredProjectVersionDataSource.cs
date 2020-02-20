bc8b2c505ee70f1fe79ff26b63525b201b7445fc
Lifeng Lu <lifengl@microsoft.com>
2020-02-20 13:05:47 -0800

    Create a customized dataflow block to remove configuration versions.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/PackageRestore/PackageRestoreUnconfiguredInputDataSource.DropConfiguredProjectVersionDataSource.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/PackageRestore/PackageRestoreUnconfiguredInputDataSource.DropConfiguredProjectVersionDataSource.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.PackageRestore
@@ -12,32 +15,138 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.PackageRestore
         ///     <see cref="ProjectDataSources.ConfiguredProjectVersion"/> versions from each value of the original
         ///     <see cref="IProjectValueDataSource{T}"/>.
         /// </summary>
-        private sealed class DropConfiguredProjectVersionDataSource<T> : ChainedProjectValueDataSourceBase<T>
+        private sealed class DropConfiguredProjectVersionDataSource<T> : IProjectValueDataSource<T>
             where T : class
         {
             private readonly IProjectValueDataSource<T> _dataSource;
 
-            public DropConfiguredProjectVersionDataSource(IProjectServices commonServices, IProjectValueDataSource<T> dataSource)
-                : base(commonServices, synchronousDisposal: true, registerDataSource: false)
+            public IReceivableSourceBlock<IProjectVersionedValue<T>> SourceBlock => new DropConfiguredProjectVersionPropagator(_dataSource.SourceBlock);
+
+            public NamedIdentity? DataSourceKey => null;
+
+            public IComparable? DataSourceVersion => null;
+
+            ISourceBlock<IProjectVersionedValue<object>> IProjectValueDataSource.SourceBlock => new DropConfiguredProjectVersionPropagator(_dataSource.SourceBlock);
+
+            public DropConfiguredProjectVersionDataSource(IProjectValueDataSource<T> dataSource)
             {
                 _dataSource = dataSource;
             }
 
-            protected override IDisposable LinkExternalInput(ITargetBlock<IProjectVersionedValue<T>> targetBlock)
+            public IDisposable? Join()
             {
-                DisposableValue<ISourceBlock<IProjectVersionedValue<T>>> block = _dataSource.SourceBlock.Transform(DropConfiguredProjectVersion);
-
-                block.Value.LinkTo(targetBlock, DataflowOption.PropagateCompletion);
-
-                JoinUpstreamDataSources(_dataSource);
-
-                return block;
+                return _dataSource.Join();
             }
 
-            private static IProjectVersionedValue<T> DropConfiguredProjectVersion(IProjectVersionedValue<T> data)
+            private class DropConfiguredProjectVersionPropagator : IPropagatorBlock<IProjectVersionedValue<T>, IProjectVersionedValue<T>>, IReceivableSourceBlock<IProjectVersionedValue<T>>
             {
-                return new ProjectVersionedValue<T>(data.Value, data.DataSourceVersions.Remove(ProjectDataSources.ConfiguredProjectIdentity)
-                                                                                       .Remove(ProjectDataSources.ConfiguredProjectVersion));
+                private readonly IReceivableSourceBlock<IProjectVersionedValue<T>> _source;
+                private ITargetBlock<IProjectVersionedValue<T>>? _target;
+
+                public DropConfiguredProjectVersionPropagator(IReceivableSourceBlock<IProjectVersionedValue<T>> source)
+                {
+                    _source = source;
+                }
+
+                public Task Completion => _source.Completion;
+
+                public void Complete()
+                {
+                    _target?.Complete();
+                }
+
+#pragma warning disable CS8613 // Nullability of reference types in return type doesn't match implicitly implemented member.
+                public IProjectVersionedValue<T>? ConsumeMessage(DataflowMessageHeader messageHeader, ITargetBlock<IProjectVersionedValue<T>> target, out bool messageConsumed)
+#pragma warning restore CS8613 // Nullability of reference types in return type doesn't match implicitly implemented member.
+                {
+                    IProjectVersionedValue<T>? input = _source.ConsumeMessage(messageHeader, this, out messageConsumed);
+                    if (messageConsumed && input != null)
+                    {
+                        return DropConfiguredProjectVersion(input);
+                    }
+
+                    return null;
+                }
+
+                public void Fault(Exception exception)
+                {
+                    _target?.Fault(exception);
+                }
+
+                public IDisposable LinkTo(ITargetBlock<IProjectVersionedValue<T>> target, DataflowLinkOptions linkOptions)
+                {
+                    Requires.NotNull(target, nameof(target));
+                    Interlocked.CompareExchange(ref _target, target, null);
+
+                    if (_target != target)
+                    {
+                        throw new NotSupportedException();
+                    }
+
+                    return _source.LinkTo(this, linkOptions);
+                }
+
+                public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, IProjectVersionedValue<T> messageValue, ISourceBlock<IProjectVersionedValue<T>>? source, bool consumeToAccept)
+                {
+                    IProjectVersionedValue<T> data;
+                    try
+                    {
+                        data = DropConfiguredProjectVersion(messageValue);
+                    }
+                    catch (Exception ex)
+                    {
+                        Fault(ex);
+                        return DataflowMessageStatus.DecliningPermanently;
+                    }
+
+                    return _target!.OfferMessage(messageHeader, data, this, consumeToAccept);
+                }
+
+                public void ReleaseReservation(DataflowMessageHeader messageHeader, ITargetBlock<IProjectVersionedValue<T>> target)
+                {
+                    _source.ReleaseReservation(messageHeader, this);
+                }
+
+                public bool ReserveMessage(DataflowMessageHeader messageHeader, ITargetBlock<IProjectVersionedValue<T>> target)
+                {
+                    return _source.ReserveMessage(messageHeader, this);
+                }
+
+                private static IProjectVersionedValue<T> DropConfiguredProjectVersion(IProjectVersionedValue<T> data)
+                {
+                    return new ProjectVersionedValue<T>(data.Value, data.DataSourceVersions.Remove(ProjectDataSources.ConfiguredProjectIdentity)
+                                                                                           .Remove(ProjectDataSources.ConfiguredProjectVersion));
+                }
+
+#pragma warning disable CS8614 // Nullability of reference types in type of parameter doesn't match implicitly implemented member.
+                public bool TryReceive(Predicate<IProjectVersionedValue<T>>? filter, out IProjectVersionedValue<T>? item)
+#pragma warning restore CS8614 // Nullability of reference types in type of parameter doesn't match implicitly implemented member.
+                {
+                    if (_source.TryReceive(
+                        filter == null ? filter : (x => filter!(DropConfiguredProjectVersion(x))),
+                        out item))
+                    {
+                        item = DropConfiguredProjectVersion(item);
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                public bool TryReceiveAll(out IList<IProjectVersionedValue<T>> items)
+                {
+                    if (_source.TryReceiveAll(out items))
+                    {
+                        for (int i = 0; i < items.Count; i++)
+                        {
+                            items[i] = DropConfiguredProjectVersion(items[i]);
+                        }
+
+                        return true;
+                    }
+
+                    return false;
+                }
             }
         }
     }
