6dd795fd4c1910fbeaf266f794864272de603400
Drew Noakes <git@drewnoakes.com>
2020-03-03 14:36:51 +1100

    Configure new dependencies node context menus

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/Commands/ReferenceManagerCommandHandler.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.ProjectSystem.References;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Commands
+{
+    /// <summary>
+    /// Enables commands for launching Reference Manager at a specific page.
+    /// </summary>
+    [ExportCommandGroup(VSConstants.CMDSETID.StandardCommandSet16_string)]
+    [AppliesTo(ProjectCapability.DependenciesTree)]
+    [Order(ProjectSystem.Order.Default)]
+    internal sealed class ReferenceManagerCommandHandler : ICommandGroupHandler
+    {
+        public const int CmdidAddAssemblyReference       = 0x200;
+        public const int CmdidAddComReference            = 0x201;
+        public const int CmdidAddProjectReference        = 0x202;
+        public const int CmdidAddSharedProjectReference  = 0x203;
+        public const int CmdidAddWinRTReference          = 0x204;
+
+        private readonly UnconfiguredProject _unconfiguredProject;
+        private readonly IReferencesUI _referencesUI;
+
+        [ImportingConstructor]
+        public ReferenceManagerCommandHandler(UnconfiguredProject unconfiguredProject, IReferencesUI referencesUI)
+        {
+            Requires.NotNull(unconfiguredProject, nameof(unconfiguredProject));
+            Requires.NotNull(referencesUI, nameof(referencesUI));
+
+            _unconfiguredProject = unconfiguredProject;
+            _referencesUI = referencesUI;
+        }
+
+        public CommandStatusResult GetCommandStatus(IImmutableSet<IProjectTree> items, long commandId, bool focused, string? commandText, CommandStatus status)
+        {
+            bool enable = commandId switch
+            {
+                CmdidAddAssemblyReference      => _unconfiguredProject.Capabilities.AppliesTo(ProjectCapabilities.AssemblyReferences),
+                CmdidAddComReference           => _unconfiguredProject.Capabilities.AppliesTo(ProjectCapabilities.ComReferences),
+                CmdidAddProjectReference       => _unconfiguredProject.Capabilities.AppliesTo(ProjectCapabilities.ProjectReferences),
+                CmdidAddSharedProjectReference => _unconfiguredProject.Capabilities.AppliesTo(ProjectCapabilities.SharedProjectReferences),
+                CmdidAddWinRTReference         => _unconfiguredProject.Capabilities.AppliesTo(ProjectCapabilities.WinRTReferences) && _unconfiguredProject.Capabilities.AppliesTo(ProjectCapabilities.SdkReferences),
+                _ => false
+            };
+
+            if (enable)
+            {
+                status &= ~CommandStatus.Invisible;
+                status |= CommandStatus.Enabled | CommandStatus.Supported;
+                return new CommandStatusResult(handled: true, commandText, status);
+            }
+
+            return CommandStatusResult.Unhandled;
+        }
+
+        public bool TryHandleCommand(IImmutableSet<IProjectTree> items, long commandId, bool focused, long commandExecuteOptions, IntPtr variantArgIn, IntPtr variantArgOut)
+        {
+            Guid guid = commandId switch
+            {
+                CmdidAddAssemblyReference      => VSConstants.AssemblyReferenceProvider_Guid,
+                CmdidAddComReference           => VSConstants.ComReferenceProvider_Guid,
+                CmdidAddProjectReference       => VSConstants.ProjectReferenceProvider_Guid,
+                CmdidAddSharedProjectReference => VSConstants.SharedProjectReferenceProvider_Guid,
+                CmdidAddWinRTReference         => VSConstants.PlatformReferenceProvider_Guid,
+                _ => Guid.Empty
+            };
+
+            // Other known provider GUIDs:
+            //
+            // - VSConstants.FileReferenceProvider_Guid
+            // - VSConstants.ConnectedServiceInstanceReferenceProvider_Guid
+
+            if (guid != Guid.Empty)
+            {
+                _referencesUI.ShowReferenceManagerDialog(guid);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/Commands/SuppressAddReferenceCommand.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.Input;
+using Microsoft.VisualStudio.Threading;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Commands
+{
+    /// <summary>
+    /// Suppresses the "Add Reference..." command in specific dependencies tree context menus.
+    /// </summary>
+    [ProjectCommand(VSConstants.CMDSETID.StandardCommandSet2K_string, (long)VSConstants.VSStd2KCmdID.ADDREFERENCE)]
+    [AppliesTo(ProjectCapability.DependenciesTree)]
+    [Order(ProjectSystem.Order.Default)]
+    internal sealed class SuppressAddReferenceCommand : AbstractSingleNodeProjectCommand
+    {
+        // TODO once CPS inserts, use this field along with ContainsAny
+//      private static readonly ProjectTreeFlags s_hideAddReferenceForFlags = DependencyTreeFlags.DependenciesRootNode + DependencyTreeFlags.TargetNode;
+
+        protected override Task<CommandStatusResult> GetCommandStatusAsync(IProjectTree node, bool focused, string? commandText, CommandStatus progressiveStatus)
+        {
+//          if (node.Flags.ContainsAny(s_hideAddReferenceForFlags))
+            if (node.Flags.Contains(DependencyTreeFlags.DependenciesRootNode) || node.Flags.Contains(DependencyTreeFlags.TargetNode))
+            {
+                return GetCommandStatusResult.Suppressed;
+            }
+
+            return GetCommandStatusResult.Unhandled;
+        }
+
+        protected override Task<bool> TryHandleCommandAsync(IProjectTree node, bool focused, long commandExecuteOptions, IntPtr variantArgIn, IntPtr variantArgOut)
+        {
+            return TaskResult.False;
+        }
+    }
+}
