5ccbc12ecaba13a848a37deaf518746fbeefa10f
Drew Noakes <git@drewnoakes.com>
2020-03-04 22:41:50 +1100

    Only hide "Add Reference..." command

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/Commands/HideAddReferenceCommand.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/Commands/HideAddReferenceCommand.cs
@@ -8,7 +8,7 @@ using Microsoft.VisualStudio.Threading;
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Commands
 {
     /// <summary>
-    /// Suppresses the "Add Reference..." command in various menus.
+    /// Hides the "Add Reference..." command in various menus.
     /// </summary>
     /// <remarks>
     /// All places that previously had this command should now feature <c>IDG_VS_CTXT_REFERENCEMANAGEMENT</c>
@@ -17,11 +17,12 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Commands
     [ProjectCommand(VSConstants.CMDSETID.StandardCommandSet2K_string, (long)VSConstants.VSStd2KCmdID.ADDREFERENCE)]
     [AppliesTo(ProjectCapability.DependenciesTree)]
     [Order(ProjectSystem.Order.Default)]
-    internal sealed class SuppressAddReferenceCommand : AbstractSingleNodeProjectCommand
+    internal sealed class HideAddReferenceCommand : AbstractSingleNodeProjectCommand
     {
         protected override Task<CommandStatusResult> GetCommandStatusAsync(IProjectTree node, bool focused, string? commandText, CommandStatus progressiveStatus)
         {
-            return GetCommandStatusResult.Suppressed;
+            progressiveStatus |= CommandStatus.Invisible;
+            return GetCommandStatusResult.Handled(commandText, progressiveStatus);
         }
 
         protected override Task<bool> TryHandleCommandAsync(IProjectTree node, bool focused, long commandExecuteOptions, IntPtr variantArgIn, IntPtr variantArgOut)
