670814af98944da3f03988179c85731ddca76b25
Drew Noakes <git@drewnoakes.com>
2020-03-04 15:21:41 +1100

    Make Add Reference suppression unconditional
    We've now added `IDG_VS_CTXT_REFERENCEMANAGEMENT` to all known places that used to show "Add Reference...".

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/Commands/SuppressAddReferenceCommand.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/Commands/SuppressAddReferenceCommand.cs
@@ -8,25 +8,20 @@ using Microsoft.VisualStudio.Threading;
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Commands
 {
     /// <summary>
-    /// Suppresses the "Add Reference..." command in specific dependencies tree context menus.
+    /// Suppresses the "Add Reference..." command in various menus.
     /// </summary>
+    /// <remarks>
+    /// All places that previously had this command should now feature <c>IDG_VS_CTXT_REFERENCEMANAGEMENT</c>
+    /// which includes specific "Add ___ Reference..." commands, including "Add Assembly Reference...".
+    /// </remarks>
     [ProjectCommand(VSConstants.CMDSETID.StandardCommandSet2K_string, (long)VSConstants.VSStd2KCmdID.ADDREFERENCE)]
     [AppliesTo(ProjectCapability.DependenciesTree)]
     [Order(ProjectSystem.Order.Default)]
     internal sealed class SuppressAddReferenceCommand : AbstractSingleNodeProjectCommand
     {
-        // TODO once CPS inserts, use this field along with ContainsAny
-//      private static readonly ProjectTreeFlags s_hideAddReferenceForFlags = DependencyTreeFlags.DependenciesRootNode + DependencyTreeFlags.TargetNode;
-
         protected override Task<CommandStatusResult> GetCommandStatusAsync(IProjectTree node, bool focused, string? commandText, CommandStatus progressiveStatus)
         {
-//          if (node.Flags.ContainsAny(s_hideAddReferenceForFlags))
-            if (node.Flags.Contains(DependencyTreeFlags.DependenciesRootNode) || node.Flags.Contains(DependencyTreeFlags.TargetNode))
-            {
-                return GetCommandStatusResult.Suppressed;
-            }
-
-            return GetCommandStatusResult.Unhandled;
+            return GetCommandStatusResult.Suppressed;
         }
 
         protected override Task<bool> TryHandleCommandAsync(IProjectTree node, bool focused, long commandExecuteOptions, IntPtr variantArgIn, IntPtr variantArgOut)
