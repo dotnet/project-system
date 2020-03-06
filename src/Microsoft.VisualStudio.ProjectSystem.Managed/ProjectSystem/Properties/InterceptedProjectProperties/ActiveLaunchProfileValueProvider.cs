93739ed952b5a30bdf4dabfe28bd2c3e3bff7bb4
Tom Meschter <tomescht@microsoft.com>
2020-03-06 10:52:05 -0800

    Add a Debug page with the current launch profile
    This change adds a "Debug" property page with a single property: the active launch profile. There are a couple of distinct pieces worth explaining:
    
    1. The "Debug" page itself. Currently this uses the "generic" page template rather than the "debugger" template. Pages utilizing the debugger template get some special handling in the property page system. Notably, the properties we want to show the user effectively all depend on the current profile, so if the profile changes we need to immediately refresh all of the other properties so they display the correct values. We also need support for adding and removing profiles via the page. The debugger page experience in VS Online does not currently support these things, and instead utilizes a single, hard-coded profile which will exist for C++ scenarios but not .NET scenarios. So for the time being we will stick with the generic page template in order to make progress on exposing the desired properties, though the page behavior will not always be correct (notably, if you change other values and _then_ change the profile without clicking "Apply" first you are likely to lose the changes, or end up with a non-sensical combination of settings).
    2. The enum provider exposing the set of available profiles to populate the drop down. It turns out we already have this in the form of the `DebugProfileDebugTargetGenerator` and `DebugProfileEnumValuesGenerator` types. A later change will update these to support adding a new profile. Currently there is no UI support for that.
    3. A means of getting and setting the active profile. Here I implement an `IInterceptingPropertyValueProvider` that gets called on attempts to get and set the "ActiveLaunchProfile" property. Instead of going back to the project file for the value, it goes to the `ILaunchSettingsProvider`.
    
    An alternative to the `IInterceptingPropertyValueProvider` would be to create a whole new `IProjectPropertiesProvider` around the `ILaunchSettingsProvider` similar to the ones built into CPS for reading and writing *proj and *user files. The upside of the current approach is that it is much less code for the same effect. The downside is that `IInterceptingPropertyValueProvider` was originally created to support scenarios where property values read (or written to) the project file may need to be modified in some way, or where one property depends on another and they need to be updated together (see the `ApplicationManifestValuedProvider` for an example). As the debug settings will _always_ come from the `ILaunchSettingsProvider` rather than the project file there is a bit of a disjoint between how the system was intended to be used, and how it is actually being used.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/ActiveLaunchProfileValueProvider.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.Debug;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties
+{
+    /// <summary>
+    /// Intercepts attempts to read or write the name of the active launch profile and
+    /// redirects them to an <see cref="ILaunchSettingsProvider" />.
+    /// </summary>
+    [ExportInterceptingPropertyValueProvider(ActiveLaunchProfilePropertyName, ExportInterceptingPropertyValueProviderFile.ProjectFile)]
+    internal class ActiveLaunchProfileValueProvider : InterceptingPropertyValueProviderBase
+    {
+        private const string ActiveLaunchProfilePropertyName = "ActiveLaunchProfile";
+
+        private readonly ILaunchSettingsProvider _launchSettings;
+
+        [ImportingConstructor]
+        public ActiveLaunchProfileValueProvider(ILaunchSettingsProvider launchSettings)
+        {
+            _launchSettings = launchSettings;
+        }
+
+        public override Task<string> OnGetEvaluatedPropertyValueAsync(string evaluatedPropertyValue, IProjectProperties defaultProperties)
+        {
+            return GetPropertyValueAsync();
+        }
+
+        public override Task<string> OnGetUnevaluatedPropertyValueAsync(string unevaluatedPropertyValue, IProjectProperties defaultProperties)
+        {
+            return GetPropertyValueAsync();
+        }
+
+        /// <summary>
+        /// Handles the core logic of retrieving the property.
+        /// </summary>
+        /// <remarks>
+        /// Since we're redirecting the properties through the <see cref="ILaunchSettingsProvider"/>
+        /// there's no distinction between "evaluated" and "unevaluated" properties, so
+        /// we handle both in the same way.
+        /// </remarks>
+        private async Task<string> GetPropertyValueAsync()
+        {
+            ILaunchSettings launchSettings = await _launchSettings.WaitForFirstSnapshot(Timeout.Infinite);
+
+            return launchSettings.ActiveProfile?.Name ?? string.Empty;
+        }
+
+        public override async Task<string?> OnSetPropertyValueAsync(string unevaluatedPropertyValue, IProjectProperties defaultProperties, IReadOnlyDictionary<string, string>? dimensionalConditions = null)
+        {
+            await _launchSettings.SetActiveProfileAsync(unevaluatedPropertyValue);
+
+            // We've intercepted the "set" operation and redirected it to the launch settings.
+            // Return "null" to indicate that the value should _not_ be set in the project file
+            // as well.
+            return null;
+        }
+    }
+}
---
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/Properties/InterceptedProjectProperties/ActiveLaunchProfilesValueProviderTests.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.Debug;
+using Moq;
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties
+{
+    public class ActiveLaunchProfilesValueProviderTests
+    {
+        [Fact]
+        public async Task OnGetEvaluatedProperty_GetsActiveProfileName()
+        {
+            string activeProfileName = "Alpha";
+            var settingsProvider = CreateLaunchSettingsProvider(activeProfileName);
+
+            var launchProfileProvider = new ActiveLaunchProfileValueProvider(settingsProvider);
+
+            var actualValue = await launchProfileProvider.OnGetEvaluatedPropertyValueAsync(string.Empty, Mock.Of<IProjectProperties>());
+
+            Assert.Equal(expected: activeProfileName, actual: actualValue);
+        }
+
+        [Fact]
+        public async Task OnGetUnevaluatedProperty_GetsActiveProfileName()
+        {
+            string activeProfileName = "Beta";
+            var settingsProvider = CreateLaunchSettingsProvider(activeProfileName);
+            var launchProfileProvider = new ActiveLaunchProfileValueProvider(settingsProvider);
+
+            var actualValue = await launchProfileProvider.OnGetUnevaluatedPropertyValueAsync(string.Empty, Mock.Of<IProjectProperties>());
+
+            Assert.Equal(expected: activeProfileName, actual: actualValue);
+        }
+
+        [Fact]
+        public async Task OnSetPropertyValue_SetsActiveProfile()
+        {
+            string activeProfileName = "Gamma";
+            var settingsProvider = CreateLaunchSettingsProvider(activeProfileName, v => activeProfileName = v);
+            var launchProfileProvider = new ActiveLaunchProfileValueProvider(settingsProvider);
+
+            var result = await launchProfileProvider.OnSetPropertyValueAsync("Delta", Mock.Of<IProjectProperties>());
+
+            Assert.Null(result);
+            Assert.Equal(expected: "Delta", actual: activeProfileName);
+        }
+
+        /// <summary>
+        /// Creates a mock <see cref="ILaunchSettingsProvider"/> for testing purposes.
+        /// </summary>
+        /// <param name="activeProfileName">The name of the active profile in the new object.</param>
+        /// <param name="setActiveProfileCallback">An optional method to call when the active profile is set.</param>
+        private static ILaunchSettingsProvider CreateLaunchSettingsProvider(string activeProfileName, Action<string>? setActiveProfileCallback = null)
+        {
+            var launchProfile = new LaunchProfile { Name = activeProfileName };
+
+            var launchSettingsMock = new Mock<ILaunchSettings>();
+            launchSettingsMock.Setup(t => t.ActiveProfile).Returns(launchProfile);
+            var launchSettings = launchSettingsMock.Object;
+
+            var settingsProviderMock = new Mock<ILaunchSettingsProvider>();
+            settingsProviderMock.Setup(t => t.WaitForFirstSnapshot(It.IsAny<int>())).Returns(Task.FromResult(launchSettings));
+
+            if (setActiveProfileCallback != null)
+            {
+                settingsProviderMock.Setup(t => t.SetActiveProfileAsync(It.IsAny<string>()))
+                    .Returns<string>(v =>
+                    {
+                        setActiveProfileCallback(v);
+                        return Task.CompletedTask;
+                    });
+            }
+
+            var settingsProvider = settingsProviderMock.Object;
+            return settingsProvider;
+        }
+    }
+}
