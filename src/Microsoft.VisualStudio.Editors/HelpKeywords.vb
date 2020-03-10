f4ee921be0cf9bdcb68f81f42e51d6ac34d3e4ff
Daniel Monroy <dmon@microsoft.com>
2020-03-10 12:02:35 -0700

    Switch header in files from apache to mit license

--- src/Microsoft.VisualStudio.Editors/HelpKeywords.vb
+++ src/Microsoft.VisualStudio.Editors/HelpKeywords.vb
@@ -1,4 +1,4 @@
-﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+﻿' Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE file in the project root for more information.
 
 ' All help keywords for anything inside this assembly should be defined here,
 '   so that it is easier for UE to find them.
