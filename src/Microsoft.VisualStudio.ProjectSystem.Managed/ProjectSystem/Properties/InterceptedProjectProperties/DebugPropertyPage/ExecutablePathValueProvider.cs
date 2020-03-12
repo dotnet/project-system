d8a98a891c8bbf51141ca4bcbf29e02e375e2074
Tom Meschter <tomescht@microsoft.com>
2020-03-12 13:29:49 -0700

    Add ExecutablePath property
    Add an `ExecutablePath` property to the Debug property page.
    
    1. Define the property in the .xaml file. This includes creating a "Command Line" category to group this and future command line settings (like working directory and arguments).
    2. Add `ExecutablePathValueProvider` to redirect reads and writes of the "ExecutablePath" property to the active debug profile exposed through `ILaunchSettingsProvider`.
    3. Add unit tests.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/DebugPropertyPage/ExecutablePathValueProvider.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.Debug;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties
+{
+    /// <summary>
+    /// Intercepts attempts to read or write the executable path and redirects them to
+    /// the <see cref="ILaunchSettingsProvider"/> to update the active launch profile,
+    /// if any.
+    /// </summary>
+    [ExportInterceptingPropertyValueProvider(ExecutablePathPropertyName, ExportInterceptingPropertyValueProviderFile.ProjectFile)]
+    internal class ExecutablePathValueProvider : InterceptingPropertyValueProviderBase
+    {
+        private const string ExecutablePathPropertyName = "ExecutablePath";
+
+        private readonly UnconfiguredProject _project;
+        private readonly ILaunchSettingsProvider _launchSettingsProvider;
+        private readonly IProjectThreadingService _projectThreadingService;
+
+        [ImportingConstructor]
+        public ExecutablePathValueProvider(UnconfiguredProject project, ILaunchSettingsProvider launchSettingsProvider, IProjectThreadingService projectThreadingService)
+        {
+            _project = project;
+            _launchSettingsProvider = launchSettingsProvider;
+            _projectThreadingService = projectThreadingService;
+        }
+
+        public override Task<string> OnGetEvaluatedPropertyValueAsync(string evaluatedPropertyValue, IProjectProperties defaultProperties)
+        {
+            return GetPropertyValueAsync();
+        }
+
+        public override Task<string> OnGetUnevaluatedPropertyValueAsync(string unevaluatedPropertyValue, IProjectProperties defaultProperties)
+        {
+            return GetPropertyValueAsync();
+        }
+
+        /// <summary>
+        /// Handles the core logic of retrieving the property.
+        /// </summary>
+        /// <remarks>
+        /// Since we're redirecting the properties through the <see cref="ILaunchSettingsProvider"/>
+        /// there's no distinction between "evaluated" and "unevaluated" properties, so
+        /// we handle both in the same way.
+        /// </remarks>
+        private async Task<string> GetPropertyValueAsync()
+        {
+            ILaunchSettings launchSettings = await _launchSettingsProvider.WaitForFirstSnapshot(Timeout.Infinite);
+
+            return launchSettings.ActiveProfile?.ExecutablePath ?? string.Empty;
+        }
+
+        public override Task<string?> OnSetPropertyValueAsync(string unevaluatedPropertyValue, IProjectProperties defaultProperties, IReadOnlyDictionary<string, string>? dimensionalConditions = null)
+        {
+            _projectThreadingService.RunAndForget(async () =>
+            {
+                ILaunchSettings launchSettings = await _launchSettingsProvider.WaitForFirstSnapshot(Timeout.Infinite);
+
+                var writableLaunchSettings = launchSettings.ToWritableLaunchSettings();
+                var activeProfile = writableLaunchSettings.ActiveProfile;
+                if (activeProfile != null)
+                {
+                    activeProfile.ExecutablePath = unevaluatedPropertyValue;
+
+                    await _launchSettingsProvider.UpdateAndSaveSettingsAsync(writableLaunchSettings.ToLaunchSettings());
+                }
+            },
+            options: ForkOptions.HideLocks,
+            unconfiguredProject: _project);
+
+            // We've intercepted the "set" operation and redirected it to the launch settings.
+            // Return "null" to indicate that the value should _not_ be set in the project file
+            // as well.
+            return Task.FromResult<string?>(null);
+        }
+    }
+}
---
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/Properties/InterceptedProjectProperties/ExecutablePathValueProviderTests.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.Debug;
+using Moq;
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties
+{
+    public class ExecutablePathValueProviderTests
+    {
+        [Fact]
+        public async Task OnGetEvaluatedPropertyValueAsync_GetsExecutableFromActiveProfile()
+        {
+            string activeProfileName = "Alpha";
+            string activeProfileLaunchTarget = "AlphaCommand";
+            string activeProfileExecutablePath = @"C:\user\bin\alpha.exe";
+            var profile = new WritableLaunchProfile
+            {
+                Name = activeProfileName,
+                CommandName = activeProfileLaunchTarget,
+                ExecutablePath = activeProfileExecutablePath
+            };
+            var settingsProvider = ILaunchSettingsProviderFactory.Create(activeProfileName, new[] { profile.ToLaunchProfile() });
+
+            var project = UnconfiguredProjectFactory.Create();
+            var threadingService = IProjectThreadingServiceFactory.Create();
+            var launchProfileProvider = new ExecutablePathValueProvider(project, settingsProvider, threadingService);
+
+            var actualValue = await launchProfileProvider.OnGetEvaluatedPropertyValueAsync(string.Empty, Mock.Of<IProjectProperties>());
+
+            Assert.Equal(expected: activeProfileExecutablePath, actual: actualValue);
+        }
+
+        [Fact]
+        public async Task OnGetUnevaluatedPropertyValueAsync_GetsExecutableFromActiveProfile()
+        {
+            string activeProfileName = "Beta";
+            string activeProfileLaunchTarget = "BetaCommand";
+            string activeProfileExecutablePath = @"C:\user\bin\beta.exe";
+            var profile = new WritableLaunchProfile
+            {
+                Name = activeProfileName,
+                CommandName = activeProfileLaunchTarget,
+                ExecutablePath = activeProfileExecutablePath
+            };
+            var settingsProvider = ILaunchSettingsProviderFactory.Create(activeProfileName, new[] { profile.ToLaunchProfile() });
+
+            var project = UnconfiguredProjectFactory.Create();
+            var threadingService = IProjectThreadingServiceFactory.Create();
+            var launchProfileProvider = new ExecutablePathValueProvider(project, settingsProvider, threadingService);
+
+            var actualValue = await launchProfileProvider.OnGetUnevaluatedPropertyValueAsync(string.Empty, Mock.Of<IProjectProperties>());
+
+            Assert.Equal(expected: activeProfileExecutablePath, actual: actualValue);
+        }
+
+        [Fact]
+        public async Task OnSetPropertyValueAsync_SetsTargetInActiveProfile()
+        {
+            string activeProfileName = "Gamma";
+            string activeProfileLaunchTarget = "GammaCommand";
+            string activeProfileExecutablePath = @"C:\user\bin\gamma.exe";
+            var profile = new WritableLaunchProfile
+            {
+                Name = activeProfileName,
+                CommandName = activeProfileLaunchTarget,
+                ExecutablePath = activeProfileExecutablePath
+            };
+            var settingsProvider = ILaunchSettingsProviderFactory.Create(
+                activeProfileName,
+                new[] { profile.ToLaunchProfile() },
+                updateLaunchSettingsCallback: s =>
+                {
+                    activeProfileExecutablePath = s.ActiveProfile!.ExecutablePath;
+                });
+
+            var project = UnconfiguredProjectFactory.Create();
+            var threadingService = IProjectThreadingServiceFactory.Create();
+            var launchProfileProvider = new ExecutablePathValueProvider(project, settingsProvider, threadingService);
+
+            await launchProfileProvider.OnSetPropertyValueAsync(@"C:\user\bin\delta.exe", Mock.Of<IProjectProperties>());
+
+            Assert.Equal(expected: @"C:\user\bin\delta.exe", actual: activeProfileExecutablePath);
+        }
+    }
+}
