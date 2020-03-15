4235ec5318e2f6184e11aa27018ff0253240189f
Drew Noakes <git@drewnoakes.com>
2020-03-16 10:55:16 +1100

    Remove unused constants

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/Tree/Dependencies/Subscriptions/ProjectItemMetadata.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/Tree/Dependencies/Subscriptions/ProjectItemMetadata.cs
@@ -8,19 +8,10 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Subscription
         public const string Name = "Name";
         public const string Type = "Type";
         public const string Version = "Version";
-        public const string FileGroup = "FileGroup";
-        public const string Path = "Path";
-        public const string Resolved = "Resolved";
         public const string Dependencies = "Dependencies";
         public const string IsImplicitlyDefined = "IsImplicitlyDefined";
         public const string Severity = "Severity";
         public const string DiagnosticCode = "DiagnosticCode";
         public const string OriginalItemSpec = "OriginalItemSpec";
-
-        // Target Metadata
-        public const string RuntimeIdentifier = "RuntimeIdentifier";
-        public const string TargetFrameworkMoniker = "TargetFrameworkMoniker";
-        public const string FrameworkName = "FrameworkName";
-        public const string FrameworkVersion = "FrameworkVersion";
     }
 }
