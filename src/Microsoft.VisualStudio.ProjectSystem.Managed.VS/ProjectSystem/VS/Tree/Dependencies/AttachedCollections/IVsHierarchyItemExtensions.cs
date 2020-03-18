f9f284ceb0ecd20ffcfbc194942d6800c5fc47bb
Drew Noakes <git@drewnoakes.com>
2020-03-18 14:28:17 +1100

    Add HierarchyItemFlagsDetector and IVsHierarchyItemExtensions

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/AttachedCollections/IVsHierarchyItemExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.AttachedCollections
+{
+    /// <summary>
+    /// Extension methods for <see cref="IVsHierarchyItem"/> that support attached collections.
+    /// </summary>
+    internal static class IVsHierarchyItemExtensions
+    {
+        public static bool TryGetFlagsString(this IVsHierarchyItem item, [NotNullWhen(returnValue: true)] out string? flagsString)
+        {
+            IVsHierarchyItemIdentity identity = item.HierarchyIdentity;
+
+            if (identity.NestedHierarchy.GetProperty(identity.NestedItemID, (int)__VSHPROPID7.VSHPROPID_ProjectTreeCapabilities, out object? value) == HResult.OK)
+            {
+                flagsString = (string)value;
+                return true;
+            }
+
+            flagsString = default;
+            return false;
+        }
+    }
+}
---
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Tree/Dependencies/AttachedCollections/IVsHierarchyItemExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Moq;
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.AttachedCollections
+{
+    public sealed class IVsHierarchyItemExtensionsTests
+    {
+        [Fact]
+        public void TryGetFlagsString()
+        {
+            IVsHierarchyItem item = CreateItemWithFlags("A B C");
+
+            Assert.True(item.TryGetFlagsString(out string? s));
+            Assert.NotNull(s);
+            Assert.Equal("A B C", s);
+        }
+
+        private static IVsHierarchyItem CreateItemWithFlags(string flagsString)
+        {
+            var hierarchy = IVsHierarchyFactory.Create();
+            hierarchy.ImplementGetProperty((VsHierarchyPropID)__VSHPROPID7.VSHPROPID_ProjectTreeCapabilities, flagsString);
+
+            var identity = new Mock<IVsHierarchyItemIdentity>(MockBehavior.Strict);
+            identity.SetupGet(i => i.NestedHierarchy).Returns(hierarchy);
+            identity.SetupGet(i => i.NestedItemID).Returns(0);
+
+            var hierarchyItem = new Mock<IVsHierarchyItem>(MockBehavior.Strict);
+            hierarchyItem.SetupGet(i => i.HierarchyIdentity).Returns(identity.Object);
+
+            return hierarchyItem.Object;
+        }
+    }
+}
