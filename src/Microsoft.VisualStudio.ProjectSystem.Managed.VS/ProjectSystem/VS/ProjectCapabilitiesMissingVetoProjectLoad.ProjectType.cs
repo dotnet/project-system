052181057a15ae5ec677d43f1204a5e6759143b5
David Kean <davkean@microsoft.com>
2020-03-19 14:48:35 +1100

    Veto loading a project with missing capabilities
    This veto loads a project with missing capabilities to catch situations such as https://devdiv.visualstudio.com/DevDiv/_workitems/edit/1083914/.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/ProjectCapabilitiesMissingVetoProjectLoad.ProjectType.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS
+{
+    internal partial class ProjectCapabilitiesMissingVetoProjectLoad
+    {
+        private class ProjectType
+        {
+            public ProjectType(string extension, IEnumerable<string> capabilities)
+            {
+                Extension = extension;
+                Capabilities = capabilities.ToImmutableArray();
+            }
+
+            public string Extension;
+            public ImmutableArray<string> Capabilities;
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/ProjectCapabilitiesMissingVetoProjectLoad.cs
@@ -0,0 +1,83 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+using System.ComponentModel.Composition;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Threading;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS
+{
+    /// <summary>
+    ///     Verifies that the project was loaded with the expected capabilities to catch
+    ///     when the project type in the solution does not match the project itself, for
+    ///     example when the user has renamed csproj -> vbproj without updating the project 
+    ///     type in the solution.
+    /// </summary>
+    [Export(typeof(IVetoProjectPreLoad))]
+    [AppliesTo(ProjectCapability.DotNet)]
+    internal partial class ProjectCapabilitiesMissingVetoProjectLoad : IVetoProjectPreLoad
+    {
+        private static readonly ImmutableArray<ProjectType> s_projectTypes = GetProjectTypes();
+        private readonly UnconfiguredProject _project;
+        private readonly IProjectCapabilitiesService _projectCapabilitiesService;
+
+        [ImportingConstructor]
+        public ProjectCapabilitiesMissingVetoProjectLoad(UnconfiguredProject project, IProjectCapabilitiesService projectCapabilitiesService)
+        {
+            _project = project;
+            _projectCapabilitiesService = projectCapabilitiesService;
+        }
+
+        public Task<bool> AllowProjectLoadAsync(bool isNewProject, ProjectConfiguration activeConfiguration, CancellationToken cancellationToken = default)
+        {
+            ProjectType? projectType = GetCurrentProjectType();
+            if (projectType == null)    // Unrecognized, probably a Shared Project
+                return TaskResult.True;
+
+            foreach (string capability in projectType.Capabilities)
+            {
+                if (!_projectCapabilitiesService.Contains(capability))
+                {   
+                    // Throw instead of returning false so that we can control message and the HRESULT
+                    throw new COMException(string.Format(CultureInfo.CurrentCulture, Resources.ProjectLoadedWithWrongProjectType, _project.FullPath),
+                                           HResult.Fail);
+                }
+            }
+
+            return TaskResult.True;
+        }
+
+        private ProjectType? GetCurrentProjectType()
+        {
+            Assumes.True(s_projectTypes.Length > 0);
+
+            string extension = Path.GetExtension(_project.FullPath);
+
+            foreach (ProjectType projectType in s_projectTypes)
+            {
+                if (StringComparers.Paths.Equals(projectType.Extension, extension))
+                {
+                    return projectType;
+                }
+            }
+
+            return null;
+        }
+
+        private static ImmutableArray<ProjectType> GetProjectTypes()
+        {
+            Assembly assembly = typeof(ProjectCapabilitiesMissingVetoProjectLoad).Assembly;
+
+            return assembly.GetCustomAttributes<ProjectTypeRegistrationAttribute>()
+                           .Select(a => new ProjectType('.' + a.DefaultProjectExtension, a.Capabilities!.Split(new[] { ';', ' ' }, StringSplitOptions.RemoveEmptyEntries)))
+                           .ToImmutableArray();
+        }
+    }
+}
