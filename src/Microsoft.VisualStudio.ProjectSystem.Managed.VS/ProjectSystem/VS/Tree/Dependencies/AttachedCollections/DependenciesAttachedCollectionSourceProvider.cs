5e22244cca2d318706626d43c5f129ee5a3832be
Drew Noakes <git@drewnoakes.com>
2020-03-19 15:05:09 +1100

    Attach diagnostic items to package reference items
    Additionally, this commit introduces the first infrastructure for
    working with attached collections in the tree.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/AttachedCollections/DependenciesAttachedCollectionSourceProvider.cs
@@ -0,0 +1,80 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Microsoft.Internal.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.ProjectSystem.Tree.Dependencies.Assets;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Threading;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.AttachedCollections
+{
+    /// <summary>
+    /// Identifies which hierarchy items in the "Dependencies" tree should have collections of child
+    /// items attached, and provides source objects for those collections.
+    /// </summary>
+    [Export(typeof(IAttachedCollectionSourceProvider))]
+    [Name(nameof(DependenciesAttachedCollectionSourceProvider))]
+    [VisualStudio.Utilities.Order(Before = HierarchyItemsProviderNames.Contains)]
+    internal sealed class DependenciesAttachedCollectionSourceProvider : IAttachedCollectionSourceProvider
+    {
+        private readonly HierarchyItemFlagsDetector _packageReferenceTest = new HierarchyItemFlagsDetector(DependencyTreeFlags.PackageDependency);
+        private readonly JoinableTaskContext _joinableTaskContext;
+
+        [ImportingConstructor]
+        public DependenciesAttachedCollectionSourceProvider(JoinableTaskContext joinableTaskContext)
+        {
+            _joinableTaskContext = joinableTaskContext;
+        }
+
+        public IAttachedCollectionSource? CreateCollectionSource(object item, string relationshipName)
+        {
+            if (relationshipName == KnownRelationships.Contains)
+            {
+                if (item is IVsHierarchyItem hierarchyItem)
+                {
+                    if (_packageReferenceTest.Matches(hierarchyItem))
+                    {
+                        // This is a package reference
+                        return CreatePackageReferenceSource(hierarchyItem, _joinableTaskContext);
+                    }
+                }
+            }
+
+            return null;
+
+            static IAttachedCollectionSource? CreatePackageReferenceSource(IVsHierarchyItem hierarchyItem, JoinableTaskContext joinableTaskContext)
+            {
+                UnconfiguredProject? unconfiguredProject = hierarchyItem.HierarchyIdentity.Hierarchy.AsUnconfiguredProject();
+                
+                if (unconfiguredProject == null)
+                {
+                    return null;
+                }
+
+                // Determine the package's ID and version
+                if (!hierarchyItem.TryGetPackageDetails(out string? packageId, out string? packageVersion))
+                {
+                    return null;
+                }
+
+                // Find the data source
+                IAssetsFileDependenciesDataSource? dataSource = unconfiguredProject.Services.ExportProvider.GetExportedValueOrDefault<IAssetsFileDependenciesDataSource>();
+
+                if (dataSource != null)
+                {
+                    // Configuration will be null if project is not multi-targeting
+                    hierarchyItem.TryFindConfiguration(out string? configuration);
+
+                    return new PackageReferenceAttachedCollectionSource(hierarchyItem, configuration, packageId, packageVersion, dataSource, joinableTaskContext);
+                }
+
+                return null;
+            }
+        }
+
+        public IEnumerable<IAttachedRelationship> GetRelationships(object item) => Enumerable.Empty<IAttachedRelationship>();
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/AttachedCollections/DiagnosticItem.cs
@@ -0,0 +1,64 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Microsoft.VisualStudio.Imaging;
+using Microsoft.VisualStudio.Imaging.Interop;
+using Microsoft.VisualStudio.ProjectSystem.Tree.Dependencies.Assets;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.AttachedCollections
+{
+    /// <summary>
+    /// Models a diagnostic message attached to a package reference in the dependencies tree.
+    /// </summary>
+    internal sealed class DiagnosticItem : AttachedCollectionItemBase
+    {
+        private readonly AssetsFileLogMessage _log;
+
+        public DiagnosticItem(AssetsFileLogMessage log)
+            : base(log.Message)
+        {
+            _log = log;
+        }
+
+        public override int Priority => AttachedItemPriority.Diagnostic;
+
+        public override ImageMoniker IconMoniker => _log.Level switch
+        {
+            NuGet.Common.LogLevel.Error => KnownMonikers.StatusError,
+            NuGet.Common.LogLevel.Warning => KnownMonikers.StatusWarning,
+            _ => KnownMonikers.StatusInformation
+        };
+
+        public override object GetBrowseObject() => new BrowseObject(_log);
+
+        private sealed class BrowseObject : BrowseObjectBase
+        {
+            private readonly AssetsFileLogMessage _log;
+
+            public BrowseObject(AssetsFileLogMessage log) => _log = log;
+
+            public override string GetComponentName() => Code;
+
+            public override string GetClassName() => VSResources.DiagnosticBrowseObjectClassName;
+
+            [BrowseObjectDisplayName(nameof(VSResources.DiagnosticMessageDisplayName))]
+            [BrowseObjectDescription(nameof(VSResources.DiagnosticMessageDescription))]
+            public string Message => _log.Message;
+
+            [BrowseObjectDisplayName(nameof(VSResources.DiagnosticCodeDisplayName))]
+            [BrowseObjectDescription(nameof(VSResources.DiagnosticCodeDescription))]
+            public string Code => _log.Code.ToString();
+
+            [BrowseObjectDisplayName(nameof(VSResources.DiagnosticLibraryDisplayName))]
+            [BrowseObjectDescription(nameof(VSResources.DiagnosticLibraryDescription))]
+            public string LibraryId => _log.LibraryId;
+
+            [BrowseObjectDisplayName(nameof(VSResources.DiagnosticLevelDisplayName))]
+            [BrowseObjectDescription(nameof(VSResources.DiagnosticLevelDescription))]
+            public string Level => _log.Level.ToString();
+
+            [BrowseObjectDisplayName(nameof(VSResources.DiagnosticWarningLevelDisplayName))]
+            [BrowseObjectDescription(nameof(VSResources.DiagnosticWarningLevelDescription))]
+            public string WarningLevel => _log.Level == NuGet.Common.LogLevel.Warning ? _log.WarningLevel.ToString() : "";
+        }
+    }
+}
