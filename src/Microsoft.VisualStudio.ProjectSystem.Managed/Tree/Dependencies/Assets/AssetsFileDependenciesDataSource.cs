bf2a984e28b3f53e5035cff4423615c6d5a6ea4f
Drew Noakes <git@drewnoakes.com>
2020-03-19 15:03:14 +1100

    Add AssetsFileDataSource and snapshot
    Initially this only includes diagnostic log messages. It will be
    extended in future to include information about package and project
    references across all targets in the project.
    
    Future work will also update the snapshot based on the diff
    between the current snapshot and parsed data.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/Tree/Dependencies/Assets/AssetsFileDependenciesDataSource.cs
@@ -0,0 +1,112 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+using Microsoft.VisualStudio.ProjectSystem.Utilities;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Tree.Dependencies.Assets
+{
+    [Export(typeof(IAssetsFileDependenciesDataSource))]
+    [AppliesTo(ProjectCapability.DependenciesTree)]
+    internal sealed class AssetsFileDependenciesDataSource : ChainedProjectValueDataSourceBase<AssetsFileDependenciesSnapshot>, IAssetsFileDependenciesDataSource
+    {
+        private readonly IActiveConfiguredProjectSubscriptionService _activeConfiguredProjectSubscriptionService;
+        private readonly IActiveConfiguredProjectSnapshotService _activeConfiguredProjectSnapshotService;
+
+        [ImportingConstructor]
+        public AssetsFileDependenciesDataSource(
+            UnconfiguredProject unconfiguredProject,
+            IActiveConfiguredProjectSubscriptionService activeConfiguredProjectSubscriptionService,
+            IActiveConfiguredProjectSnapshotService activeConfiguredProjectSnapshotService)
+            : base(unconfiguredProject.Services, synchronousDisposal: false, registerDataSource: false)
+        {
+            _activeConfiguredProjectSubscriptionService = activeConfiguredProjectSubscriptionService;
+            _activeConfiguredProjectSnapshotService = activeConfiguredProjectSnapshotService;
+        }
+
+        protected override IDisposable LinkExternalInput(ITargetBlock<IProjectVersionedValue<AssetsFileDependenciesSnapshot>> targetBlock)
+        {
+            JoinUpstreamDataSources(_activeConfiguredProjectSubscriptionService.ProjectRuleSource);
+            JoinUpstreamDataSources(_activeConfiguredProjectSnapshotService);
+
+            string? lastAssetsFilePath = null;
+            DateTime lastTimestampUtc = DateTime.MinValue;
+            AssetsFileDependenciesSnapshot lastSnapshot = AssetsFileDependenciesSnapshot.Empty;
+
+            var intermediateBlock =
+                new BufferBlock<IProjectVersionedValue<IProjectSubscriptionUpdate>>(
+                    new ExecutionDataflowBlockOptions { NameFormat = nameof(AssetsFileDependenciesDataSource) + " Intermediate: {1}" });
+
+            IReceivableSourceBlock<IProjectVersionedValue<IProjectSubscriptionUpdate>> projectRuleSource
+                = _activeConfiguredProjectSubscriptionService.ProjectRuleSource.SourceBlock;
+
+            IPropagatorBlock<IProjectVersionedValue<ValueTuple<IProjectSnapshot, IProjectSubscriptionUpdate>>, IProjectVersionedValue<AssetsFileDependenciesSnapshot>> transformBlock
+                = DataflowBlockSlim.CreateTransformBlock<IProjectVersionedValue<ValueTuple<IProjectSnapshot, IProjectSubscriptionUpdate>>, IProjectVersionedValue<AssetsFileDependenciesSnapshot>>(Transform, skipIntermediateInputData: true, skipIntermediateOutputData: true);
+
+            return new DisposableBag
+            {
+                // Subscribe to "ConfigurationGeneral" rule data
+                projectRuleSource.LinkTo(
+                    intermediateBlock,
+                    ruleNames: ConfigurationGeneral.SchemaName,
+                    suppressVersionOnlyUpdates: false,
+                    linkOptions: DataflowOption.PropagateCompletion),
+
+                // Sync link inputs, joining on versions, and passing joined data to our transform block
+                ProjectDataSources.SyncLinkTo(
+                    _activeConfiguredProjectSnapshotService.SourceBlock.SyncLinkOptions(),
+                    intermediateBlock.SyncLinkOptions(),
+                    transformBlock,
+                    linkOptions: DataflowOption.PropagateCompletion),
+
+                // Flow transformed data to the output/target
+                transformBlock.LinkTo(targetBlock, DataflowOption.PropagateCompletion)
+            };
+
+            IProjectVersionedValue<AssetsFileDependenciesSnapshot> Transform(IProjectVersionedValue<ValueTuple<IProjectSnapshot, IProjectSubscriptionUpdate>> update)
+            {
+                var projectSnapshot = (IProjectSnapshot2)update.Value.Item1;
+                IProjectSubscriptionUpdate subscriptionUpdate = update.Value.Item2;
+
+                string? path = null;
+                DateTime timestampUtc = DateTime.MinValue;
+
+                AssetsFileDependenciesSnapshot snapshot = lastSnapshot;
+
+                if (subscriptionUpdate.CurrentState.TryGetValue(ConfigurationGeneral.SchemaName, out IProjectRuleSnapshot ruleSnapshot))
+                {
+                    if (ruleSnapshot.Properties.TryGetValue(ConfigurationGeneral.ProjectAssetsFileProperty, out path))
+                    {
+                        if (!string.IsNullOrEmpty(path))
+                        {
+                            if (projectSnapshot.AdditionalDependentFileTimes == null || !projectSnapshot.AdditionalDependentFileTimes.TryGetValue(path, out timestampUtc))
+                            {
+                                try
+                                {
+                                    timestampUtc = File.GetLastWriteTimeUtc(path);
+                                }
+                                catch
+                                {
+                                    // ignore
+                                }
+                            }
+
+                            if (path != lastAssetsFilePath || timestampUtc != lastTimestampUtc)
+                            {
+                                lastAssetsFilePath = path;
+                                lastTimestampUtc = timestampUtc;
+
+                                lastSnapshot = snapshot = lastSnapshot.UpdateFromAssetsFile(path);
+                            }
+                        }
+                    }
+                }
+
+                return new ProjectVersionedValue<AssetsFileDependenciesSnapshot>(snapshot, update.DataSourceVersions);
+            }
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/Tree/Dependencies/Assets/AssetsFileDependenciesSnapshot.cs
@@ -0,0 +1,83 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using NuGet.Common;
+using NuGet.ProjectModel;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Tree.Dependencies.Assets
+{
+    /// <summary>
+    /// Immutable snapshot of data captured from <c>project.assets.json</c>.
+    /// </summary>
+    internal sealed class AssetsFileDependenciesSnapshot
+    {
+        private static readonly LockFileFormat s_lockFileFormat = new LockFileFormat();
+
+        public static AssetsFileDependenciesSnapshot Empty { get; } = new AssetsFileDependenciesSnapshot(null);
+
+        /// <summary>
+        /// List of diagnostic messages included in the snapshot. May be empty.
+        /// </summary>
+        public ImmutableArray<AssetsFileLogMessage> Logs { get; }
+
+        /// <summary>
+        /// Produces an updated snapshot by reading the <c>project.assets.json</c> file at <paramref name="path"/>.
+        /// If the file could not be read, or no changes are detected, the current snapshot (this) is returned.
+        /// </summary>
+        public AssetsFileDependenciesSnapshot UpdateFromAssetsFile(string? path)
+        {
+            try
+            {
+                // Parse the file
+                using var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096 * 10, FileOptions.SequentialScan);
+
+                LockFile lockFile = s_lockFileFormat.Read(fileStream, path);
+
+                return new AssetsFileDependenciesSnapshot(lockFile);
+            }
+            catch (IOException)
+            {
+                return this;
+            }
+        }
+
+        private AssetsFileDependenciesSnapshot(LockFile? lockFile)
+        {
+            if (lockFile == null || lockFile.LogMessages.Count == 0)
+            {
+                Logs = ImmutableArray<AssetsFileLogMessage>.Empty;
+            }
+            else
+            {
+                ImmutableArray<AssetsFileLogMessage>.Builder builder = ImmutableArray.CreateBuilder<AssetsFileLogMessage>(lockFile.LogMessages.Count);
+                foreach (IAssetsLogMessage logMessage in lockFile.LogMessages)
+                {
+                    builder.Add(new AssetsFileLogMessage(logMessage));
+                }
+                Logs = builder.MoveToImmutable();
+            }
+        }
+    }
+
+    internal readonly struct AssetsFileLogMessage
+    {
+        public AssetsFileLogMessage(IAssetsLogMessage logMessage)
+        {
+            Code = logMessage.Code;
+            Level = logMessage.Level;
+            WarningLevel = logMessage.WarningLevel;
+            Message = logMessage.Message;
+            LibraryId = logMessage.LibraryId;
+            TargetGraphs = logMessage.TargetGraphs;
+        }
+
+        public NuGetLogCode Code { get; }
+        public NuGet.Common.LogLevel Level { get; }
+        public WarningLevel WarningLevel { get; }
+        public string Message { get; }
+        public string LibraryId { get; }
+        public IReadOnlyList<string> TargetGraphs { get; }
+    }
+}
