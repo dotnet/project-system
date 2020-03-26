31bc9bd9d5ea1c371df4bf539806e42f1f2415d3
David Kean <davkean@microsoft.com>
2020-03-26 15:04:59 +1100

    Add method for listening to faulted blocks

--- tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Mocks/IProjectFaultHandlerServiceFactory.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Mocks/IProjectFaultHandlerServiceFactory.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using Moq;
 
 namespace Microsoft.VisualStudio.ProjectSystem
@@ -9,6 +10,15 @@ namespace Microsoft.VisualStudio.ProjectSystem
         public static IProjectFaultHandlerService Create()
         {
             return Mock.Of<IProjectFaultHandlerService>();
+        }
+
+        public static IProjectFaultHandlerService ImplementHandleFaultAsync(Action<Exception, ErrorReportSettings?, ProjectFaultSeverity, UnconfiguredProject?> action)
+        {
+            var mock = new Mock<IProjectFaultHandlerService>();
+            mock.Setup(s => s.HandleFaultAsync(It.IsAny<Exception>(), It.IsAny<ErrorReportSettings?>(), It.IsAny<ProjectFaultSeverity>(), It.IsAny<UnconfiguredProject?>()))
+                .ReturnsAsync(action);
+
+            return mock.Object;
         }
     }
 }
