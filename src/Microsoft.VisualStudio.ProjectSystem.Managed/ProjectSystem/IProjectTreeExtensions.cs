f4cfea525c7920e18497b82e4f6846b1a888984a
Drew Noakes <git@drewnoakes.com>
2020-04-03 19:26:44 +1100

    Annotate pure methods

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/IProjectTreeExtensions.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/IProjectTreeExtensions.cs
@@ -1,5 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System.Diagnostics.Contracts;
+
 namespace Microsoft.VisualStudio.ProjectSystem
 {
     internal static class IProjectTreeExtensions
@@ -8,6 +10,7 @@ namespace Microsoft.VisualStudio.ProjectSystem
         /// Gets the direct child of <paramref name="tree"/> with <paramref name="caption"/>
         /// if found, otherwise <see langword="null"/>.
         /// </summary>
+        [Pure]
         public static IProjectTree? FindChildWithCaption(this IProjectTree tree, string caption)
         {
             return tree.Children.FirstOrDefault(
@@ -18,6 +21,7 @@ namespace Microsoft.VisualStudio.ProjectSystem
         /// <summary>
         /// Finds the first child node having <paramref name="flags"/>, or <see langword="null"/> if no child matches.
         /// </summary>
+        [Pure]
         internal static IProjectTree? FindChildWithFlags(this IProjectTree self, ProjectTreeFlags flags)
         {
             foreach (IProjectTree child in self.Children)
