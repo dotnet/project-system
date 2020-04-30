506e9c3b6970d2fe4d8a4ab6c02becd4716412a3
Drew Noakes <git@drewnoakes.com>
2020-04-30 15:26:53 +1000

    Show diagnostics for project references

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/AttachedCollections/NuGet/Relations/ProjectToDiagnosticRelation.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.AttachedCollections;
+
+namespace Microsoft.VisualStudio.NuGet
+{
+    [Export(typeof(IRelation))]
+    internal sealed class ProjectToDiagnosticRelation : RelationBase<ProjectReferenceItem, DiagnosticItem>
+    {
+        protected override bool HasContainedItems(ProjectReferenceItem parent)
+        {
+            return parent.Target.Logs.Any(log => log.LibraryName == parent.Library.Name);
+        }
+
+        protected override void UpdateContainsCollection(ProjectReferenceItem parent, AggregateContainsRelationCollectionSpan span)
+        {
+            span.UpdateContainsItems(
+                parent.Target.Logs.Where(log => log.LibraryName == parent.Library.Name).OrderBy(log => log.LibraryName).ThenBy(log => log.Message),
+                (log, item) => StringComparer.Ordinal.Compare(log.LibraryName, item.Library.Name),
+                (log, item) => item.TryUpdateState(parent.Target, parent.Library, log),
+                log => new DiagnosticItem(parent.Target, parent.Library, log));
+        }
+
+        protected override IEnumerable<ProjectReferenceItem>? CreateContainedByItems(DiagnosticItem child)
+        {
+            yield return new ProjectReferenceItem(child.Target, child.Library);
+        }
+    }
+}
