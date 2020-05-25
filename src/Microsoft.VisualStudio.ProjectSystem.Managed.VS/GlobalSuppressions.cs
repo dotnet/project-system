d8f407cbe6b3723cdb31c7fa5596a63f24a9d471
David Wengier <david.wengier@microsoft.com>
2020-05-25 12:08:08 +1000

    Remove old suppressions

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/GlobalSuppressions.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/GlobalSuppressions.cs
@@ -2,19 +2,13 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/1432", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.EditAndContinue.EditAndContinueProvider.Dispose")]
 [assembly: SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/1432", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.PropertyPages.BuildMacroInfo.Dispose")]
 [assembly: SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/1432", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.References.DesignTimeAssemblyResolution.Dispose")]
 [assembly: SuppressMessage("Usage", "VSTHRD110:Observe result of async calls", Justification = "https://github.com/dotnet/project-system/issues/3921", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.PropertyPages.PropertyPage.SetObjects(System.UInt32,System.Object[])")]
-[assembly: SuppressMessage("Usage", "CA2215:Dispose methods should call base class dispose", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/1654", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.TempPE.TempPEBuildManager.DisposeCoreAsync(System.Boolean)~System.Threading.Tasks.Task")]
 
-[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/2416", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.NuGet.ProjectAssetFileWatcher.ProjectAssetFileWatcherInstance.GetFileHashOrNull(System.String)~System.Byte[]")]
 [assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/2416", Scope = "member", Target = "~M:Microsoft.VisualStudio.Telemetry.VsTelemetryService.HashValue(System.String)~System.String")]
 [assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/2416", Scope = "member", Target = "~P:Microsoft.VisualStudio.ProjectSystem.VS.PropertyPages.DebugPageViewModel.BrowseDirectoryCommand")]
 [assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/2416", Scope = "member", Target = "~P:Microsoft.VisualStudio.ProjectSystem.VS.PropertyPages.DebugPageViewModel.BrowseExecutableCommand")]
-[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/2416", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.PackageRestore.ProjectAssetFileWatcher.ProjectAssetFileWatcherInstance.GetFileHashOrNull(System.String)~System.Byte[]")]
-
-[assembly: SuppressMessage("Usage", "CA2215:Dispose methods should call base class dispose", Justification = "<Pending>", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.PackageRestore.PackageRestoreService.DisposeCoreAsync(System.Boolean)~System.Threading.Tasks.Task")]
 
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "https://github.com/dotnet/roslyn/issues/41531", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.VS.Automation.VSProject.get_Extender(System.String)~System.Object")]
 
--- tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/GlobalSuppressions.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/GlobalSuppressions.cs
@@ -3,5 +3,3 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Usage", "xUnit1025:InlineData should be unique within the Theory it belongs to", Justification = "Work around https://github.com/xunit/xunit/issues/1877", Scope = "member", Target = "~M:Microsoft.VisualStudio.ProjectSystem.SetDiffTests.ProducesCorrectDiff(System.Int32[],System.Int32[],System.Int32[],System.Int32[])")]
-
-[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Part of Xunit theory", Scope = "type", Target = "~T:Microsoft.VisualStudio.ProjectSystem.Rules.XamlRuleTests")]
