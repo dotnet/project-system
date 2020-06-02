b35dba8f4cfbff7898e36bad7feceb6939e01ca9
David Wengier <david.wengier@microsoft.com>
2020-06-02 15:56:45 +1000

    Add prerequisite check for missing reference assemblies

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Retargetting/InstallTargetingPackTargetDescription.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Retargetting
+{
+    internal class InstallTargetingPackTargetDescription : InstallerComponentMissingTargetDescriptionBase
+    {
+        private readonly Guid _targetDescriptionId = new Guid("0ABC0204-20D6-4A24-86F7-2F953B929C03");
+
+        public InstallTargetingPackTargetDescription(string component)
+            : base(component)
+        {
+        }
+
+        public override Guid TargetId => _targetDescriptionId;
+
+        public override string DisplayName => "Install Targeting Pack";
+
+        public override string CommandTitle => "Install Targeting Pack...";
+
+        public override Guid SetupDriver => VSConstants.SetupDrivers.SetupDriver_VS;
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Retargetting/MissingNetFrameworkTargetingPackRetarget.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Retargetting
+{
+    [Export(typeof(IProjectPrerequisiteCheckProvider))]
+    [AppliesTo(ProjectCapability.DotNet)]
+    internal class MissingNetFrameworkTargetingPackRetarget : IProjectPrerequisiteCheckProvider
+    {
+        private static readonly IEnumerable<string> s_rules = new string[] { ConfigurationGeneral.SchemaName };
+
+        public IEnumerable<string> GetProjectEvaluationRuleNames() => s_rules;
+
+        public Task<TargetDescriptionBase?> CheckAsync(IImmutableDictionary<string, IProjectRuleSnapshot> projectState)
+        {
+            string? targetFrameworkIdentifier = projectState.GetPropertyOrDefault(ConfigurationGeneral.SchemaName, ConfigurationGeneral.TargetFrameworkIdentifierProperty, string.Empty);
+            string? targetFrameworkVersion = projectState.GetPropertyOrDefault(ConfigurationGeneral.SchemaName, ConfigurationGeneral.TargetFrameworkVersionProperty, string.Empty);
+            string? targetFrameworkProfile = projectState.GetPropertyOrDefault(ConfigurationGeneral.SchemaName, ConfigurationGeneral.TargetFrameworkProfileProperty, string.Empty);
+
+            if (targetFrameworkVersion.Length > 1)
+            {
+                string versionWithoutTheV = targetFrameworkVersion.Substring(1);
+
+                if (!Microsoft.Build.Utilities.ToolLocationHelper.GetPathToReferenceAssemblies(targetFrameworkIdentifier, targetFrameworkVersion, targetFrameworkProfile).Any())
+                {
+                    return Task.FromResult((TargetDescriptionBase?)new InstallTargetingPackTargetDescription($"Microsoft.Net.Component.{versionWithoutTheV}.TargetingPack"));
+                }
+            }
+
+            return Task.FromResult((TargetDescriptionBase?)null);
+        }
+    }
+}
