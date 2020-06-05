0e299bb70c329ede6e4c573327ce9999e789d6c8
Tom Meschter <tomescht@microsoft.com>
2020-06-05 15:43:06 -0700

    Add an authentication mode enum provider
    Add a dynamic enum provider for the "Authentication mode" property on the Executable and Project debug pages.

---
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Mocks/IRemoteAuthenticationProviderFactory.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Microsoft.VisualStudio.ProjectSystem.Debug;
+using Moq;
+
+namespace Microsoft.VisualStudio.Mocks
+{
+    public class IRemoteAuthenticationProviderFactory
+    {
+        internal static IRemoteAuthenticationProvider Create(string name, string displayName)
+        {
+            var provider = new Mock<IRemoteAuthenticationProvider>();
+
+            provider.SetupGet(o => o.Name).Returns(name);
+            provider.SetupGet(o => o.DisplayName).Returns(displayName);
+
+            return provider.Object;
+        }
+    }
+}
---
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Properties/AuthenticationModeEnumProviderTests.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Mocks;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Properties
+{
+    public class AuthenticationModeEnumProviderTests
+    {
+        [Fact]
+        public async Task GetProviderAsync_ReturnsNonNullGenerator()
+        {
+            var remoteDebuggerAuthenticationService = IRemoteDebuggerAuthenticationServiceFactory.Create();
+
+            var provider = new AuthenticationModeEnumProvider(remoteDebuggerAuthenticationService);
+            var generator = await provider.GetProviderAsync(options: null);
+
+            Assert.NotNull(generator);
+        }
+
+        [Fact]
+        public async Task TryCreateEnumValueAsync_Throws()
+        {
+            var remoteDebuggerAuthenticationService = IRemoteDebuggerAuthenticationServiceFactory.Create();
+
+            var provider = new AuthenticationModeEnumProvider(remoteDebuggerAuthenticationService);
+            var generator = await provider.GetProviderAsync(options: null);
+
+            await Assert.ThrowsAsync<NotImplementedException>(() => generator.TryCreateEnumValueAsync("MyMode"));
+        }
+
+        [Fact]
+        public async Task GetListValuesAsync_ReturnsPageNamesAndDisplayNames()
+        {
+            var remoteDebuggerAuthenticationService = IRemoteDebuggerAuthenticationServiceFactory.Create(
+                IRemoteAuthenticationProviderFactory.Create("cheetah", "Fast authentication!"),
+                IRemoteAuthenticationProviderFactory.Create("tortoise", "Sloooow authentication..."));
+
+            var provider = new AuthenticationModeEnumProvider(remoteDebuggerAuthenticationService);
+            var generator = await provider.GetProviderAsync(options: null);
+
+            var values = await generator.GetListedValuesAsync();
+
+            Assert.Collection(values, new Action<IEnumValue>[]
+            {
+                ev => { Assert.Equal(expected: "cheetah", actual: ev.Name); Assert.Equal(expected: "Fast authentication!", actual: ev.DisplayName); },
+                ev => { Assert.Equal(expected: "tortoise", actual: ev.Name); Assert.Equal(expected: "Sloooow authentication...", actual: ev.DisplayName); }
+            });
+        }
+    }
+}
