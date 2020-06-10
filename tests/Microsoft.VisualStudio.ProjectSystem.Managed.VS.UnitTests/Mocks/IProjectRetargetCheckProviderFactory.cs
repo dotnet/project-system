7ee09d162923cb4d2b04a186090892fd5e39e9a4
David Wengier <david.wengier@microsoft.com>
2020-06-10 14:36:34 +1000

    Add basic prioritization of changes, and test

---
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/IProjectRetargetCheckProviderFactory.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Moq;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Retargetting
+{
+    internal static class IProjectRetargetCheckProviderFactory
+    {
+        internal static IProjectRetargetCheckProvider Create()
+        {
+            var mock = new Mock<IProjectRetargetCheckProvider>();
+
+            return mock.Object;
+        }
+    }
+}
---
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Retargeting/UnconfiguredProjectRetargetingDataSourceTests.Prerequisite.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using Microsoft.VisualStudio.ProjectSystem.VS.Retargetting;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Retargeting
+{
+    public partial class UnconfiguredProjectRetargetingDataSourceTests
+    {
+        private class Prerequisite : InstallerComponentMissingTargetDescriptionBase
+        {
+            private readonly Guid _setupDriver;
+
+            public Prerequisite(Guid setupDriver)
+                : base("")
+            {
+                _setupDriver = setupDriver;
+            }
+
+            public override Guid TargetId => Guid.NewGuid();
+
+            public override string DisplayName => throw new NotImplementedException();
+
+            public override Guid SetupDriver => _setupDriver;
+        }
+    }
+}
