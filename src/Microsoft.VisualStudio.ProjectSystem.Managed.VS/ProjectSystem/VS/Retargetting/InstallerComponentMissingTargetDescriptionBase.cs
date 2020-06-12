8de6258e7162bc08db0fa423e5d96d454e3ef22b
David Wengier <david.wengier@microsoft.com>
2020-06-12 13:35:32 +1000

    Add unload information

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Retargetting/InstallerComponentMissingTargetDescriptionBase.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Retargetting/InstallerComponentMissingTargetDescriptionBase.cs
@@ -15,12 +15,27 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Retargetting
 
         public override bool Supported => false;
 
+        /// <summary>
+        /// Text for the menu item that fixes the problem with the project
+        /// </summary>
         public override string CommandTitle => "Install Missing Component(s)...";
+
+        /// <summary>
+        /// A brief description of why the project can't be loaded, displayed in parentheses after the project name
+        /// </summary>
+        public virtual string UnloadReason => "Components missing";
+
+        /// <summary>
+        /// A longer description of why the project can't be loaded, displayed as a child node of the project
+        /// </summary>
+        public virtual string UnloadDescription => "Visual Studio components required to load this project are missing.";
 
         public override object? GetProperty(uint prop) => ((__VSPTDPROPID)prop) switch
         {
             __VSPTDPROPID.VSPTDPROPID_MissingPrerequisites => true,
             __VSPTDPROPID.VSPTDPROPID_AcquisitionComponents => _component,
+            __VSPTDPROPID.VSPTDPROPID_UnloadInfoLine => UnloadDescription,
+            __VSPTDPROPID.VSPTDPROPID_UnloadTitle => UnloadReason,
             _ => base.GetProperty(prop),
         };
     }
