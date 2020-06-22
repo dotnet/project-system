3862ee7666f137da5a060b02ed4515a52e99d951
Osvaldo Calles <osvaldo.calles@gmail.com>
2020-06-22 15:00:58 -0700

    Add fixup for copying configuration

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Properties/CloneConfigurationFixup.cs
@@ -0,0 +1,103 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Build.Construction;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Properties
+{
+    /// <summary>
+    /// Interface for components that provide post-configuration cloning propertyName fix ups.
+    /// </summary>
+    internal class CloneConfigurationFixup : IClonePlatformFixup
+    {
+        private IEnumerable<string> _propertyNames = null;
+        private List<string> _configurationProperties;
+        private readonly IUnconfiguredProjectServices _unconfiguredProjectServices;
+        private readonly ConfiguredProject? _sourceConfiguredProject;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CloneConfigurationFixup"/> class.
+        /// </summary>
+        public CloneConfigurationFixup(IUnconfiguredProjectServices unconfiguredProjectServices,
+            ConfiguredProject? sourceConfiguredProject,
+            string newConfigurationName)
+        {
+            _unconfiguredProjectServices = unconfiguredProjectServices;
+            _sourceConfiguredProject = sourceConfiguredProject;
+        }
+
+        /// <summary>
+        /// Gets the list of property names that factor on Configuration builds.
+        /// </summary>
+        private async Task<IEnumerable<string>> GetConfigurationPropertiesNamesToClone()
+        {
+            if (_propertyNames != null)
+            {
+                return _propertyNames;
+            }
+
+            IProjectProperties? commonProperties =
+                _unconfiguredProjectServices.ActiveConfiguredProjectProvider?.ActiveConfiguredProject?.Services.ProjectPropertiesProvider?.GetCommonProperties();
+            string value = await commonProperties.GetEvaluatedPropertyValueAsync("CopyingConfigurationProperties");
+
+            _propertyNames = value.Split(';').Distinct().Where(s => !string.IsNullOrEmpty(s));
+
+            return _propertyNames;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this propertyName factors or not on configuration build
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        private async Task<bool> isInConfigurationDefaultsAsync(string propertyName)
+        {
+            _configurationProperties ??= (await GetConfigurationPropertiesNamesToClone()).ToList();
+            return _configurationProperties.Contains(propertyName);
+        }
+
+        /// <summary>
+        /// Read the value for propertyName via Targets
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns>Default propertyName value</returns>
+        private async Task<string> GetDefaultPropertyValueAsync(string propertyName)
+        {
+            string propertyValue = "COFFEEEE"; 
+
+            // How to get the property value from the configured project ?
+            //_sourceConfiguredProject.GetPropertyValue(propertyName);
+
+            return propertyValue;
+        }
+
+
+        public bool ShouldElementBeCloned(ProjectPropertyElement propertyElement, ref string? alternativeValue)
+        {
+            bool shouldClone = false;
+            if (isInConfigurationDefaultsAsync(propertyElement.Name).GetAwaiter().GetResult())
+            {
+                alternativeValue = GetDefaultPropertyValueAsync(propertyElement.Name).GetAwaiter().GetResult();
+                shouldClone = true;
+            }
+
+            return shouldClone;
+        }
+
+
+        public bool ShouldElementBeCloned(ProjectMetadataElement metadataElement, ref string? alternativeValue)
+        {
+            bool shouldClone = false;
+            if (isInConfigurationDefaultsAsync(metadataElement.Name).GetAwaiter().GetResult())
+            {
+                alternativeValue = GetDefaultPropertyValueAsync(metadataElement.Name).GetAwaiter().GetResult();
+                shouldClone = true;
+            }
+
+            return shouldClone;
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Properties/CloneConfigurationFixupFactory.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.ProjectSystem.VS.Properties;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties
+{
+    /// <summary>
+    /// Interface for components that provide post-configuration cloning property fix ups.
+    /// </summary>
+    [Export(typeof(ICloneConfigurationFixupFactory))]
+    [AppliesTo(ProjectCapabilities.Managed)]
+    internal class CloneConfigurationFixupFactory : ICloneConfigurationFixupFactory
+    {
+        private readonly IUnconfiguredProjectServices _unconfiguredProjectServices;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CloneConfigurationFixup"/> class.
+        /// </summary>>
+        [ImportingConstructor]
+        public CloneConfigurationFixupFactory(IUnconfiguredProjectServices unconfiguredProjectServices)
+        {
+            _unconfiguredProjectServices = unconfiguredProjectServices;
+        }
+
+        /// <summary>
+        /// Prepares for a configuration cloning operation.
+        /// </summary>
+        /// <param name="sourceConfiguredProject">The template configuration.</param>
+        /// <param name="newConfigurationName">The configuration being cloned to.</param>
+        /// <returns>
+        /// A fixup dedicated to a single cloning operation, or null if
+        /// this factory has no special handling that is applicable for the
+        /// given configurations.
+        /// </returns>
+        public IClonePlatformFixup CreateCloneFixup(ConfiguredProject? sourceConfiguredProject, string newConfigurationName)
+        {
+            return new CloneConfigurationFixup(_unconfiguredProjectServices,
+                                                sourceConfiguredProject,
+                                                newConfigurationName);
+        }
+    }
+}
