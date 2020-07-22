183d4626caf94fa85a2b1a4d14473c57cb343221
Jonathon Marolf <jmarolf@microsoft.com>
2020-07-22 16:09:17 -0700

    do not override PostInitPage if you are only going to call into its base method

--- src/Microsoft.VisualStudio.Editors/PropPages/CodeAnalysisPropPage.vb
+++ src/Microsoft.VisualStudio.Editors/PropPages/CodeAnalysisPropPage.vb
@@ -1,4 +1,4 @@
-' Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+﻿' Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
 Imports System.Windows.Forms
 
@@ -19,10 +19,6 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
 
             'Add any initialization after the InitializeComponent() call
             AddChangeHandlers()
-        End Sub
-
-        Protected Overrides Sub PostInitPage()
-            MyBase.PostInitPage()
         End Sub
 
         Protected Overrides ReadOnly Property ControlData As PropertyControlData()
