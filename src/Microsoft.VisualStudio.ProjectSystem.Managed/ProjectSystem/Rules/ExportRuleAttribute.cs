ff1fbe64f4fe0ecfaedf5f88ba6a42605006afe5
David Kean <davkean@microsoft.com>
2020-07-24 17:23:41 +1000

    First pass at embedding rules
    This is the first pass at embedding non-localized rules, which will let us use rules even in cases where our design-time targets are not imported. This is important for enabling PackageReference support for C++ and other project types.
    
    This is first pass and picked some of the easy rules that lived in a single context. Localization support needs CPS changes.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Rules/ExportRuleAttribute.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Rules
+{
+    /// <summary>
+    ///     Exports a XAML-based embedded rule.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    internal class ExportRuleAttribute : ExportPropertyXamlRuleDefinitionAttribute
+    {
+        /// <summary>
+        ///     Initializes the <see cref="ExportRuleAttribute"/> class with the specified rule name and context.
+        /// </summary>
+        /// <param name="ruleName">
+        ///     The name of the rule without '.xaml', for example, 'ConfigurationGeneral'.
+        /// </param>
+        /// <param name="contexts">
+        ///     One or more of <see cref="PropertyPageContexts"/>.
+        /// </param>
+        public ExportRuleAttribute(string ruleName, params string[] contexts)
+            : base(ThisAssembly.FullName, $"XamlRuleToCode:{ruleName}.xaml", string.Join(";", contexts))
+        {
+            RuleName = ruleName;
+        }
+
+        /// <summary>
+        ///     Gets the name of the rule without '.xaml', for example, 'ConfigurationGeneral'.
+        /// </summary>
+        public string RuleName
+        {
+            get;
+        }
+    }
+}
--- tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Setup/SwrTests.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Setup/SwrTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.ProjectSystem.Rules;
 using Microsoft.VisualStudio.Utilities;
 using Xunit;
 using Xunit.Abstractions;
@@ -63,6 +64,12 @@ namespace Microsoft.VisualStudio.Setup
             {
                 var setupFiles = setupFilesByCulture[culture];
                 var ruleFiles = ruleFilesByCulture[culture];
+
+                var embeddedRules = RuleServices.GetAllEmbeddedRules()
+                                                .Select(name => name + ".xaml");
+
+                // Exclude the ones that are embedded, they won't be installed
+                ruleFiles = ruleFiles.Except(embeddedRules);
 
                 foreach (var missing in ruleFiles.Except(setupFiles, StringComparer.OrdinalIgnoreCase))
                 {
