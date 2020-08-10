ca3df43e87945b72233c9a9486330f9b51a930cf
David Kean <davkean@microsoft.com>
2020-08-10 12:52:45 +1000

    Mark package content items as "user read-only"
    Fixes: https://github.com/dotnet/project-system/issues/2141
    
    This marks package content items as user read-only, which causes them to be opened read-only in the editor.
    
    This depends on https://devdiv.visualstudio.com/DevDiv/_git/CPS/pullrequest/266425.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Tree/PackageContentProjectTreePropertiesProvider.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.ComponentModel.Composition;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Tree
+{
+    /// <summary>
+    ///     Marks content items that come from packages as "user read-only".
+    /// </summary>
+    [Export(typeof(IProjectTreePropertiesProvider))]
+    [AppliesTo(ProjectCapability.PackageReferences)]
+    internal class PackageContentProjectTreePropertiesProvider : IProjectTreePropertiesProvider
+    {
+        public void CalculatePropertyValues(IProjectTreeCustomizablePropertyContext propertyContext, IProjectTreeCustomizablePropertyValues propertyValues)
+        {
+            // Package content items always come in as linked items, so to reduce
+            // the number of items we look at, we limit ourselves to them
+            if (propertyValues.Flags.Contains(ProjectTreeFlags.LinkedItem) && 
+                propertyContext.Metadata != null &&
+                propertyContext.Metadata.TryGetValue(None.NuGetPackageIdProperty, out string packageId) && packageId.Length > 0)
+            {
+                // TODO: Replace this with strongly typed value when we next update
+                propertyValues.Flags |= ProjectTreeFlags.Create("UserReadOnly");
+            }
+        }
+    }
+}
