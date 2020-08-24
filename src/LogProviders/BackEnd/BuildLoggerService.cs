85565979b930289eebf584345f5eeea966d33d3b
Eric Yoon <t-eriyoo@microsoft.com>
2020-08-24 13:35:21 -0700

    Removing MEFComponent export from BuildLoggerService

--- src/LogProviders/BackEnd/BuildLoggerService.cs
+++ src/LogProviders/BackEnd/BuildLoggerService.cs
@@ -13,7 +13,6 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tools.BuildLogging.Model.BackEnd
     /// <summary>
     /// Implements IBuildLoggerService that interacts with Codespaces
     /// </summary>
-    [Export (typeof(IBuildLoggerService))]
     public sealed class BuildLoggerService : IBuildLoggerService
     {
         private readonly ILoggingDataSource _dataSource;
