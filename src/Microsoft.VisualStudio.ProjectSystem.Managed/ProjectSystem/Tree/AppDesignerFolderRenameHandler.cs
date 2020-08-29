c3fe27e283b0a0c30be6e3d6cda37232d86b0fc4
David Kean <davkean@microsoft.com>
2020-08-29 20:16:34 +1000

    Fix AppDesignerFolder when renamed
    Fixes: https://github.com/dotnet/project-system/issues/81

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Tree/AppDesignerFolderRenameHandler.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Tree
+{
+    /// <summary>
+    ///     Responsible for setting the AppDesignerFolder property for when the AppDesigner
+    ///     folder ("Properties" in C# and "My Project" in Visual Basic) is renamed.
+    /// </summary>
+    [Order(Order.Default)]
+    [Export(typeof(IProjectTreeActionHandler))]
+    [AppliesTo(ProjectCapability.AppDesigner)]
+    internal class AppDesignerFolderRenameHandler : ProjectTreeActionHandlerBase
+    {
+        private readonly IActiveConfiguredValue<ProjectProperties> _properties;
+
+        [ImportingConstructor]
+        public AppDesignerFolderRenameHandler(IActiveConfiguredValue<ProjectProperties> properties)
+        {
+            _properties = properties;
+        }
+
+        public override async Task RenameAsync(IProjectTreeActionHandlerContext context, IProjectTree node, string value)
+        {
+            await base.RenameAsync(context, node, value);
+
+            if (node.Flags.Contains(ProjectTreeFlags.AppDesignerFolder))
+            {
+                AppDesigner appDesigner = await _properties.Value.GetAppDesignerPropertiesAsync();
+
+                await appDesigner.FolderName.SetUnevaluatedValueAsync(value);
+            }
+        }
+    }
+}
