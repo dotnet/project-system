4d4985947e621863d047fe6ead5d2ff1bc3f90e6
Eric Yoon <t-eriyoo@microsoft.com>
2020-08-31 18:52:20 -0700

    change header

--- src/LogModel/RpcContracts/RpcDescriptors.cs
+++ src/LogModel/RpcContracts/RpcDescriptors.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
 using Microsoft.ServiceHub.Framework;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Tools.BuildLogging.Model.RpcContracts
