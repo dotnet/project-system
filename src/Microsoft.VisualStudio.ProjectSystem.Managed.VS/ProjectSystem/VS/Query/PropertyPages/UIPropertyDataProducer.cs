e1bac37fd2b56c9f76fd1af151554bfaff5df180
Drew Noakes <git@drewnoakes.com>
2020-10-07 12:29:19 +1100

    Provide empty SearchTerms to avoid exception on client

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Query/PropertyPages/UIPropertyDataProducer.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Query/PropertyPages/UIPropertyDataProducer.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework.XamlTypes;
 using Microsoft.VisualStudio.ProjectSystem.Properties;
@@ -89,6 +90,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Query
             if (requestedProperties.SearchTerms)
             {
                 // TODO: extract search terms from property metadata.
+                newUIProperty.SearchTerms = ImmutableList<string>.Empty;
             }
 
             ((IEntityValueFromProvider)newUIProperty).ProviderState = (cache, property.ContainingRule, property.Name);
