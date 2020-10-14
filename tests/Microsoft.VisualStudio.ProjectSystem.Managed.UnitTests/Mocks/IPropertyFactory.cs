da498a6a43c86ce7d962087ceb7e6b973d5c5735
Tom Meschter <tomescht@microsoft.com>
2020-10-14 13:54:56 -0700

    Add ProjectActionProvider tests.
    Also expand the set of helper methods for creating mocks.

--- tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Mocks/IPropertyFactory.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Mocks/IPropertyFactory.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.ProjectSystem.Properties;
 using Moq;
 
@@ -15,6 +17,26 @@ namespace Microsoft.VisualStudio.ProjectSystem
             if (admissibleValues is not null)
             {
                 mock.Setup(m => m.GetAdmissibleValuesAsync()).ReturnsAsync(new ReadOnlyCollection<IEnumValue>(admissibleValues));
+            }
+
+            return mock.Object;
+        }
+
+        public static IProperty Create(
+            string? name = null,
+            Action<object?>? setValue = null)
+        {
+            var mock = new Mock<IProperty>();
+
+            if (name is not null)
+            {
+                mock.SetupGet(m => m.Name).Returns(name);
+            }
+
+            if (setValue is not null)
+            {
+                mock.Setup(m => m.SetValueAsync(It.IsAny<object?>()))
+                    .Returns((object? o) => { setValue(o); return Task.CompletedTask; }); 
             }
 
             return mock.Object;
--- tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/IPropertyPageQueryCacheFactory.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/IPropertyPageQueryCacheFactory.cs
@@ -1,5 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
 using Microsoft.VisualStudio.ProjectSystem.VS.Query;
 using Moq;
 
@@ -7,9 +11,40 @@ namespace Microsoft.VisualStudio.ProjectSystem
 {
     public static class IPropertyPageQueryCacheFactory
     {
-        internal static IPropertyPageQueryCache Create()
+        internal static IPropertyPageQueryCache Create(
+            IImmutableSet<ProjectConfiguration>? projectConfigurations = null,
+            Func<ProjectConfiguration, string, IRule>? bindToRule = null)
         {
             var mock = new Mock<IPropertyPageQueryCache>();
+
+            if (projectConfigurations is not null)
+            {
+                mock.Setup(cache => cache.GetKnownConfigurationsAsync()).ReturnsAsync(projectConfigurations);
+            }
+
+            if (bindToRule is not null)
+            {
+                mock.Setup(cache => cache
+                    .BindToRule(It.IsAny<ProjectConfiguration>(), It.IsAny<string>()))
+                    .Returns((ProjectConfiguration config, string schema) => Task.FromResult<IRule?>(bindToRule(config, schema)));
+            }
+
+            return mock.Object;
+        }
+    }
+
+    public static class IPropertyPageQueryCacheProviderFactory
+    {
+        internal static IPropertyPageQueryCacheProvider Create()
+        {
+            var cache = IPropertyPageQueryCacheFactory.Create();
+            return Create(cache);
+        }
+
+        internal static IPropertyPageQueryCacheProvider Create(IPropertyPageQueryCache cache)
+        {
+            var mock = new Mock<IPropertyPageQueryCacheProvider>();
+            mock.Setup(f => f.CreateCache(It.IsAny<UnconfiguredProject>())).Returns(cache);
 
             return mock.Object;
         }
