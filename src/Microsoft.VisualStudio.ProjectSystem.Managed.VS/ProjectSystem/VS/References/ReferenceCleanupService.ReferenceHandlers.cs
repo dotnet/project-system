2d5906414a6eb90092ff11108dc6e030aca1bd7b
Osvaldo Calles <osvaldo.calles@gmail.com>
2020-10-19 13:03:35 -0700

    Implement IReferenceCleanupReference.
    Feature Remove Unused References.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/References/ReferenceCleanupService.ReferenceHandlers.cs
@@ -0,0 +1,190 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+using Microsoft.VisualStudio.ProjectSystem.VS.References.Roslyn;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.References
+{
+    internal partial class ReferenceCleanupService
+    {
+        private abstract class ReferenceHandler
+        {
+            protected readonly ReferenceType _referenceType;
+            protected readonly string _schema;
+            private IProjectRuleSnapshot _snapshotReferences;
+
+            protected ReferenceHandler(ReferenceType referenceType, string schema)
+            {
+                _referenceType = referenceType;
+                _schema = schema;
+            }
+
+            public IProjectRuleSnapshot GetProjectSnapshot(ConfiguredProject selectedConfiguredProject)
+            {
+                IProjectSubscriptionService? serviceSubscription = selectedConfiguredProject.Services.ProjectSubscription;
+                Assumes.Present(serviceSubscription);
+
+                serviceSubscription.ProjectRuleSource.SourceBlock.TryReceive(filter, out IProjectVersionedValue<IProjectSubscriptionUpdate> item);
+
+                _snapshotReferences = item.Value.CurrentState[_schema];
+
+                return _snapshotReferences;
+            }
+
+            private static bool filter(IProjectVersionedValue<IProjectSubscriptionUpdate> obj)
+            {
+                return true;
+            }
+
+            public void GetAndAppendReferences(List<Reference> references)
+            {
+                foreach (var item in _snapshotReferences.Items)
+                {
+                    string treatAsUsed = GetAttributeTreatAsUsed(item);
+
+                    string itemSpecification = GetAttributeItemSpecification(item);
+
+                    references.Add(new Reference(_referenceType, itemSpecification, treatAsUsed == "True"));
+                }
+            }
+
+            private string GetAttributeTreatAsUsed(KeyValuePair<string, IImmutableDictionary<string, string>> item)
+            {
+                item.Value.TryGetValue("TreatAsUsed", out string treatAsUsed);
+                return treatAsUsed;
+            }
+
+            protected abstract string GetAttributeItemSpecification(KeyValuePair<string, IImmutableDictionary<string, string>> item);
+
+            public abstract void RemoveReference(ConfiguredProject configuredProject, Reference reference);
+            public abstract void AddReference(ConfiguredProject configuredProject, Reference reference);
+            public abstract Task UpdateReferenceAsync(ConfiguredProject configuredProject, Reference reference);
+        }
+
+        private class ProjectReferenceHandler : ReferenceHandler
+        {
+            internal ProjectReferenceHandler() : base(ReferenceType.Project, ProjectReference.SchemaName)
+            { }
+
+            public override void AddReference(ConfiguredProject configuredProject, Reference reference)
+            {
+                Assumes.Present(configuredProject.Services.ProjectReferences);
+                configuredProject.Services.ProjectReferences.AddAsync(reference.ItemSpecification);
+            }
+
+            public override void RemoveReference(ConfiguredProject configuredProject, Reference reference)
+            {
+                Assumes.Present(configuredProject.Services.ProjectReferences);
+                configuredProject.Services.ProjectReferences.RemoveAsync(reference.ItemSpecification);
+
+            }
+
+            public override Task UpdateReferenceAsync(ConfiguredProject configuredProject, Reference reference)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override string GetAttributeItemSpecification(KeyValuePair<string, IImmutableDictionary<string, string>> item)
+            {
+                item.Value.TryGetValue("Identity", out string itemSpecification);
+                return itemSpecification;
+            }
+        }
+
+        private class PackageReferenceHandler : ReferenceHandler
+        {
+            internal PackageReferenceHandler() : base(ReferenceType.Package, PackageReference.SchemaName)
+            { }
+
+            public override void AddReference(ConfiguredProject configuredProject, Reference reference)
+            {
+                Assumes.Present(configuredProject.Services.PackageReferences);
+                string packageIdentity = reference.ItemSpecification;
+                string version = "";
+                configuredProject.Services.PackageReferences.AddAsync(packageIdentity, version);
+            }
+
+            public override void RemoveReference(ConfiguredProject configuredProject, Reference reference)
+            {
+                Assumes.Present(configuredProject.Services.PackageReferences);
+                configuredProject.Services.PackageReferences.RemoveAsync(reference.ItemSpecification);
+            }
+
+            public override Task UpdateReferenceAsync(ConfiguredProject configuredProject, Reference reference)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override string GetAttributeItemSpecification(KeyValuePair<string, IImmutableDictionary<string, string>> item)
+            {
+                item.Value.TryGetValue("Name", out string itemSpecification);
+                return itemSpecification;
+            }
+        }
+
+        private class AssemblyReferenceHandler : ReferenceHandler
+        {
+            internal AssemblyReferenceHandler() : base(ReferenceType.Assembly, AssemblyReference.SchemaName)
+            { }
+
+            public override void AddReference(ConfiguredProject configuredProject, Reference reference)
+            {
+                Assumes.Present(configuredProject.Services.AssemblyReferences);
+                AssemblyName assemblyName;
+                string assemblyPath = "";
+                //_configuredProject.Services.AssemblyReferences.AddAsync(assemblyName: assemblyName, assemblyPath);
+            }
+
+            public override void RemoveReference(ConfiguredProject configuredProject, Reference reference)
+            {
+                Assumes.Present(configuredProject.Services.PackageReferences);
+                configuredProject.Services.AssemblyReferences.RemoveAsync(null, reference.ItemSpecification);
+            }
+
+            public override Task UpdateReferenceAsync(ConfiguredProject configuredProject, Reference reference)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override string GetAttributeItemSpecification(KeyValuePair<string, IImmutableDictionary<string, string>> item)
+            {
+                item.Value.TryGetValue("SDKName", out string itemSpecification);
+                return itemSpecification;
+            }
+        }
+
+        private class SdkReferenceHandler : ReferenceHandler
+        {
+            internal SdkReferenceHandler() : base(ReferenceType.Unknown, SdkReference.SchemaName)
+            { }
+
+            public override void AddReference(ConfiguredProject configuredProject, Reference reference)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void RemoveReference(ConfiguredProject configuredProject, Reference reference)
+            {
+                // Do not remove Sdks
+            }
+
+            public override Task UpdateReferenceAsync(ConfiguredProject configuredProject, Reference reference)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override string GetAttributeItemSpecification(KeyValuePair<string, IImmutableDictionary<string, string>> item)
+            {
+                item.Value.TryGetValue("Name", out string itemSpecification);
+                return itemSpecification;
+            }
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/References/ReferenceCleanupService.cs
@@ -0,0 +1,251 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Build.Exceptions;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+using Microsoft.VisualStudio.ProjectSystem.VS.References.Roslyn;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.References
+{
+    [Export(typeof(IReferenceCleanupService))]
+    internal partial class ReferenceCleanupService : IReferenceCleanupService
+    {
+        ReferenceHandler _projectReferenceHandler = new ProjectReferenceHandler();
+        ReferenceHandler _packageReferenceHandler = new PackageReferenceHandler();
+        ReferenceHandler _assemblyReferenceHandler = new AssemblyReferenceHandler();
+        ReferenceHandler _sdkReferenceHandler = new SdkReferenceHandler();
+
+        private readonly ConfiguredProject _configuredProject;
+        private readonly IVsUIService<DTE> _dte;
+        private readonly IVsUIService<SVsSolution, IVsSolution> _solution;
+
+        private Dictionary<ReferenceType, string> _referenceTypes = new Dictionary<ReferenceType, string>();
+
+        [ImportingConstructor]
+        public ReferenceCleanupService(ConfiguredProject configuredProject, IVsUIService<SDTE, DTE> dte, IVsUIService<SVsSolution, IVsSolution> solution)
+        {
+            _configuredProject = configuredProject;
+            _dte = dte;
+            _solution = solution;
+        }
+
+        public Task<string> GetProjectAssetsFilePathAsync(string projectPath, string targetFramework, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ImmutableArray<Reference>> GetProjectReferencesAsync(string projectPath, string targetFramework, CancellationToken cancellationToken)
+        {
+            List<Reference> references;
+
+            try
+            {
+                ConfiguredProject activeConfiguredProject = await GetActiveConfiguredProjectByPathAsync(projectPath);
+
+                references = await GetAllReferencesInConfiguredProjectAsync(activeConfiguredProject);
+            }
+            catch
+            {
+                throw new InvalidProjectFileException();
+            }
+
+            return references.ToImmutableArray();
+        }
+
+        private async Task<ConfiguredProject> GetActiveConfiguredProjectByPathAsync(string projectPath)
+        {
+            var unconfiguredProjectPath = FindUnconfiguredProjectByPath(projectPath);
+
+            var activeConfiguredProject = await unconfiguredProjectPath.GetSuggestedConfiguredProjectAsync();
+            if (activeConfiguredProject is null)
+            {
+                throw new InvalidProjectFileException();
+            }
+
+            return activeConfiguredProject;
+        }
+
+        private UnconfiguredProject FindUnconfiguredProjectByPath(string projectPath)
+        {
+            var unconfigProjects = _configuredProject.Services.ProjectService.LoadedUnconfiguredProjects;
+
+            UnconfiguredProject unconfiguredProject = unconfiguredProject = unconfigProjects.First(project =>
+                StringComparers.Paths.Equals((string)project.FullPath, projectPath));
+
+            return unconfiguredProject;
+        }
+
+        private async Task<List<Reference>> GetAllReferencesInConfiguredProjectAsync(ConfiguredProject selectedConfiguredProject)
+        {
+            GetProjectSnapshotAsync(selectedConfiguredProject);
+
+            var references = GetReferences();
+
+            return references;
+        }
+
+        private void GetProjectSnapshotAsync(ConfiguredProject selectedConfiguredProject)
+        {
+            _projectReferenceHandler.GetProjectSnapshot(selectedConfiguredProject);
+            _packageReferenceHandler.GetProjectSnapshot(selectedConfiguredProject);
+            _assemblyReferenceHandler.GetProjectSnapshot(selectedConfiguredProject);
+            _sdkReferenceHandler.GetProjectSnapshot(selectedConfiguredProject);
+        }
+
+        private List<Reference> GetReferences()
+        {
+            List<Reference> references = new List<Reference>();
+
+            _projectReferenceHandler.GetAndAppendReferences(references);
+            _packageReferenceHandler.GetAndAppendReferences(references);
+            _assemblyReferenceHandler.GetAndAppendReferences(references);
+            _sdkReferenceHandler.GetAndAppendReferences(references);
+
+            return references;
+        }
+
+        public async Task<bool> UpdateReferencesAsync(string projectPath, string targetFramework,
+            ImmutableArray<ReferenceUpdate> referenceUpdates, CancellationToken cancellationToken)
+        {
+            ConfiguredProject activeConfiguredProject = await GetActiveConfiguredProjectByPathAsync(projectPath);
+
+            referenceUpdates.Where(c => c.Action == UpdateAction.Remove).ToList().ForEach(e =>
+            {
+                ReferenceHandler referenceHandler = FindReferenceHandler(e);
+                referenceHandler.RemoveReference(activeConfiguredProject, e.Reference);
+            });
+
+            referenceUpdates.Where(c => c.Action == UpdateAction.Add).ToList().ForEach(e =>
+            {
+                ReferenceHandler referenceHandler = FindReferenceHandler(e);
+                referenceHandler.AddReference(activeConfiguredProject, e.Reference);
+            });
+
+            var updateActions = referenceUpdates.Where(c => c.Action == UpdateAction.Update);
+            ExecuteUpdateReference(activeConfiguredProject, updateActions);
+            
+            return true;
+        }
+
+        private ReferenceHandler FindReferenceHandler(ReferenceUpdate referenceUpdate)
+        {
+            ReferenceHandler referenceHandler;
+            if (referenceUpdate.Reference.Type == ReferenceType.Project)
+            {
+                referenceHandler = _projectReferenceHandler;
+            }
+            else if (referenceUpdate.Reference.Type == ReferenceType.Package)
+            {
+                referenceHandler = _packageReferenceHandler;
+            }
+            else if (referenceUpdate.Reference.Type == ReferenceType.Assembly)
+            {
+                referenceHandler = _assemblyReferenceHandler;
+            }
+            else
+            {
+                referenceHandler = _sdkReferenceHandler;
+            }
+
+            return referenceHandler;
+        }
+
+        private async Task ExecuteUpdateReference(ConfiguredProject activeConfiguredProject, IEnumerable<ReferenceUpdate> updateActions)
+        {
+            // Handle all update actions together because opening the project file for edits uses
+            // locks and we don't want to delay other processes.
+            string projectPath = activeConfiguredProject.UnconfiguredProject.FullPath;
+
+            // TODO - open the project and and write to TreatAsUsed attribute using msbuild
+            await activeConfiguredProject.Services.ProjectLockService.WriteLockAsync(async access =>
+            {
+                var projectXmlAsync = await access.GetProjectXmlAsync("");
+                var items = projectXmlAsync.Items;
+
+                foreach (var update in updateActions)
+                {
+                    ReferenceHandler referenceHandler = FindReferenceHandler(update);
+
+                    foreach (var item in items)
+                    {
+
+                        if (item.ItemType == _referenceTypes[update.Reference.Type] && item.Include == update.Reference.ItemSpecification)
+                        {
+                            foreach (var child in item.Children)
+                            {
+                                if (child.ElementName == "TreatAsUsed")
+                                {
+
+                                }
+                            }
+                        }
+                    }
+                }
+
+                access.CheckoutAsync(projectPath);
+            }, CancellationToken.None);
+        }
+
+        public bool IsProjectCpsBased(string projectPath)
+        {
+            IVsHierarchy? projectHierarchy = GetProjectHierarchy(projectPath);
+            var isCps = projectHierarchy.IsCapabilityMatch("CPS");
+            return isCps;
+        }
+
+        private IVsHierarchy? GetProjectHierarchy(string projectPath)
+        {
+            Project project = TryGetProjectFromPath(projectPath);
+            if (project == null)
+            {
+                return null;
+            }
+
+            return TryGetIVsHierarchy(project);
+        }
+
+        private IVsHierarchy? TryGetIVsHierarchy(Project project)
+        {
+            if (_solution.Value.GetProjectOfUniqueName(project.UniqueName, out IVsHierarchy projectHierarchy) == HResult.OK)
+            {
+                return projectHierarchy;
+            }
+
+            return null;
+        }
+
+        private Project? TryGetProjectFromPath(string projectPath)
+        {
+            foreach (Project project in _dte.Value.Solution.Projects)
+            {
+                string? fullName;
+                try
+                {
+                    fullName = project.FullName;
+                }
+                catch (Exception)
+                {
+                    // DTE COM calls can fail for any number of valid reasons.
+                    continue;
+                }
+
+                if (StringComparers.Paths.Equals(fullName, projectPath))
+                {
+                    return project;
+                }
+            }
+
+            return null;
+        }
+    }
+}
