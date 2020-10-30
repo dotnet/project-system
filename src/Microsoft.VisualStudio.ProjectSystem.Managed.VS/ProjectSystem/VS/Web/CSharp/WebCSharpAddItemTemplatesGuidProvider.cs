3dcd086de03a76991f604e1b55bf49a4f7d6de53
Bill Hiebert <billhie@microsoft.com>
2020-10-30 15:50:02 -0700

    Add support for C#/VB item templates guid

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Web/CSharp/WebCSharpAddItemTemplatesGuidProvider.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.ComponentModel.Composition;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Web
+{
+    /// <summary>
+    /// Required by CPS in Dev15+ to specify the add item guid of the Web project system for C# tempaltes
+    /// </summary>
+    [Export(typeof(IAddItemTemplatesGuidProvider))]
+    [AppliesTo(ProjectCapability.AspNet + " & " + ProjectCapability.CSharp)]
+    [Order(10000)]
+    internal class WebCSharpAddItemTemplatesGuidProvider : IAddItemTemplatesGuidProvider
+    {
+        private const string WebCSharpAddItemTemplateGuidString = "349C5853-65DF-11DA-9384-00065B846F21";
+        private static Guid s_itemTemplateGuid = new Guid(WebCSharpAddItemTemplateGuidString);
+
+        [ImportingConstructor]
+        public WebCSharpAddItemTemplatesGuidProvider(UnconfiguredProject unconfiguredProject)
+        {
+            UnconfiguredProject = unconfiguredProject;
+        }
+
+        private UnconfiguredProject UnconfiguredProject { get; }
+
+        public Guid AddItemTemplatesGuid => s_itemTemplateGuid;
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Web/VB/WebVBAddItemTemplatesGuidProvider.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.ComponentModel.Composition;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Web
+{
+    /// <summary>
+    /// Required by CPS in Dev15+ to specify the add item guid of the Web project system for C# tempaltes
+    /// </summary>
+    [Export(typeof(IAddItemTemplatesGuidProvider))]
+    [AppliesTo(ProjectCapability.AspNet + " & " + ProjectCapability.VisualBasic)]
+    [Order(10000)]
+    internal class WebVBAddItemTemplatesGuidProvider : IAddItemTemplatesGuidProvider
+    {
+        private const string WebVBAddItemTemplateGuidString = "349C5854-65DF-11DA-9384-00065B846F21";
+        private static Guid s_itemTemplateGuid = new Guid(WebVBAddItemTemplateGuidString);
+
+        [ImportingConstructor]
+        public WebVBAddItemTemplatesGuidProvider(UnconfiguredProject unconfiguredProject)
+        {
+            UnconfiguredProject = unconfiguredProject;
+        }
+
+        private UnconfiguredProject UnconfiguredProject { get; }
+
+        public Guid AddItemTemplatesGuid => s_itemTemplateGuid;
+    }
+}
