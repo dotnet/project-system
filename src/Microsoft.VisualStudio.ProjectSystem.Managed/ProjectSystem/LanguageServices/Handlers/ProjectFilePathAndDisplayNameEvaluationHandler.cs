341b22993c8b3ec0c4877d36a55acb6c06e85389
Osvaldo Calles <osvaldo.calles@gmail.com>
2020-11-05 14:10:58 -0800

    Fix unit-tests

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/LanguageServices/Handlers/ProjectFilePathAndDisplayNameEvaluationHandler.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/LanguageServices/Handlers/ProjectFilePathAndDisplayNameEvaluationHandler.cs
@@ -42,7 +42,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.LanguageServices.Handlers
             VerifyInitialized();
 
             if (projectChange.Difference.ChangedProperties.Contains(ConfigurationGeneral.MSBuildProjectFullPathProperty) &&
-                projectChange.After.Properties.TryGetKey(ConfigurationGeneral.MSBuildProjectFullPathProperty, out string projectFilePath))
+                projectChange.After.Properties.TryGetValue(ConfigurationGeneral.MSBuildProjectFullPathProperty, out string projectFilePath))
             {
                 string displayName = GetDisplayName(projectFilePath);
 
