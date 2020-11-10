ce751bce359e0feab5f5a4ae0f0582e4e4f87641
Daniel Monroy <dmon@microsoft.com>
2020-11-10 12:37:45 -0800

    Create client package

--- setup/ClientSetup/AssemblyRedirects.cs
+++ setup/ClientSetup/AssemblyRedirects.cs
@@ -5,5 +5,4 @@ using Microsoft.VisualStudio;
 // These provide code-base binding redirects for the assemblies we provide in this VSIX. For NGEN purposes,
 // we also need to update src/appid/devenv/stub/devenv.urt.config.tt inside VS repo, which is done automatically 
 // via the Roslyn Insertion tool when it consumes artifacts\Debug\DevDivInsertionFiles\DependentAssemblyVersions.csv.
-[assembly: ProvideCodeBaseBindingRedirection("Microsoft.VisualStudio.ProjectSystem.Managed")]
-[assembly: ProvideCodeBaseBindingRedirection("Microsoft.VisualStudio.ProjectSystem.Managed.VS")]
+[assembly: ProvideCodeBaseBindingRedirection("Microsoft.VisualStudio.ProjectSystem.Managed.VS.Client")]
