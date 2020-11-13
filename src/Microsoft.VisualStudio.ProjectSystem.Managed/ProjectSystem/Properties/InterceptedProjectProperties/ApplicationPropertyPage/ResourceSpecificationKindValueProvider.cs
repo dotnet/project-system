c174d378db9c1f3a4c65642d574f39790480f31b
Drew Noakes <git@drewnoakes.com>
2020-11-13 12:53:09 +1100

    Add conditional visibility for resource properties

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/ApplicationPropertyPage/ResourceSpecificationKindValueProvider.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties
+{
+    [ExportInterceptingPropertyValueProvider("ResourceSpecificationKind", ExportInterceptingPropertyValueProviderFile.ProjectFile)]
+    internal sealed class ResourceSpecificationKindValueProvider : InterceptingPropertyValueProviderBase
+    {
+        // TODO should the rule file generate property and enum value constants that we can use here instead of these string literals?
+
+        public override Task<string?> OnSetPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IProjectProperties defaultProperties, IReadOnlyDictionary<string, string>? dimensionalConditions = null)
+        {
+            return Task.FromResult<string?>(null);
+        }
+
+        public override async Task<string> OnGetEvaluatedPropertyValueAsync(string propertyName, string evaluatedPropertyValue, IProjectProperties defaultProperties)
+        {
+            string win32Resource = await defaultProperties.GetEvaluatedPropertyValueAsync("Win32Resource");
+
+            return ComputeValue(win32Resource);
+        }
+
+        public override async Task<string> OnGetUnevaluatedPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IProjectProperties defaultProperties)
+        {
+            string? win32Resource = await defaultProperties.GetUnevaluatedPropertyValueAsync("Win32Resource");
+
+            return ComputeValue(win32Resource);
+        }
+
+        private static string ComputeValue(string? win32Resource)
+        {
+            return string.IsNullOrEmpty(win32Resource) ? "IconAndManifest" : "ResourceFile";
+        }
+    }
+}
