4dd596fa775e4cb077a7b072f070f978b1deff1a
Saint Wesonga <sawesong@microsoft.com>
2020-11-16 11:27:04 -0800

    Annotate GetTargetFramework method

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Frameworks/ITargetFrameworkProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Frameworks/ITargetFrameworkProvider.cs
@@ -1,5 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.VisualStudio.Composition;
 
 namespace Microsoft.VisualStudio.ProjectSystem
@@ -11,6 +12,7 @@ namespace Microsoft.VisualStudio.ProjectSystem
         /// Returns a <see cref="TargetFramework"/> instance for the target framework moniker
         /// or <see langword="null" /> if the framework moniker is null or empty.
         /// </summary>
+        [return: NotNullIfNotNull("targetFrameworkMoniker")]
         TargetFramework? GetTargetFramework(string? targetFrameworkMoniker);
     }
 }
