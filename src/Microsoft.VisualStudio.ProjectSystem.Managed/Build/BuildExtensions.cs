8f4648da2ca941202279e007ea67a74c296b5c23
David Kean <davkean@microsoft.com>
2020-11-16 15:56:31 +1100

    Move IsImported -> BuildExtensions

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/Build/BuildExtensions.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/Build/BuildExtensions.cs
@@ -3,6 +3,7 @@
 using System;
 using Microsoft.Build.Construction;
 using Microsoft.Build.Evaluation;
+using Microsoft.Build.Execution;
 
 namespace Microsoft.VisualStudio.Build
 {
@@ -19,6 +20,27 @@ namespace Microsoft.VisualStudio.Build
             Requires.NotNull(element, nameof(element));
 
             return ProjectCollection.Unescape(element.Value);
+        }
+
+        /// <summary>
+        ///     Returns a value indicating if the specified <see cref="ProjectItemInstance"/>
+        ///     originated in an imported file.
+        /// </summary>
+        /// <returns>
+        ///     <see langword="true"/> if <paramref name="item"/> originated in an imported file;
+        ///     otherwise, <see langword="false"/> if it was defined in the project being built.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="item"/> is <see langword="null"/>.
+        /// </exception>
+        public static bool IsImported(this ProjectItemInstance item)
+        {
+            Requires.NotNull(item, nameof(item));
+
+            string definingProjectFullPath = item.GetMetadataValue("DefiningProjectFullPath");
+            string projectFullPath = item.Project.FullPath; // NOTE: This returns project being built, not owning target
+
+            return !StringComparers.Paths.Equals(definingProjectFullPath, projectFullPath);
         }
     }
 }
