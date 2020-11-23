eda3f49304ddf6f8e3c088db62c6b4591c247b29
Daniel Monroy <dmon@microsoft.com>
2020-11-23 14:17:35 -0600

    Create new shared assembly

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.Shared/Packaging/ManagedProjectSystemSharedPackage.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace Microsoft.VisualStudio.Packaging
+{
+    [Guid(PackageGuid)]
+    [PackageRegistration(AllowsBackgroundLoading = true, RegisterUsing = RegistrationMethod.CodeBase, UseManagedResourcesOnly = true)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    internal sealed class ManagedProjectSystemSharedPackage : AsyncPackage
+    {
+        public const string PackageGuid = "72847D19-D894-4FB4-8A66-58BE8777A859";
+    }
+}
