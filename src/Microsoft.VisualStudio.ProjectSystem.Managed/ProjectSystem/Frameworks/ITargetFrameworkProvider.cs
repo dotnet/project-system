2c5dde824855e33805155aaa2c97aca4a7b603f5
Drew Noakes <git@drewnoakes.com>
2021-01-12 22:11:27 +1100

    Remove invalid annotation
    This method will return null if the input is an empty string.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Frameworks/ITargetFrameworkProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Frameworks/ITargetFrameworkProvider.cs
@@ -1,6 +1,5 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
-using System.Diagnostics.CodeAnalysis;
 using Microsoft.VisualStudio.Composition;
 
 namespace Microsoft.VisualStudio.ProjectSystem
@@ -12,7 +11,6 @@ namespace Microsoft.VisualStudio.ProjectSystem
         /// Returns a <see cref="TargetFramework"/> instance for the target framework moniker
         /// or <see langword="null" /> if the framework moniker is null or empty.
         /// </summary>
-        [return: NotNullIfNotNull("targetFrameworkMoniker")]
         TargetFramework? GetTargetFramework(string? targetFrameworkMoniker);
     }
 }
