46aae76f3266b06816df35758eacf22f1f359768
Drew Noakes <git@drewnoakes.com>
2021-01-21 21:34:44 +1100

    Add IActiveConfigurationComponent
    Similar to IImplicitlyActiveConfigurationComponent, this type is intended for configured-project scope exports to allow them to be activated and deactivated in response to their configuration becoming active or inactive.
    
    The existing IImplicitlyActiveConfigurationComponent does this for _implicitly_ active configurations, of which there may be many (when multi-targeting, for example). This alternative exists for the _singular_ active configuration, which is usually the first when multi-targeting.
    
    This pattern can be useful for deferring initialisation of components until their configuration is activated, reducing work on solution load for example. It can also help clean up components which are no longer applicable when the configuration is changed.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/ConfiguredProjectActivationTracking.Instance.cs
@@ -0,0 +1,103 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    internal sealed partial class ConfiguredProjectActivationTracking
+    {
+        internal sealed class ConfiguredProjectActivationTrackingInstance : OnceInitializedOnceDisposedAsync, IMultiLifetimeInstance
+        {
+            private readonly ConfiguredProject _project;
+            private readonly IActiveConfiguredProjectProvider _activeConfiguredProjectProvider;
+            private readonly ITargetBlock<IProjectVersionedValue<(IProjectCapabilitiesSnapshot, ConfiguredProject)>> _targetBlock;
+            private readonly OrderPrecedenceImportCollection<IActiveConfigurationComponent> _components;
+
+            private IReadOnlyCollection<IActiveConfigurationComponent> _activeComponents = Array.Empty<IActiveConfigurationComponent>();
+            private IDisposable? _subscription;
+
+            public ConfiguredProjectActivationTrackingInstance(
+                IProjectThreadingService threadingService,
+                ConfiguredProject project,
+                IActiveConfiguredProjectProvider activeConfiguredProjectProvider,
+                OrderPrecedenceImportCollection<IActiveConfigurationComponent> components)
+                : base(threadingService.JoinableTaskContext)
+            {
+                _project = project;
+                _activeConfiguredProjectProvider = activeConfiguredProjectProvider;
+                _targetBlock = DataflowBlockFactory.CreateActionBlock<IProjectVersionedValue<(IProjectCapabilitiesSnapshot, ConfiguredProject)>>(OnChange, project.UnconfiguredProject, ProjectFaultSeverity.LimitedFunctionality);
+                _components = components;
+            }
+
+            public Task InitializeAsync()
+            {
+                return InitializeAsync(CancellationToken.None);
+            }
+
+            protected override Task InitializeCoreAsync(CancellationToken cancellationToken)
+            {
+                _subscription = ProjectDataSources.SyncLinkTo(
+                    _project.Capabilities.SourceBlock.SyncLinkOptions(),
+                    _activeConfiguredProjectProvider.ActiveConfiguredProjectBlock.SyncLinkOptions(),
+                    linkOptions: DataflowOption.PropagateCompletion,
+                    target: _targetBlock,
+                    cancellationToken: cancellationToken);
+
+                return Task.CompletedTask;
+            }
+
+            protected override Task DisposeCoreAsync(bool initialized)
+            {
+                _subscription?.Dispose();
+                _targetBlock.Complete();
+
+                return DeactivateAsync(_activeComponents);
+            }
+
+            private async Task OnChange(IProjectVersionedValue<(IProjectCapabilitiesSnapshot, ConfiguredProject)> e)
+            {
+                // We'll get called back in main two situations (notwithstanding version-only updates):
+                //
+                //   - The active configuration changed, and our configuration might
+                //     be now active or might no longer be active.
+                //
+                //   - The capabilities changed in our configuration.
+                //
+                // In both situations, we may need to activate or deactivate 
+                // IActiveConfigurationComponent instances.
+
+                IProjectCapabilitiesSnapshot snapshot = e.Value.Item1;
+                bool isActive = e.Value.Item2.ProjectConfiguration.Equals(_project.ProjectConfiguration);
+
+                using var capabilitiesContext = ProjectCapabilitiesContext.CreateIsolatedContext(_project, snapshot);
+
+                // If we're not active, there are no future services to activate
+                IReadOnlyCollection<IActiveConfigurationComponent> futureComponents = isActive
+                    ? _components.Select(s => s.Value).ToList()
+                    : Array.Empty<IActiveConfigurationComponent>();
+
+                var diff = new SetDiff<IActiveConfigurationComponent>(_activeComponents, futureComponents);
+
+                await DeactivateAsync(diff.Removed);
+                await ActivateAsync(diff.Added);
+
+                _activeComponents = futureComponents;
+            }
+
+            private static Task DeactivateAsync(IEnumerable<IActiveConfigurationComponent> services)
+            {
+                return Task.WhenAll(services.Select(c => c.DeactivateAsync()));
+            }
+
+            private static Task ActivateAsync(IEnumerable<IActiveConfigurationComponent> services)
+            {
+                return Task.WhenAll(services.Select(c => c.ActivateAsync()));
+            }
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/IActiveConfigurationComponent.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Composition;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    /// <summary>
+    ///     A configured-project service which will be activated when its configured project becomes active, or deactivated when it is not.
+    /// </summary>
+    /// <remarks>
+    ///     Exports of this interface must update <see cref="ConfiguredProjectActivationTracking"/> with its associated capabilities.
+    /// </remarks>
+    [ProjectSystemContract(ProjectSystemContractScope.ConfiguredProject, ProjectSystemContractProvider.Private, Cardinality = ImportCardinality.ZeroOrMore)]
+    internal interface IActiveConfigurationComponent
+    {
+        /// <summary>
+        ///     Activates the service.
+        /// </summary>
+        Task ActivateAsync();
+
+        /// <summary>
+        ///     Deactivates the service.
+        /// </summary>
+        Task DeactivateAsync();
+    }
+}
