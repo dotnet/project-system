7dc577091278fd46e1fb6538572fe0b3160d45ff
Saint Wesonga <sawesong@microsoft.com>
2021-02-03 16:21:39 -0800

    Minor comments cleanup

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/Telemetry/IConfiguredProjectPackageRestoreTelemetryService.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/Telemetry/IConfiguredProjectPackageRestoreTelemetryService.cs
@@ -11,9 +11,14 @@ namespace Microsoft.VisualStudio.Telemetry
      * like intellisense is generated. Once nomination has completed and the package restore is up
      * to date, the package restore process needs to notify the operation progress system that package
      * restore has completed. This interface provides methods to enable this notification by posting
-     * a <see cref="TelemetryEventName.ProcessPackageRestore"/> event with the
-     * <see cref="TelemetryPropertyName.PackageRestoreOperation"/> operation property value
-     * PackageRestoreOperationNames.PackageRestoreProgressTrackerRestoreCompleted
+     * this event:
+     * 
+     *  ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * | Event Name                                             | Property Name                                                | Property Value                                                                           |
+     * |------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+     * | <see cref="TelemetryEventName.ProcessPackageRestore"/> | <see cref="TelemetryPropertyName.PackageRestoreOperation"/>  | <see cref="PackageRestoreOperationNames.PackageRestoreProgressTrackerRestoreCompleted"/> |
+     * |                                                        | <see cref="TelemetryPropertyName.PackageRestoreIsUpToDate"/> | True when operation progress is notified                                                 |
+     *  ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
      *
      * It also enables the package restore components to post telemetry events indicating
      * whether the initialization of the package restore components (for the ConfiguredProject) suceeded.
@@ -35,7 +40,9 @@ namespace Microsoft.VisualStudio.Telemetry
         void PostPackageRestoreEvent(string packageRestoreOperationName, long packageRestoreProgressTrackerId);
 
         /// <summary>
-        /// Posts a telemetry event from package restore components including whether package restore is up to date.
+        /// Posts a <see cref="TelemetryEventName.ProcessPackageRestore"/> event with a
+        /// <see cref="TelemetryPropertyName.PackageRestoreIsUpToDate"/> property whose value will be set to
+        /// indicate whether package restore is up to date.
         /// </summary>
         /// <param name="isRestoreUpToDate">Flag indicating whether the restore is up to date.</param>
         /// <param name="packageRestoreProgressTrackerId">An identifier to enable correlation of events.</param>
