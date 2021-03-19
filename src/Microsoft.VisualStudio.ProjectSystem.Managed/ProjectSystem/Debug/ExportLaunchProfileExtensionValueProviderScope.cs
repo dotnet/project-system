b7ca1044b00c18e0cf05e45d1606438071443cf2
Tom Meschter <tomescht@microsoft.com>
2021-03-19 14:57:54 -0700

    Define extension points for launch profiles
    Define the `ILaunchProfileExtensionValueProvider` and `IGlobalSettingExtensionValueProvider` interfaces to support reading and writing arbitrary properties in an `ILaunchProfile` and the `ILaunchSettings.GlobalSettings`.
    
    `ILaunchProfile` has a few pre-defined properties (like ExecutablePath, CommandLineArgs, LaunchBrowser, etc.) but it also supports storage of arbitrary objects via the `ImmutableDictionary<string, object>` accessible via the `OtherSettings` property, and in practice the bulk of the properties we care about are stored there. Separately, `ILaunchSettings.GlobalSettings` is another `ImmutableDictionary<string, object>` storing launch settings that are global (that is, not tied to a particular launch profile).
    
    In each case the property value comes in as a `string`, but needs to be stored in the dictionary as an `object`. While we could store it as a `string` by default this would be incorrect in many cases, and so we need access to a converter on a property-by-property basis. We also need to convert back from an `object` to a string, and changing one property may also require changes to other properties, and so we also need a way to modify the launch settings (or global settings) as a whole. These new interfaces provide the necessary abilities.
    
    The idea here is that the extension class will be tagged with the `ExportLaunchProfileExtensionValueProviderAttribute` indicating the names of the properties it handles, as well as whether it applies to the launch profiles or the global settings. In the former case it must implement `ILaunchProfileExtensionValueProvider`; in the latter `IGlobalSettingExtensionValueProvider`. The extensions will be imported by the `LaunchProfileProjectPropertiesProvider` which will redirect all reads and writes of the related properties through them.
    
    These interfaces do almost the same thing, but there are some important differences which lead to different API shapes. When accessing the properties of a launch profile we may want to know the current global settings, and so those are passed along. However, when modifying a global setting we would not expect it to depend on or modify values in individual launch profiles, so those are _not_ available.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/ExportLaunchProfileExtensionValueProviderScope.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+namespace Microsoft.VisualStudio.ProjectSystem.Debug
+{
+    /// <summary>
+    /// Specifies the "backing store" for an <see cref="ILaunchProfileExtensionValueProvider"/>
+    /// or <see cref="IGlobalSettingExtensionValueProvider"/>. This determines where the
+    /// property value is read from/stored to.
+    /// </summary>
+    public enum ExportLaunchProfileExtensionValueProviderScope
+    {
+        /// <summary>
+        /// Extension properties are backed by an <see cref="ILaunchProfile"/>. The related
+        /// type must implement <see cref="ILaunchProfileExtensionValueProvider"/>.
+        /// </summary>
+        LaunchProfile,
+        /// <summary>
+        /// Extensions properties are backed by <see cref="ILaunchSettings.GlobalSettings"/>.
+        /// The related type must implement <see cref="IGlobalSettingExtensionValueProvider"/>.
+        /// </summary>
+        GlobalSettings
+    }
+}
