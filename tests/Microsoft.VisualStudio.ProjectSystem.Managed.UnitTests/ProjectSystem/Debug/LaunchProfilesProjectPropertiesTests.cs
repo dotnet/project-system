6e1f758a79b3690029ab24ae4a69a38c453b23d8
Tom Meschter <tomescht@microsoft.com>
2021-03-22 13:21:22 -0700

    Simplify tests
    When testing the `LaunchProfilesProjectProperties` type we don't need to create a `LaunchProfilesProjectPropertiesProvider` first; we can create the type directly. This simplifies and shortens the tests.

--- tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/Debug/LaunchProfilesProjectPropertiesTests.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/ProjectSystem/Debug/LaunchProfilesProjectPropertiesTests.cs
@@ -2,7 +2,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Collections.Immutable;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.ProjectSystem.Properties;
 using Xunit;
@@ -13,23 +13,21 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
     {
         private const string DefaultTestProjectPath = @"C:\alpha\beta\gamma.csproj";
 
-        private static readonly IEnumerable<Lazy<ILaunchProfileExtensionValueProvider, ILaunchProfileExtensionValueProviderMetadata>> EmptyLaunchProfileExtensionValueProviders =
-            Enumerable.Empty<Lazy<ILaunchProfileExtensionValueProvider, ILaunchProfileExtensionValueProviderMetadata>>();
-        private static readonly IEnumerable<Lazy<IGlobalSettingExtensionValueProvider, ILaunchProfileExtensionValueProviderMetadata>> EmptyGlobalSettingExtensionValueProviders =
-            Enumerable.Empty<Lazy<IGlobalSettingExtensionValueProvider, ILaunchProfileExtensionValueProviderMetadata>>();
+        private static readonly ImmutableArray<Lazy<ILaunchProfileExtensionValueProvider, ILaunchProfileExtensionValueProviderMetadata>> EmptyLaunchProfileExtensionValueProviders =
+            ImmutableArray<Lazy<ILaunchProfileExtensionValueProvider, ILaunchProfileExtensionValueProviderMetadata>>.Empty;
+        private static readonly ImmutableArray<Lazy<IGlobalSettingExtensionValueProvider, ILaunchProfileExtensionValueProviderMetadata>> EmptyGlobalSettingExtensionValueProviders =
+            ImmutableArray<Lazy<IGlobalSettingExtensionValueProvider, ILaunchProfileExtensionValueProviderMetadata>>.Empty;
 
         [Fact]
         public void WhenRetrievingItemProperties_TheContextHasTheExpectedValues()
         {
-            var provider = new LaunchProfileProjectPropertiesProvider(
-                CreateDefaultTestProject(),
+            var properties = new LaunchProfileProjectProperties(
+                DefaultTestProjectPath,
+                "Profile1",
                 CreateDefaultTestLaunchSettings(),
                 EmptyLaunchProfileExtensionValueProviders,
                 EmptyGlobalSettingExtensionValueProviders);
 
-            var properties = provider.GetItemProperties(
-                itemType: LaunchProfileProjectItemProvider.ItemType,
-                item: "Profile1");
             var context = properties.Context;
 
             Assert.Equal(expected: DefaultTestProjectPath, actual: context.File);
@@ -41,15 +39,12 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
         [Fact]
         public void WhenRetrievingItemProperties_TheFilePathIsTheProjectPath()
         {
-            var provider = new LaunchProfileProjectPropertiesProvider(
-                CreateDefaultTestProject(),
+            var properties = new LaunchProfileProjectProperties(
+                DefaultTestProjectPath,
+                "Profile1",
                 CreateDefaultTestLaunchSettings(),
                 EmptyLaunchProfileExtensionValueProviders,
                 EmptyGlobalSettingExtensionValueProviders);
-
-            var properties = provider.GetItemProperties(
-                itemType: LaunchProfileProjectItemProvider.ItemType,
-                item: "Profile1");
 
             Assert.Equal(expected: DefaultTestProjectPath, actual: properties.FileFullPath);
         }
@@ -57,15 +52,12 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
         [Fact]
         public void WhenRetrievingItemProperties_ThePropertyKindIsItemGroup()
         {
-            var provider = new LaunchProfileProjectPropertiesProvider(
-                CreateDefaultTestProject(),
+            var properties = new LaunchProfileProjectProperties(
+                DefaultTestProjectPath,
+                "Profile1",
                 CreateDefaultTestLaunchSettings(),
                 EmptyLaunchProfileExtensionValueProviders,
                 EmptyGlobalSettingExtensionValueProviders);
-
-            var properties = provider.GetItemProperties(
-                itemType: LaunchProfileProjectItemProvider.ItemType,
-                item: "Profile1");
 
             Assert.Equal(expected: PropertyKind.ItemGroup, actual: properties.PropertyKind);
         }
@@ -77,14 +69,13 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
             var launchSettingsProvider = ILaunchSettingsProviderFactory.Create(
                 launchProfiles: new[] { profile1.ToLaunchProfile() });
 
-            var provider = new LaunchProfileProjectPropertiesProvider(
-                CreateDefaultTestProject(),
+            var properties = new LaunchProfileProjectProperties(
+                DefaultTestProjectPath,
+                "Profile1",
                 launchSettingsProvider,
                 EmptyLaunchProfileExtensionValueProviders,
                 EmptyGlobalSettingExtensionValueProviders);
-            var properties = provider.GetItemProperties(
-                itemType: LaunchProfileProjectItemProvider.ItemType,
-                item: "Profile1");
+
             var propertyNames = await properties.GetPropertyNamesAsync();
 
             Assert.Contains("CommandName", propertyNames);
@@ -103,14 +94,12 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
             var launchSettingsProvider = ILaunchSettingsProviderFactory.Create(
                 launchProfiles: new[] { profile1.ToLaunchProfile() });
 
-            var provider = new LaunchProfileProjectPropertiesProvider(
-                CreateDefaultTestProject(),
+            var properties = new LaunchProfileProjectProperties(
+                DefaultTestProjectPath,
+                "Profile1",
                 launchSettingsProvider,
                 EmptyLaunchProfileExtensionValueProviders,
                 EmptyGlobalSettingExtensionValueProviders);
-            var properties = provider.GetItemProperties(
-                itemType: LaunchProfileProjectItemProvider.ItemType,
-                item: "Profile1");
 
             var standardPropertyNames = new[]
             {
@@ -138,14 +127,12 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
             var launchSettingsProvider = ILaunchSettingsProviderFactory.Create(
                 launchProfiles: new[] { profile1.ToLaunchProfile() });
 
-            var provider = new LaunchProfileProjectPropertiesProvider(
-                CreateDefaultTestProject(),
+            var properties = new LaunchProfileProjectProperties(
+                DefaultTestProjectPath,
+                "Profile1",
                 launchSettingsProvider,
                 EmptyLaunchProfileExtensionValueProviders,
                 EmptyGlobalSettingExtensionValueProviders);
-            var properties = provider.GetItemProperties(
-                itemType: LaunchProfileProjectItemProvider.ItemType,
-                item: "Profile1");
 
             var evaluatedValue = await properties.GetEvaluatedPropertyValueAsync("LaunchBrowser");
             Assert.Equal(expected: "false", actual: evaluatedValue);
@@ -170,14 +157,12 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
 
             var launchSettingsProvider = ILaunchSettingsProviderFactory.Create(
                 launchProfiles: new[] { profile1.ToLaunchProfile() });
-            var provider = new LaunchProfileProjectPropertiesProvider(
-                CreateDefaultTestProject(),
+            var properties = new LaunchProfileProjectProperties(
+                DefaultTestProjectPath,
+                "Profile1",
                 launchSettingsProvider,
                 EmptyLaunchProfileExtensionValueProviders,
                 EmptyGlobalSettingExtensionValueProviders);
-            var properties = provider.GetItemProperties(
-                itemType: LaunchProfileProjectItemProvider.ItemType,
-                item: "Profile1");
 
             var expectedValues = new Dictionary<string, string>
             {
@@ -197,15 +182,6 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
                 Assert.Equal(expectedPropertyValue, actualUnevaluatedValue);
                 Assert.Equal(expectedPropertyValue, actualEvaluatedValue);
             }
-        }
-
-        /// <summary>
-        /// Creates an <see cref="UnconfiguredProject"/> where the <see cref="UnconfiguredProject.FullPath"/>
-        /// is set to <see cref="DefaultTestProjectPath"/>.
-        /// </summary>
-        private static UnconfiguredProject CreateDefaultTestProject()
-        {
-            return UnconfiguredProjectFactory.ImplementFullPath(DefaultTestProjectPath);
         }
 
         /// <summary>
