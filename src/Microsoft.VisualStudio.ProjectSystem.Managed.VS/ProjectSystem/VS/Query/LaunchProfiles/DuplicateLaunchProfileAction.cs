8bf9f08a32c96d16e81787991577d319cea04937
Tom Meschter <tomescht@microsoft.com>
2021-03-26 11:05:24 -0700

    Pass CancellationToken
    Pass `CancellationToken` to implementations of `LaunchProfileActionBase.ExecuteAsync`, and make use of it where possible. Most implementations just call into equivalent methods on `ILaunchSettingsProvider` which does not currently take a `CancellationToken`. Fixing that is being tracked by #7056.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Query/LaunchProfiles/DuplicateLaunchProfileAction.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Query/LaunchProfiles/DuplicateLaunchProfileAction.cs
@@ -7,6 +7,7 @@ using Microsoft.VisualStudio.ProjectSystem.Debug;
 using Microsoft.VisualStudio.ProjectSystem.Query;
 using Microsoft.VisualStudio.ProjectSystem.Query.ProjectModelMethods.Actions;
 using Microsoft.VisualStudio.ProjectSystem.Query.QueryExecution;
+using Microsoft.VisualStudio.Threading;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Query
 {
@@ -22,9 +23,9 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Query
             _executableStep = executableStep;
         }
 
-        protected override async Task ExecuteAsync(ILaunchSettingsProvider launchSettingsProvider)
+        protected override async Task ExecuteAsync(ILaunchSettingsProvider launchSettingsProvider, CancellationToken cancellationToken)
         {
-            ILaunchSettings? launchSettings = await launchSettingsProvider.WaitForFirstSnapshot(Timeout.Infinite);
+            ILaunchSettings? launchSettings = await launchSettingsProvider.WaitForFirstSnapshot(Timeout.Infinite).WithCancellation(cancellationToken);
             Assumes.NotNull(launchSettings);
 
             ILaunchProfile? existingProfile = launchSettings.Profiles.FirstOrDefault(p => StringComparers.LaunchProfileNames.Equals(p.Name, _executableStep.CurrentProfileName));
--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Query/LaunchProfiles/LaunchProfileActionBase.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Query/LaunchProfiles/LaunchProfileActionBase.cs
@@ -1,5 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.ProjectSystem.Debug;
 using Microsoft.VisualStudio.ProjectSystem.Query;
@@ -25,12 +26,12 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Query
             if (((IEntityValueFromProvider)result.Result).ProviderState is UnconfiguredProject project
                 && project.Services.ExportProvider.GetExportedValueOrDefault<ILaunchSettingsProvider>() is ILaunchSettingsProvider launchSettingsProvider)
             {
-                await ExecuteAsync(launchSettingsProvider);
+                await ExecuteAsync(launchSettingsProvider, result.Request.QueryExecutionContext.CancellationToken);
             }
 
             await ResultReceiver.ReceiveResultAsync(result);
         }
 
-        protected abstract Task ExecuteAsync(ILaunchSettingsProvider launchSettingsProvider);
+        protected abstract Task ExecuteAsync(ILaunchSettingsProvider launchSettingsProvider, CancellationToken cancellationToken);
     }
 }
