788c82cc169c571412e30b8d2bf9ca9e3b270e16
Tom Meschter <tomescht@microsoft.com>
2021-03-29 15:53:49 -0700

    Implement IQueryDataSourceVersionProvider for launch profiles
    The `IQueryDataSourceVersionProvider` is responsible for informing the Project Query engine that there have been updates to data sources of interest to queries. When a client is observing a query (meaning they are interested in receiving push updates to the query data), the engine subscribes to the appropriate `IQueryDataSourceVersionProvider`s so it knows when it needs to re-run the query to get updated data and send it to the client.
    
    There are three main parts to this implementation:
    
    1. At the `UnconfiguredProject` level, the `LaunchSettingsTracker` continually watches for changes to the set of launch settings. As they change, it notifies the `LaunchSettingsQueryVersionProvider`.
    2. The `LaunchSettingsQueryVersionProvider` is a singleton that aggregates the data from the trackers to create the total set of launch profile versions that may be needed by the Project Query API. It passes the aggregated data on to `LaunchSettingsQueryVersionProviderExport`.
    3. `LaunchSettingsQueryVersionProviderExport` is the Project Query-facing counterpart to `LaunchSettingsQueryVersionProvider`. Logically, they work together as a single unit. However, we don't want the `LaunchSettingsQueryVersionProvider` to depend directly on types from the Project Query API as that would cause the query assemblies to load even in scenarios where we aren't using them. To avoid this we need the functionality into these two types, with communication occuring via the `ILaunchSettingsVersionPublisher` interface. The "export" type manages the set of subscribers and asynchronously passes along new version information.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Query/LaunchProfiles/LaunchSettingsTracker.cs
@@ -0,0 +1,107 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.Debug;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Query
+{
+    /// <summary>
+    /// Tracks the launch settings version for a particular project, and reports the version information to the <see cref="LaunchSettingsQueryVersionProvider"/>.
+    /// </summary>
+    /// <remarks>
+    /// Note that as an <see cref="IProjectDynamicLoadComponent"/> the <see cref="LoadAsync"/>
+    /// and <see cref="UnloadAsync"/> may be called multiple times as the project is
+    /// loaded, unloaded, and as the capabilities change.
+    /// </remarks>
+    [Export(ExportContractNames.Scopes.UnconfiguredProject, typeof(IProjectDynamicLoadComponent))]
+    [AppliesTo(ProjectCapability.LaunchProfiles)]
+    internal class LaunchSettingsTracker : IProjectDynamicLoadComponent
+    {
+        /// <remarks>
+        /// Ensures each <see cref="LaunchSettingsTracker"/> is given a unique version key.
+        /// </remarks>
+        private static int s_nextTrackerId;
+
+        private readonly UnconfiguredProject _project;
+        private readonly LaunchSettingsQueryVersionProvider _versionProvider;
+
+        private string? _versionKey;
+        private IDisposable? _launchSettingsProviderLink;
+
+        [ImportingConstructor]
+        public LaunchSettingsTracker(
+            UnconfiguredProject project,
+            LaunchSettingsQueryVersionProvider versionProvider)
+        {
+            _project = project;
+            _versionProvider = versionProvider;
+        }
+
+        public Task LoadAsync()
+        {
+            ILaunchSettingsProvider launchSettingsProvider = _project.Services.ExportProvider.GetExportedValue<ILaunchSettingsProvider>();
+            _launchSettingsProviderLink = launchSettingsProvider.SourceBlock.LinkToAction(OnLaunchSettingsChanged, _project);
+
+            ILaunchSettings? currentSnapshot = launchSettingsProvider.CurrentSnapshot;
+            if (currentSnapshot is IVersionedLaunchSettings versionedSettings)
+            {
+                CurrentVersion = versionedSettings.Version;
+                _versionProvider.OnLaunchSettingsTrackerActivated(this);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task UnloadAsync()
+        {
+            if (_launchSettingsProviderLink is not null)
+            {
+                _launchSettingsProviderLink.Dispose();
+                _launchSettingsProviderLink = null;
+            }
+
+            _versionProvider.OnLaunchSettingsTrackerDeactivated(this);
+
+            return Task.CompletedTask;
+        }
+
+        internal void OnLaunchSettingsChanged(ILaunchSettings newSettings)
+        {
+            if (newSettings is IVersionedLaunchSettings versionedSettings)
+            {
+                CurrentVersion = versionedSettings.Version;
+                _versionProvider.OnLaunchSettingsVersionUpdated(this);
+            }
+        }
+
+        internal long CurrentVersion { get; private set; }
+
+        internal string VersionKey
+        {
+            get
+            {
+                if (_versionKey is null)
+                {
+                    // Get a unique ID number
+                    int trackerIdNumber = Interlocked.Increment(ref s_nextTrackerId);
+
+                    // The project name is only for diagnostic purposes. It is not guaranteed to be unique, 
+                    // and it is OK that this won't get updated if the project is renamed.
+                    string projectName = Path.GetFileNameWithoutExtension(_project.FullPath);
+
+                    // Assemble the version key.
+                    string versionKey = $"LaunchSettings:{trackerIdNumber}:{projectName}";
+
+                    // Ensure that the field is only set once, even when called from multiple threads.
+                    Interlocked.CompareExchange(location1: ref _versionKey, value: versionKey, comparand: null);
+                }
+
+                return _versionKey!;
+            }
+        }
+    }
+}
