2af4791b0b8ed399b41d0529c7089b276fa0e817
Tom Meschter <tomescht@microsoft.com>
2021-05-18 11:22:20 -0700

    Revert parts of e7cde330
    We temporarily needed to set `Option Strict Off` for various VB files while the VS interop changes were in flux, but now we can back that out.

--- src/Microsoft.VisualStudio.Editors/ResourceEditor/ResourcesFolderService.vb
+++ src/Microsoft.VisualStudio.Editors/ResourceEditor/ResourcesFolderService.vb
@@ -1,7 +1,7 @@
 ﻿' Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
-Option Explicit Off
-Option Strict Off
+Option Explicit On
+Option Strict On
 Option Compare Binary
 
 Imports System.IO
