e2061e2b61f7916afadfd60fb501524b1dfb7c06
Drew Noakes <git@drewnoakes.com>
2021-05-21 15:00:03 +1000

    Add project capability ProjectPropertiesEditor
    This capability will control which project types are opted into the new Project Properties UI. For now, only enable C# projects running on CPS.

--- src/Microsoft.VisualStudio.AppDesigner/ApplicationDesigner/ApplicationDesignerEditorFactory.vb
+++ src/Microsoft.VisualStudio.AppDesigner/ApplicationDesigner/ApplicationDesignerEditorFactory.vb
@@ -178,7 +178,7 @@ Namespace Microsoft.VisualStudio.Editors.ApplicationDesigner
         Implements IVsEditorFactory.CreateEditorInstance
 
             ' If we're using the new project properties editor, delegate to its editor factory
-            Dim shouldUseNewEditor As Boolean = UseNewEditor(Hierarchy)
+            Dim shouldUseNewEditor As Boolean = Hierarchy.IsCapabilityMatch("ProjectPropertiesEditor")
 
             Common.TelemetryLogger.LogEditorCreation(shouldUseNewEditor, FileName, PhysicalView)
 
@@ -226,11 +226,6 @@ Namespace Microsoft.VisualStudio.Editors.ApplicationDesigner
             End If
 
             Return hr
-        End Function
-
-        Private Shared Function UseNewEditor(vsHierarchy As IVsHierarchy) As Boolean
-            ' The new editor is only available for CPS-based C# projects
-            Return vsHierarchy.IsCapabilityMatch("CPS & CSharp")
         End Function
 
         Private Function GetNewEditorFactory() As IVsEditorFactory
