f89e886738c2e24c79a77f08adf1e92624f75374
Daniel Monroy <dmon@microsoft.com>
2021-05-21 14:20:26 -0700

    Suppress VSTHRD110

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.Client/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "VSTHRD110:Observe result of async calls", Justification = "https://github.com/dotnet/project-system/issues/7256", Scope = "member", Target = "~M:Microsoft.VisualStudio.SolutionExplorer.ProjectNodeCommandHandler.OpenFile(Microsoft.VisualStudio.Workspace.VSIntegration.UI.WorkspaceVisualNodeBase)")]
