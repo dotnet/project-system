f62201acab486ba2cff833a25457a3f51e483a07
Drew Noakes <git@drewnoakes.com>
2021-05-28 21:52:22 +1000

    Don't assume en-US culture for customer-facing URLs
    When a customer clicks a link to documentation, they expect to see it in their own language (where applicable).
    
    This commit removes the assumption on en-US culture from the URLs we allow customers to navigate to.
    
    More information at https://review.docs.microsoft.com/help/contribute/links-how-to?branch=master#locale-codes-in-external-links

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/VSResources.Designer.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/VSResources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.VisualStudio {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class VSResources {
@@ -450,7 +450,7 @@ namespace Microsoft.VisualStudio {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Xproj project files are no longer supported. For details on migrating to csproj see https://docs.microsoft.com/en-us/dotnet/core/migration/..
+        ///   Looks up a localized string similar to Xproj project files are no longer supported. For details on migrating to csproj see https://docs.microsoft.com/dotnet/core/migration/..
         /// </summary>
         internal static string XprojNotSupported {
             get {
