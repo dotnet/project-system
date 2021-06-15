495af7335197e1ea76e8b91981b53839e37e0f5f
Michael Yanni <miyanni@microsoft.com>
2021-06-14 17:14:54 -0700

    Adding unit test project for the Editors project.

---
+++ tests/Microsoft.VisualStudio.Common.UnitTests/UnitTest1.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Imports Xunit
+'Imports Microsoft.VisualStudio.Editors.Common
+
+Namespace Microsoft.VisualStudio.Common.UnitTests
+    Public Class UnitTest1
+        <Fact>
+        Sub TestSub()
+            'Utils.
+        End Sub
+    End Class
+End Namespace
+
---
+++ tests/Microsoft.VisualStudio.Editors.UnitTests/UnitTest1.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports Xunit
+Imports Microsoft.VisualStudio.Editors.Common
+Imports System.IO
+
+Namespace Microsoft.VisualStudio.Editors.UnitTests
+    Public Class UnitTest1
+        <Fact>
+        Sub TestSub()
+            Dim stream As New MemoryStream()
+            Utils.SerializationProvider.Serialize(stream, "Caketown")
+        End Sub
+    End Class
+End Namespace
+
