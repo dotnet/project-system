6517345b45855f2be3f3b39d7cd7ea7864f4bc49
Tom Meschter <tomescht@microsoft.com>
2021-06-15 14:19:54 -0700

    Add interfaces
    Add the public interfaces the project system will need to implement/use to support Hot Reload.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/HotReload/IProjectHotReloadSessionCallback.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.HotReload
+{
+    public interface IProjectHotReloadSessionCallback
+    {
+        bool IsSupported { get; }
+
+        bool SupportsRestart { get; }
+
+        Task OnAfterChangesAppliedAsync(CancellationToken cancellationToken);
+
+        Task<bool> StopProjectAsync(CancellationToken cancellationToken);
+
+        Task<bool> RestartProjectAsync(CancellationToken cancellationToken);
+
+        // TODO: IDeltaApplier will be defined elsewhere. Add this back in once we
+        // can reference it in the final location.
+        // IDeltaApplier GetDeltaApplier();
+    }
+}
