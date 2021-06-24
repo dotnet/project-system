cde809eaee79212f8b954dcb7d04fcf40762192e
joj <joj@microsoft.com>
2021-06-24 09:30:33 -0300

    Added documentation for IDefaultLaunchProfileProvider

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/IDefaultLaunchProfileProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/IDefaultLaunchProfileProvider.cs
@@ -4,9 +4,15 @@ using Microsoft.VisualStudio.Composition;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Debug
 {
+    /// <summary>
+    /// Provides a default <see cref="ILaunchProfile"/> to be displayed when no profiles are provided.
+    /// </summary>
     [ProjectSystemContract(ProjectSystemContractScope.UnconfiguredProject, ProjectSystemContractProvider.Extension, Cardinality = ImportCardinality.ZeroOrMore)]
     public interface IDefaultLaunchProfileProvider
     {
+        /// <summary>
+        /// Gets the default <see cref="ILaunchProfile"/>. Return null to remove the default profile.
+        /// </summary>
         ILaunchProfile? CreateDefaultProfile();
     }
 }
