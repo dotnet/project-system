fe099e638bccda233d73821e9faac20c0ec6d24a
Melissa Treviño <melissat@microsoft.com>
2021-07-19 10:35:26 -0700

    Improve discoverabililty for EnumProviders.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Frameworks/SupportedTargetFrameworksEnumProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Frameworks/SupportedTargetFrameworksEnumProvider.cs
@@ -1,6 +1,5 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
-using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.ComponentModel.Composition;
@@ -10,35 +9,35 @@ using Microsoft.VisualStudio.ProjectSystem.Properties;
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Frameworks
 {
     /// <summary>
-    ///     Responsible for producing valid values for the SdkSupportedTargetPlatformIdentifier property from evaluation.
+    ///     Responsible for producing valid values for the TargetFramework property from evaluation.
     /// </summary>
-    [ExportDynamicEnumValuesProvider("SdkSupportedTargetPlatformIdentifierEnumProvider")]
+    [ExportDynamicEnumValuesProvider("SupportedTargetFrameworksEnumProvider")]
     [AppliesTo(ProjectCapability.DotNet)]
-    internal class SdkSupportedTargetPlatformIdentifierProvider : SupportedValuesProvider
+    internal class SupportedTargetFrameworksEnumProvider : SupportedValuesProvider
     {
-        protected override string RuleName => SdkSupportedTargetPlatformIdentifier.SchemaName;
+        protected override string RuleName => SupportedTargetFramework.SchemaName;
 
         [ImportingConstructor]
-        public SdkSupportedTargetPlatformIdentifierProvider(
+        public SupportedTargetFrameworksEnumProvider(
             ConfiguredProject project,
             IProjectSubscriptionService subscriptionService)
-            : base(project, subscriptionService, useNoneValue: true) { }
+            : base(project, subscriptionService) {}
 
         protected override IEnumValue ToEnumValue(KeyValuePair<string, IImmutableDictionary<string, string>> item)
         {
             return new PageEnumValue(new EnumValue()
             {
-                // Example: <SdkSupportedTargetPlatformIdentifier Include="windows" DisplayName="Windows"/>
-                //          <SdkSupportedTargetPlatformIdentifier Include="ios" DisplayName="iOS"/>
+                // Example: <SupportedTargetFramework  Include=".NETCoreApp,Version=v5.0"
+                //                                     DisplayName=".NET 5.0" />
 
                 Name = item.Key,
-                DisplayName = item.Value[SdkSupportedTargetPlatformIdentifier.DisplayNameProperty]
+                DisplayName = item.Value[SupportedTargetFramework.DisplayNameProperty],
             });
         }
 
         protected override int SortValues(IEnumValue a, IEnumValue b)
         {
-            return StringComparer.OrdinalIgnoreCase.Compare(a.DisplayName, b.DisplayName);
+            return NaturalStringComparer.Instance.Compare(a.DisplayName, b.DisplayName);
         }
     }
 }
