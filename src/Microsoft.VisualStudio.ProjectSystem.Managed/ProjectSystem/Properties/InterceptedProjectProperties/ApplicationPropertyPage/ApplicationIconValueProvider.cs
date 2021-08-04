1134ee8842a8d098713f78ae0d65334e37cec51b
Michael Yanni <miyanni@microsoft.com>
2021-08-04 15:15:09 -0700

    Clean up commented code. Tested known scenarios.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/ApplicationPropertyPage/ApplicationIconValueProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/ApplicationPropertyPage/ApplicationIconValueProvider.cs
@@ -8,24 +8,12 @@ using Microsoft.VisualStudio.IO;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Properties.Package
 {
-    // Sets the PackageIcon property to the path from the root of the package, and creates the
-    // None item associated with the file on disk. The Include metadata of the None item is a relative
-    // path to the file on disk from the project's directory. The None item includes 2 metadata elements
-    // as Pack (set to True to be included in the package) and PackagePath (directory structure in the package
-    // for the file to be placed). The PackageIcon property will use the directory indicated in PackagePath,
-    // and the filename of the file in the Include filepath.
-    //
-    // Example:
-    // <PropertyGroup>
-    //   <PackageIcon>content\shell32_192.png</PackageIcon>
-    // </PropertyGroup>
-    //
-    // <ItemGroup>
-    //   <None Include="..\..\..\shell32_192.png">
-    //     <Pack>True</Pack>
-    //     <PackagePath>content</PackagePath>
-    //   </None>
-    // </ItemGroup>
+    // Sets the ApplicationIcon property to the path to the .ico file. This path is relative
+    // to the project file directory. If the selected file is not within the project directory
+    // tree, the file will be copied into the project file directory. Previously copied files
+    // are not deleted when a new file is selected. ApplicationIcon allows for both full paths
+    // and relative paths anywhere on disk, but we are not utilizing that in this implementation.
+    // The icon file does not need to be included in the project (such as a None/Content item).
     [ExportInterceptingPropertyValueProvider(ApplicationIconPropertyName, ExportInterceptingPropertyValueProviderFile.ProjectFile)]
     internal sealed class ApplicationIconValueProvider : InterceptingPropertyValueProviderBase
     {
@@ -41,8 +29,6 @@ namespace Microsoft.VisualStudio.ProjectSystem.Properties.Package
             _fileSystem = fileSystem;
         }
 
-        // TODO: Check if we need to delete previous file
-        // TODO: Check if file is already imported as a none/content item
         public override async Task<string?> OnSetPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IProjectProperties defaultProperties, IReadOnlyDictionary<string, string>? dimensionalConditions = null)
         {
             // Allows the user to clear the application icon.
@@ -68,32 +54,6 @@ namespace Microsoft.VisualStudio.ProjectSystem.Properties.Package
             }
 
             return PathHelper.MakeRelative(_unconfiguredProject, unevaluatedPropertyValue);
-
-            //string relativePath = PathHelper.MakeRelative(_unconfiguredProject, unevaluatedPropertyValue);
-            //string existingPropertyValue = await defaultProperties.GetEvaluatedPropertyValueAsync(PackageIconPropertyName);
-            //IProjectItem? existingItem = await GetExistingNoneItemAsync(existingPropertyValue);
-            //string packagePath = string.Empty;
-            //if (existingItem != null)
-            //{
-            //    packagePath = await existingItem.Metadata.GetEvaluatedPropertyValueAsync(PackagePathMetadataName);
-            //    // The new filepath is the same as the current. No item changes are required.
-            //    if (relativePath.Equals(existingItem.EvaluatedInclude, StringComparisons.Paths))
-            //    {
-            //        return CreatePackageIcon(existingItem.EvaluatedInclude, packagePath);
-            //    }
-            //}
-
-            //// None items outside of the project file cannot be updated.
-            //if (existingItem?.PropertiesContext.IsProjectFile ?? false)
-            //{
-            //    await existingItem.SetUnevaluatedIncludeAsync(relativePath);
-            //}
-            //else
-            //{
-            //    await _sourceItemsProvider.AddAsync(None.SchemaName, relativePath, new Dictionary<string, string> { { PackMetadataName, bool.TrueString }, { PackagePathMetadataName, packagePath } });
-            //}
-
-            //return CreatePackageIcon(relativePath, packagePath);
         }
     }
 }
