baef42ea1359f9e39756654df35fc4b346f049b3
Tom Meschter <tomescht@microsoft.com>
2021-08-18 12:51:47 -0700

    Extract a helper method
    Extract the conditions controlling whether or not to try and setup a Hot Reload session into a helper method.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Debug/ProjectLaunchTargetsProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Debug/ProjectLaunchTargetsProvider.cs
@@ -435,10 +435,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Debug
                 settings.Options = JsonConvert.SerializeObject(debuggerLaunchOptions);
             }
 
-            if (IsRunProjectCommand(resolvedProfile)
-                && resolvedProfile.IsHotReloadEnabled()
-                && !resolvedProfile.IsRemoteDebugEnabled()
-                && (launchOptions & DebugLaunchOptions.Profiling) != DebugLaunchOptions.Profiling
+            if (HotReloadShouldBeEnabled(resolvedProfile, launchOptions)
                 && await _hotReloadSessionManager.Value.TryCreatePendingSessionAsync(settings.Environment))
             {
                 // Enable XAML Hot Reload
@@ -451,6 +448,14 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Debug
             }
 
             return settings;
+        }
+
+        private static bool HotReloadShouldBeEnabled(ILaunchProfile resolvedProfile, DebugLaunchOptions launchOptions)
+        {
+            return IsRunProjectCommand(resolvedProfile)
+                && resolvedProfile.IsHotReloadEnabled()
+                && !resolvedProfile.IsRemoteDebugEnabled()
+                && (launchOptions & DebugLaunchOptions.Profiling) != DebugLaunchOptions.Profiling;
         }
 
         private static bool UseCmdShellForConsoleLaunch(ILaunchProfile profile, DebugLaunchOptions options)
