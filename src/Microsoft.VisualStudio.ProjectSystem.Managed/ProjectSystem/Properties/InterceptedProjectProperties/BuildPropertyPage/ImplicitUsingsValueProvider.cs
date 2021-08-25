200d7d6815f62eade96a6325e04414897d973d9c
Drew Noakes <git@drewnoakes.com>
2021-08-25 14:25:18 +1000

    Initial support for global implicit usings
    This adds a single check box that enables/disables this feature.
    
    In future we should add UI to allow modifying the set of usings directly.
    
    We will also need to add logic to support VB separately, as that uses a different property to control this.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/BuildPropertyPage/ImplicitUsingsValueProvider.cs
@@ -0,0 +1,63 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties
+{
+    [ExportInterceptingPropertyValueProvider("ImplicitUsings", ExportInterceptingPropertyValueProviderFile.ProjectFile)]
+    internal sealed class ImplicitUsingsValueProvider : InterceptingPropertyValueProviderBase
+    {
+        public override async Task<string> OnGetEvaluatedPropertyValueAsync(string propertyName, string evaluatedPropertyValue, IProjectProperties defaultProperties)
+        {
+            string value = await base.OnGetEvaluatedPropertyValueAsync(propertyName, evaluatedPropertyValue, defaultProperties);
+            
+            return ToBooleanString(value);
+        }
+
+        public override async Task<string> OnGetUnevaluatedPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IProjectProperties defaultProperties)
+        {
+            string value = await base.OnGetUnevaluatedPropertyValueAsync(propertyName, unevaluatedPropertyValue, defaultProperties);
+
+            return ToBooleanString(value);
+        }
+
+        public override async Task<string?> OnSetPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IProjectProperties defaultProperties, IReadOnlyDictionary<string, string>? dimensionalConditions = null)
+        {
+            string? value = await base.OnSetPropertyValueAsync(propertyName, unevaluatedPropertyValue, defaultProperties, dimensionalConditions);
+
+            return FromBooleanString(value);
+        }
+
+        private static string ToBooleanString(string value)
+        {
+            if (StringComparer.OrdinalIgnoreCase.Equals(value, "enable"))
+            {
+                return bool.TrueString;
+            }
+
+            if (StringComparer.OrdinalIgnoreCase.Equals(value, "disable"))
+            {
+                return bool.FalseString;
+            }
+
+            return value;
+        }
+
+        private static string? FromBooleanString(string? value)
+        {
+            if (StringComparer.OrdinalIgnoreCase.Equals(value, bool.TrueString))
+            {
+                return "enable";
+            }
+
+            if (StringComparer.OrdinalIgnoreCase.Equals(value, bool.FalseString))
+            {
+                return "disable";
+            }
+
+            return value;
+        }
+    }
+}
