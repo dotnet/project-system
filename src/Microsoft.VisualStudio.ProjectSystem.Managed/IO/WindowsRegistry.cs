01f04f158e2c0bcf903ae784ea0909469c40c451
Tom Meschter <tomescht@microsoft.com>
2021-08-27 13:43:14 -0700

    Add IRegistry service
    Define an `IRegistry` service and corresponding implementation. For now we only need to read values for the current user (HKCU).

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/IO/WindowsRegistry.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.ComponentModel.Composition;
+using Microsoft.Win32;
+
+namespace Microsoft.VisualStudio.IO
+{
+    [Export(typeof(IRegistry))]
+    internal class WindowsRegistry : IRegistry
+    {
+        public object? ReadValueForCurrentUser(string keyPath, string name)
+        {
+            using (RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(keyPath))
+            {
+                if (registryKey is not null)
+                {
+                    try
+                    {
+                        return registryKey.GetValue(name);
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
