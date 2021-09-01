8084e9732ec3a69f670905790796cebabab44b67
Drew Noakes <git@drewnoakes.com>
2021-09-01 18:48:00 +1000

    Remove redundant suppressions

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/Shell/Interop/VsHierarchyExtensions.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/Shell/Interop/VsHierarchyExtensions.cs
@@ -37,9 +37,7 @@ namespace Microsoft.VisualStudio.Shell.Interop
         /// </summary>
         public static T? GetProperty<T>(this IVsHierarchy hierarchy, HierarchyId item, VsHierarchyPropID property, T? defaultValue = default)
         {
-#pragma warning disable CS8717 // Needs https://github.com/dotnet/roslyn/issues/38638
             Verify.HResult(GetProperty(hierarchy, item, property, defaultValue, out T? result));
-#pragma warning restore CS8717
 
             return result;
         }
@@ -49,9 +47,7 @@ namespace Microsoft.VisualStudio.Shell.Interop
         /// </summary>
         public static int GetProperty<T>(this IVsHierarchy hierarchy, VsHierarchyPropID property, T? defaultValue, out T? result)
         {
-#pragma warning disable CS8717 // Needs https://github.com/dotnet/roslyn/issues/38638
             return GetProperty(hierarchy, HierarchyId.Root, property, defaultValue, out result);
-#pragma warning restore CS8717 
         }
 
         /// <summary>
