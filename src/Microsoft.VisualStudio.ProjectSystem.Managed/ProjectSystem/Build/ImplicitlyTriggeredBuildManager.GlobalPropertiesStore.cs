ea6140951f72609e0b4462ebfc20149bf4f56592
Manish Vasani <mavasani@microsoft.com>
2021-09-14 04:38:55 -0700

    Move the Global properties provider for implicit builds from TestWindow to project system
    Work towards addressing the RPS assembly load regression [AB#1350848](https://devdiv.visualstudio.com/DevDiv/_workitems/edit/1350848)
    A subsequent change would be made in TestWindow to call into the exposed interface during implicit builds.
    
    **NOTE:** This is a targeted temporary fix to the existing implementation for skipping analyzers during implicitly triggered builds, primary targeted towards fixing the RPS regression in Preview5. Post Preview5, we are going consider alternate approaches for this to address some performance concerns around use of global properties from unnecessary target evaluations.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Build/ImplicitlyTriggeredBuildManager.GlobalPropertiesStore.cs
@@ -0,0 +1,65 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Build
+{
+    internal sealed partial class ImplicitlyTriggeredBuildManager
+    {
+        /// <summary>
+        /// Global properties store for implicitly triggered builds from commands such as Run/Debug Tests, Start Debugging, etc.
+        /// </summary>
+        private sealed class GlobalPropertiesStore
+        {
+            private const string IsImplicitlyTriggeredBuildPropertyName = "IsImplicitlyTriggeredBuild";
+            private const string FastUpToDateCheckIgnoresKindsGlobalPropertyName = "FastUpToDateCheckIgnoresKinds";
+            private const string FastUpToDateCheckIgnoresKindsGlobalPropertyValue = "ImplicitBuild";
+
+            private readonly ImmutableDictionary<string, string>.Builder _properties = ImmutableDictionary.CreateBuilder<string, string>(StringComparer.OrdinalIgnoreCase);
+            private readonly object _gate = new();
+
+            public static readonly GlobalPropertiesStore Instance = new();
+
+            private GlobalPropertiesStore()
+            {
+            }
+
+            internal ImmutableDictionary<string, string> GetProperties()
+            {
+                lock (_gate)
+                {
+                    return _properties.ToImmutable();
+                }
+            }
+
+            public void OnBuildStart()
+            {
+                SetProperty(IsImplicitlyTriggeredBuildPropertyName, "true");
+                SetProperty(FastUpToDateCheckIgnoresKindsGlobalPropertyName, FastUpToDateCheckIgnoresKindsGlobalPropertyValue);
+            }
+
+            public void OnBuildEndOrCancel()
+            {
+                RemoveProperty(IsImplicitlyTriggeredBuildPropertyName);
+                RemoveProperty(FastUpToDateCheckIgnoresKindsGlobalPropertyName);
+            }
+
+            private void SetProperty(string key, string value)
+            {
+                lock (_gate)
+                {
+                    _properties[key] = value;
+                }
+            }
+
+            private void RemoveProperty(string key)
+            {
+                lock (_gate)
+                {
+                    _properties.Remove(key);
+                }
+            }
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Build/ImplicitlyTriggeredBuildManager.GlobalProviderProvider.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Collections.Immutable;
+using System.ComponentModel.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Build
+{
+    internal sealed partial class ImplicitlyTriggeredBuildManager
+    {
+        /// <summary>
+        /// Global properties provider for implicitly triggered builds from commands such as Run/Debug Tests, Start Debugging, etc.
+        /// This provider does not affect the property collection for design time builds.
+        /// </summary>
+        /// <remarks>
+        /// Currently, the provider is only for CPS based SDK-style projects, not for legacy csproj projects.
+        /// https://github.com/dotnet/project-system/issues/7346 tracks implementing the project system support for legacy csproj projects.
+        /// </remarks>
+        [ExportBuildGlobalPropertiesProvider]
+        [AppliesTo(ProjectCapability.DotNet)]
+        private sealed class GlobalProviderProvider : StaticGlobalPropertiesProviderBase
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="GlobalProviderProvider"/> class.
+            /// </summary>
+            [ImportingConstructor]
+            public GlobalProviderProvider(UnconfiguredProject unconfiguredProject)
+                : base(unconfiguredProject.Services)
+            {
+            }
+
+            /// <summary>
+            /// Gets the set of global properties that should apply to the project(s) in this scope.
+            /// </summary>
+            /// <value>A new dictionary whose keys are case insensitive.  Never null, but may be empty.</value>
+            public override Task<IImmutableDictionary<string, string>> GetGlobalPropertiesAsync(CancellationToken cancellationToken)
+            {
+                return Task.FromResult<IImmutableDictionary<string, string>>(GlobalPropertiesStore.Instance.GetProperties());
+            }
+        }
+    }
+}
