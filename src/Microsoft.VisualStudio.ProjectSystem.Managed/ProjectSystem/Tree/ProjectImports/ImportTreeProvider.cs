9d210facb419b4ac667e63a3a9d35eb8a6db4bfc
Saint Wesonga <sawesong@microsoft.com>
2021-09-28 15:29:48 -0600

    Remove Enumerable.ToDictionary call to prevent faults when there are duplicate entries

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Tree/ProjectImports/ImportTreeProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Tree/ProjectImports/ImportTreeProvider.cs
@@ -282,7 +282,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tree.ProjectImports
 
                                 Dictionary<string, string> GetCaptionByProjectPath()
                                 {
-                                    var result = imports.ToDictionary(i => i.ProjectPath, i => Path.GetFileName(i.ProjectPath));
+                                    var result = CreateProjectPathDictionaryFromImports();
 
                                     var isDuplicateByFileName = new Dictionary<string, bool>(StringComparers.Paths);
 
@@ -299,6 +299,23 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tree.ProjectImports
                                         {
                                             result[import.ProjectPath] = $"{fileName} ({Path.GetDirectoryName(import.ProjectPath)})";
                                         }
+                                    }
+
+                                    return result;
+                                }
+
+                                // Creates a map from each import's project path to the filename of that project path.
+                                // Although LINQ's Enumerable.ToDictionary extension method can be used to create such
+                                // a map, that implementation fails when there are duplicate entries in the imports.
+                                // Therefore, this implementation loops through the imports to create the map.
+                                Dictionary<string, string> CreateProjectPathDictionaryFromImports()
+                                {
+                                    Dictionary<string, string> result = new(StringComparers.Paths);
+
+                                    for (int i = 0; i < imports.Count; i++)
+                                    {
+                                        var importAtIndex = imports[i];
+                                        result[importAtIndex.ProjectPath] = Path.GetFileName(importAtIndex.ProjectPath);
                                     }
 
                                     return result;
