7c2a51d402530d9f4c7b69c90c0422686bfe7a72
Michael Yanni <miyanni@microsoft.com>
2021-11-12 17:17:49 -0800

    Fixed node selection issue because PowerShell is 😖

--- build/optprof/UpdateRunSettings.ps1
+++ build/optprof/UpdateRunSettings.ps1
@@ -1,8 +1,13 @@
 param ([Parameter(Mandatory=$true)] [String] $profilingInputsPath, [String] $bootstrapperInfoPath, [String] $buildDropPath)
 
+Write-Host "profilingInputsPath: $profilingInputsPath"
+Write-Host "bootstrapperInfoPath: $bootstrapperInfoPath"
+Write-Host "buildDropPath: $buildDropPath"
+
 $runsettingsPath = (Get-Item "$PSScriptRoot\OptProf.runsettings").FullName
 $runsettingsXml = [Xml.XmlDocument](Get-Content $runsettingsPath)
-$testStores = $runsettingsXml.RunSettings.TestConfiguration.TestStores
+# https://stackoverflow.com/questions/33813700/empty-xml-node-rendered-as-string-in-powershell
+$testStores = $runsettingsXml.RunSettings.TestConfiguration.SelectSingleNode('TestStores')
 
 $profilingInputsStore = $runsettingsXml.CreateElement('TestStore')
 $profilingInputsStore.SetAttribute('Uri', $profilingInputsPath)
