ceb266680f2b418e92632940b742c572c7e2db41
Drew Noakes <git@drewnoakes.com>
2022-01-10 14:14:07 +1100

    Add ProjectConfiguration.GetDisplayString extension method

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/ProjectConfigurationExtensions.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/ProjectConfigurationExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
 
 namespace Microsoft.VisualStudio.ProjectSystem
 {
@@ -51,6 +53,52 @@ namespace Microsoft.VisualStudio.ProjectSystem
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Produces a string containing each dimension's value, in idiomatic order (configuration, platform, then any targets),
+        /// such as <c>Debug|AnyCPU|net7.0</c>.
+        /// </summary>
+        /// <remarks>
+        /// Calling <c>ToString</c> on <see cref="ProjectConfiguration"/> excludes the target framework from the first configuration,
+        /// meaning it is not suitable when the full set of dimensions is needed.
+        /// </remarks>
+        /// <param name="projectConfiguration"></param>
+        /// <returns></returns>
+        internal static string GetDisplayString(this ProjectConfiguration projectConfiguration)
+        {
+            // ImmutableDictionary does not preserve order, so we manually produce the idiomatic order
+
+            IImmutableDictionary<string, string> dims = projectConfiguration.Dimensions;
+
+            var sb = new StringBuilder();
+
+            // Configuration and platform are always first
+            if (dims.TryGetValue(ConfigurationGeneral.ConfigurationProperty, out string? configuration))
+                Append(configuration);
+            if (dims.TryGetValue(ConfigurationGeneral.PlatformProperty, out string? platform))
+                Append(platform);
+
+            // Any other dimensions later
+            foreach ((string name, string value) in dims)
+            {
+                if (StringComparers.ConfigurationDimensionNames.Equals(name, ConfigurationGeneral.ConfigurationProperty) ||
+                    StringComparers.ConfigurationDimensionNames.Equals(name, ConfigurationGeneral.PlatformProperty))
+                {
+                    continue;
+                }
+
+                Append(value);
+            }
+
+            return sb.ToString();
+
+            void Append(string s)
+            {
+                if (sb.Length != 0)
+                    sb.Append('|');
+                sb.Append(s);
+            }
         }
     }
 }
