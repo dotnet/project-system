72415dbad0d06940ca3a79843a53f8391e950d14
Michael Yanni <miyanni@microsoft.com>
2022-01-16 06:46:51 -0800

    More cleanup. Merged vsix signing into Directory.Build.targets.

--- build/optprof/UpdateRunSettings.ps1
+++ build/optprof/UpdateRunSettings.ps1
@@ -1,3 +1,5 @@
+# Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
 # This updates OptProf.runsettings with the bootstrapper information and the profiling inputs path, as TestStore nodes.
 
 param ([Parameter(Mandatory=$true)] [String] $profilingInputsPath, [String] $bootstrapperInfoPath, [String] $buildDropPath)
