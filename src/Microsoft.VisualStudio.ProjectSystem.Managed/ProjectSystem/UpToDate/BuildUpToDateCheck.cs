8ec3e6a418019382acf0db720482ac3951d85035
Drew Noakes <git@drewnoakes.com>
2022-02-09 20:37:45 +1100

    Rename local

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/UpToDate/BuildUpToDateCheck.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/UpToDate/BuildUpToDateCheck.cs
@@ -553,7 +553,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.UpToDate
                 return true;
             }
 
-            string markerFile = _configuredProject.UnconfiguredProject.MakeRooted(state.CopyUpToDateMarkerItem);
+            string outputMarkerFile = _configuredProject.UnconfiguredProject.MakeRooted(state.CopyUpToDateMarkerItem);
 
             if (log.Level >= LogLevel.Verbose)
             {
@@ -570,7 +570,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.UpToDate
 
                 log.Verbose(nameof(Resources.FUTD_AddingOutputReferenceCopyMarker));
                 log.Indent++;
-                log.VerboseLiteral(markerFile);
+                log.VerboseLiteral(outputMarkerFile);
                 log.Indent--;
             }
 
@@ -584,15 +584,15 @@ namespace Microsoft.VisualStudio.ProjectSystem.UpToDate
                 return true;
             }
 
-            DateTime? outputMarkerTime = timestampCache.GetTimestampUtc(markerFile);
+            DateTime? outputMarkerTime = timestampCache.GetTimestampUtc(outputMarkerFile);
 
             if (outputMarkerTime != null)
             {
-                log.Info(nameof(Resources.FUTD_WriteTimeOnOutputMarker_2), outputMarkerTime, markerFile);
+                log.Info(nameof(Resources.FUTD_WriteTimeOnOutputMarker_2), outputMarkerTime, outputMarkerFile);
             }
             else
             {
-                log.Info(nameof(Resources.FUTD_NoOutputMarkerExists_1), markerFile);
+                log.Info(nameof(Resources.FUTD_NoOutputMarkerExists_1), outputMarkerFile);
                 return true;
             }
 
