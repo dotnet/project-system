ffbaba3f70e0ca709a9ff1b03e72b77e2a4e8326
Michael Yanni <miyanni@microsoft.com>
2022-03-04 10:20:14 -0800

    Still in-progress script.

--- build/script/UpdateVsixProjForSbom.ps1
+++ build/script/UpdateVsixProjForSbom.ps1
@@ -1,5 +1,5 @@
 # Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
-# This updates 
+# This updates the provided swixproj/vsmanproj with the necessary MergeManifest node attribute for SBOM metadata to propegate to the VSIX packages.
 
-param ([Parameter(Mandatory=$true)] [String] $profilingInputsPath, [String] $bootstrapperInfoPath, [String] $buildDropPath)
+param ([Parameter(Mandatory=$true)] [String] $projPath)
