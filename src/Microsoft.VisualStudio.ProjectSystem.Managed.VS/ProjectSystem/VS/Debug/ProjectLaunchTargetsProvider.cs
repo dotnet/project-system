a10b002cf82149e4440be4451c1fa2ef995c223c
Adam Ratzman <adamratzman@microsoft.com>
2022-03-14 13:28:17 -0700

    Unescape newlines when launching application

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Debug/ProjectLaunchTargetsProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Debug/ProjectLaunchTargetsProvider.cs
@@ -267,7 +267,12 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Debug
                 }
             }
 
-            string? commandLineArgs = resolvedProfile.CommandLineArgs?.Replace("\r\n", " ").Replace("\r", " ").Replace("\n", " ");
+            string? commandLineArgs = resolvedProfile.CommandLineArgs?
+                .Replace("\r\n", " ")
+                .Replace("\r", " ")
+                .Replace("\n", " ")
+                .Replace("\\r\\n", "\r\n")
+                .Replace("\\n", "\n");
 
             // Is this profile just running the project? If so we ignore the exe
             if (IsRunProjectCommand(resolvedProfile))
