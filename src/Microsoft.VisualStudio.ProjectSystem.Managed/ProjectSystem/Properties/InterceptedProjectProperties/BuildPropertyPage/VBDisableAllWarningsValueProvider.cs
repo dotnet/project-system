62ff6c1445b84e2c0a76da70ea8ac5519eaf51bd
Tom Meschter <tomescht@microsoft.com>
2022-05-16 11:44:42 -0700

    Add VBDisableAllWarningsValueProvider
    The VB "Compile" page has a "Disable all warnings" check box. However, this doesn't correspond to a property in the project file with a true or false value. Rather, if the "WarningLevel" property is 0, all warnings are disabled; if it has any other value, warnings are enabled. The `VBDisableAllWarningsValueProvider` serves to map between the two.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/BuildPropertyPage/VBDisableAllWarningsValueProvider.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties;
+
+[ExportInterceptingPropertyValueProvider(DisableAllWarningsPropertyName, ExportInterceptingPropertyValueProviderFile.ProjectFile)]
+internal sealed class VBDisableAllWarningsValueProvider : InterceptingPropertyValueProviderBase
+{
+    internal const string DisableAllWarningsPropertyName = "DisableAllWarnings";
+
+    internal const string WarningLevelPropertyName = "WarningLevel";
+
+    public override Task<string> OnGetUnevaluatedPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IProjectProperties defaultProperties)
+    {
+        return OnGetPropertyValueAsync(defaultProperties);
+    }
+
+    public override Task<string> OnGetEvaluatedPropertyValueAsync(string propertyName, string evaluatedPropertyValue, IProjectProperties defaultProperties)
+    {
+        return OnGetPropertyValueAsync(defaultProperties);
+    }
+
+    public override async Task<string?> OnSetPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IProjectProperties defaultProperties, IReadOnlyDictionary<string, string>? dimensionalConditions = null)
+    {
+        if (StringComparers.PropertyLiteralValues.Equals(unevaluatedPropertyValue, "true"))
+        {
+            await defaultProperties.SetPropertyValueAsync(WarningLevelPropertyName, "0", dimensionalConditions);
+        }
+        else if (StringComparers.PropertyLiteralValues.Equals(unevaluatedPropertyValue, "false"))
+        {
+            await defaultProperties.SetPropertyValueAsync(WarningLevelPropertyName, "1", dimensionalConditions);
+        }
+
+        return null;
+    }
+
+    private async Task<string> OnGetPropertyValueAsync(IProjectProperties projectProperties)
+    {
+        string warningLevelValue = await projectProperties.GetEvaluatedPropertyValueAsync(WarningLevelPropertyName);
+
+        return warningLevelValue == "0" ? "true" : "false";
+    }
+}
