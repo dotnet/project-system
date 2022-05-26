35f59b83675c2ca9b983bfd18c539b23bbf3929e
Tom Meschter <tomescht@microsoft.com>
2022-05-26 15:17:43 -0700

    Add WPF-related properties
    1. Update the `AppDotXamlDocument` type to support reading and writing the "Startup URI" and "Shutdown Mode" properties. This also involves a change to return `null` when one of these properties is not set in the file, rather than the empty string. This lets the caller distinguish between the value not being set, and being set to an actual empty value.
    2. Introduce an `IApplicationXamlFileAccessor` interface and related implementation to manage access to a project's Application.xaml file. This type handles loading the Application.xaml file into a `DocData`, managing the lifetime of the `DocData`, and creating the `AppDotXamlDocument` as needed.
    3. Add the `WPFValueProvider` interceptor that uses the `IApplicationXamlFileAccessor` to expose "Startup URI" and "Shutdown Mode" as properties.
    4. Update the VB Application property page to include the WPF-related properties, but only show them for projects that produce executables, use WPF, and use the Application Framework.
    5. Update the `ApplicationFrameworkValueProvider` to support WPF projects. Checking or unchecking the "Use Application Framework" check box has a much different effect in WPF than other projects; we need to toggle the "StartupObject" property between "Sub Main" (when the application framework is off) and undefined (when it is), and toggle whether or not the Application.xaml file is handled as an "ApplicationDefinition" item type or not. In this commit we basically create two totally different code paths for reading/writing: one for WPF projects, and one for everything else. We may want to revisit this to clean it up.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/ApplicationPropertyPage/WPFValueProvider.cs
@@ -0,0 +1,76 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties;
+
+[ExportInterceptingPropertyValueProvider(
+    new[]
+    {
+        StartupURIPropertyName,
+        ShutdownModePropertyName
+    },
+    ExportInterceptingPropertyValueProviderFile.ProjectFile)]
+internal class WPFValueProvider : InterceptingPropertyValueProviderBase
+{
+    private const string StartupURIPropertyName = "StartupURI";
+    private const string ShutdownModePropertyName = "ShutdownMode_WPF";
+
+    private readonly IApplicationXamlFileAccessor _applicationXamlFileAccessor;
+
+    [ImportingConstructor]
+    public WPFValueProvider(IApplicationXamlFileAccessor applicationXamlFileAccessor)
+    {
+        _applicationXamlFileAccessor = applicationXamlFileAccessor;
+    }
+
+    public override async Task<string> OnGetEvaluatedPropertyValueAsync(string propertyName, string evaluatedPropertyValue, IProjectProperties defaultProperties)
+    {
+        return await GetPropertyValueAsync(propertyName);
+    }
+
+    public override async Task<string> OnGetUnevaluatedPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IProjectProperties defaultProperties)
+    {
+        return await GetPropertyValueAsync(propertyName);
+    }
+
+    public override async Task<string?> OnSetPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IProjectProperties defaultProperties, IReadOnlyDictionary<string, string>? dimensionalConditions = null)
+    {
+        switch (propertyName)
+        {
+            case StartupURIPropertyName:
+                await _applicationXamlFileAccessor.SetStartupUriAsync(unevaluatedPropertyValue);
+                break;
+
+            case ShutdownModePropertyName:
+                await _applicationXamlFileAccessor.SetShutdownModeAsync(unevaluatedPropertyValue);
+                break;
+
+            default:
+                throw new InvalidOperationException($"The {nameof(WPFValueProvider)} does not support the '{propertyName}' property.");
+        }
+
+        return null;
+    }
+
+    private async Task<string> GetPropertyValueAsync(string propertyName)
+    {
+        return propertyName switch
+        {
+            StartupURIPropertyName => await _applicationXamlFileAccessor.GetStartupUriAsync() ?? string.Empty,
+            ShutdownModePropertyName => await _applicationXamlFileAccessor.GetShutdownModeAsync() ?? "OnLastWindowClose",
+
+            _ => throw new InvalidOperationException($"The {nameof(WPFValueProvider)} does not support the '{propertyName}' property.")
+        };
+    }
+}
+
+[ProjectSystemContract(ProjectSystemContractScope.UnconfiguredProject, ProjectSystemContractProvider.Extension, Cardinality = Composition.ImportCardinality.ExactlyOne)]
+internal interface IApplicationXamlFileAccessor
+{
+    Task<bool> ApplicationXamlFileExistsAsync();
+
+    Task<string?> GetStartupUriAsync();
+    Task SetStartupUriAsync(string startupUri);
+
+    Task<string?> GetShutdownModeAsync();
+    Task SetShutdownModeAsync(string shutdownMode);
+}
