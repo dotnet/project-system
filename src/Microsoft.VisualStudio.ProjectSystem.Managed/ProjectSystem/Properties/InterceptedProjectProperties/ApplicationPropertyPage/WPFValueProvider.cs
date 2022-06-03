1f57d57ffaf22be1d79bc1559c0731645f62c359
Tom Meschter <tomescht@microsoft.com>
2022-06-03 10:17:42 -0700

    Move IApplicationXamlFileAccessor to its own file
    Move `IApplicationXamlFileAccessor` to its own file, and put it in the Microsoft.VisualStudio.ProjectSystem.WPF namespace.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/ApplicationPropertyPage/WPFValueProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/ApplicationPropertyPage/WPFValueProvider.cs
@@ -1,5 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using Microsoft.VisualStudio.ProjectSystem.WPF;
+
 namespace Microsoft.VisualStudio.ProjectSystem.Properties;
 
 [ExportInterceptingPropertyValueProvider(
@@ -61,14 +63,4 @@ internal class WPFValueProvider : InterceptingPropertyValueProviderBase
             _ => throw new InvalidOperationException($"The {nameof(WPFValueProvider)} does not support the '{propertyName}' property.")
         };
     }
-}
-
-[ProjectSystemContract(ProjectSystemContractScope.UnconfiguredProject, ProjectSystemContractProvider.Extension, Cardinality = Composition.ImportCardinality.ExactlyOne)]
-internal interface IApplicationXamlFileAccessor
-{
-    Task<string?> GetStartupUriAsync();
-    Task SetStartupUriAsync(string startupUri);
-
-    Task<string?> GetShutdownModeAsync();
-    Task SetShutdownModeAsync(string shutdownMode);
 }
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/WPF/IApplicationXamlFileAccessor.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+namespace Microsoft.VisualStudio.ProjectSystem.WPF;
+
+[ProjectSystemContract(ProjectSystemContractScope.UnconfiguredProject, ProjectSystemContractProvider.Extension, Cardinality = Composition.ImportCardinality.ExactlyOne)]
+internal interface IApplicationXamlFileAccessor
+{
+    Task<string?> GetStartupUriAsync();
+    Task SetStartupUriAsync(string startupUri);
+
+    Task<string?> GetShutdownModeAsync();
+    Task SetShutdownModeAsync(string shutdownMode);
+}
