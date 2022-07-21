43397546f28cbc78a35cdfea376d806efea27349
Michael Yanni <miyanni@microsoft.com>
2022-07-20 17:04:49 -0700

    Mid progress on Build.proj refactoring. Simplifying build process.

--- build.cmd
+++ build.cmd
@@ -9,14 +9,9 @@ set RootSuffixCmdLine=
 set OptBuild=true
 set OptDiagnostic=false
 set OptRebuild=false
-set OptDeploy=true
 set OptTest=true
-set OptIntegrationTest=false
 set OptCI=false
-set OptSign=false
-set OptIbc=false
 set OptNodeReuse=true
-set OptClearNuGetCache=false
 
 :ParseArguments
 if    "%1" == ""                                                                                    goto :DoneParsing
@@ -26,20 +21,10 @@ if /I "%1" == "/no-build"             set "OptBuild=false" && set "OptRebuild=fa
 if /I "%1" == "/rebuild"              set "OptBuild=false" && set "OptRebuild=true"                 && shift && goto :ParseArguments
 if /I "%1" == "/test"                 set "OptTest=true"                                            && shift && goto :ParseArguments
 if /I "%1" == "/no-test"              set "OptTest=false"                                           && shift && goto :ParseArguments
-if /I "%1" == "/integration"          set "OptIntegrationTest=true"                                 && shift && goto :ParseArguments
-if /I "%1" == "/no-integration"       set "OptIntegrationTest=false"                                && shift && goto :ParseArguments
-if /I "%1" == "/deploy"               set "OptDeploy=true"                                          && shift && goto :ParseArguments
-if /I "%1" == "/no-deploy"            set "OptDeploy=false"                                         && shift && goto :ParseArguments
 if /I "%1" == "/diagnostic"           set "OptDiagnostic=true"                                      && shift && goto :ParseArguments
 if /I "%1" == "/no-diagnostic"        set "OptDiagnostic=false"                                     && shift && goto :ParseArguments
-if /I "%1" == "/sign"                 set "OptSign=true"                                            && shift && goto :ParseArguments
-if /I "%1" == "/no-sign"              set "OptSign=false"                                           && shift && goto :ParseArguments
 if /I "%1" == "/ci"                   set "OptCI=true"     && set "OptNodeReuse=false"              && shift && goto :ParseArguments
 if /I "%1" == "/no-ci"                set "OptCI=false"    && set "OptNodeReuse=true"               && shift && goto :ParseArguments
-if /I "%1" == "/ibc"                  set "OptIbc=true"                                             && shift && goto :ParseArguments
-if /I "%1" == "/no-ibc"               set "OptIbc=false"                                            && shift && goto :ParseArguments
-if /I "%1" == "/clearnugetcache"      set "OptClearNuGetCache=true"                                 && shift && goto :ParseArguments
-if /I "%1" == "/no-clearnugetcache"   set "OptClearNuGetCache=false"                                && shift && goto :ParseArguments
 if /I "%1" == "/rootsuffix"           set "RootSuffixCmdLine=/p:RootSuffix=%2"                      && shift && shift && goto :ParseArguments
 if /I "%1" == "/configuration"        set "BuildConfiguration=%2"                                   && shift && shift && goto :ParseArguments
 
@@ -57,9 +42,11 @@ if "%OptDiagnostic%" == "true" (
 
 call "%Root%\eng\scripts\SetVSEnvironment.cmd" || exit /b 1
 
-msbuild %Root%eng\Build.proj /m /warnaserror /nologo /clp:Summary /nodeReuse:%OptNodeReuse% /p:Configuration=%BuildConfiguration% /p:Build=%OptBuild% /p:Rebuild=%OptRebuild% /p:Deploy=%OptDeploy% /p:Test=%OptTest% /p:IntegrationTest=%OptIntegrationTest% /p:Sign=%OptSign% /p:CIBuild=%OptCI% /p:EnableIbc=%OptIbc% /p:ClearNuGetCache=%OptClearNuGetCache% %LogCmdLine% %RootSuffixCmdLine%
+set MSBuildDebugEngine=1
+msbuild %Root%eng\Build.proj /m /warnaserror /nologo /clp:Summary /nodeReuse:%OptNodeReuse% /p:Configuration=%BuildConfiguration% /p:Build=%OptBuild% /p:Rebuild=%OptRebuild% /p:Test=%OptTest% /p:CIBuild=%OptCI% %LogCmdLine% %RootSuffixCmdLine%
 set MSBuildErrorLevel=%ERRORLEVEL%
 
+REM Runs the binlog after build.
 if "%OptDiagnostic%" == "true" if "%OptCI%" == "false" (
     start %LogFile%
 )
@@ -75,15 +62,10 @@ echo     /[no]-build               Perform a build (default) or not
 echo.
 echo   Test targets:
 echo     /[no-]test                Run (default) or skip unit tests
-echo     /[no-]integration         Run or skip (default) integration tests
 echo.
 echo   Build options:
 echo     /[no-]diagnostic          Turns on or turns off (default) logging to a binlog
-echo     /[no-]deploy              Deploy (default) or skip deploying Visual Studio extensions
-echo     /[no-]sign                Sign (default) or skip signing build outputs
 echo     /[no-]ci                  Turns on or turns off (default) a continuous integration build
-echo     /[no-]ibc                 Turns on or turns off (default) IBC (OptProf) optimization data usage
-echo     /[no-]clearnugetcache     Clears or skips clearing (default) NuGet package cache
 echo     /rootsuffix ^<hive^>        Hive to use when deploying Visual Studio extensions (default is 'Exp')
 echo     /configuration ^<config^>   Use Debug (default) or Release build configuration
 goto :eof
