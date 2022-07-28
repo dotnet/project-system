6b014238374cdf343c8b0c685d2ff313921b8514
Osvaldo Calles <oscalles@microsoft.com>
2022-07-28 10:28:39 -0700

    Code cleanup

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/UpToDate/BuildUpToDateCheck.ItemHashing.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/UpToDate/BuildUpToDateCheck.ItemHashing.cs
@@ -1,7 +1,5 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
-using Microsoft.VisualStudio.Text;
-
 namespace Microsoft.VisualStudio.ProjectSystem.UpToDate
 {
     internal sealed partial class BuildUpToDateCheck
@@ -22,17 +20,72 @@ namespace Microsoft.VisualStudio.ProjectSystem.UpToDate
 
                 foreach (UpToDateCheckInputItem item in items)
                 {
-                    itemHash ^= item.Path.GetDeterministicHashCode();
+                    itemHash ^= GetStableHashCode(item.Path);
                 }
 
                 // Multiply by the item type hash, so that if an item changes type the hash will change.
                 // The rest of the system does not really need this though, as it is assumed the only way
                 // an item can change type is if a project file changes, which would be detected via
                 // file timestamp changes.
-                hash ^= itemHash * itemType.GetDeterministicHashCode();
+                hash ^= itemHash * GetStableHashCode(itemType);
             }
 
             return hash;
+        }
+
+        /// <summary>
+        /// Please, do not make changes to this hash algorithm.
+        /// Current hash value is persisted in a file in the .vs folder, 
+        /// changing this algorithm may regress performance and break compatibility.
+        /// 
+        /// The original code was taken from string.GetHashCode() with some minor changes
+        /// https://github.com/microsoft/referencesource/blob/master/mscorlib/system/string.cs
+        /// </summary>
+        internal static int GetStableHashCode(string str)
+        {
+            unsafe
+            {
+                fixed (char* src = str)
+                {
+#if WIN32
+                int hash1 = (5381<<16) + 5381;
+#else
+                    int hash1 = 5381;
+#endif
+                    int hash2 = hash1;
+
+#if WIN32
+                // 32 bit machines.
+                int* pint = (int *)src;
+                int len = this.Length;
+                while (len > 2)
+                {
+                    hash1 = ((hash1 << 5) + hash1 + (hash1 >> 27)) ^ pint[0];
+                    hash2 = ((hash2 << 5) + hash2 + (hash2 >> 27)) ^ pint[1];
+                    pint += 2;
+                    len  -= 4;
+                }
+
+                if (len > 0)
+                {
+                    hash1 = ((hash1 << 5) + hash1 + (hash1 >> 27)) ^ pint[0];
+                }
+#else
+                    int c;
+                    char* s = src;
+                    while ((c = s[0]) != 0)
+                    {
+                        hash1 = ((hash1 << 5) + hash1) ^ c;
+                        c = s[1];
+                        if (c == 0)
+                            break;
+                        hash2 = ((hash2 << 5) + hash2) ^ c;
+                        s += 2;
+                    }
+#endif
+                    return hash1 + (hash2 * 1566083941);
+                }
+            }
         }
     }
 }
