923f6ca99041171b2cf6b129e1a8da7523d656a7
Michael Yanni <miyanni@microsoft.com>
2022-08-24 18:24:56 -0700

    Now using the DotNet-Project-System variable group. Added parameter for running optimization optionally. Testing setting the version variable manually in the pipeline.

--- eng/scripts/GetMSBuildPath.ps1
+++ eng/scripts/GetMSBuildPath.ps1
@@ -2,13 +2,19 @@
 
 # Gets the path to MSBuild via vswhere.exe. Returns nothing if a suitable Visual Studio version is not installed.
 
-param ([Parameter(Mandatory=$true)] [string] $version)
+param ([string] $version)
 
 # https://github.com/microsoft/vswhere/wiki/Installing
 $installerPath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer"
 if(-Not (Test-Path -Path $installerPath))
 {
   return
+}
+
+# If the version is not provided, use the version fron the version.json at the root of the repo.
+if(-Not $version)
+{
+  $version = $(. '.\GetVisualStudioMinimumVersion.ps1')
 }
 
 # Note: Along with VS installations, this finds BuildTools' MSBuild.exe via '-products *'. However, the repo currently cannot deploy the VS Extensions via BuildTools.
