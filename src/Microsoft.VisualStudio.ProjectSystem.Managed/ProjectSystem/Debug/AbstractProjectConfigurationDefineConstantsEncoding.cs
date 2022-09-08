99229bef40e3dae2e714d41e7feb3b7e7c47000f
Hailey McKelvie <hmckelvie@microsoft.com>
2022-09-08 16:27:30 -0700

    class converts defineConstants input to Dict

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/AbstractProjectConfigurationDefineConstantsEncoding.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+namespace Microsoft.VisualStudio.ProjectSystem.Debug
+{
+    /// <summary>
+    ///  Converts the single string representation og multiple key, value pairs into a dictionary
+    ///  for defining constants.
+    /// </summary>
+    /// <remarks>
+    /// There are many constants that could be set, implying the dictionary key, value format.
+    /// </remarks>
+    internal static class AbstractProjectConfigurationDefineConstants
+    {
+        private static readonly KeyValuePairListEncoding _encoding = new();
+
+        public static void ParseIntoDictionary(string inputValue, Dictionary<string, string> dictionary)
+        {
+            dictionary.Clear();
+            foreach ((string key, string value) in _encoding.Parse(inputValue))
+            {
+                if (!string.IsNullOrEmpty(key))
+                {
+                    dictionary.Add(key, value);
+                }
+            }
+        }
+
+    }
+}
