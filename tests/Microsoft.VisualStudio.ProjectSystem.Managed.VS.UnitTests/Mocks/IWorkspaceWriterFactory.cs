a69ad6cc92c10d8b49669343357da0ace981c137
Drew Noakes <git@drewnoakes.com>
2022-09-19 09:58:25 +1000

    Prevent exception when language service not enabled
    Fixes #8500
    
    In some cases the language service host environment may signal that language services should not be enabled. Command line mode /build is an example of this.
    
    Users of the language service receive exceptions when attempting to use its APIs when it is disabled.
    
    This change allows callers to validate that the language service is enabled in order to avoid such exceptions.
    
    #8502 tracks using project capabilities instead of this manual check. As part of that work, we would revert this change, instead updating the project capabilities on `LanguageServiceErrorListProvider` itself (or just making the `LanguageServices` capability dynamic).

--- tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/IWorkspaceWriterFactory.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/IWorkspaceWriterFactory.cs
@@ -1,5 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using Microsoft.VisualStudio.Threading;
+
 namespace Microsoft.VisualStudio.ProjectSystem.LanguageServices
 {
     internal static class IWorkspaceWriterFactory
@@ -35,6 +37,11 @@ namespace Microsoft.VisualStudio.ProjectSystem.LanguageServices
             public WorkspaceWriter(IWorkspace workspace)
             {
                 _workspace = workspace;
+            }
+
+            public Task<bool> IsEnabledAsync(CancellationToken cancellationToken = default)
+            {
+                return TaskResult.True;
             }
 
             public Task WhenInitialized(CancellationToken cancellationToken = default)
