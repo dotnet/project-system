3877ee44e0c2733ff15a0433615c4144c5000780
Drew Noakes <git@drewnoakes.com>
2022-09-19 16:05:35 +1000

    Fix and extend IVSShellServices
    Fixes #8504
    
    This interface and its implementation (`VSShellServices`) was copied from CPS in #8239, but nothing ever initialised the implementation.
    
    This change changes the API so that these values can be lazily implemented and efficiently queried.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/UI/InfoBarService/VsInfoBarService.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/UI/InfoBarService/VsInfoBarService.cs
@@ -37,7 +37,7 @@ internal partial class VsInfoBarService : IInfoBarService
         Requires.NotNullOrEmpty(message, nameof(message));
         Requires.NotNull(items, nameof(items));
 
-        if (!_vsShellServices.IsInServerMode)
+        if (!await _vsShellServices.IsInServerModeAsync(cancellationToken))
         {
             await _threadingService.SwitchToUIThread(cancellationToken);
 
