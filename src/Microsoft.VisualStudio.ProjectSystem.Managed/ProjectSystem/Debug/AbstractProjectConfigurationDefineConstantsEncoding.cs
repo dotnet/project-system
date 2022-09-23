b3ce66d6555bd17da0dbfe3e09ed5bf9fdb00f9b
Hailey McKelvie <hmckelvie@microsoft.com>
2022-09-23 13:19:13 -0700

    update defineConstant encoding to include formatting, edit documentation

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/AbstractProjectConfigurationDefineConstantsEncoding.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/AbstractProjectConfigurationDefineConstantsEncoding.cs
@@ -1,17 +1,18 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System.Collections.Generic;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+
 namespace Microsoft.VisualStudio.ProjectSystem.Debug
 {
     /// <summary>
-    ///  Converts the single string representation og multiple key, value pairs into a dictionary
+    ///  Converts the single string representation to multiple key, value pairs used
     ///  for defining constants.
     /// </summary>
-    /// <remarks>
-    /// There are many constants that could be set, implying the dictionary key, value format.
-    /// </remarks>
-    internal static class AbstractProjectConfigurationDefineConstants
+    internal static class AbstractProjectConfigurationDefineConstantsEncoding
     {
         private static readonly KeyQuotedValuePairListEncoding _encoding = new();
+        private static readonly KeyValuePairListEncoding _dispEncoding = new();
 
         public static Dictionary<string, string> ParseIntoDictionary(string inputValue)
         {
@@ -20,11 +21,31 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
             foreach ((string key, string value) in _encoding.Parse(inputValue))
             {
                 if (!string.IsNullOrEmpty(key))
-                {
-                    constantsDict.Add(key, value);
-                }
+                    if (!string.IsNullOrEmpty(key))
+                    {
+                        constantsDict.Add(key, value);
+                    }
             }
             return constantsDict;
+        }
+
+        public static string Format(Dictionary<string, string> constantsDict)
+        {
+            return _encoding.Format(EnumerateConstantsDict(constantsDict));
+        }
+
+        public static string DisplayFormat(Dictionary<string, string> constantsDict)
+        {
+            return _dispEncoding.Format(EnumerateConstantsDict(constantsDict));
+        }
+        private static IEnumerable<(string key, string value)> EnumerateConstantsDict(Dictionary<string, string> constantsDict)
+        {
+            return constantsDict switch
+            {
+                null => Enumerable.Empty<(string key, string value)>(),
+                { Count: 0 } => Enumerable.Empty<(string key, string value)>(),
+                _ => constantsDict.OrderBy(kvp => kvp.Key).Select(kvp => (kvp.Key, kvp.Value))
+            };
         }
     }
 }
