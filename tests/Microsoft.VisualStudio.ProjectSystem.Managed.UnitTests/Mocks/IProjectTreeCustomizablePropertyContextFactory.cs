37142ca46345b5bcabf371c06d29471c15fd26bf
Osvaldo Calles <oscalles@microsoft.com>
2022-10-12 18:11:02 -0700

    Refactor move TreeItemOrderProvider to ProjectSystem.Managed project

--- tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Mocks/IProjectTreeCustomizablePropertyContextFactory.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Mocks/IProjectTreeCustomizablePropertyContextFactory.cs
@@ -8,5 +8,21 @@ namespace Microsoft.VisualStudio.ProjectSystem
         {
             return Mock.Of<IProjectTreeCustomizablePropertyContext>();
         }
+
+        public static IProjectTreeCustomizablePropertyContext Implement(
+            string? itemName = null,
+            string? itemType = null,
+            bool isFolder = false,
+            ProjectTreeFlags flags = default,
+            IImmutableDictionary<string, string>? metadata = null)
+        {
+            var mock = new Mock<IProjectTreeCustomizablePropertyContext>();
+            mock.Setup(x => x.ItemName).Returns(itemName ?? string.Empty);
+            mock.Setup(x => x.ItemType).Returns(itemType);
+            mock.Setup(x => x.IsFolder).Returns(isFolder);
+            mock.Setup(x => x.ParentNodeFlags).Returns(flags);
+            mock.Setup(x => x.Metadata).Returns(metadata ?? ImmutableStringDictionary<string>.EmptyOrdinal);
+            return mock.Object;
+        }
     }
 }
