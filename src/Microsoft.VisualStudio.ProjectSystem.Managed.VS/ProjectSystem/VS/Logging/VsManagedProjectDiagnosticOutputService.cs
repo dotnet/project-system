92a52c6883a67f1e3d1cbe04e2525fd6eb8200aa
Tom Meschter <tomescht@microsoft.com>
2022-10-13 13:58:25 -0700

    Ban IProjectDiagnosticOutputService
    Mark `IProjectDiagnosticOutputService` as banned to encourage the use of `IManagedProjectDiagnosticOutputService` which is available at lower layers of the code, not just at the VS layer.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Logging/VsManagedProjectDiagnosticOutputService.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Logging/VsManagedProjectDiagnosticOutputService.cs
@@ -2,11 +2,18 @@
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Logging;
 
+#pragma warning disable RS0030 // Do not used banned APIs
 /// <summary>
 ///   An implementation of the <see cref="IManagedProjectDiagnosticOutputService"/> that
 ///   delegates to the CPS <see cref="IProjectDiagnosticOutputService"/>.
 /// </summary>
+/// <remarks>
+///   Note <see cref="IProjectDiagnosticOutputService"/> has been banned in order to
+///   encourage the use of the more widely available <see cref="IManagedProjectDiagnosticOutputService"/>,
+///   not for any technical reason.
+/// </remarks>
 [Export(typeof(IManagedProjectDiagnosticOutputService))]
+#pragma warning restore RS0030 // Do not used banned APIs
 [AppliesTo(ProjectCapability.DotNet)]
 internal class VsManagedProjectDiagnosticOutputService : IManagedProjectDiagnosticOutputService
 {
@@ -18,10 +25,14 @@ internal class VsManagedProjectDiagnosticOutputService : IManagedProjectDiagnost
         _projectDiagnosticOutputService = projectDiagnosticOutputService;
     }
 
+#pragma warning disable RS0030 // Do not used banned APIs
     public bool IsEnabled => _projectDiagnosticOutputService.IsEnabled;
+#pragma warning restore RS0030 // Do not used banned APIs
 
     public void WriteLine(string outputMessage)
     {
+#pragma warning disable RS0030 // Do not used banned APIs
         _projectDiagnosticOutputService.WriteLine(outputMessage);
+#pragma warning restore RS0030 // Do not used banned APIs
     }
 }
