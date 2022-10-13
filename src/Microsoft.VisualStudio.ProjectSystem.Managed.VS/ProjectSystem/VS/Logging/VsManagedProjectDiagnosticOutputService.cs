f020522969e47f730b7d484ce217df685f41e756
Tom Meschter <tomescht@microsoft.com>
2022-10-13 13:26:51 -0700

    Add implementation of IManagedProjectDiagnosticOutputService
    Add a new `VsManagedProjectDiagnosticOutputService` to implement `IManagedProjectDiagnosticOutputService`. The new type delegates to the CPS `IProjectDiagnosticOutputService` implementation.

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Logging/VsManagedProjectDiagnosticOutputService.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Logging;
+
+/// <summary>
+///   An implementation of the <see cref="IManagedProjectDiagnosticOutputService"/> that
+///   delegates to the CPS <see cref="IProjectDiagnosticOutputService"/>.
+/// </summary>
+[Export(typeof(IManagedProjectDiagnosticOutputService))]
+[AppliesTo(ProjectCapability.DotNet)]
+internal class VsManagedProjectDiagnosticOutputService : IManagedProjectDiagnosticOutputService
+{
+    private readonly IProjectDiagnosticOutputService _projectDiagnosticOutputService;
+
+    [ImportingConstructor]
+    public VsManagedProjectDiagnosticOutputService(IProjectDiagnosticOutputService projectDiagnosticOutputService)
+    {
+        _projectDiagnosticOutputService = projectDiagnosticOutputService;
+    }
+
+    public bool IsEnabled => _projectDiagnosticOutputService.IsEnabled;
+
+    public void WriteLine(string outputMessage)
+    {
+        _projectDiagnosticOutputService.WriteLine(outputMessage);
+    }
+}
