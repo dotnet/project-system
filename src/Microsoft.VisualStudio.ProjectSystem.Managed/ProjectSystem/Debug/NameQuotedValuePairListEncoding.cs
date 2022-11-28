26d848428b1adf2cbb0b8bf309d6e5b56a567d53
haileymck <111816896+haileymck@users.noreply.github.com>
2022-11-28 10:15:22 -0800

    VB define constants property provider

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/NameQuotedValuePairListEncoding.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/NameQuotedValuePairListEncoding.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Microsoft.VisualStudio.ProjectSystem.VS.PropertyPages.Designer;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Debug
 {
+
     [Export(typeof(INameValuePairListEncoding))]
     [ExportMetadata("Encoding", "NameQuotedValuePairListEncoding")]
     internal sealed class NameQuotedValuePairListEncoding : INameValuePairListEncoding
     {
-        public static KeyQuotedValuePairListEncoding Instance { get; } = new();
         public IEnumerable<(string Name, string Value)> Parse(string value)
         {
             if (string.IsNullOrWhiteSpace(value))
@@ -21,67 +19,14 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
             foreach (var entry in ReadEntries(value))
             {
                 var (entryKey, entryValue) = SplitEntry(entry);
-                var decodedEntryKey = Decode(entryKey);
-                var decodedEntryValue = Decode(entryValue);
 
-                if (!string.IsNullOrEmpty(decodedEntryKey))
+                if (!string.IsNullOrEmpty(entryKey))
                 {
-                    yield return (decodedEntryKey, decodedEntryValue);
+                    yield return (entryKey, entryValue);
                 }
-            }
-
-            static IEnumerable<string> ReadEntries(string rawText)
-            {
-                bool escaped = false;
-                int entryStart = 0;
-                for (int i = 0; i < rawText.Length; i++)
-                {
-                    if (rawText[i] == ',' && !escaped)
-                    {
-                        yield return rawText.Substring(entryStart, i - entryStart);
-                        entryStart = i + 1;
-                        escaped = false;
-                    }
-                    else if (rawText[i] == '/')
-                    {
-                        escaped = !escaped;
-                    }
-                    else
-                    {
-                        escaped = false;
-                    }
-                }
-
-                yield return rawText.Substring(entryStart);
-            }
-
-            static (string EncodedKey, string EncodedValue) SplitEntry(string entry)
-            {
-                bool escaped = false;
-                for (int i = 0; i < entry.Length; i++)
-                {
-                    if (entry[i] == '=' && !escaped)
-                    {
-                        return (entry.Substring(0, i), entry.Substring(i + 1));
-                    }
-                    else if (entry[i] == '/')
-                    {
-                        escaped = !escaped;
-                    }
-                    else
-                    {
-                        escaped = false;
-                    }
-                }
-
-                return (string.Empty, string.Empty);
-            }
-
-            static string Decode(string value)
-            {
-                return value.Replace("/=", "=").Replace("/,", ",").Replace("//", "/");
             }
         }
+
         public string Format(IEnumerable<(string Name, string Value)> pairs)
         {
             return string.Join(",", pairs.Select(pair => $"{EncodeCharacters(pair.Name)}=\"{EncodeCharacters(pair.Value)}\""));
@@ -90,6 +35,94 @@ namespace Microsoft.VisualStudio.ProjectSystem.Debug
             {
                 return value.Replace("/", "//").Replace(",", "/,").Replace("=", "/=");
             }
+        }
+
+        public IEnumerable<(string Name, string Value)> Decode(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                yield break;
+            }
+
+            foreach (var entry in ReadEntries(value))
+            {
+                var (entryKey, entryQuotedValue) = SplitEntry(entry);
+                var decodedEntryKey = DecodeCharacters(entryKey);
+                var decodedEntryValue = DecodeCharacters(Unquote(entryQuotedValue));
+
+                if (!string.IsNullOrEmpty(decodedEntryKey))
+                {
+                    yield return (decodedEntryKey, decodedEntryValue);
+                }
+            }
+
+            static string Unquote(string value)
+            {   if (value.Length > 2)
+                {
+                    return value.Substring(1, value.Length - 2);
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+
+            static string DecodeCharacters(string value)
+            {
+                return value.Replace("/=", "=").Replace("/,", ",").Replace("//", "/");
+            }
+        }
+
+        public string DisplayFormat(IEnumerable<(string Name, string Value)> pairs)
+        {
+            return string.Join(",", pairs.Select(pair => $"{pair.Name}={pair.Value}"));
+        }
+
+        private static IEnumerable<string> ReadEntries(string rawText)
+        {
+            bool escaped = false;
+            int entryStart = 0;
+            for (int i = 0; i < rawText.Length; i++)
+            {
+                if (rawText[i] == ',' && !escaped)
+                {
+                    yield return rawText.Substring(entryStart, i - entryStart);
+                    entryStart = i + 1;
+                    escaped = false;
+                }
+                else if (rawText[i] == '/')
+                {
+                    escaped = !escaped;
+                }
+                else
+                {
+                    escaped = false;
+                }
+            }
+
+            yield return rawText.Substring(entryStart);
+        }
+
+        private static (string EncodedKey, string EncodedValue) SplitEntry(string entry)
+        {
+            bool escaped = false;
+            for (int i = 0; i < entry.Length; i++)
+            {
+                if (entry[i] == '=' && !escaped)
+                {
+                    return (entry.Substring(0, i), entry.Substring(i + 1));
+                }
+                else if (entry[i] == '/')
+                {
+                    escaped = !escaped;
+                }
+                else
+                {
+                    escaped = false;
+                }
+            }
+
+            return (string.Empty, string.Empty);
         }
     }
 }
--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/BuildPropertyPage/DefineConstantsVBValueProvider.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/BuildPropertyPage/DefineConstantsVBValueProvider.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
 using Microsoft.VisualStudio.ProjectSystem.Debug;
 
@@ -14,19 +10,22 @@ internal sealed class DefineConstantsVBValueProvider : InterceptingPropertyValue
 {
     internal const string DefineConstantsPropertyName = "DefineConstants";
 
+    private readonly NameQuotedValuePairListEncoding _encoding = new();
+
     public override Task<string?> OnSetPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IProjectProperties defaultProperties, IReadOnlyDictionary<string, string>? dimensionalConditions = null)
     {
-        return Task.FromResult<string?>(NameQuotedValuePairListEncoding.Instance.Format(NameQuotedValuePairListEncoding.Instance.Parse(unevaluatedPropertyValue)));
+        return Task.FromResult<string?>(_encoding.Format(_encoding.Parse(unevaluatedPropertyValue)));
     }
 
     public override Task<string> OnGetEvaluatedPropertyValueAsync(string propertyName, string evaluatedPropertyValue, IProjectProperties defaultProperties)
     {
-        return Task.FromResult<string?>(NameQuotedValuePairListEncoding.Instance.Format(evaluatedPropertyValue));
+
+        return Task.FromResult<string>(_encoding.DisplayFormat(_encoding.Decode(evaluatedPropertyValue)));
     }
 
     public override Task<string> OnGetUnevaluatedPropertyValueAsync(string propertyName, string unevaluatedPropertyValue, IProjectProperties defaultProperties)
     {
-        return Task.FromResult<string?>(NameQuotedValuePairListEncoding.Instance.Format(unevaluatedPropertyValue));
+        return Task.FromResult<string>(_encoding.DisplayFormat(_encoding.Decode(unevaluatedPropertyValue)));
     }
 }
 
