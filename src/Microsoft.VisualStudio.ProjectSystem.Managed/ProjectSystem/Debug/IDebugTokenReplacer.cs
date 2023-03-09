3108fc1235783fca4bfcef910b5a89cba3cf62b5
Drew Noakes <git@drewnoakes.com>
2023-03-09 21:12:07 +1100

    Consolidate and extend documentation

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/IDebugTokenReplacer.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Debug/IDebugTokenReplacer.cs
@@ -3,14 +3,32 @@
 namespace Microsoft.VisualStudio.ProjectSystem.Debug
 {
     /// <summary>
-    /// Given an ILaunchProfile, it will enumerate the items and do replacement on the each string
-    /// entry.
+    /// Replaces tokens of form <c>%VAR%</c> with their environment variable value, and of form
+    /// <c>$(Property)</c> with the active configured project's MSBuild evaluation values.
     /// </summary>
+    /// <remarks>
+    /// Supports token substitution in <see cref="ILaunchProfile"/> data via <see cref="ReplaceTokensInProfileAsync"/>,
+    /// but can also substitute individual strings via <see cref="ReplaceTokensInStringAsync"/>.
+    /// </remarks>
     [ProjectSystemContract(ProjectSystemContractScope.UnconfiguredProject, ProjectSystemContractProvider.Private, Cardinality = ImportCardinality.ExactlyOne)]
     public interface IDebugTokenReplacer
     {
+        /// <summary>
+        /// Walks the profile and returns a new one where all the tokens have been replaced. Tokens can consist of
+        /// environment variables (<c>%VAR%</c>), or any MSBuild property (<c>$(Property)</c>).
+        /// </summary>
+        /// <remarks>
+        /// Environment variables are replaced first, followed by MSBuild properties.
+        /// </remarks>
         Task<ILaunchProfile> ReplaceTokensInProfileAsync(ILaunchProfile profile);
 
+        /// <summary>
+        /// Replaces the tokens and environment variables in a single string. If expandEnvironmentVars
+        /// is true, they are expanded first before replacement happens.
+        /// </summary>
+        /// <remarks>
+        /// If <paramref name="rawString"/> is <see langword="null"/> or empty, it is returned as is.
+        /// </remarks>
         Task<string> ReplaceTokensInStringAsync(string rawString, bool expandEnvironmentVars);
     }
 }
