f699a3257d08462db6370d544082e9cab788b14a
Tom Meschter <tomescht@microsoft.com>
2023-03-14 13:24:40 -0700

    Switch to file-scope namespaces

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/Telemetry/ITelemetryService.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/Telemetry/ITelemetryService.cs
@@ -2,108 +2,107 @@
 
 using Microsoft.VisualStudio.ProjectSystem;
 
-namespace Microsoft.VisualStudio.Telemetry
+namespace Microsoft.VisualStudio.Telemetry;
+
+[ProjectSystemContract(ProjectSystemContractScope.Global, ProjectSystemContractProvider.Private, Cardinality = ImportCardinality.ExactlyOne)]
+internal interface ITelemetryService
 {
-    [ProjectSystemContract(ProjectSystemContractScope.Global, ProjectSystemContractProvider.Private, Cardinality = ImportCardinality.ExactlyOne)]
-    internal interface ITelemetryService
-    {
-        /// <summary>
-        ///     Posts an event with the specified event name.
-        /// </summary>
-        /// <param name="eventName">
-        ///     The name of the event.
-        /// </param>
-        /// <exception cref="ArgumentNullException">
-        ///     <paramref name="eventName"/> is <see langword="null"/>.
-        /// </exception>
-        /// <exception cref="ArgumentException">
-        ///     <paramref name="eventName"/> is an empty string ("").
-        /// </exception>
-        void PostEvent(string eventName);
+    /// <summary>
+    ///     Posts an event with the specified event name.
+    /// </summary>
+    /// <param name="eventName">
+    ///     The name of the event.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    ///     <paramref name="eventName"/> is <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    ///     <paramref name="eventName"/> is an empty string ("").
+    /// </exception>
+    void PostEvent(string eventName);
 
-        /// <summary>
-        ///     Posts an event with the specified event name and property with the
-        ///     specified name and value.
-        /// </summary>
-        /// <param name="eventName">
-        ///     The name of the event.
-        /// </param>
-        /// <param name="propertyName">
-        ///     The name of the property.
-        /// </param>
-        /// <param name="propertyValue">
-        ///     The value of the property.
-        /// </param>
-        /// <exception cref="ArgumentNullException">
-        ///     <paramref name="eventName"/> is <see langword="null"/>.
-        ///     <para>
-        ///         -or-
-        ///     </para>
-        ///     <paramref name="propertyName"/> is <see langword="null"/>.
-        ///     <para>
-        ///         -or-
-        ///     </para>
-        ///     <paramref name="propertyValue"/> is <see langword="null"/>.
-        /// </exception>
-        /// <exception cref="ArgumentException">
-        ///     <paramref name="eventName"/> is an empty string ("").
-        ///     <para>
-        ///         -or
-        ///     </para>
-        ///     <paramref name="propertyName"/> is an empty string ("").
-        /// </exception>
-        void PostProperty(string eventName, string propertyName, object propertyValue);
+    /// <summary>
+    ///     Posts an event with the specified event name and property with the
+    ///     specified name and value.
+    /// </summary>
+    /// <param name="eventName">
+    ///     The name of the event.
+    /// </param>
+    /// <param name="propertyName">
+    ///     The name of the property.
+    /// </param>
+    /// <param name="propertyValue">
+    ///     The value of the property.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    ///     <paramref name="eventName"/> is <see langword="null"/>.
+    ///     <para>
+    ///         -or-
+    ///     </para>
+    ///     <paramref name="propertyName"/> is <see langword="null"/>.
+    ///     <para>
+    ///         -or-
+    ///     </para>
+    ///     <paramref name="propertyValue"/> is <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    ///     <paramref name="eventName"/> is an empty string ("").
+    ///     <para>
+    ///         -or
+    ///     </para>
+    ///     <paramref name="propertyName"/> is an empty string ("").
+    /// </exception>
+    void PostProperty(string eventName, string propertyName, object propertyValue);
 
-        /// <summary>
-        ///     Posts an event with the specified event name and properties with the
-        ///     specified names and values.
-        /// </summary>
-        /// <param name="eventName">
-        ///     The name of the event.
-        /// </param>
-        /// <param name="properties">
-        ///     An <see cref="IEnumerable{T}"/> of property names and values.
-        /// </param>
-        /// <exception cref="ArgumentNullException">
-        ///     <paramref name="eventName"/> is <see langword="null"/>.
-        ///     <para>
-        ///         -or-
-        ///     </para>
-        ///     <paramref name="properties"/> is <see langword="null"/>.
-        /// </exception>
-        /// <exception cref="ArgumentException">
-        ///     <paramref name="eventName"/> is an empty string ("").
-        ///     <para>
-        ///         -or-
-        ///     </para>
-        ///     <paramref name="properties"/> is contains no elements.
-        /// </exception>
-        void PostProperties(string eventName, IEnumerable<(string propertyName, object propertyValue)> properties);
+    /// <summary>
+    ///     Posts an event with the specified event name and properties with the
+    ///     specified names and values.
+    /// </summary>
+    /// <param name="eventName">
+    ///     The name of the event.
+    /// </param>
+    /// <param name="properties">
+    ///     An <see cref="IEnumerable{T}"/> of property names and values.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    ///     <paramref name="eventName"/> is <see langword="null"/>.
+    ///     <para>
+    ///         -or-
+    ///     </para>
+    ///     <paramref name="properties"/> is <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    ///     <paramref name="eventName"/> is an empty string ("").
+    ///     <para>
+    ///         -or-
+    ///     </para>
+    ///     <paramref name="properties"/> is contains no elements.
+    /// </exception>
+    void PostProperties(string eventName, IEnumerable<(string propertyName, object propertyValue)> properties);
 
-        /// <summary>
-        ///     Begins an operation with a recorded duration. Consumers must call <see cref="ITelemetryOperation.End(TelemetryResult)"/>
-        ///     on the returned <see cref="ITelemetryOperation"/> to signal the end of the operation and post the
-        ///     telemetry events.
-        /// </summary>
-        /// <param name="eventName">
-        ///     The name of the event to associate with this operation.
-        /// </param>
-        /// <exception cref="ArgumentNullException">
-        ///     <paramref name="eventName"/> is <see langword="null"/>.
-        /// </exception>
-        /// <exception cref="ArgumentException">
-        ///     <paramref name="eventName"/> is an empty string ("").
-        /// </exception>
-        /// <returns>
-        ///     An <see cref="ITelemetryOperation"/> representing the operation. 
-        /// </returns>
-        ITelemetryOperation BeginOperation(string eventName);
+    /// <summary>
+    ///     Begins an operation with a recorded duration. Consumers must call <see cref="ITelemetryOperation.End(TelemetryResult)"/>
+    ///     on the returned <see cref="ITelemetryOperation"/> to signal the end of the operation and post the
+    ///     telemetry events.
+    /// </summary>
+    /// <param name="eventName">
+    ///     The name of the event to associate with this operation.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    ///     <paramref name="eventName"/> is <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    ///     <paramref name="eventName"/> is an empty string ("").
+    /// </exception>
+    /// <returns>
+    ///     An <see cref="ITelemetryOperation"/> representing the operation. 
+    /// </returns>
+    ITelemetryOperation BeginOperation(string eventName);
 
-        /// <summary>
-        /// Hashes personally identifiable information for telemetry consumption.
-        /// </summary>
-        /// <param name="value">Value to hashed.</param>
-        /// <returns>Hashed value.</returns>
-        string HashValue(string value);
-    }
+    /// <summary>
+    /// Hashes personally identifiable information for telemetry consumption.
+    /// </summary>
+    /// <param name="value">Value to hashed.</param>
+    /// <returns>Hashed value.</returns>
+    string HashValue(string value);
 }
--- src/Microsoft.VisualStudio.ProjectSystem.Managed/Telemetry/ManagedTelemetryService.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/Telemetry/ManagedTelemetryService.cs
@@ -3,134 +3,133 @@
 using System.Security.Cryptography;
 using System.Text;
 
-namespace Microsoft.VisualStudio.Telemetry
+namespace Microsoft.VisualStudio.Telemetry;
+
+[Export(typeof(ITelemetryService))]
+internal class ManagedTelemetryService : ITelemetryService
 {
-    [Export(typeof(ITelemetryService))]
-    internal class ManagedTelemetryService : ITelemetryService
+    public void PostEvent(string eventName)
     {
-        public void PostEvent(string eventName)
+        Requires.NotNullOrEmpty(eventName);
+
+        PostTelemetryEvent(new TelemetryEvent(eventName));
+    }
+
+    public void PostProperty(string eventName, string propertyName, object propertyValue)
+    {
+        Requires.NotNullOrEmpty(eventName);
+        Requires.NotNullOrEmpty(propertyName);
+        Requires.NotNull(propertyValue);
+
+        var telemetryEvent = new TelemetryEvent(eventName);
+        telemetryEvent.Properties.Add(propertyName, propertyValue);
+
+        PostTelemetryEvent(telemetryEvent);
+    }
+
+    public void PostProperties(string eventName, IEnumerable<(string propertyName, object propertyValue)> properties)
+    {
+        Requires.NotNullOrEmpty(eventName);
+        Requires.NotNullOrEmpty(properties);
+
+        var telemetryEvent = new TelemetryEvent(eventName);
+        AddPropertiesToEvent(properties, telemetryEvent);
+
+        PostTelemetryEvent(telemetryEvent);
+    }
+
+    private static void AddPropertiesToEvent(IEnumerable<(string propertyName, object propertyValue)> properties, TelemetryEvent telemetryEvent)
+    {
+        foreach ((string propertyName, object propertyValue) in properties)
         {
-            Requires.NotNullOrEmpty(eventName);
-
-            PostTelemetryEvent(new TelemetryEvent(eventName));
-        }
-
-        public void PostProperty(string eventName, string propertyName, object propertyValue)
-        {
-            Requires.NotNullOrEmpty(eventName);
-            Requires.NotNullOrEmpty(propertyName);
-            Requires.NotNull(propertyValue);
-
-            var telemetryEvent = new TelemetryEvent(eventName);
-            telemetryEvent.Properties.Add(propertyName, propertyValue);
-
-            PostTelemetryEvent(telemetryEvent);
-        }
-
-        public void PostProperties(string eventName, IEnumerable<(string propertyName, object propertyValue)> properties)
-        {
-            Requires.NotNullOrEmpty(eventName);
-            Requires.NotNullOrEmpty(properties);
-
-            var telemetryEvent = new TelemetryEvent(eventName);
-            AddPropertiesToEvent(properties, telemetryEvent);
-
-            PostTelemetryEvent(telemetryEvent);
-        }
-
-        private static void AddPropertiesToEvent(IEnumerable<(string propertyName, object propertyValue)> properties, TelemetryEvent telemetryEvent)
-        {
-            foreach ((string propertyName, object propertyValue) in properties)
+            if (propertyValue is ComplexPropertyValue complexProperty)
             {
-                if (propertyValue is ComplexPropertyValue complexProperty)
-                {
-                    telemetryEvent.Properties.Add(propertyName, new TelemetryComplexProperty(complexProperty.Data));
-                }
-                else
-                {
-                    telemetryEvent.Properties.Add(propertyName, propertyValue);
-                }
+                telemetryEvent.Properties.Add(propertyName, new TelemetryComplexProperty(complexProperty.Data));
+            }
+            else
+            {
+                telemetryEvent.Properties.Add(propertyName, propertyValue);
             }
         }
+    }
 
-        private void PostTelemetryEvent(TelemetryEvent telemetryEvent)
+    private void PostTelemetryEvent(TelemetryEvent telemetryEvent)
+    {
+#if DEBUG
+        Assumes.True(telemetryEvent.Name.StartsWith("vs/projectsystem/managed/", StringComparisons.TelemetryEventNames));
+
+        foreach (string propertyName in telemetryEvent.Properties.Keys)
+        {
+            Assumes.True(propertyName.StartsWith("vs.projectsystem.managed.", StringComparisons.TelemetryEventNames));
+        }
+#endif
+
+        PostEventToSession(telemetryEvent);
+    }
+
+    protected virtual void PostEventToSession(TelemetryEvent telemetryEvent)
+    {
+        TelemetryService.DefaultSession.PostEvent(telemetryEvent);
+    }
+
+    public ITelemetryOperation BeginOperation(string eventName)
+    {
+        Requires.NotNullOrEmpty(eventName);
+        
+#if DEBUG
+        Assumes.True(eventName.StartsWith("vs/projectsystem/managed/", StringComparisons.TelemetryEventNames));
+#endif
+        return new TelemetryOperation(TelemetryService.DefaultSession.StartOperation(eventName));            
+    }
+
+    public string HashValue(string value)
+    {
+        // Don't hash PII for internal users since we don't need to.
+        if (TelemetryService.DefaultSession.IsUserMicrosoftInternal)
+        {
+            return value;
+        }
+
+        byte[] inputBytes = Encoding.UTF8.GetBytes(value);
+        using var cryptoServiceProvider = new SHA256CryptoServiceProvider();
+        return BitConverter.ToString(cryptoServiceProvider.ComputeHash(inputBytes));
+    }
+
+    private class TelemetryOperation : ITelemetryOperation
+    {
+        private readonly TelemetryScope<OperationEvent> _scope;
+
+        public TelemetryOperation(TelemetryScope<OperationEvent> scope)
+        {
+            _scope = scope;
+        }
+
+        public void Dispose()
         {
 #if DEBUG
-            Assumes.True(telemetryEvent.Name.StartsWith("vs/projectsystem/managed/", StringComparisons.TelemetryEventNames));
+            Assumes.True(_scope.IsEnd, $"Failed to call '{nameof(ITelemetryOperation.End)}' on {nameof(ITelemetryOperation)} instance.");
+#endif
 
-            foreach (string propertyName in telemetryEvent.Properties.Keys)
+            _scope.End(TelemetryResult.None);
+        }
+
+        public void End(TelemetryResult result)
+        {
+            _scope.End(result);
+        }
+
+        public void SetProperties(IEnumerable<(string propertyName, object propertyValue)> properties)
+        {
+            Requires.NotNullOrEmpty(properties);
+            
+#if DEBUG
+            foreach ((string propertyName, _) in properties)
             {
                 Assumes.True(propertyName.StartsWith("vs.projectsystem.managed.", StringComparisons.TelemetryEventNames));
             }
 #endif
 
-            PostEventToSession(telemetryEvent);
-        }
-
-        protected virtual void PostEventToSession(TelemetryEvent telemetryEvent)
-        {
-            TelemetryService.DefaultSession.PostEvent(telemetryEvent);
-        }
-
-        public ITelemetryOperation BeginOperation(string eventName)
-        {
-            Requires.NotNullOrEmpty(eventName);
-            
-#if DEBUG
-            Assumes.True(eventName.StartsWith("vs/projectsystem/managed/", StringComparisons.TelemetryEventNames));
-#endif
-            return new TelemetryOperation(TelemetryService.DefaultSession.StartOperation(eventName));            
-        }
-
-        public string HashValue(string value)
-        {
-            // Don't hash PII for internal users since we don't need to.
-            if (TelemetryService.DefaultSession.IsUserMicrosoftInternal)
-            {
-                return value;
-            }
-
-            byte[] inputBytes = Encoding.UTF8.GetBytes(value);
-            using var cryptoServiceProvider = new SHA256CryptoServiceProvider();
-            return BitConverter.ToString(cryptoServiceProvider.ComputeHash(inputBytes));
-        }
-
-        private class TelemetryOperation : ITelemetryOperation
-        {
-            private readonly TelemetryScope<OperationEvent> _scope;
-
-            public TelemetryOperation(TelemetryScope<OperationEvent> scope)
-            {
-                _scope = scope;
-            }
-
-            public void Dispose()
-            {
-#if DEBUG
-                Assumes.True(_scope.IsEnd, $"Failed to call '{nameof(ITelemetryOperation.End)}' on {nameof(ITelemetryOperation)} instance.");
-#endif
-
-                _scope.End(TelemetryResult.None);
-            }
-
-            public void End(TelemetryResult result)
-            {
-                _scope.End(result);
-            }
-
-            public void SetProperties(IEnumerable<(string propertyName, object propertyValue)> properties)
-            {
-                Requires.NotNullOrEmpty(properties);
-                
-#if DEBUG
-                foreach ((string propertyName, _) in properties)
-                {
-                    Assumes.True(propertyName.StartsWith("vs.projectsystem.managed.", StringComparisons.TelemetryEventNames));
-                }
-#endif
-
-                AddPropertiesToEvent(properties, _scope.EndEvent);
-            }
+            AddPropertiesToEvent(properties, _scope.EndEvent);
         }
     }
 }
