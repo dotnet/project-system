99737f64e5f6f01812842aab6c68917b0742ac22
Drew Noakes <git@drewnoakes.com>
2023-04-05 14:44:02 +1000

    Ban TplExtensions.True/False tasks
    We have our own properties for these.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/Threading/Tasks/TaskResult.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/Threading/Tasks/TaskResult.cs
@@ -1,5 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+#pragma warning disable RS0030 // Do not used banned APIs
+
 namespace Microsoft.VisualStudio.Threading
 {
     /// <summary>
