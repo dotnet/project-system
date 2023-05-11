2d61bb7db149fc7a8527a1678483cde1597932c5
Tom Meschter <tomescht@microsoft.com>
2023-05-11 11:49:10 -0700

    Remove OutputTypeChecker use
    Remove the explicit creation and use of the `OutputTypeChecker` class from `ProjectLaunchTargetsProviderTests` and replace it with a mock of `IOutputTypeChecker`. The main benefit here is that it makes it clear that the `ProjectLaunchTargetsProvider` doesn't care about the "OutputType" property per se, but really only cares if the project produces a traditional library, a console app, or "something else", as those are the only possibilities that are actually exposed by `IOutputTypeChecker`.
    
    This does raise the question about whether the behavior is really what we want in the case of "something else". E.g., do we really want to have different behavior for a "library" project versus a "winmdobj"? I don't attempt to answer that question here, but at least the tests more clearly document the existing behavior.

---
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Mocks/IOutputTypeCheckerFactory.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Microsoft.VisualStudio.ProjectSystem.Debug;
+
+namespace Microsoft.VisualStudio.ProjectSystem;
+
+internal static class IOutputTypeCheckerFactory
+{
+    public static IOutputTypeChecker Create(bool? isLibrary = false, bool? isConsole = false)
+    {
+        var mock = new Mock<IOutputTypeChecker>();
+
+        if (isLibrary is not null)
+        {
+            mock.Setup(m => m.IsLibraryAsync()).ReturnsAsync(isLibrary.Value);
+        }
+
+        if (isConsole is not null)
+        {
+            mock.Setup(m => m.IsConsoleAsync()).ReturnsAsync(isConsole.Value);
+        }
+
+        return mock.Object;
+    }
+}
