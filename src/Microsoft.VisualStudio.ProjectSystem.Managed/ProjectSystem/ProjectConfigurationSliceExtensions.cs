2345ac04159b3292868ab7ea0e71530ab5ab42d1
Drew Noakes <git@drewnoakes.com>
2023-05-16 13:27:11 +1000

    Invert boolean logic

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/ProjectConfigurationSliceExtensions.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/ProjectConfigurationSliceExtensions.cs
@@ -12,12 +12,12 @@ internal static class ProjectConfigurationSliceExtensions
         // If all slice dimensions are present with the same value in the configuration, then this is a match.
         foreach ((string name, string value) in slice.Dimensions)
         {
-            if (activeProjectConfiguration.Dimensions.TryGetValue(name, out string activeValue) &&
-                StringComparers.ConfigurationDimensionValues.Equals(value, activeValue))
-                continue;
-
-            // The dimension's value is either unknown, or the value differs. This is not a match.
-            return false;
+            if (!activeProjectConfiguration.Dimensions.TryGetValue(name, out string activeValue) ||
+                !StringComparers.ConfigurationDimensionValues.Equals(value, activeValue))
+            {
+                // The dimension's value is either unknown, or the value differs. This is not a match.
+                return false;
+            }
         }
 
         // All dimensions in the slice match the project configuration.
