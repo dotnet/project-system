eda9f8675449f09357e95515cf2c475fb3cd94c5
Drew Noakes <git@drewnoakes.com>
2023-05-30 14:19:14 +1000

    Use file-scoped namespace

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/PackageRestore/PackageRestoreCycleDetector.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/PackageRestore/PackageRestoreCycleDetector.cs
@@ -2,96 +2,96 @@
 
 using Microsoft.VisualStudio.Text;
 
-namespace Microsoft.VisualStudio.ProjectSystem.PackageRestore
+namespace Microsoft.VisualStudio.ProjectSystem.PackageRestore;
+
+// This algorithm is to detect the pattern A -> B -> A -> B -> A in the most recent N values.
+// To keep track of the most N recent values we are using a queue of fixed size, where:
+//   The most recent value is inserted at the front in O(1) time, and the oldest value is removed at the 
+// back in O(1) time.
+//   The counter will keep track of how many times values have appeared in the queue consecutively.
+// i.e.
+// A -> B -> C -> A -> B -> D -> X -> Y -> X -> Y -> X -> Y -> X
+//                                        |---cycle detected----|
+[Export(typeof(IPackageRestoreCycleDetector))]
+internal sealed class PackageRestoreCycleDetector
 {
-    // This algorithm is to detect the pattern A -> B -> A -> B -> A in the most recent N values.
-    // To keep track of the most N recent values we are using a queue of fixed size, where:
-    //   The most recent value is inserted at the front in O(1) time, and the oldest value is removed at the 
-    // back in O(1) time.
-    //   The counter will keep track of how many times values have appeared in the queue consecutively.
-    // i.e.
-    // A -> B -> C -> A -> B -> D -> X -> Y -> X -> Y -> X -> Y -> X
-    //                                        |---cycle detected----|
-    internal sealed class PackageRestoreCycleDetector
+    /// <summary>
+    ///     Fixed size of the numbers of values to store.
+    /// </summary>
+    /// <remarks>
+    ///     This represents how deep to search for hash cycle.
+    /// </remarks>
+    private const int Size = 5;
+
+    private readonly object _lock = new();
+    private readonly Queue<Hash> _values = new();
+    private readonly Dictionary<Hash, int> _lookupTable = new();
+    private int _counter;
+
+    /// <summary>
+    ///     Validate if hash1 cycle exist. 
+    ///     If no cycle exist, then the hashValue is stored.
+    /// </summary>
+    /// <param name="hashValue">This is hashValue that is used to verify if it exists in previous restores</param>
+    /// <returns>True if it contains hash1 cycle, otherwise false</returns>
+    public bool ComputeCycleDetection(Hash hashValue)
     {
-        /// <summary>
-        ///     Fixed size of the numbers of values to store.
-        /// </summary>
-        /// <remarks>
-        ///     This represents how deep to search for hash cycle.
-        /// </remarks>
-        private const int Size = 5;
-
-        private readonly object _lock = new();
-        private readonly Queue<Hash> _values = new();
-        private readonly Dictionary<Hash, int> _lookupTable = new();
-        private int _counter;
-
-        /// <summary>
-        ///     Validate if hash1 cycle exist. 
-        ///     If no cycle exist, then the hashValue is stored.
-        /// </summary>
-        /// <param name="hashValue">This is hashValue that is used to verify if it exists in previous restores</param>
-        /// <returns>True if it contains hash1 cycle, otherwise false</returns>
-        public bool ComputeCycleDetection(Hash hashValue)
+        lock (_lock)
         {
-            lock (_lock)
+            if (QueueContainsValue(hashValue))
             {
-                if (QueueContainsValue(hashValue))
+                _counter++;
+
+                // Verify that a hashValue has repeated in almost all cases
+                if (_counter > Size)
                 {
-                    _counter++;
-
-                    // Verify that a hashValue has repeated in almost all cases
-                    if (_counter > Size)
-                    {
-                        Clear();
-                        return true;
-                    }
-                }
-                else
-                {
-                    _counter = 0;
-                }
-
-                Add(hashValue);
-            }
-
-            return false;
-
-            bool QueueContainsValue(Hash hashValue)
-            {
-                if (_lookupTable.TryGetValue(hashValue, out int hashCounter) && hashCounter > 0)
-                {
+                    Clear();
                     return true;
                 }
-                return false;
-            }
-        }
-
-        private void Add(Hash newHashValue)
-        {
-            if (_values.Count >= Size)
-            {
-                var oldestHashValue = _values.Dequeue();
-                _lookupTable[oldestHashValue]--;
-            }
-
-            _values.Enqueue(newHashValue);
-            if (_lookupTable.ContainsKey(newHashValue))
-            {
-                _lookupTable[newHashValue]++;
             }
             else
             {
-                _lookupTable.Add(newHashValue, 1);
+                _counter = 0;
             }
+
+            Add(hashValue);
         }
 
-        public void Clear()
+        return false;
+
+        bool QueueContainsValue(Hash hashValue)
         {
-            _counter = 0;
-            _values.Clear();
-            _lookupTable.Clear();
+            if (_lookupTable.TryGetValue(hashValue, out int hashCounter) && hashCounter > 0)
+            {
+                return true;
+            }
+            return false;
         }
+    }
+
+    private void Add(Hash newHashValue)
+    {
+        if (_values.Count >= Size)
+        {
+            var oldestHashValue = _values.Dequeue();
+            _lookupTable[oldestHashValue]--;
+        }
+
+        _values.Enqueue(newHashValue);
+        if (_lookupTable.ContainsKey(newHashValue))
+        {
+            _lookupTable[newHashValue]++;
+        }
+        else
+        {
+            _lookupTable.Add(newHashValue, 1);
+        }
+    }
+
+    public void Clear()
+    {
+        _counter = 0;
+        _values.Clear();
+        _lookupTable.Clear();
     }
 }
