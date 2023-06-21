c6f268b430d533244d50e3c4c4cb058a2b9e8547
Drew Noakes <git@drewnoakes.com>
2023-06-21 22:39:31 +1000

    Add missing reference image monikers

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/KnownProjectImageMonikers.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/KnownProjectImageMonikers.cs
@@ -33,8 +33,7 @@ internal static class KnownProjectImageMonikers
 
     public static ProjectImageMoniker Reference  { get; } = KnownMonikers.Reference.ToProjectSystemType();
     public static ProjectImageMoniker ReferenceWarning { get; } = KnownMonikers.ReferenceWarning.ToProjectSystemType();
-    // TODO get a "ReferenceError" icon https://github.com/dotnet/project-system/issues/8946
-    public static ProjectImageMoniker ReferenceError { get; } = KnownMonikers.ReferenceWarning.ToProjectSystemType();
+    public static ProjectImageMoniker ReferenceError { get; } = KnownMonikers.ReferenceError.ToProjectSystemType();
     public static ProjectImageMoniker ReferencePrivate { get; } = KnownMonikers.ReferencePrivate.ToProjectSystemType();
 
     public static ProjectImageMoniker COM { get; } = KnownMonikers.COM.ToProjectSystemType();
@@ -44,8 +43,7 @@ internal static class KnownProjectImageMonikers
 
     public static ProjectImageMoniker Library { get; } = KnownMonikers.Library.ToProjectSystemType();
     public static ProjectImageMoniker LibraryWarning { get; } = KnownMonikers.LibraryWarning.ToProjectSystemType();
-    // TODO get a "LibraryError" icon https://github.com/dotnet/project-system/issues/8946
-    public static ProjectImageMoniker LibraryError { get; } = KnownMonikers.LibraryWarning.ToProjectSystemType();
+    public static ProjectImageMoniker LibraryError { get; } = KnownMonikers.LibraryError.ToProjectSystemType();
 
     public static ProjectImageMoniker Framework { get; } = KnownMonikers.Framework.ToProjectSystemType();
     public static ProjectImageMoniker FrameworkWarning { get; } = KnownMonikers.FrameworkWarning.ToProjectSystemType();
