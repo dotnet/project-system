7eb775e0167eb1ff5c24efff529b5f4576052778
Melissa Treviño <melissat@microsoft.com>
2023-07-03 16:58:54 -0700

    Introduce PublishAot and Trimming properties. These properties should only visible for projects targeting .NET 8 or higher, and should be hidden for project types: class library, WinForms, WPF, MAUI. I found we had an EvaluationEnvironment function to check if a project capability is present 'has-project-capability', still trying to figure out why it is not being picked up. For MAUI and class library projects, I still need to think of a way to detect them. This commit also includes the TrimmingEnumProvider which allows us to surface the 3 expected values (default, none, full) and surfaces any user-defined value (i.e. partial).

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Properties/InterceptedProjectProperties/BuildPropertyPage/TrimmingEnumProvider.cs
@@ -0,0 +1,50 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Microsoft.Build.Framework.XamlTypes;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+
+namespace Microsoft.VisualStudio.ProjectSystem.VS.Properties;
+
+[ExportDynamicEnumValuesProvider("TrimmingEnumProvider")]
+[AppliesTo(ProjectCapability.DotNet)]
+internal class TrimmingEnumProvider : IDynamicEnumValuesProvider
+{
+
+    [ImportingConstructor]
+    public TrimmingEnumProvider()
+    {
+    }
+
+    public Task<IDynamicEnumValuesGenerator> GetProviderAsync(IList<NameValuePair>? options)
+    {
+        return Task.FromResult<IDynamicEnumValuesGenerator>(new TrimmingEnumGenerator());
+    }
+}
+
+internal class TrimmingEnumGenerator : IDynamicEnumValuesGenerator
+{
+
+    public bool AllowCustomValues => true;
+
+    public TrimmingEnumGenerator()
+    {
+    }
+
+    public Task<ICollection<IEnumValue>> GetListedValuesAsync()
+    {
+        List<IEnumValue> enumValues = new()
+        {
+            new PageEnumValue(new EnumValue { Name = "", DisplayName = "(Default)" }),
+            new PageEnumValue(new EnumValue { Name = "none", DisplayName = "None" }),
+            new PageEnumValue(new EnumValue { Name = "full", DisplayName = "Full" })
+        };
+
+        return Task.FromResult<ICollection<IEnumValue>>(enumValues);
+    }
+
+    public Task<IEnumValue?> TryCreateEnumValueAsync(string userSuppliedValue)
+    {
+        var value = new PageEnumValue(new EnumValue { Name = userSuppliedValue, DisplayName = userSuppliedValue });
+        return Task.FromResult<IEnumValue?>(value);
+    }
+}
