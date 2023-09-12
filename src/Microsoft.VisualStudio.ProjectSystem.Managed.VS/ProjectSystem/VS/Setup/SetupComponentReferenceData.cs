f11d7206af5e63a50e79bb87c2d127ba461f8ff7
Drew Noakes <git@drewnoakes.com>
2023-09-12 13:17:13 +1000

    Add more comments around hard-coded setup components

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Setup/SetupComponentReferenceData.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Setup/SetupComponentReferenceData.cs
@@ -11,11 +11,29 @@ internal static class SetupComponentReferenceData
 {
     public const string WebComponentId = "Microsoft.VisualStudio.Component.Web";
 
+    /// <summary>
+    /// A hard-coded map from .NET Core <c>TargetFrameworkVersion</c> to VS Setup component ID.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Having this data in the project system is fragile. It must be updated every time we release a new runtime version.
+    /// We are tracking removing this in https://github.com/dotnet/project-system/issues/8186 and using data from
+    /// the SDK instead, tracked in https://github.com/dotnet/sdk/issues/25575.
+    /// </para>
+    /// <para>
+    /// The source of truth for the available VS Setup components is:
+    /// https://dev.azure.com/devdiv/DevDiv/_git/VS?path=/src/SetupPackages/Group/Product/Shared/product.shared.swr
+    /// </para>
+    /// <para>
+    /// Note there are some mismatches in the below data, as VS doesn't ship installable components for all older
+    /// versions of .NET Core.
+    /// </para>
+    /// </remarks>
     private static readonly ImmutableDictionary<string, string> s_componentIdByRuntimeVersion = ImmutableStringDictionary<string>.EmptyOrdinalIgnoreCase
-        .Add("v2.0", "Microsoft.Net.Core.Component.SDK.2.1")
+        .Add("v2.0", "Microsoft.Net.Core.Component.SDK.2.1")    // mismatch
         .Add("v2.1", "Microsoft.Net.Core.Component.SDK.2.1")
-        .Add("v2.2", "Microsoft.Net.Core.Component.SDK.2.1")
-        .Add("v3.0", "Microsoft.NetCore.Component.Runtime.3.1")
+        .Add("v2.2", "Microsoft.Net.Core.Component.SDK.2.1")    // mismatch
+        .Add("v3.0", "Microsoft.NetCore.Component.Runtime.3.1") // mismatch
         .Add("v3.1", "Microsoft.NetCore.Component.Runtime.3.1")
         .Add("v5.0", "Microsoft.NetCore.Component.Runtime.5.0")
         .Add("v6.0", "Microsoft.NetCore.Component.Runtime.6.0")
