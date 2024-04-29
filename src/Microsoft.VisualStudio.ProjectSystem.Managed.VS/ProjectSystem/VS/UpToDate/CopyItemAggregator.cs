9a4d38ce070304de27ddae33ab540f22cc6e4cd5
Drew Noakes <git@drewnoakes.com>
2024-04-29 10:52:28 +1000

    Add some comments

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/UpToDate/CopyItemAggregator.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/UpToDate/CopyItemAggregator.cs
@@ -27,6 +27,8 @@ internal class CopyItemAggregator : ICopyItemAggregator
 
         // The queue of projects yet to be visited.
         Queue<string> frontier = new();
+
+        // Start with the requested project.
         frontier.Enqueue(targetPath);
 
         // If we find a reference to a project that did not call SetProjectData then we will not know the
@@ -59,6 +61,10 @@ internal class CopyItemAggregator : ICopyItemAggregator
 
                 if (!_projectData.TryGetValue(project, out ProjectCopyData data))
                 {
+                    // We don't have a list of project references for this project, so we cannot continue
+                    // walking the project reference tree. As we might now know all possible copy items,
+                    // we disable build acceleration. Note that we still walk the rest of the tree in order
+                    // to detect copy items, so that we can decide whether the project is up to date.
                     logger.Verbose(nameof(VSResources.FUTDC_AccelerationDataMissingForProject_1), project);
                     isComplete = false;
                     continue;
@@ -71,6 +77,7 @@ internal class CopyItemAggregator : ICopyItemAggregator
                     referencesNotProducingReferenceAssembly.Add(data.TargetPath);
                 }
 
+                // Breadth-first traversal of the tree.
                 foreach (string referencedProjectTargetPath in data.ReferencedProjectTargetPaths)
                 {
                     frontier.Enqueue(referencedProjectTargetPath);
