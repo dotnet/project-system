2a0c63171ab97a60ad9a24fb0f6a7d7e2ef4d9bf
Drew Noakes <git@drewnoakes.com>
2024-05-02 12:21:28 +1000

    Add comment to Suppress method

--- tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Utilities/SynchronizationContextUtil.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.UnitTests/Utilities/SynchronizationContextUtil.cs
@@ -4,6 +4,24 @@ namespace Microsoft.VisualStudio;
 
 internal static class SynchronizationContextUtil
 {
+    /// <summary>
+    /// Sets a <see langword="null"/> <see cref="SynchronizationContext"/>, and restores
+    /// whatever context was <see cref="SynchronizationContext.Current"/> when disposed.
+    /// </summary>
+    /// <remarks>
+    /// This method is intended for use in tests where we use JTF and test code that
+    /// wants to switch to the main thread. Some of our tests don't actually construct
+    /// JTF in a way that creates a main thread that can be switched to. For these cases,
+    /// whatever thread creates the <see cref="Threading.JoinableTaskContext"/> is considered
+    /// the main thread. In unit tests, it's likely this is a thread pool thread, and
+    /// attempting to switch to the main thread will land on another thread pool thread.
+    /// <see cref="Threading.JoinableTaskFactory.SwitchToMainThreadAsync(CancellationToken)"/>
+    /// attempts to validate the switch was successful, and throws when not. This causes test
+    /// failures where we don't have a main thread to switch to. A workaround for this is
+    /// to suppress the synchronization context, which disables the check and allows the test
+    /// to pass.
+    /// </remarks>
+    /// <returns>An object that restores the previous synchronization context when disposed.</returns>
     public static IDisposable Suppress()
     {
         SynchronizationContext old = SynchronizationContext.Current;
