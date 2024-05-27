94813992815e71f9c70f20d897fbe372223ba99c
Drew Noakes <git@drewnoakes.com>
2024-05-27 10:17:39 +1000

    Remove unnecessary using directive

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/PackageRestore/Snapshots/RestoreComparer.ReferenceItemEqualityComparer.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/PackageRestore/Snapshots/RestoreComparer.ReferenceItemEqualityComparer.cs
@@ -1,7 +1,5 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
-using System.Xaml.Schema;
-
 namespace Microsoft.VisualStudio.ProjectSystem.PackageRestore
 {
     internal static partial class RestoreComparer
