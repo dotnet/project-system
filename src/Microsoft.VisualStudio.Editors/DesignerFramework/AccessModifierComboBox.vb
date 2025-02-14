382328652a71adec0791592d1bb5bc006d532361
Drew Noakes <git@drewnoakes.com>
2025-02-14 11:27:31 +1100

    Rename field

--- src/Microsoft.VisualStudio.Editors/DesignerFramework/AccessModifierComboBox.vb
+++ src/Microsoft.VisualStudio.Editors/DesignerFramework/AccessModifierComboBox.vb
@@ -65,7 +65,7 @@ Namespace Microsoft.VisualStudio.Editors.DesignerFramework
 
         Private _isDisposed As Boolean
         Private ReadOnly _rootDesigner As BaseRootDesigner
-        Private ReadOnly _resxFileProjectItem As EnvDTE.ProjectItem
+        Private ReadOnly _projectItem As EnvDTE.ProjectItem
         Private ReadOnly _serviceProvider As IServiceProvider
         Private ReadOnly _namespaceToOverrideIfCustomToolIsEmpty As String
         Private ReadOnly _codeGeneratorEntries As New List(Of CodeGenerator)
@@ -300,7 +300,7 @@ Namespace Microsoft.VisualStudio.Editors.DesignerFramework
             Requires.NotNull(serviceProvider)
 
             _rootDesigner = rootDesigner
-            _resxFileProjectItem = projectItem
+            _projectItem = projectItem
             _serviceProvider = serviceProvider
             _namespaceToOverrideIfCustomToolIsEmpty = namespaceToOverrideIfCustomToolIsEmpty
         End Sub
@@ -409,7 +409,7 @@ Namespace Microsoft.VisualStudio.Editors.DesignerFramework
             Dim customToolProperty As EnvDTE.Property = Nothing
 
             Try
-                customToolProperty = DTEUtils.GetProjectItemProperty(_resxFileProjectItem, DTEUtils.PROJECTPROPERTY_CUSTOMTOOL)
+                customToolProperty = DTEUtils.GetProjectItemProperty(_projectItem, DTEUtils.PROJECTPROPERTY_CUSTOMTOOL)
             Catch ex As KeyNotFoundException
                 ' Possible limitation of Cps. In some cases Cps is not able to maintain the same item id for items,
                 ' causing them to Not be found. In some scenarios (i.e., when the item Is moved), it ends up having
@@ -454,8 +454,8 @@ Namespace Microsoft.VisualStudio.Editors.DesignerFramework
         ''' <param name="value"></param>
         Private Sub TrySetCustomToolValue(value As String)
             Try
-                Dim customToolProperty As EnvDTE.Property = DTEUtils.GetProjectItemProperty(_resxFileProjectItem, DTEUtils.PROJECTPROPERTY_CUSTOMTOOL)
-                Dim customToolNamespaceProperty As EnvDTE.Property = DTEUtils.GetProjectItemProperty(_resxFileProjectItem, DTEUtils.PROJECTPROPERTY_CUSTOMTOOLNAMESPACE)
+                Dim customToolProperty As EnvDTE.Property = DTEUtils.GetProjectItemProperty(_projectItem, DTEUtils.PROJECTPROPERTY_CUSTOMTOOL)
+                Dim customToolNamespaceProperty As EnvDTE.Property = DTEUtils.GetProjectItemProperty(_projectItem, DTEUtils.PROJECTPROPERTY_CUSTOMTOOLNAMESPACE)
 
                 If customToolProperty IsNot Nothing Then
                     Dim previousCustomToolValue As String = TryCast(customToolProperty.Value, String)
@@ -568,7 +568,7 @@ Namespace Microsoft.VisualStudio.Editors.DesignerFramework
         ''' </summary>
         Protected Overridable ReadOnly Property Hierarchy As IVsHierarchy
             Get
-                Return ShellUtil.VsHierarchyFromDTEProject(_serviceProvider, _resxFileProjectItem.ContainingProject)
+                Return ShellUtil.VsHierarchyFromDTEProject(_serviceProvider, _projectItem.ContainingProject)
             End Get
         End Property
 
