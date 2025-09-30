b387410cc528aad26af39097dbb150a73175e5eb
XiaoYun Zhang <xiaoyuz@microsoft.com>
2025-09-30 13:08:33 -0700

    Refactor ICssHotReloadService to improve code clarity and maintainability

--- src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/HotReload/Contracts/ICssHotReloadService.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/HotReload/Contracts/ICssHotReloadService.cs
@@ -1,5 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using Microsoft.VisualStudio.Debugger.Contracts.HotReload;
 namespace Microsoft.VisualStudio.ProjectSystem.HotReload;
 
 public interface ICssHotReloadService
