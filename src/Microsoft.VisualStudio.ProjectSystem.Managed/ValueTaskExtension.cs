abeccf3f0693bff5e1edc8dc3120a8d8748069a5
Xiaoyun Zhang <bigmiao.zhang@gmail.com>
2025-10-06 15:38:45 -0700

    Update MicrosoftDotNetHotReloadPackageVersion and CPSPackageVersion to latest pre-release versions (#9802)

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ValueTaskExtension.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+#pragma warning disable VSTHRD200 // Use "Async" suffix for async methods
+
+namespace System.Threading.Tasks;
+
+internal static partial class ValueTaskExtensions
+{
+#if !NET
+    extension(ValueTask)
+    {
+        public static ValueTask<T> FromResult<T>(T result)
+            => new(result);
+
+        public static ValueTask CompletedTask
+            => new();
+    }
+#endif
+}
