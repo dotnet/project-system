47f29d093e40adf21b60933be3f79cf51ba26d08
Phil Henning <phenning@microsoft.com>
2025-10-22 18:36:51 -0400

    Cleanup usings

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Setup/DotNetEnvironment.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Setup/DotNetEnvironment.cs
@@ -1,10 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System.Runtime.InteropServices;
+using IEnvironment = Microsoft.VisualStudio.ProjectSystem.Utilities.IEnvironment;
 using IFileSystem = Microsoft.VisualStudio.IO.IFileSystem;
 using IRegistry = Microsoft.VisualStudio.ProjectSystem.VS.Utilities.IRegistry;
-using IEnvironment = Microsoft.VisualStudio.ProjectSystem.Utilities.IEnvironment;
-using Microsoft.VisualStudio.Threading;
-using System.Runtime.InteropServices;
 
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Setup;
 
