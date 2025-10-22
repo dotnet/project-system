7cf0a47beff60f0ccce1dcaf06abfa065dfdf9ab
Drew Noakes <git@drewnoakes.com>
2025-10-22 12:48:48 +1100

    Add ProjectCapability.SupportsHotReload

--- tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/HotReload/ProjectHotReloadSessionManagerTests.cs
+++ tests/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/HotReload/ProjectHotReloadSessionManagerTests.cs
@@ -12,7 +12,7 @@ public class ProjectHotReloadSessionManagerTests
     [Fact]
     public async Task WhenActiveFrameworkMeetsRequirements_APendingSessionIsCreated()
     {
-        var capabilities = new[] { "SupportsHotReload" };
+        var capabilities = new[] { ProjectCapability.SupportsHotReload };
         var propertyNamesAndValues = new Dictionary<string, string?>()
         {
             { "TargetFramework", "net6.0" },
@@ -68,7 +68,7 @@ public class ProjectHotReloadSessionManagerTests
     [Fact]
     public async Task WhenTheTargetFrameworkVersionIsNotDefined_APendingSessionIsNotCreated()
     {
-        var capabilities = new[] { "SupportsHotReload" };
+        var capabilities = new[] { ProjectCapability.SupportsHotReload };
         var propertyNamesAndValues = new Dictionary<string, string?>()
         {
             { "ARandomProperty", "WithARandomValue" },
@@ -95,7 +95,7 @@ public class ProjectHotReloadSessionManagerTests
     [Fact]
     public async Task WhenStartupHooksAreDisabled_APendingSessionIsNotCreated()
     {
-        var capabilities = new[] { "SupportsHotReload" };
+        var capabilities = new[] { ProjectCapability.SupportsHotReload };
         var propertyNamesAndValues = new Dictionary<string, string?>()
         {
             { "TargetFramework", "net6.0" },
@@ -126,7 +126,7 @@ public class ProjectHotReloadSessionManagerTests
     [Fact]
     public async Task WhenOptimizationIsEnabled_APendingSessionIsNotCreated()
     {
-        var capabilities = new[] { "SupportsHotReload" };
+        var capabilities = new[] { ProjectCapability.SupportsHotReload };
         var propertyNamesAndValues = new Dictionary<string, string?>()
         {
             { "TargetFrameworkVersion", "v6.0" },
@@ -154,7 +154,7 @@ public class ProjectHotReloadSessionManagerTests
     [Fact]
     public async Task WhenDebugSymbolsIsNotSpecified_APendingSessionIsNotCreated()
     {
-        var capabilities = new[] { "SupportsHotReload" };
+        var capabilities = new[] { ProjectCapability.SupportsHotReload };
         var propertyNamesAndValues = new Dictionary<string, string?>()
         {
             { "TargetFrameworkVersion", "v6.0" }
@@ -180,7 +180,7 @@ public class ProjectHotReloadSessionManagerTests
     [Fact]
     public async Task WhenDebugSymbolsIsFalse_APendingSessionIsNotCreated()
     {
-        var capabilities = new[] { "SupportsHotReload" };
+        var capabilities = new[] { ProjectCapability.SupportsHotReload };
         var propertyNamesAndValues = new Dictionary<string, string?>()
         {
             { "TargetFrameworkVersion", "v6.0" },
@@ -207,7 +207,7 @@ public class ProjectHotReloadSessionManagerTests
     [Fact]
     public void WhenNoActiveSession_HasSessionsReturnsFalse()
     {
-        var capabilities = new[] { "SupportsHotReload" };
+        var capabilities = new[] { ProjectCapability.SupportsHotReload };
         var propertyNamesAndValues = new Dictionary<string, string?>()
         {
             { "TargetFrameworkVersion", "v6.0" },
