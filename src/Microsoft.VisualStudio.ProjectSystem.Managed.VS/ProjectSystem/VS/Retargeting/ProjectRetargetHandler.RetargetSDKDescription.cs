cbad8706494d80a5e6c617e10496371d9538be1f
Phil Henning <phenning@microsoft.com>
2025-10-29 21:50:01 -0400

    Consider architecture when generating guidance link for retargeting (#9847)
    * Consider architecture when generating guidance link for retargeting
    
    * Review feedback
    
    * DotNetEnvironment.GetArchitectureSubKey(Architecture) into an Architecture extension method.
    * Updated old call sites
    * Updated this PR to pass the Architecture to RetargetSDKDescription and do the string replacement there.
    * Updated unit test to verify expected architecture is present in the suggesion Url.

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Retargeting/ProjectRetargetHandler.RetargetSDKDescription.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Retargeting/ProjectRetargetHandler.RetargetSDKDescription.cs
@@ -1,24 +1,26 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System.Runtime.InteropServices;
+
 namespace Microsoft.VisualStudio.ProjectSystem.VS.Retargeting;
 
 internal partial class ProjectRetargetHandler
 {
     internal class RetargetSDKDescription : TargetDescriptionBase
     {
-        internal static RetargetSDKDescription Create(string sdkVersion)
+        internal static RetargetSDKDescription Create(string sdkVersion, Architecture arch)
         {
-            return new RetargetSDKDescription(sdkVersion);
+            return new RetargetSDKDescription(sdkVersion, arch);
         }
 
-        private RetargetSDKDescription(string sdkVersion) : base(
+        private RetargetSDKDescription(string sdkVersion, Architecture arch) : base(
             targetId: Guid.NewGuid(),
             displayName: $".NET SDK {sdkVersion}",
             order: 1,
             supported: true,
             description: string.Format(VSResources.RetargetingSDKDescription, sdkVersion),
             canRetarget: true, // this means we want to show this option in the retarget dialog
-            guidanceLink: $"https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/sdk-{sdkVersion}-windows-x64-installer")
+            guidanceLink: $"https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/sdk-{sdkVersion}-windows-{arch.GetArchitectureString()}-installer")
         {
         }
     }
