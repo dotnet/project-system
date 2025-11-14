72a80f526b75bcfad5c0db19184232fe49268d02
XiaoYun Zhang <xiaoyuz@microsoft.com>
2025-11-14 12:23:59 -0800

    Remove unused Application Insights namespace from ProjectHotReloadSessionManager

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/HotReload/ProjectHotReloadSessionManager.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/HotReload/ProjectHotReloadSessionManager.cs
@@ -1,7 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
 
 using System.Diagnostics;
-using Microsoft.VisualStudio.ApplicationInsights.DataContracts;
 using Microsoft.VisualStudio.Debugger.Contracts.HotReload;
 using Microsoft.VisualStudio.Debugger.Interop;
 using Microsoft.VisualStudio.HotReload.Components.DeltaApplier;
