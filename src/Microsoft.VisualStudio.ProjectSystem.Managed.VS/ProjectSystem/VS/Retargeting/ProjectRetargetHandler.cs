a46bfbbedcd3549d13b71ebe59085110cc157540
Drew Noakes <git@drewnoakes.com>
2025-11-21 12:58:13 +1100

    Local functions and primary constructor

--- src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Retargeting/ProjectRetargetHandler.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Retargeting/ProjectRetargetHandler.cs
@@ -13,46 +13,126 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Retargeting;
 [Export(typeof(IProjectRetargetHandler))]
 [AppliesTo(ProjectCapability.DotNet)]
 [Order(Order.Default)]
-internal sealed partial class ProjectRetargetHandler : IProjectRetargetHandler, IDisposable
+[method: ImportingConstructor]
+internal sealed partial class ProjectRetargetHandler(
+#pragma warning disable CS9113 // Only used for scoping to ensure an instance per project
+    UnconfiguredProject project,
+#pragma warning restore CS9113
+    IDotNetReleasesProvider releasesProvider,
+    IFileSystem fileSystem,
+    IVsService<SVsTrackProjectRetargeting, IVsTrackProjectRetargeting2> projectRetargetingService,
+    ISolutionService solutionService,
+    IEnvironment environment,
+    IDotNetEnvironment dotnetEnvironment)
+    : IProjectRetargetHandler, IDisposable
 {
-    private readonly IDotNetReleasesProvider _releasesProvider;
-    private readonly IFileSystem _fileSystem;
-    private readonly IVsService<IVsTrackProjectRetargeting2> _projectRetargetingService;
-    private readonly ISolutionService _solutionService;
-    private readonly IEnvironment _environment;
-    private readonly IDotNetEnvironment _dotnetEnvironment;
-
     private IVsTrackProjectRetargeting2? _projectRetargeting;
     private Guid _currentSdkDescriptionId = Guid.Empty;
     private Guid _sdkRetargetId = Guid.Empty;
 
-    [ImportingConstructor]
-    public ProjectRetargetHandler(
-        IDotNetReleasesProvider releasesProvider,
-        IFileSystem fileSystem,
-        IVsService<SVsTrackProjectRetargeting, IVsTrackProjectRetargeting2> projectRetargetingService,
-        ISolutionService solutionService,
-        IEnvironment environment,
-        IDotNetEnvironment dotnetEnvironment)
-    {
-        _releasesProvider = releasesProvider;
-        _fileSystem = fileSystem;
-        _projectRetargetingService = projectRetargetingService;
-        _solutionService = solutionService;
-        _environment = environment;
-        _dotnetEnvironment = dotnetEnvironment;
-    }
-
-    public Task<IProjectTargetChange?> CheckForRetargetAsync(RetargetCheckOptions options)
+    public async Task<IProjectTargetChange?> CheckForRetargetAsync(RetargetCheckOptions options)
     {
         RetargetCheckOptions flags = RetargetCheckOptions.ProjectRetarget | RetargetCheckOptions.SolutionRetarget | RetargetCheckOptions.ProjectLoad;
 
         if ((options & flags) == 0)
         {
-            return TaskResult.Null<IProjectTargetChange>();
+            return null;
         }
 
-        return GetTargetChangeAndRegisterTargetsAsync();
+        string? sdkVersion = await GetSdkVersionForSolutionAsync();
+
+        if (sdkVersion is null)
+        {
+            return null;
+        }
+
+        string? retargetVersion = await releasesProvider.GetNewerSupportedSdkVersionAsync(sdkVersion);
+
+        if (retargetVersion is null)
+        {
+            return null;
+        }
+
+        // Check if the retarget is already installed globally
+        if (dotnetEnvironment.IsSdkInstalled(retargetVersion))
+        {
+            return null;
+        }
+
+        _projectRetargeting ??= await projectRetargetingService.GetValueAsync();
+
+        if (_currentSdkDescriptionId == Guid.Empty)
+        {
+            // register the current and retarget versions, note there is a bug in the current implementation
+            // ultimately we will need to just set retarget. Right now, we need to register two
+            // targets, we want to create two distinct ones, as the bug workaround requires different guids
+
+            var currentSdkDescription = RetargetSDKDescription.Create(retargetVersion, environment.ProcessArchitecture); // this won't be needed
+            _projectRetargeting.RegisterProjectTarget(currentSdkDescription);  // this wont be needed.
+            _currentSdkDescriptionId = currentSdkDescription.TargetId;
+        }
+
+        if (_sdkRetargetId == Guid.Empty)
+        {
+            var retargetSdkDescription = RetargetSDKDescription.Create(retargetVersion, environment.ProcessArchitecture);
+            _projectRetargeting.RegisterProjectTarget(retargetSdkDescription);
+            _sdkRetargetId = retargetSdkDescription.TargetId;
+        }
+
+        return new TargetChange()
+        {
+            NewTargetId = _currentSdkDescriptionId, // this is for workaround only
+            //NewTargetId = Guid.Empty, // this is what we want ultimately
+            CurrentTargetId = _sdkRetargetId
+        };
+
+        async Task<string?> GetSdkVersionForSolutionAsync()
+        {
+            string? solutionDirectory = await solutionService.GetSolutionDirectoryAsync();
+
+            if (!Strings.IsNullOrEmpty(solutionDirectory))
+            {
+                string? globalJsonPath = FindGlobalJsonPath();
+
+                if (globalJsonPath is not null)
+                {
+                    try
+                    {
+                        using Stream stream = fileSystem.OpenTextStream(globalJsonPath);
+                        using JsonDocument doc = await JsonDocument.ParseAsync(stream);
+                        if (doc.RootElement.TryGetProperty("sdk", out JsonElement sdkProp) &&
+                            sdkProp.TryGetProperty("version", out JsonElement versionProp))
+                        {
+                            return versionProp.GetString();
+                        }
+                    }
+                    catch (Exception ex) when (ex.IsCatchable())
+                    {
+                    }
+                }
+            }
+
+            return null;
+
+            string? FindGlobalJsonPath()
+            {
+                string? dir = solutionDirectory;
+
+                while (!string.IsNullOrEmpty(dir))
+                {
+                    string globalJsonPath = Path.Join(dir, "global.json");
+
+                    if (fileSystem.FileExists(globalJsonPath))
+                    {
+                        return globalJsonPath;
+                    }
+
+                    dir = Path.GetDirectoryName(dir);
+                }
+
+                return null;
+            }
+        }
     }
 
     public Task<IImmutableList<string>> GetAffectedFilesAsync(IProjectTargetChange projectTargetChange)
@@ -65,103 +145,6 @@ internal sealed partial class ProjectRetargetHandler : IProjectRetargetHandler, 
     {
         // no operation needed, as we are only providing guidance.
         return Task.CompletedTask;
-    }
-
-    private async Task<IProjectTargetChange?> GetTargetChangeAndRegisterTargetsAsync()
-    {
-        string? sdkVersion = await GetSdkVersionForSolutionAsync();
-
-        if (sdkVersion is null)
-        {
-            return null;
-        }
-
-        string? retargetVersion = await _releasesProvider.GetNewerSupportedSdkVersionAsync(sdkVersion);
-
-        if (retargetVersion is null)
-        {
-            return null;
-        }
-
-        // Check if the retarget is already installed globally
-        if (_dotnetEnvironment.IsSdkInstalled(retargetVersion))
-        {
-            return null;
-        }
-
-        _projectRetargeting ??= await _projectRetargetingService.GetValueAsync();
-
-        if (_currentSdkDescriptionId == Guid.Empty)
-        {
-            // register the current and retarget versions, note there is a bug in the current implementation
-            // ultimately we will need to just set retarget. Right now, we need to register two
-            // targets, we want to create two distinct ones, as the bug workaround requires different guids
-
-            IVsProjectTargetDescription currentSdkDescription = RetargetSDKDescription.Create(retargetVersion, _environment.ProcessArchitecture); // this won't be needed
-            _projectRetargeting.RegisterProjectTarget(currentSdkDescription);  // this wont be needed.
-            _currentSdkDescriptionId = currentSdkDescription.TargetId;
-        }
-
-        if (_sdkRetargetId == Guid.Empty)
-        {
-            IVsProjectTargetDescription retargetSdkDescription = RetargetSDKDescription.Create(retargetVersion, _environment.ProcessArchitecture);
-            _projectRetargeting.RegisterProjectTarget(retargetSdkDescription);
-            _sdkRetargetId = retargetSdkDescription.TargetId;
-        }
-
-        return new TargetChange()
-        {
-            NewTargetId = _currentSdkDescriptionId, // this is for workaround only
-            //NewTargetId = Guid.Empty, // this is what we want ultimately
-            CurrentTargetId = _sdkRetargetId
-        };
-    }
-
-    private string? FindGlobalJsonPath(string startingDirectory)
-    {
-        string? dir = startingDirectory;
-
-        while (!string.IsNullOrEmpty(dir))
-        {
-            string globalJsonPath = Path.Join(dir, "global.json");
-
-            if (_fileSystem.FileExists(globalJsonPath))
-            {
-                return globalJsonPath;
-            }
-
-            dir = Path.GetDirectoryName(dir);
-        }
-
-        return null;
-    }
-
-    private async Task<string?> GetSdkVersionForSolutionAsync()
-    {
-        string? solutionDirectory = await _solutionService.GetSolutionDirectoryAsync();
-
-        if (!Strings.IsNullOrEmpty(solutionDirectory))
-        {
-            string? globalJsonPath = FindGlobalJsonPath(solutionDirectory);
-            if (globalJsonPath is not null)
-            {
-                try
-                {
-                    using Stream stream = _fileSystem.OpenTextStream(globalJsonPath);
-                    using JsonDocument doc = await JsonDocument.ParseAsync(stream);
-                    if (doc.RootElement.TryGetProperty("sdk", out JsonElement sdkProp) &&
-                        sdkProp.TryGetProperty("version", out JsonElement versionProp))
-                    {
-                        return versionProp.GetString();
-                    }
-                }
-                catch (Exception ex) when (ex.IsCatchable())
-                {
-                }
-            }
-        }
-
-        return null;
     }
 
     public void Dispose()
